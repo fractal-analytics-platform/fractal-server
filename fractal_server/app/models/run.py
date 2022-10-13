a9af87cc7a0d15b5862e317304efdc4cdc66b9fc
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-10-13 19:46:30 +0200

    job provides paths
    ref #134

--- fractal_server/app/models/run.py
+++ fractal_server/app/models/run.py
@@ -1,11 +1,17 @@
 from datetime import datetime
 from enum import Enum
+from pathlib import Path
 from typing import Optional
 
 from sqlmodel import Field
+from sqlmodel import Relationship
 
+from ...config_runner import settings
 from ...utils import get_timestamp
+from ..project import Dataset
+from ..project import Project
 from ..schemas import ApplyWorkflowBase
+from ..workflow import Workflow
 
 
 class StatusType(str, Enum):
@@ -19,9 +25,37 @@ class ApplyWorkflow(ApplyWorkflowBase, table=True):  # type: ignore
     project_id: int = Field(foreign_key="project.id")
     input_dataset_id: int = Field(foreign_key="dataset.id")
     output_dataset_id: int = Field(foreign_key="dataset.id")
-    workflow_id: int = Field(foreign_key="task.id")
+    workflow_id: int = Field(foreign_key="workflow.id")
+
+    project: Project = Relationship()
+    input_dataset: Dataset = Relationship(
+        sa_relationship_kwargs=dict(
+            lazy="selectin",
+            primaryjoin="ApplyWorkflow.input_dataset_id==Dataset.id",
+        )
+    )
+    output_dataset: Dataset = Relationship(
+        sa_relationship_kwargs=dict(
+            lazy="selectin",
+            primaryjoin="ApplyWorkflow.output_dataset_id==Dataset.id",
+        )
+    )
+    workflow: Workflow = Relationship()
 
     start_timestamp: datetime = Field(
         default_factory=get_timestamp, nullable=False
     )
     status: StatusType = StatusType.SUBMITTED
+
+    @property
+    def job_root_path(self) -> Path:
+        return settings.RUNNER_ROOT_DIR / f"job_{self.id:06d}"
+
+    @property
+    def log_path(self) -> Path:
+        return self.job_root_path / "job.log"
+
+    def make_job_dir(self):
+        _path = self.job_root_path
+        if not _path.is_dir():
+            _path.mkdir(exists_ok=True, parents=True)
