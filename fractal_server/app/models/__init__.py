b3ee84ec820c8104e989765a714eab09768c725f
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-10-13 12:20:59 +0200

    remove deprecated import

--- fractal_server/app/models/__init__.py
+++ fractal_server/app/models/__init__.py
@@ -1,5 +1,6 @@
 from ..schemas import *  # noqa F401
 from .project import *  # noqa: F403, F401
+from .run import *  # noqa: F403, F401
 from .security import *  # noqa: F403, F401
 from .task import *  # noqa: F403, F401
 
