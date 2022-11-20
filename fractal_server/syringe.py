1cb219d13b578bd576e165d1de37f501218111f3
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-20 21:19:34 +0100

    syringe documentation and doc styling

--- fractal_server/syringe.py
+++ fractal_server/syringe.py
@@ -1,12 +1,38 @@
+# Copyright 2022 (C) eXact lab S.r.l.
+#
+# Original author:
+# Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
+#
+# This file is part of `Syringe`, a simple Python module for dependency
+# injection. Redistribution and use must comply with the 3-clause BSD License
+# (see `LICENSE` file).
 """
-Copyright 2022 (C) eXact lab S.r.l.
+This module provides an extremely simple utility for dependency injection.
 
-Original author:
-Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
+It's made up of a single singleton class that provides a directory for the
+dependencies. The dependencies are stored in a dictionary and can be overridden
+or popped from the directory.
 
-This file is part of `Syringe`, a simple Python module for dependency
-injection. Redistribution and use must comply with the 3-clause BSD License
-(see `LICENSE` file).
+## Usage:
+
+    >>> from syringe import Inject
+    >>> def foo():
+    >>>     return 42
+    >>>
+    >>> def oof():
+    >>>     return 24
+    >>>
+    >>> def bar():
+    >>>     return Inject(foo)
+    >>>
+    >>> bar()
+    42
+    >>> Inject.override(foo, oof)
+    >>> bar()
+    24
+    >>> Inject.pop(foo)
+    >>> bar()
+    42
 """
 from typing import Any
 from typing import Callable
@@ -19,6 +45,16 @@ _instance_count = 0
 
 
 class _Inject:
+    """
+    Injection class
+
+    This is a private class that is never directly instantiated.
+
+    Attributes:
+        _dependencies:
+            The dependency directory
+    """
+
     _dependencies: Dict[Any, Any] = {}
 
     def __init__(self):
@@ -29,6 +65,16 @@ class _Inject:
 
     @classmethod
     def __call__(cls, _callable: Callable[..., T]) -> T:
+        """
+        Call the dependency
+
+        Args:
+            _callable:
+                Callable dependency object
+
+        Returns:
+            The output of calling `_callalbe` or its dependency override.
+        """
         try:
             return cls._dependencies[_callable]()
         except KeyError:
@@ -36,6 +82,13 @@ class _Inject:
 
     @classmethod
     def pop(cls, _callable: Callable[..., T]) -> T:
+        """
+        Remove the dependency from the directory
+
+        Args:
+            _callable:
+                Callable dependency object
+        """
         try:
             return cls._dependencies.pop(_callable)
         except KeyError:
@@ -45,8 +98,22 @@ class _Inject:
     def override(
         cls, _callable: Callable[..., T], value: Callable[..., T]
     ) -> None:
+        """
+        Override dependency
+
+        Substitute a dependency with a different arbitrary callable.
+
+        Args:
+            _callable:
+                Callable dependency object
+            value:
+                Callable override
+        """
         cls._dependencies[_callable] = value
 
 
 # NOTE: This is a singleton instance
 Inject = _Inject()
+"""
+The singleton instance of `_Inject`, the only public member of this module.
+"""
