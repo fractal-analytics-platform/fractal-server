aa008922008170ff7bd3068d592b2d912e945f4f
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-21 17:35:51 +0100

    check sudo

--- tests/test_backend_slurm.py
+++ tests/test_backend_slurm.py
@@ -1,6 +1,7 @@
 from concurrent.futures import Executor
 from typing import Callable
 
+import pytest
 from devtools import debug
 
 from fractal_server.app.runner._slurm.executor import FractalSlurmExecutor
@@ -11,28 +12,25 @@ def submit(executor: Executor, fun: Callable, *args, **kwargs):
         return executor.submit(fun, *args, **kwargs)
     except Exception as e:
         debug(e)
-        pass
 
 
-def test_submit_pre_command(fake_process):
+@pytest.mark.parametrize(("username"), [None, "my_user"])
+def test_submit_pre_command(fake_process, username, tmp_path):
+    """
+    GIVEN a FractalSlurmExecutor
+    WHEN it is initialised with / without a username
+    THEN the sbatch call contains / does not contain the sudo pre-command
+    """
     fake_process.register(["sbatch", fake_process.any()])
     fake_process.register(["sudo", fake_process.any()])
 
-    def foo(a, b):
-        return a + b
+    with FractalSlurmExecutor(
+        username=username, script_dir=tmp_path
+    ) as executor:
+        submit(executor, lambda: None)
 
-    with FractalSlurmExecutor() as executor:
-        submit(executor, foo, 1, 2)
-
+    debug(fake_process.calls)
     call = fake_process.calls.pop()
-    assert "sudo" not in call
     assert "sbatch" in call
-
-    USERNAME = "my_user"
-    with FractalSlurmExecutor(username=USERNAME) as executor:
-        submit(executor, foo, 1, 3)
-
-    call = fake_process.calls.pop()
-    assert "sudo" in call
-    assert USERNAME in call
-    assert "sbatch" in call
+    if username:
+        assert f"sudo --non-interactive -u {username}" in call
