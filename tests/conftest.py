2c3d86948c5a4ae8bf609388b59112d0d46837be
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-22 16:50:28 +0100

    add fixtures to create mode 777 temporary directories

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,26 @@ async def testdata_path() -> Path:
     return TEST_DIR / "data/"
 
 
+@pytest.fixture(scope="session")
+def tmp777_session_path(tmp_path_factory):
+    """
+    Makes a subdir of the tmp_path with 777 access rights
+    """
+
+    def _tmp_path_factory(relative_path: str):
+        tmp = tmp_path_factory.mktemp(relative_path)
+        tmp.chmod(0o777)
+        return tmp
+
+    yield _tmp_path_factory
+
+
+@pytest.fixture
+def tmp777_path(tmp_path):
+    tmp_path.chmod(0o777)
+    yield tmp_path
+
+
 from .fixtures_server import *  # noqa F403
 from .fixtures_tasks import *  # noqa F403
 from .fixtures_slurm import *  # noqa F403
