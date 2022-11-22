1240a9df71119b84805b3411631c04a95b5d87cf
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-22 09:55:16 +0100

    set file paths so that it works in a cluster

--- fractal_server/app/runner/_slurm/executor.py
+++ fractal_server/app/runner/_slurm/executor.py
@@ -1,11 +1,22 @@
+# This adapts clusterfutures <https://github.com/sampsyo/clusterfutures>
+# Original Copyright
+# Copyright 2021 Adrian Sampson <asampson@cs.washington.edu>
+# License: MIT
+#
+# Copyright 2022 Jacopo Nepsolo <jacopo.nespolo@exact-lab.it>
 import logging
-import os
 import shlex
 import subprocess  # nosec
+import sys
+from concurrent import futures
 from pathlib import Path
+from typing import Any
+from typing import Callable
 from typing import List
 from typing import Optional
 
+import cloudpickle
+from cfut import RemoteException
 from cfut import SlurmExecutor  # type: ignore
 from cfut.util import random_string
 
@@ -16,17 +27,22 @@ logger = logging.getLogger("fractal")
 logger.setLevel(logging.DEBUG)
 
 
-def local_filename(filename=""):
-    return os.path.join(os.getenv("CFUT_DIR", ".cfut"), filename)
-
-
-LOG_FILE = local_filename("slurmpy.log")
-
-
-def get_out_filename() -> str:
+def get_slurm_script_dir() -> Path:
     settings = Inject(get_settings)
-    script_dir = settings.RUNNER_ROOT_DIR / "slurm_backend"  # type: ignore
-    return script_dir.as_posix() + "/slurmpy.stdout.{}.log"
+    script_dir = settings.RUNNER_ROOT_DIR  # type: ignore
+    return script_dir  # type: ignore
+
+
+def get_stdout_filename(arg: str = "%j") -> Path:
+    return get_slurm_script_dir() / f"slurmpy.stdout.{arg}.log"
+
+
+def get_in_filename(arg) -> Path:
+    return get_slurm_script_dir() / f"cfut.in.{arg}.pickle"
+
+
+def get_out_filename(arg) -> Path:
+    return get_slurm_script_dir() / f"cfut.out.{arg}.pickle"
 
 
 def submit_sbatch(
@@ -73,15 +89,18 @@ def submit_sbatch(
 def compose_sbatch_script(
     cmdline: List[str],
     # NOTE: In SLURM, `%j` is the placeholder for the job_id.
-    outpat: Optional[str] = None,
+    outpat: Optional[Path] = None,
     additional_setup_lines=[],
 ) -> str:
     if outpat is None:
-        get_out_filename().format("%j")
+        outpat = get_stdout_filename()
     script_lines = [
         "#!/bin/sh",
-        "#SBATCH --output={}".format(outpat),
+        f"#SBATCH --output={outpat}",
         *additional_setup_lines,
+        # Export the slurm script directory so that nodes can find the pickled
+        # payload
+        f"export CFUT_DIR={get_slurm_script_dir()}",
         shlex.join(["srun", *cmdline]),
     ]
     return "\n".join(script_lines)
@@ -105,6 +124,63 @@ class FractalSlurmExecutor(SlurmExecutor):
         super().__init__(*args, **kwargs)
         self.username = username
         self.script_dir = script_dir
+
+    def submit(
+        self,
+        fun: Callable[..., Any],
+        *args,
+        additional_setup_lines: List[str] = None,
+        **kwargs,
+    ):
+        """Submit a job to the pool.
+        If additional_setup_lines is passed, it overrides the lines given
+        when creating the executor.
+        """
+        fut: futures.Future = futures.Future()
+
+        # Start the job.
+        workerid = random_string()
+        funcser = cloudpickle.dumps((fun, args, kwargs))
+        with get_in_filename(workerid).open("wb") as f:
+            f.write(funcser)
+        jobid = self._start(workerid, additional_setup_lines)
+
+        if self.debug:
+            print("job submitted: %i" % jobid, file=sys.stderr)
+
+        # Thread will wait for it to finish.
+        self.wait_thread.wait(get_out_filename(workerid).as_posix(), jobid)
+
+        with self.jobs_lock:
+            self.jobs[jobid] = (fut, workerid)
+        return fut
+
+    def _completion(self, jobid):
+        """Called whenever a job finishes."""
+        with self.jobs_lock:
+            fut, workerid = self.jobs.pop(jobid)
+            if not self.jobs:
+                self.jobs_empty_cond.notify_all()
+        if self.debug:
+            print("job completed: %i" % jobid, file=sys.stderr)
+
+        out_path = get_out_filename(workerid)
+        in_path = get_in_filename(workerid)
+
+        with out_path.open("rb") as f:
+            outdata = f.read()
+        success, result = cloudpickle.loads(outdata)
+
+        if success:
+            fut.set_result(result)
+        else:
+            fut.set_exception(RemoteException(result))
+
+        # Clean up communication files.
+        in_path.unlink()
+        out_path.unlink()
+
+        self._cleanup(jobid)
 
     def _start(self, workerid, additional_setup_lines):
         if additional_setup_lines is None:
