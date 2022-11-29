9ea47dc4c587af632a5c8c111b22838900f10058
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-29 13:35:39 +0100

    Rename variable in test (workflow_init -> sbatch_init_lines)

--- tests/test_backend_slurm.py
+++ tests/test_backend_slurm.py
@@ -148,12 +148,16 @@ def test_sbatch_script_slurm_config(
 
     # Assign a non existent username so that the sudo call will fail with a
     # FileNotFoundError. This will allow inspection of the sbatch script file.
-    workflow_init = ["export FOO=bar", "#SBATCH --common-non-existent-option"]
+    sbatch_init_lines = [
+        "export FOO=bar",
+        "#SBATCH --common-non-existent-option",
+    ]
     with FractalSlurmExecutor(
         username="NO_USER",
         script_dir=tmp_path,
-        common_script_lines=workflow_init,
+        common_script_lines=sbatch_init_lines,
     ) as executor:
+
         try:
             recursive_task_submission(
                 executor=executor,
@@ -187,7 +191,7 @@ def test_sbatch_script_slurm_config(
         assert len(job_name.split()[-1]) == len(task.name)
 
         sbatch_script = "".join(sbatch_script_lines)
-        for line in workflow_init:
+        for line in sbatch_init_lines:
             assert line in sbatch_script_lines
         debug(sbatch_script)
         if "task_parallel" in sbatch_script:
