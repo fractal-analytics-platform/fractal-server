fecbd70eff6d5c0c8fcd1215af85995b4476b070
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-11-30 14:23:23 +0100

    make a new copy of the settings object
    as it is of course mutable

--- tests/test_fixtures_settings_factory.py
+++ tests/test_fixtures_settings_factory.py
@@ -2,7 +2,6 @@
 These two tests simply check that the fixture `override_settings_factory`
 restores the settings to the original values, so as to guarantee statelessness.
 """
-import pytest
 from devtools import debug
 
 from fractal_server.config import get_settings
@@ -11,7 +10,6 @@ from fractal_server.syringe import Inject
 new_runner_backend = "pippo"
 
 
-@pytest.mark.skip("FIXME")
 def test0(override_settings_factory):
     settings = Inject(get_settings)
     orig_runner_backend = settings.RUNNER_BACKEND
@@ -23,7 +21,6 @@ def test0(override_settings_factory):
     debug("test0 done")
 
 
-@pytest.mark.skip("FIXME")
 def test1():
     settings = Inject(get_settings)
     orig_runner_backend = settings.RUNNER_BACKEND
