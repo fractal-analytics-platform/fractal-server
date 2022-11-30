46a7db40357275e848216865d46360aff7f49f72
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-30 08:54:24 +0100

    (third try) Change permissions of temp folder, trying to fix github CI

--- tests/fixtures_server.py
+++ tests/fixtures_server.py
@@ -55,7 +55,9 @@ def check_python_has_venv(python_path: str, temp_path: Path):
     import shlex
     import os
 
+    current_umask = os.umask(0)
     os.chmod(temp_path.parent.as_posix(), 0o777)
+    _ = os.umask(current_umask)
 
     cmd = f"{python_path} -m venv {temp_path.as_posix()}"
     p = subprocess.run(
