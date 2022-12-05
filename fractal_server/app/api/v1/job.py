3bf5c58d9eef4cd015d5c232cdb21ad73b53b7ac
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2022-12-05 14:56:50 +0100

    endpoint acl

--- fractal_server/app/api/v1/job.py
+++ fractal_server/app/api/v1/job.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
 
 from ...db import AsyncSession
 from ...db import get_db
@@ -9,6 +11,7 @@ from ...models import ApplyWorkflow
 from ...models import ApplyWorkflowRead
 from ...security import current_active_user
 from ...security import User
+from .project import get_project_check_owner
 
 
 router = APIRouter()
@@ -20,7 +23,13 @@ async def get_job(
     user: User = Depends(current_active_user),
     db: AsyncSession = Depends(get_db),
 ) -> Optional[ApplyWorkflow]:
-    # FIXME acl
-
     job = await db.get(ApplyWorkflow, job_id)
+    if not job:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
+        )
+    await get_project_check_owner(
+        project_id=job.project_id, user_id=user.id, db=db
+    )
+
     return job
