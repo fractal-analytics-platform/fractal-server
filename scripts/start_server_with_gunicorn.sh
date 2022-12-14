44b70c5e2a36dea352c9feca175e43b06e36f7c6
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-12-14 15:58:46 +0100

    Update gunicorn script example

--- scripts/start_server_with_gunicorn.sh
+++ scripts/start_server_with_gunicorn.sh
@@ -1,1 +1,11 @@
-gunicorn fractal_server.main:app --worker-class uvicorn.workers.UvicornWorker --access-logfile=-
+# Other relevant options:
+# Specify IP and port:
+#     --bind 127.0.0.1:8000
+# Specify number of workers
+#     --workers 4
+
+gunicorn fractal_server.main:app \
+    --worker-class uvicorn.workers.UvicornWorker \
+    --access-logfile=-\
+    --workers 2\
+    --bind 127.0.0.1:8000
