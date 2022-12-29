e2d6aaa22fdb9854e90887d9eebe0343d3689d8c
mfranzon <franzonmarco94@gmail.com>
2022-12-29 18:34:06 +0100

    move get_dataset endpoint and check function into project module

--- fractal_server/app/api/v1/project.py
+++ fractal_server/app/api/v1/project.py
@@ -73,6 +73,50 @@ async def get_project_check_owner(
     return project
 
 
+async def get_dataset_check_owner(
+    *,
+    project_id: int,
+    dataset_id: int,
+    user_id: UUID4,
+    db: AsyncSession = Depends(get_db),
+) -> Project:
+    """
+    Check that user is a member of project and return
+    Raise 403_FORBIDDEN if the user is not a member
+    Raise 404_NOT_FOUND if the project does not exist
+    """
+    project, dataset, link_user_project = await asyncio.gather(
+        db.get(Project, project_id),
+        db.get(Dataset, dataset_id),
+        db.get(LinkUserProject, (project_id, user_id)),
+    )
+    if not project:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
+        )
+    if not link_user_project:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Not allowed on project {project_id}",
+        )
+    return dataset
+
+
+@router.get("/{project_id}/{dataset_id}", response_model=DatasetRead)
+async def get_dataset(
+    project_id: int,
+    dataset_id: int,
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_db),
+) -> Optional[DatasetRead]:
+
+    dataset = await get_dataset_check_owner(
+        project_id=project_id, dataset_id=dataset_id, user_id=user.id, db=db
+    )
+
+    return dataset
+
+
 @router.get("/", response_model=List[ProjectRead])
 async def get_list_project(
     user: User = Depends(current_active_user),
@@ -292,7 +336,7 @@ async def delete_dataset(
 
 
 @router.get(
-    "/{project_id}/{dataset_id}",
+    "/{project_id}/{dataset_id}/resources/",
     response_model=List[ResourceRead],
 )
 async def get_resource(
