28142fc97764419ca30af43ee5ab3e2d80ae8401
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-12-29 09:05:56 +0100

    Rename app/models/run.py into app/models/job.py (close #391)

--- fractal_server/app/models/__init__.py
+++ fractal_server/app/models/__init__.py
@@ -1,6 +1,6 @@
 from ...common.schemas import *  # noqa F401
+from .job import *  # noqa: F403, F401
 from .project import *  # noqa: F403, F401
-from .run import *  # noqa: F403, F401
 from .security import *  # noqa: F403, F401
 from .state import State  # noqa: F401
 from .task import *  # noqa: F403, F401
