b01b03fd3a4bd32e2ce18e2bc2d6d29efed6469c
mfranzon <franzonmarco94@gmail.com>
2023-01-11 17:07:24 +0100

    add test for /users/me endpoint

--- tests/test_security.py
+++ tests/test_security.py
@@ -4,8 +4,25 @@ from devtools import debug
 PREFIX = "/auth"
 
 
-async def test_me(app, client, MockCurrentUser):
+async def test_me_unkonwn(client):
     # Anonymous
     res = await client.get(f"{PREFIX}/users/me")
     debug(res.json())
     assert res.status_code == 401
+
+
+async def test_me_known(client):
+    data_register = dict(
+        email="test@test.com", slurm_user="test", password="123"
+    )
+
+    data_login = dict(
+        username="test@test.com", password="123", lurm_user="test"
+    )
+    res = await client.post(f"{PREFIX}/register", json=data_register)
+    res = await client.post(f"{PREFIX}/token/login", data=data_login)
+    token = res.json()["access_token"]
+    client.headers["Authorization"] = f"Bearer {token}"
+    res = await client.get(f"{PREFIX}/users/me")
+    debug(res.json())
+    assert res.status_code == 200
