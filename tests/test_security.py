3b7529cc263baa105724d8c29d304aa9ff10f0aa
mfranzon <franzonmarco94@gmail.com>
2023-01-12 11:12:00 +0100

    rename fixure

--- tests/test_security.py
+++ tests/test_security.py
@@ -11,7 +11,7 @@ async def test_me_unkonwn(client):
     assert res.status_code == 401
 
 
-async def test_me_known(client_register):
-    res = await client_register.get(f"{PREFIX}/users/me")
+async def test_me_known(registered_client):
+    res = await registered_client.get(f"{PREFIX}/users/me")
     debug(res.json())
     assert res.status_code == 200
