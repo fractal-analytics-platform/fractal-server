7e342bae95584415445a58a732fd74627494d054
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-01-19 15:32:14 +0100

    Improve ACL bash script example

--- tests/sandbox_acl/example_1.sh
+++ tests/sandbox_acl/example_1.sh
@@ -11,7 +11,8 @@ as_user() {
 
 # Function to remove test users and their home folders
 cleanup_and_exit(){
-    echo "Clean up test users and exit"
+    echo "Clean up and exit"
+    # sudo rm -rf /tmp/acl_tests;
     sudo rm -r /home/testuser1;
     sudo rm -r /home/testuser2;
     sudo rm -r /home/testuser3;
@@ -39,80 +40,74 @@ as_user testuser1 id
 as_user testuser2 id
 as_user testuser3 id
 
-
 # Check the umask of these users
 as_user testuser1 "umask"
 
+# NOTE: setting "umask 0000" here is useless, since it is then refreshed before
+# the next command. This is achieved by combining the two commands, as in
+as_user testuser1 "umask 0000 && umask"
+
+# Create a folder
+DIR=/tmp/acl_tests
+sudo rm -r $DIR
+as_user testuser1 "mkdir $DIR"
+as_user testuser1 "chmod 700 $DIR"
+as_user testuser1 "ls -lh /tmp | grep acl_tests"
+echo
+
+# Set ACL for $DIR
+as_user testuser1 "umask 0000 && setfacl -b $DIR"
+as_user testuser1 "umask 0000 && setfacl --modify user:testuser1:rwx,user:testuser2:rwx,group::---,other::--- $DIR"
+as_user testuser1 "umask 0000 && setfacl --default --modify user:testuser1:rwx,user:testuser2:rwx,group::---,other::--- $DIR"
+as_user testuser1 "umask 0000 && getfacl -p $DIR"
+echo
+
+# Define some files in $DIR
+FILE1=${DIR}/file-of-testuser1.txt
+FILE2=${DIR}/file-of-testuser2.txt
+FILE3=${DIR}/file-of-testuser3.txt
+
+# Write a file as testuser1
+as_user testuser1 "umask 0000 && echo This-is-written-by-testuser1 > $FILE1"
+as_user testuser1 "umask 0000 && cat $FILE1"
+as_user testuser1 "umask 0000 && getfacl -p $FILE1"
+echo
+
+# NOTE: output includes:
+#   user:testuser1:rwx		#effective:rw-
+#   user:testuser2:rwx		#effective:rw-
+# which looks wrong
+
+# Check (authorized) access by testuser2
+as_user testuser2 "cat $FILE1"
+echo
+
+# Check (unauthorized) access by testuser3
+as_user testuser3 "cat $FILE1"
+echo
+
+# Write a file as testuser2
+as_user testuser2 "umask 0000 && echo This-is-written-by-testuser2 > $FILE2"
+as_user testuser2 "umask 0000 && cat $FILE2"
+as_user testuser2 "umask 0000 && getfacl -p $FILE2"
+echo
+
+# Check (authorized) access by testuser1
+as_user testuser1 "cat $FILE2"
+echo
+
+# Check (unauthorized) access by testuser3
+as_user testuser3 "cat $FILE2"
+echo
+
+# Write a file as testuser3 (unauthorized)
+as_user testuser3 "umask 0000 && echo This-is-written-by-testuser3 > $FILE3"
+echo
+
+# Check permissions and ACL of $DIR
+as_user testuser1 "ls -lh /tmp | grep acl_tests"
+as_user testuser1 "umask 0000 && getfacl -p $DIR"
+echo
+
 # Exit
 cleanup_and_exit
-
-
-
-
-DIR=/tmp/acl_dir
-sudo rm -r $DIR
-AFILE=${DIR}/logA.txt
-BFILE=${DIR}/logB.txt
-as_user testuser1 "mkdir $DIR"
-as_user testuser1 "ls -lh /tmp | grep acl"
-echo
-
-
-as_user testuser1 "setfacl -b $DIR"
-as_user testuser1 "setfacl --modify user:testuser1:rwx,user:testuser2:rwx,group::---,other::--- $DIR"
-as_user testuser1 "setfacl --default --modify user:testuser1:rwx,user:testuser2:rwx,group::---,other::--- $DIR"
-as_user testuser1 "getfacl -p $DIR"
-echo
-
-# as_user testuser1 "mkdir $DIR/subdir"
-# as_user testuser1 "getfacl -p $DIR/subdir"
-# echo
-
-as_user testuser1 "echo 123 > $AFILE"
-as_user testuser1 "cat $AFILE"
-as_user testuser1 "getfacl -p $AFILE"
-echo
-
-as_user testuser2 "cat $AFILE"
-echo
-
-as_user testuser2 "echo 456 > $BFILE"
-echo
-
-#as_user testuser2 "touch $BFILE"
-#as_user testuser2 "echo 123 >> $BFILE"
-#as_user testuser2 "cat $BFILE"
-#as_user testuser2 "getfacl -p $BFILE"
-echo
-
-umask $UMASK
-
-
-#as_user testuser1 "rm -rf $DIR"
-
-#$U1 getfacl -p $DIR
-#echo
-
-#$U1 setfacl -b $DIR
-#$U1 setfacl --default --modify user:testuser1:rwx,user:testuser2:rwx $DIR
-#echo
-#$U1 getfacl -p $DIR
-#echo
-#$U1 setfacl --modify u:testuser2:rwx,u:testuser1:rwx $DIR
-
-#$U1 echo "This is a log by testuser1" > $FILE
-#$U1 getfacl -p $FILE
-
-#$U1 echo "User testuser1 can write on this file" >> $FILE
-#$U3 echo "User testuser3 cannot write on this file" >> $FILE
-
-#$U1 rm -rf $DIR
-
-
-#$ chmod 700 /tmp/w5
-
-#$ ls -la /tmp/w5
-
-#$ setfacl -m u:testuser2:rwx,u:testuser1:rwx /tmp/w5
-
-#$ getfacl /tmp/w5
