2ad0deee30c291184825f3dfc1f7fdc4a663ead7
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-01-20 12:25:42 +0100

    BROKEN keep going with acl_utils.py

--- fractal_server/app/runner/acl_utils.py
+++ fractal_server/app/runner/acl_utils.py
@@ -2,66 +2,63 @@ import os
 import shlex
 import subprocess
 from pathlib import Path
-from typing import Sequence
+from typing import Optional
 
 from devtools import debug
 
 from ...config import get_settings
 from ...syringe import Inject
-from ...utils import execute_command
 
 
 def _execute_command(cmd: str):
-    cmd_split = shlex.split(cmd)
+    debug(cmd)
     res = subprocess.run(
-        cmd_split, encoding="utf-8", check=True, capture_output=True
+        shlex.split(cmd), encoding="utf-8", capture_output=True
     )
     debug(res)
     print(res.stdout)
     print()
     print(res.stderr)
     print()
-    assert res.returncode == 0
+    if res.returncode != 0:
+        raise RuntimeError(str(res))
 
 
-def _wrap_posix_setfacl(folder: Path, users: Sequence[str]):
+def _wrap_posix_setfacl(folder: Path, current_user: str, workflow_user: str):
     _execute_command(f"setfacl -b {folder}")
-    for user in users:
-        _execute_command(
-            f"setfacl --modify user:{user}:rwx,group::---,other::--- {str(folder)}"
-        )
-    _execute_command(f"getfacl -p {folder}")
+    _execute_command(
+        "setfacl --default --recursive --modify "
+        f"user:{current_user}:rwx,user:{workflow_user}:rwx,"
+        f"group::---,other::--- {folder}"
+    )
 
 
 def _wrap_nfs_setfacl(folder: Path, user: str):
     raise NotImplementedError()
 
 
-def mkdir_with_acl(folder: Path, *, workflow_user: str):
+def mkdir_with_acl(
+    folder: Path, *, workflow_user: str, acl_options: Optional[str] = None
+):
     """
     TBD
     """
-
-    # Always create the folder, and make it 700
+    # Preliminary check
     if folder.exists():
         raise ValueError(f"{str(folder)} already exists.")
-    # folder.mkdir(parents=True)
-    # folder.chmod(0o700)
-    _execute_command(f"mkdir -p {str(folder)}")
-    _execute_command(f"ls -la {str(folder)}")
-    _execute_command(f"chmod 700 {str(folder)}")
-    _execute_command(f"ls -la {str(folder)}")
 
-    current_user = os.getlogin()
+    # Create the folder, and make it 700
+    folder.mkdir(parents=True, mode=0o700)
 
     # Apply permissions
-    settings = Inject(get_settings)
-    FRACTAL_ACL_OPTIONS = settings.FRACTAL_ACL_OPTIONS
-    if FRACTAL_ACL_OPTIONS == "none":
+    if not acl_options:
+        settings = Inject(get_settings)
+        acl_options = settings.FRACTAL_ACL_OPTIONS
+    if acl_options == "none":
         return
-    if FRACTAL_ACL_OPTIONS == "standard":  # FIXME say posix
+    elif acl_options == "posix":
         _wrap_posix_setfacl(
-            folder, users=[current_user, workflow_user, "fractal"]
+            folder, current_user=os.getlogin(), workflow_user=workflow_user
         )
     else:
-        raise ValueError(f"{FRACTAL_ACL_OPTIONS=} not supported")
+        raise ValueError(f"{acl_options=} not supported")
