56a08fb3bd9880ed418528e4e3c36e4510f62282
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-01-25 11:46:48 +0100

    Remove obsolete test

--- tests/test_backend_runners.py
+++ tests/test_backend_runners.py
@@ -11,7 +11,6 @@ This file is part of Fractal and was originally developed by eXact lab S.r.l.
 Institute for Biomedical Research and Pelkmans Lab from the University of
 Zurich.
 """
-import datetime
 import logging
 
 import pytest
@@ -115,86 +114,3 @@ async def test_runner(
         args = f.read()
         debug(args)
         assert "logger_name" not in args
-
-
-@pytest.mark.skip(reason="Skip in favor of test_call_parallel_task_max_tasks")
-@pytest.mark.parametrize("max_tasks", [None, 1])
-async def test_runner_max_number_tasks(
-    db,
-    project_factory,
-    MockCurrentUser,
-    collect_packages,
-    tmp777_path,
-    request,
-    override_settings_factory,
-    max_tasks,
-):
-    """
-    GIVEN A workflow with a single task, parallelized over two components
-    WHEN This workflow is submitted through the local backend
-    THEN The FRACTAL_RUNNER_MAX_TASKS_PER_WORKFLOW env variable is used
-         correctly
-    """
-
-    SLEEP_TIME = 1
-    INPUT_METADATA = dict(index=["0", "1"])
-    process_workflow = _backends["local"]
-    override_settings_factory(FRACTAL_RUNNER_MAX_TASKS_PER_WORKFLOW=max_tasks)
-
-    async with MockCurrentUser(persist=True) as user:
-        prj = await project_factory(user=user)
-
-    # Create a workflow with the dummy_parallel task
-    task_dummy_parallel = collect_packages[1]
-    wf = Workflow(name="wf", project_id=prj.id)
-    db.add(wf)
-    await db.commit()
-    await db.refresh(wf)
-    await wf.insert_task(
-        task_dummy_parallel.id,
-        db=db,
-        args=dict(message="task 2", sleep_time=SLEEP_TIME),
-    )
-    await db.refresh(wf)
-    debug(wf)
-
-    # Execute workflow
-    metadata = await process_workflow(
-        workflow=wf,
-        input_paths=[tmp777_path / "*.txt"],
-        output_path=tmp777_path / "out.json",
-        input_metadata=INPUT_METADATA,
-        logger_name="my-logs",
-        workflow_dir=tmp777_path,
-    )
-    debug(metadata)
-
-    # Check that the two tasks were submitted at the appropriate time,
-    # depending on FRACTAL_RUNNER_MAX_TASKS_PER_WORKFLOW. NOTE: the log parsing
-    # and log-to-datetime conversion may easily break if we change the logs
-    # format
-    with (tmp777_path / "0_par_0.err").open("r") as f:
-        first_log_task_0 = f.readlines()[0]
-    with (tmp777_path / "0_par_1.err").open("r") as f:
-        first_log_task_1 = f.readlines()[0]
-    debug(first_log_task_0)
-    debug(first_log_task_1)
-    assert "; INFO; ENTERING" in first_log_task_0
-    assert "; INFO; ENTERING" in first_log_task_1
-    # Parse times
-    fmt = "%Y-%m-%d %H:%M:%S,%f"
-    time_start_task_0 = datetime.datetime.strptime(
-        first_log_task_0.split("; INFO; ENTERING")[0], fmt
-    )
-    time_start_task_1 = datetime.datetime.strptime(
-        first_log_task_1.split("; INFO; ENTERING")[0], fmt
-    )
-    debug(time_start_task_0)
-    debug(time_start_task_1)
-    # Check time difference
-    diff = (time_start_task_1 - time_start_task_0).total_seconds()
-    debug(diff)
-    if max_tasks == 1:
-        assert diff >= SLEEP_TIME
-    else:
-        assert diff < SLEEP_TIME
