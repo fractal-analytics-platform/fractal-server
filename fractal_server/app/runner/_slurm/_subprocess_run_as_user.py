2af05b5ac0a6786146805c0ea5635f92e3d9e798
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-01-31 11:26:42 +0100

    Expose encoding in _run_command_as_user

--- fractal_server/app/runner/_slurm/_subprocess_run_as_user.py
+++ fractal_server/app/runner/_slurm/_subprocess_run_as_user.py
@@ -1,18 +1,23 @@
 import logging
 import shlex
 import subprocess  # nosec
+from typing import Optional
 
 
-def _run_command_as_user(*, cmd: str, user: str):
+def _run_command_as_user(
+    *, cmd: str, user: str, encoding: Optional[str] = "utf-8"
+):
     """
     Use `sudo -u` to impersonate another user and run a command
+
+    FIXME docstring
     """
     # FIXME: turn INFO into DEBUG
     logging.debug(f'[_run_command_as_user] {user=}, cmd="{cmd}"')
     res = subprocess.run(  # nosec
         shlex.split(f"sudo --non-interactive -u {user} {cmd}"),
         capture_output=True,
-        encoding="utf-8",
+        encoding=encoding,
     )
     logging.debug(f"[_run_command_as_user] {res.returncode=}")
     logging.debug(f"[_run_command_as_user] {res.stdout=}")
