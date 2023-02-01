2ba87b1ed1c6f152ce6a182d861459648e053a83
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-02-01 12:02:17 +0100

    Cleanup tests folder

--- tests/Sandbox_permissions/script_load_pickle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-import pickle
-import sys
-
-with open(sys.argv[1], "rb") as f:
-    content = pickle.load(f)
-    assert content == {"key": "value"}
-    print(f"User {os.getuid()} successfully read data from {sys.argv[1]}")
