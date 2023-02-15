214b4d6e833208a6447402fa939fa811a465fb0f
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-02-15 09:36:40 +0100

    Add test_slurm_executor_map_with_exception

--- tests/test_backend_slurm.py
+++ tests/test_backend_slurm.py
@@ -1,6 +1,7 @@
 import os
 import shlex
 import subprocess
+import time
 from concurrent.futures import Executor
 from itertools import product
 from pathlib import Path
@@ -143,7 +144,7 @@ def test_slurm_executor_submit(
         slurm_user=monkey_slurm_user,
         working_dir=tmp777_path,
         working_dir_user=tmp777_path,
-        slurm_poll_interval=4,
+        slurm_poll_interval=2,
     ) as executor:
         res = executor.submit(lambda: 42)
     assert res.result() == 42
@@ -159,12 +160,46 @@ def test_slurm_executor_map(
         slurm_user=monkey_slurm_user,
         working_dir=tmp777_path,
         working_dir_user=tmp777_path,
-        slurm_poll_interval=4,
+        slurm_poll_interval=2,
     ) as executor:
         result_generator = executor.map(lambda x: 2 * x, range(4))
         results = list(result_generator)
         debug(results)
         assert results == [2 * x for x in range(4)]
+
+
+@pytest.mark.parametrize("early_late", ["early", "late"])
+def test_slurm_executor_map_with_exception(
+    early_late,
+    monkey_slurm,
+    monkey_slurm_user,
+    tmp777_path,
+    cfut_jobs_finished,
+):
+    debug(early_late)
+
+    def _raise(n: int):
+        if n == 1:
+            if early_late == "late":
+                time.sleep(2)
+            raise ValueError
+        else:
+            if early_late == "early":
+                time.sleep(2)
+            return n
+
+    with FractalSlurmExecutor(
+        slurm_user=monkey_slurm_user,
+        working_dir=tmp777_path,
+        working_dir_user=tmp777_path,
+        slurm_poll_interval=1,
+    ) as executor:
+
+        with pytest.raises(JobExecutionError) as e:
+            result_generator = executor.map(_raise, range(4))
+            for result in result_generator:
+                debug(f"While looping over results, I got to {result=}")
+        debug(e.value)
 
 
 def test_slurm_executor_submit_separate_folders(
@@ -186,7 +221,7 @@ def test_slurm_executor_submit_separate_folders(
         slurm_user=monkey_slurm_user,
         working_dir=server_working_dir,
         working_dir_user=user_working_dir,
-        slurm_poll_interval=4,
+        slurm_poll_interval=2,
     ) as executor:
         res = executor.submit(lambda: 42)
     assert res.result() == 42
@@ -221,7 +256,7 @@ def test_slurm_executor_submit_and_scancel(
             working_dir_user=user_working_dir,
             debug=True,
             keep_logs=True,
-            slurm_poll_interval=4,
+            slurm_poll_interval=2,
         ) as executor:
             fut = executor.submit(wait_and_return)
             debug(fut)
