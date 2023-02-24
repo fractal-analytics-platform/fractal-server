426cfe231b76f8c5e4d87e1c9e666763f92b3014
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-02-24 10:07:02 +0100

    Remove test_warning

--- tests/test_unit_coroutine_never_awaited.py
+++ tests/test_unit_coroutine_never_awaited.py
@@ -4,15 +4,31 @@ import pytest
 
 
 async def test_warning():
-    # Raises UserWarning
-    raise UserWarning("This will raise no error")
+    """
+    This raises a UserWarning, which is not included in filterwarnings and
+    therefore not transformed into an error.
+    """
+    raise UserWarning("This is a dummy warning, to be ignored")
 
 
 @pytest.mark.xfail()
 async def test_fail_explicit_RuntimeWarning():
+    """
+    This raises a "genunine" RuntimeWarning, which is included in
+    filterwarnings and transformed into an error.
+    """
     raise RuntimeWarning("asd")
 
 
 @pytest.mark.xfail()
 async def test_fail_implicit_RuntimeWarning():
+    """
+    This (indirectly) raises a RuntimeWarning because the coroutine 'sleep' was
+    never awaited. Note that this is likely an unhandled warning exception, see
+    multiple references mentioned in
+    https://github.com/fractal-analytics-platform/fractal-server/issues/287.
+    The presence of both RuntimeWarning and
+    pytest.PytestUnraisableExceptionWarning in filterwarnings transforms it
+    into an error.
+    """
     asyncio.sleep(0.2)
