7b7b4112ca9a4967ef624f9c62876b1920d0e269
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-03-17 15:08:16 +0100

    Update test_slurm_scripts.py

--- tests/test_slurm_scripts.py
+++ tests/test_slurm_scripts.py
@@ -1,5 +1,6 @@
 import os
 import shlex
+import shutil
 import subprocess
 import time
 
@@ -37,39 +38,52 @@ def write_script(
     for ind in range(num_tasks_tot):
         script += (
             "srun --ntasks=1 --cpus-per-task=$SLURM_CPUS_PER_TASK "
-            f"--mem=$MEM_PER_TASK {command} {ind} 5 &\n"
+            f"--mem=$MEM_PER_TASK {command} {ind} 2 {logdir} &\n"
         )
     script += "wait\n"
     return script
 
 
-@pytest.mark.skip
+N = 8
+
+
+@pytest.mark.parametrize("max_tasks", [1, 2, 4, 8])
 def test_slurm_script(
+    max_tasks,
     monkey_slurm,
     monkey_slurm_user,
     tmp777_path,
     cfut_jobs_finished,
     testdata_path,
 ):
-    sbatch_script_path = str(tmp777_path / "submit.sbatch")
+
+    # NOTE: by now we make everything 777
+
+    sbatch_script_path = tmp777_path / "submit.sbatch"
     logdir = str(tmp777_path / "logs")
-    os.mkdir(logdir)
-    task_path = str(testdata_path / "fake_task_for_timing.py")
+    umask = os.umask(0)
+    os.mkdir(logdir, 0o777)
+    _ = os.umask(umask)
+    old_task_path = str(testdata_path / "fake_task_for_timing.py")
+    task_path = str(tmp777_path / "fake_task_for_timing.py")
+    shutil.copy(old_task_path, task_path)
     command = f"/usr/bin/python3 {task_path}"
     debug(sbatch_script_path)
     debug(command)
 
-    with open(sbatch_script_path, "w") as f:
+    with sbatch_script_path.open("w") as f:
         sbatch_script = write_script(
-            num_tasks_tot=1,
-            num_tasks_max_running=1,
-            mem_per_task_MB=1,
+            num_tasks_tot=N,
+            num_tasks_max_running=max_tasks,
+            mem_per_task_MB=2,
             cpu_per_task=1,
             logdir=logdir,
             command=command,
         )
         f.write(sbatch_script)
+    sbatch_script_path.chmod(0o777)
 
+    sbatch_script_path = str(sbatch_script_path)
     res = subprocess.run(
         shlex.split(
             f"sudo -u {monkey_slurm_user} sbatch {sbatch_script_path}"
@@ -81,9 +95,9 @@ def test_slurm_script(
     debug(res.stderr)
     assert res.returncode == 0
 
-    return
-
-    for ind in range(20):
-        squeue = run_squeue()
-        debug(squeue)
+    while True:
+        squeue_list = run_squeue(header=False)
+        debug(squeue_list)
         time.sleep(1)
+        if not squeue_list:
+            break
