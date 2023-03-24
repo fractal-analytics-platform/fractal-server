f961fc06bec1feb073923250c816ef5a1d8be63d
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-03-24 14:22:01 +0100

    Add meta to MockTask and MockWorkflowTask

--- tests/fixtures_tasks.py
+++ tests/fixtures_tasks.py
@@ -1,7 +1,6 @@
 import asyncio
 from pathlib import Path
 from typing import Any
-from typing import Dict
 from typing import Optional
 
 import pytest
@@ -14,12 +13,14 @@ class MockTask(BaseModel):
     name: str
     command: str
     parallelization_level: Optional[str] = None
+    meta: dict = {}
 
 
 class MockWorkflowTask(BaseModel):
     order: int = 0
     task: MockTask
-    arguments: Dict = {}
+    arguments: dict = {}
+    meta: dict = {}
     executor: Optional[str] = "default"
 
     @property
@@ -30,13 +31,13 @@ class MockWorkflowTask(BaseModel):
     def parallelization_level(self) -> Optional[str]:
         return self.task.parallelization_level
 
-    def assemble_args(self, extra: Dict[str, Any] = None):
+    def assemble_args(self, extra: dict[str, Any] = None):
         """
         Merge of `extra` arguments and `self.arguments`.
 
         Return
         ------
-        full_arsgs (Dict):
+        full_arsgs (dict):
             A dictionary consisting of the merge of `extra` and
             self.arguments.
         """
