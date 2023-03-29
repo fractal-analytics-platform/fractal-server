e0b90330a07a2277f844896c4430e19be72f448d
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-03-29 15:38:02 +0200

    Produce table for SLURM heuristics

--- tests/test_unit_slurm_batching_heuristics.py
+++ tests/test_unit_slurm_batching_heuristics.py
@@ -1,5 +1,6 @@
+import math
+
 import pytest
-from devtools import debug
 
 from fractal_server.app.runner._grouped_slurm._batching_heuristics import (
     heuristics,
@@ -25,32 +26,39 @@ clusters = [
     ),
 ]
 
+print("")
+cols = (
+    "Cluster   | "
+    "cpus/task | "
+    "mem/task | "
+    "| "
+    "#jobs | "
+    "tasks/script | "
+    "parallel_tasks/script | "
+    "Parallelism |"
+)
+print(cols)
+
 
 @pytest.mark.parametrize("n_ftasks_tot", [1, 10, 40, 96, 400])
 @pytest.mark.parametrize(
-    "task_requirements", [(1, "4G"), (16, "62G"), (4, "16G")]
+    "task_requirements", [(1, 4000), (16, 62000), (4, 16000)]
 )
 @pytest.mark.parametrize("cluster", clusters)
 def test_heuristics(
-    n_ftasks_per_script: int | None,
-    n_parallel_ftasks_per_script: int | None,
     n_ftasks_tot: int,
-    cpus_per_task: int,
-    max_cpus_per_job: int,
-    mem_per_task: int,
-    max_mem_per_job: int,
-    max_num_jobs: int,
+    task_requirements: tuple[int, int],
+    cluster: tuple[dict[str, int]],
 ):
-    debug(n_ftasks_per_script)
-    debug(n_parallel_ftasks_per_script)
-
-    target_cpus_per_job = int(max_cpus_per_job * 0.75)
-    target_mem_per_job = int(max_mem_per_job * 0.75)
-    target_num_jobs = int(max_num_jobs * 0.75)
+    cpus_per_task, mem_per_task = task_requirements[:]
+    target_cpus_per_job = cluster["target_cpus_per_job"]
+    max_cpus_per_job = cluster["max_cpus_per_job"]
+    target_mem_per_job = cluster["target_mem_per_job"]
+    max_mem_per_job = cluster["max_mem_per_job"]
+    target_num_jobs = cluster["target_num_jobs"]
+    max_num_jobs = cluster["max_num_jobs"]
 
     n_ftasks_per_script, n_parallel_ftasks_per_script = heuristics(
-        n_ftasks_per_script=n_ftasks_per_script,
-        n_parallel_ftasks_per_script=n_parallel_ftasks_per_script,
         n_ftasks_tot=n_ftasks_tot,
         cpus_per_task=cpus_per_task,
         target_cpus_per_job=target_cpus_per_job,
@@ -61,5 +69,22 @@ def test_heuristics(
         target_num_jobs=target_num_jobs,
         max_num_jobs=max_num_jobs,
     )
-    debug(n_ftasks_per_script)
-    debug(n_parallel_ftasks_per_script)
+    num_jobs = math.ceil(n_ftasks_tot / n_ftasks_per_script)
+    parallelism = n_parallel_ftasks_per_script / n_ftasks_per_script
+    cluster_index = clusters.index(cluster)
+    cluster_name = f"cluster_{cluster_index}"
+
+    output = (
+        f"{cluster_name} | "
+        f"{cpus_per_task:9d} | "
+        f"{mem_per_task:8d} | "
+        "| "
+        f"{num_jobs:5d} | "
+        f"{n_ftasks_per_script:12d} | "
+        f"{n_parallel_ftasks_per_script:21d} | "
+        f"{parallelism:11.3f} |"
+    )
+
+    print()
+    print(output)
+    print()
