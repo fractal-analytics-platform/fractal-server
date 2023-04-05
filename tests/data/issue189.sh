5ee67a17aab9a2971a9e8067f29f85302ed7e424
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-04-05 13:06:01 +0200

    changed sh script

--- tests/data/issue189.sh
+++ tests/data/issue189.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-while [[ $# -gt 0 ]]; do
+# This script accepts two optional arguments, `--json`` and `--metadata-out`.
+# If both arguments are provided, the script copies the file specified
+# by the `--json` argument to the location specified by the `--metadata-out`
+# argument. If any other arguments are provided, the script raises an error.
+
+# used in `tests/test_full_workflow.py::test_non_python_task`
+
+JSON_FILE=
+METADATA_OUT=
+
+while [[ $# -gt 0 ]]
+do
     key="$1"
     case $key in
         --json)
-            shift
-            ;;
+        JSON_FILE="$2"
+        shift # past argument
+        shift # past value
+        ;;
         --metadata-out)
-            shift
-            ;;
+        METADATA_OUT="$2"
+        shift # past argument
+        shift # past value
+        ;;
         *)
-            echo "Unknown argument: $1"
-            exit 1
-            ;;
+        echo "Error: unknown argument $key"
+        exit 1
+        ;;
     esac
-    shift
 done
 
-echo "Hello, Fractal!"
+if [[ -n $JSON_FILE && -n $METADATA_OUT ]]
+then
+    cp "$JSON_FILE" "$METADATA_OUT"
+fi
