083db355487d70fa07daa9fcf3e383f1054fef4e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-04-20 09:48:06 +0200

    Add port 4173 to allowed origins (close #634)

--- fractal_server/main.py
+++ fractal_server/main.py
@@ -136,7 +136,12 @@ def start_application() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
+        allow_origins=[
+            "http://127.0.0.1:5173",
+            "http://localhost:5173",
+            "http://127.0.0.1:4173",
+            "http://localhost:4173",
+        ],
         allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         allow_headers=[
             "set-cookie",
