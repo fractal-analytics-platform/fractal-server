1ceb4e8832f3af9a755501b82b1507372f90608b
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-05-03 15:59:38 +0200

    STASH test_591
    ref #591

--- tests/test_project_api.py
+++ tests/test_project_api.py
@@ -646,3 +646,41 @@ async def test_create_project(
         res = await client.post(f"{PREFIX}/", json=empty_payload)
         debug(res.json())
         assert res.status_code == 422
+
+
+async def test_591(
+    override_settings_factory,
+    MockCurrentUser,
+    client,
+    project_factory,
+    dataset_factory,
+    workflow_factory,
+    task_factory,
+    db,
+):
+    override_settings_factory(FRACTAL_RUNNER_BACKEND="slurm")
+    async with MockCurrentUser(
+        persist=True, user_kwargs=dict(slurm_user=None)
+    ) as user:
+
+        project = await project_factory(user)
+        input_dataset = await dataset_factory(project, name="input")
+        output_dataset = await dataset_factory(project, name="output")
+        workflow = await workflow_factory(project_id=project.id)
+        task = await task_factory()
+        await workflow.insert_task(task.id, db=db)
+
+        payload = dict(
+            project_id=project.id,
+            input_dataset_id=input_dataset.id,
+            output_dataset_id=output_dataset.id,
+            workflow_id=workflow.id,
+            overwrite_input=False,
+        )
+
+        res = await client.post(
+            f"{PREFIX}/apply/",
+            json=payload,
+        )
+        debug(res.json())
+        assert res.status_code == 422
