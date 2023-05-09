9a1bbe0510a053b50b1c1debf491df880dbf03db
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-09 10:30:50 +0200

    Use prefix for workflow endpoints

--- fractal_server/app/api/__init__.py
+++ fractal_server/app/api/__init__.py
@@ -16,7 +16,9 @@ router_v1 = APIRouter()
 
 router_v1.include_router(project_router, prefix="/project", tags=["project"])
 router_v1.include_router(task_router, prefix="/task", tags=["task"])
-router_v1.include_router(workflow_router, prefix="", tags=["workflow"])
+router_v1.include_router(
+    workflow_router, prefix="/project/{project_id}/workflow", tags=["workflow"]
+)
 router_v1.include_router(job_router, prefix="/job", tags=["monitoring"])
 
 
