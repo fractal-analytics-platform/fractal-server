c9e65e86c5ed6d1670449776d68a6157e40a87e3
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-09 20:37:34 +0200

    Update 1.2->1.3 migration info

--- tests/data/migration_1.2_to_1.3/parse_and_compare.py
+++ tests/data/migration_1.2_to_1.3/parse_and_compare.py
@@ -9,17 +9,33 @@ with open("openapi_1_3_0.json", "r") as f:
 
 paths_12 = set(api12["paths"].keys())
 paths_13 = set(api13["paths"].keys())
-
 intersection = set.intersection(paths_12, paths_13)
 while intersection:
     element = intersection.pop()
     paths_12.remove(element)
     paths_13.remove(element)
 
-debug(sorted(paths_12))
-debug(sorted(paths_13))
+paths_12 = sorted(list(paths_12))
+paths_13 = sorted(list(paths_13))
 
+debug(paths_12)
+debug(paths_13)
+
+endpoints_12 = []
+endpoints_13 = []
 for path in paths_12:
-    assert path in api12["paths"].keys()
-    val = api12["paths"][path]
-    assert val
+    for method in api12["paths"][path].keys():
+        endpoints_12.append(f"{method.upper()} {path}")
+for path in paths_13:
+    for method in api13["paths"][path].keys():
+        endpoints_13.append(f"{method.upper()} {path}")
+endpoints_12 = sorted(endpoints_12)
+endpoints_13 = sorted(endpoints_13)
+
+assert len(endpoints_12) == len(endpoints_13)
+with open("raw_endpoints.txt", "w") as f:
+    for ind, endpoint_12 in enumerate(endpoints_12):
+        endpoint_13 = endpoints_13[ind]
+        f.write(f"OLD {endpoint_12}\n")
+        f.write(f"NEW {endpoint_13}\n")
+        f.write("\n")
