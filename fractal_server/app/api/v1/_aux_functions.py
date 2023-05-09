f9a80bc134016593b2889ec2115f3d48ad9503f0
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-09 20:48:32 +0200

    Clean up app/api/v1/_aux_functions.py

--- fractal_server/app/api/v1/_aux_functions.py
+++ fractal_server/app/api/v1/_aux_functions.py
@@ -4,14 +4,13 @@ Auxiliary functions to get object from the database or perform simple checks
 import asyncio
 from typing import Union
 
-from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
 from pydantic import UUID4
 from sqlmodel import select
 
 from ...db import AsyncSession
-from ...db import get_db
+from ...models import ApplyWorkflow
 from ...models import Dataset
 from ...models import LinkUserProject
 from ...models import Project
@@ -19,24 +18,23 @@ from ...models import Workflow
 from ...models import WorkflowTask
 
 
-async def _get_workflow_check_owner(
+async def _get_project_check_owner(
     *,
-    workflow_id: int,
     project_id: int,
     user_id: UUID4,
-    db: AsyncSession = Depends(get_db),
-) -> Workflow:
+    db: AsyncSession,
+) -> Project:
     """
-    Check that user is a member of a workflow's project and return
+    Check that user is a member of project and return the project
+
+    This function is at the basis of most access-control checks.
 
     Raises:
         HTTPException(status_code=403_FORBIDDEN): If the user is not a
                                                   member of the project
-        HTTPException(status_code=404_NOT_FOUND): If the project or workflow do
-                                                  not exist
+        HTTPException(status_code=404_NOT_FOUND): If the project does not
+                                                  exist
     """
-
-    # Access control for project
     project, link_user_project = await asyncio.gather(
         db.get(Project, project_id),
         db.get(LinkUserProject, (project_id, user_id)),
@@ -50,7 +48,25 @@ async def _get_workflow_check_owner(
             status_code=status.HTTP_403_FORBIDDEN,
             detail=f"Not allowed on project {project_id}",
         )
+    return project
 
+
+async def _get_workflow_check_owner(
+    *,
+    workflow_id: int,
+    project_id: int,
+    user_id: UUID4,
+    db: AsyncSession,
+) -> Workflow:
+    """
+    Get a workflow and a project, after access control on the project
+    """
+
+    # Access control for project
+    project = _get_project_check_owner(
+        project_id=project_id, user_id=user_id, db=db
+    )
+    # Get workflow
     workflow = await db.get(Workflow, workflow_id)
     if not workflow:
         raise HTTPException(
@@ -59,9 +75,7 @@ async def _get_workflow_check_owner(
     if workflow.project_id != project.id:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=(
-                f"Error: {workflow.project_id=} differs from {project_id=}"
-            ),
+            detail=(f"Invalid {project_id=} for {workflow_id=}."),
         )
 
     return workflow
@@ -73,7 +87,7 @@ async def _get_workflow_task_check_owner(
     workflow_id: int,
     workflow_task_id: int,
     user_id: UUID4,
-    db: AsyncSession = Depends(get_db),
+    db: AsyncSession,
 ) -> tuple[WorkflowTask, Workflow]:
     """
     Check that user has rights to access a Workflow and a WorkflowTask and
@@ -89,7 +103,7 @@ async def _get_workflow_task_check_owner(
     """
 
     # Access control for workflow
-    workflow = await _get_workflow_check_owner(  # noqa: F841
+    workflow = await _get_workflow_check_owner(
         workflow_id=workflow_id, project_id=project_id, user_id=user_id, db=db
     )
 
@@ -115,7 +129,7 @@ async def _check_workflow_exists(
     *,
     name: str,
     project_id: int,
-    db: AsyncSession = Depends(get_db),
+    db: AsyncSession,
 ):
     """
     Check that there is no existing workflow for the same project and with the
@@ -142,37 +156,6 @@ async def _check_workflow_exists(
         )
 
 
-async def _get_project_check_owner(
-    *,
-    project_id: int,
-    user_id: UUID4,
-    db: AsyncSession,
-) -> Project:
-    """
-    Check that user is a member of project and return
-
-    Raises:
-        HTTPException(status_code=403_FORBIDDEN): If the user is not a
-                                                  member of the project
-        HTTPException(status_code=404_NOT_FOUND): If the project does not
-                                                  exist
-    """
-    project, link_user_project = await asyncio.gather(
-        db.get(Project, project_id),
-        db.get(LinkUserProject, (project_id, user_id)),
-    )
-    if not project:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
-        )
-    if not link_user_project:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail=f"Not allowed on project {project_id}",
-        )
-    return project
-
-
 async def _get_dataset_check_owner(
     *,
     project_id: int,
@@ -181,30 +164,15 @@ async def _get_dataset_check_owner(
     db: AsyncSession,
 ) -> dict[str, Union[Dataset, Project]]:
     """
-    Check that user is a member of project and return
-
-    Raises:
-        HTTPException(status_code=403_FORBIDDEN): If the user is not a
-                                                         member of the project
-        HTTPException(status_code=404_NOT_FOUND): If the dataset or project do
-                                                  not exist
+    Get a dataset and a project, after access control on the project
     """
-    project, dataset, link_user_project = await asyncio.gather(
-        db.get(Project, project_id),
-        db.get(Dataset, dataset_id),
-        db.get(LinkUserProject, (project_id, user_id)),
-    )
 
     # Access control for project
-    if not project:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
-        )
-    if not link_user_project:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail=f"Not allowed on project {project_id}",
-        )
+    project = _get_project_check_owner(
+        project_id=project_id, user_id=user_id, db=db
+    )
+    # Get dataset
+    dataset = await db.get(Dataset, dataset_id)
     if not dataset:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Dataset not found"
@@ -213,5 +181,33 @@ async def _get_dataset_check_owner(
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail=f"Invalid {project_id=} for {dataset_id=}",
+        )
+    return dict(dataset=dataset, project=project)
+
+
+async def _get_job_check_owner(
+    *,
+    project_id: int,
+    job_id: int,
+    user_id: UUID4,
+    db: AsyncSession,
+) -> dict[str, Union[Dataset, Project]]:
+    """
+    Get a job and a project, after access control on the project
+    """
+    # Access control for project
+    project = _get_project_check_owner(
+        project_id=project_id, user_id=user_id, db=db
+    )
+    # Get dataset
+    dataset = await db.get(ApplyWorkflow, job_id)
+    if not dataset:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Dataset not found"
+        )
+    if dataset.project_id != project_id:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Invalid {project_id=} for {job_id=}",
         )
     return dict(dataset=dataset, project=project)
