6266d899ea3933687efa062a207b80ff76483547
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-09 13:29:41 +0200

    Update migration-instructions tool

--- tests/data/migration_1.2_to_1.3/parse_and_compare.py
+++ tests/data/migration_1.2_to_1.3/parse_and_compare.py
@@ -7,8 +7,8 @@ with open("openapi_1_2_5.json", "r") as f:
 with open("openapi_1_3_0.json", "r") as f:
     api13 = json.load(f)
 
-paths_12 = set(api12["paths"])
-paths_13 = set(api13["paths"])
+paths_12 = set(api12["paths"].keys())
+paths_13 = set(api13["paths"].keys())
 
 intersection = set.intersection(paths_12, paths_13)
 while intersection:
@@ -18,3 +18,8 @@ while intersection:
 
 debug(sorted(paths_12))
 debug(sorted(paths_13))
+
+for path in paths_12:
+    assert path in api12["paths"].keys()
+    val = api12["paths"][path]
+    assert val
