fc7890f1e2c79147bbc23a135bfa947531714e0d
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-05-10 11:35:22 +0200

    add test for `_get_workflow_task_check_owner`

--- tests/test_unit_aux_functions.py
+++ tests/test_unit_aux_functions.py
@@ -1,5 +1,4 @@
 import pytest
-from devtools import debug
 from fastapi import HTTPException
 
 from fractal_server.app.api.v1._aux_functions import _check_workflow_exists
@@ -93,8 +92,61 @@ async def test_get_workflow_check_owner(
         )
 
 
-async def test_get_workflow_task_check_owner():
-    pass
+async def test_get_workflow_task_check_owner(
+    MockCurrentUser,
+    project_factory,
+    workflow_factory,
+    task_factory,
+    workflowtask_factory,
+    db,
+):
+    async with MockCurrentUser(persist=True) as user:
+        project = await project_factory(user)
+        workflow = await workflow_factory(project_id=project.id)
+        task = await task_factory()
+        wftask = await workflowtask_factory(
+            workflow_id=workflow.id, task_id=task.id
+        )
+        other_workflow = await workflow_factory(project_id=project.id)
+        other_task = await task_factory()
+        other_wftask = await workflowtask_factory(
+            workflow_id=other_workflow.id, task_id=other_task.id
+        )
+
+        # Test success
+        await _get_workflow_task_check_owner(
+            project_id=project.id,
+            workflow_id=workflow.id,
+            workflow_task_id=wftask.id,
+            user_id=user.id,
+            db=db,
+        )
+        # Test fail 1
+        with pytest.raises(HTTPException) as err:
+            await _get_workflow_task_check_owner(
+                project_id=project.id,
+                workflow_id=workflow.id,
+                workflow_task_id=wftask.id + other_wftask.id,
+                user_id=user.id,
+                db=db,
+            )
+        assert err.value.status_code == 404
+        assert err.value.detail == "WorkflowTask not found"
+
+        # Test fail 2
+        with pytest.raises(HTTPException) as err:
+            await _get_workflow_task_check_owner(
+                project_id=project.id,
+                workflow_id=workflow.id,
+                workflow_task_id=other_wftask.id,
+                user_id=user.id,
+                db=db,
+            )
+        assert err.value.status_code == 422
+        assert err.value.detail == (
+            f"Invalid workflow_id={workflow.id} "
+            f"for workflow_task_id={other_wftask.id}"
+        )
 
 
 async def test_check_workflow_exists(
