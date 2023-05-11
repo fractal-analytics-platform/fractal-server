e5600585cf3e883421f2106248c60831a39b6609
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-05-11 12:41:16 +0200

    add `test_apply_wf_read`

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 import pytest
 
+from fractal_server.common.schemas.applyworkflow import ApplyWorkflowRead
 from fractal_server.common.schemas.project import ProjectCreate
 from fractal_server.common.schemas.project import ResourceCreate
 from fractal_server.common.schemas.workflow import WorkflowTaskCreate
@@ -27,3 +30,19 @@ async def test_fail_valint():
         WorkflowTaskCreate(order=None)
     with pytest.raises(ValueError):
         WorkflowTaskCreate(order=-1)
+
+
+async def test_apply_wf_read():
+    x = ApplyWorkflowRead(
+        id=1,
+        project_id=1,
+        workflow_id=1,
+        input_dataset_id=1,
+        output_dataset_id=1,
+        start_timestamp="2019-12-23T23:10:11.115310Z",
+        status="good",
+    )
+
+    assert isinstance(x.start_timestamp, datetime)
+    y = x.sanitised_dict()
+    assert isinstance(y["start_timestamp"], str)
