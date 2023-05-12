4a8aaaf2537b40b5d783a8064690236c9a819b89
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-05-12 11:56:33 +0200

    modify var name

--- fractal_server/config.py
+++ fractal_server/config.py
@@ -286,7 +286,9 @@ class Settings(BaseSettings):
     file is considered as missing.
     """
 
-    FRACTAL_ALLOW_ORIGIN: str = "http://127.0.0.1:5173;http://localhost:5173"
+    FRACTAL_CORS_ALLOW_ORIGIN: str = (
+        "http://127.0.0.1:5173;http://localhost:5173"
+    )
 
     ###########################################################################
     # BUSINESS LOGIC
