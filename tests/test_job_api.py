9a1a3a08c600484fc1b5f77c86878b47fb96acb6
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-12 13:07:42 +0200

    Fix test_stop_job

--- tests/test_job_api.py
+++ tests/test_job_api.py
@@ -4,13 +4,25 @@ from fractal_server.app.runner._common import SHUTDOWN_FILENAME
 
 
 async def test_stop_job(
-    db, client, MockCurrentUser, project_factory, job_factory, tmp_path
+    db,
+    client,
+    MockCurrentUser,
+    project_factory,
+    job_factory,
+    workflow_factory,
+    dataset_factory,
+    tmp_path,
 ):
     async with MockCurrentUser(persist=True) as user:
         project = await project_factory(user)
+        wf = await workflow_factory(project_id=project.id)
+        ds = await dataset_factory(project)
         job = await job_factory(
             working_dir=tmp_path.as_posix(),
             project_id=project.id,
+            input_dataset_id=ds.id,
+            output_dataset_id=ds.id,
+            working_id=wf.id,
         )
         debug(job)
 
