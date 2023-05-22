11fc6a01d11689dbce424ea77f66257d865adfff
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-22 12:59:53 +0200

    Check error for missing/invalid manifest (ref #648)

--- tests/fixtures_tasks.py
+++ tests/fixtures_tasks.py
@@ -1,9 +1,11 @@
 import asyncio
+import json
 from pathlib import Path
 from typing import Any
 from typing import Optional
 
 import pytest
+from devtools import debug  # noqa
 from pydantic import BaseModel
 
 from .fixtures_server import HAS_LOCAL_SBATCH
@@ -98,16 +100,11 @@ async def dummy_task_package(testdata_path, tmp_path_factory) -> Path:
 
 @pytest.fixture
 async def dummy_task_package_invalid_manifest(testdata_path, tmp_path) -> Path:
-    """
-    Yields
-    ------
-    wheel_path : Path
-        the path to the built wheel package
-    """
-    from fractal_server import tasks as task_package
+    from .data import tasks_dummy as task_package
 
     PACKAGE_TEMPLATE_PATH = testdata_path / "fractal-tasks-dummy"
-    PACKAGE_PATH = tmp_path / "fractal-tasks-dummy"
+    PACKAGE_PATH = tmp_path / "invalid_manifest/fractal-tasks-dummy"
+    PACKAGE_PATH.parent.mkdir(parents=True)
     SOURCE_PATH = PACKAGE_PATH / "fractal_tasks_dummy"
     DUMMY_PACKAGE = Path(task_package.__file__).parent
 
@@ -116,10 +113,38 @@ async def dummy_task_package_invalid_manifest(testdata_path, tmp_path) -> Path:
     # copy content of task_package to PACKAGE_PATH
     await execute_command(f"cp {DUMMY_PACKAGE}/*.* {SOURCE_PATH}")
 
-    # Make manifest invalid
+    # Make manifest invalid (change version from 1 to 2)
     MANIFEST_PATH = SOURCE_PATH / "__FRACTAL_MANIFEST__.json"
+    with MANIFEST_PATH.open("r") as f:
+        manifest = json.load(f)
+    manifest["manifest_version"] = 2
     with MANIFEST_PATH.open("w") as f:
-        f.write("invalid manifest")
+        json.dump(manifest, f)
+
+    await execute_command("poetry build", cwd=PACKAGE_PATH)
+    wheel_relative = await execute_command("ls dist/*.whl", cwd=PACKAGE_PATH)
+    wheel_path = PACKAGE_PATH / wheel_relative
+    yield wheel_path
+
+
+@pytest.fixture
+async def dummy_task_package_missing_manifest(testdata_path, tmp_path) -> Path:
+    from .data import tasks_dummy as task_package
+
+    PACKAGE_TEMPLATE_PATH = testdata_path / "fractal-tasks-dummy"
+    PACKAGE_PATH = tmp_path / "missing_manifest/fractal-tasks-dummy"
+    PACKAGE_PATH.parent.mkdir(parents=True)
+    SOURCE_PATH = PACKAGE_PATH / "fractal_tasks_dummy"
+    DUMMY_PACKAGE = Path(task_package.__file__).parent
+
+    # copy template to temp
+    await execute_command(f"cp -r {PACKAGE_TEMPLATE_PATH} {PACKAGE_PATH}")
+    # copy content of task_package to PACKAGE_PATH
+    await execute_command(f"cp {DUMMY_PACKAGE}/*.* {SOURCE_PATH}")
+
+    # Remove manifest
+    MANIFEST_PATH = SOURCE_PATH / "__FRACTAL_MANIFEST__.json"
+    MANIFEST_PATH.unlink()
 
     await execute_command("poetry build", cwd=PACKAGE_PATH)
     wheel_relative = await execute_command("ls dist/*.whl", cwd=PACKAGE_PATH)
