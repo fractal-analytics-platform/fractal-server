d7caa3d5d90bc0d4c39a805b3adf99fbe85b43ec
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-06-16 09:08:33 +0200

    rm test_project_workflow_relation

--- tests/test_unit_db_models.py
+++ tests/test_unit_db_models.py
@@ -29,27 +29,6 @@ async def test_project_name_not_unique(MockCurrentUser, db, project_factory):
     assert p1 in project_list
 
 
-async def test_project_workflow_relation(db, project_factory, MockCurrentUser):
-    """
-    GIVEN no preconditions
-    WHEN I create a workflow
-    THEN I must associate the workflow to a project or error
-    """
-
-    # Fail if no project_id
-    with pytest.raises(IntegrityError):
-        wf = Workflow(name="my wfl")
-        db.add(wf)
-        await db.commit()
-    await db.rollback()
-
-    async with MockCurrentUser(persist=True) as user:
-        project = await project_factory(user)
-        wf = Workflow(name="my wfl", project_id=project.id)
-        db.add(wf)
-        await db.commit()
-
-
 async def test_task_workflow_association(
     db, project_factory, MockCurrentUser, task_factory
 ):
