41df5752750f623727a2cbbd6fc57bbcfd10528f
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-06-27 10:40:32 +0200

    same but without class

--- fractal_server/migrations/env.py
+++ fractal_server/migrations/env.py
@@ -24,20 +24,14 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-class Base(SQLModel):
-    def __init__(self):
-        self.metadata.naming_convention = {
-            "ix": "ix_%(column_0_label)s",
-            "uq": "uq_%(table_name)s_%(column_0_name)s",
-            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
-            "fk": (
-                "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
-            ),
-            "pk": "pk_%(table_name)s",
-        }
-
-
-target_metadata = Base().metadata
+target_metadata = SQLModel.metadata
+target_metadata.naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
