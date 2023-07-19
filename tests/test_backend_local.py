1f4680971650cac96029cbbdce747811ef1ae6bb
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-19 13:46:36 +0200

    Move test_execute_tasks_with_wrong_submit_setup_call to test_backend_local.py

--- tests/test_backend_local.py
+++ tests/test_backend_local.py
@@ -13,6 +13,7 @@ Zurich.
 """
 import datetime
 import json
+from concurrent.futures import ThreadPoolExecutor
 
 import pytest
 from devtools import debug
@@ -325,3 +326,56 @@ def test_call_parallel_task_max_tasks(
         assert diff >= SLEEP_TIME
     else:
         assert diff < SLEEP_TIME
+
+
+@pytest.mark.parametrize("parallel_task", [True, False])
+def test_execute_tasks_with_wrong_submit_setup_call(parallel_task, tmp_path):
+    """
+    Check that an exception in `submit_setup_call` is handled correctly in
+    `execute_tasks`, both for parallel and non-parallel tasks.
+    """
+    if parallel_task:
+        task = MockTask(
+            name="name",
+            source="source",
+            command="command",
+            parallelization_level="component",
+        )
+    else:
+        task = MockTask(
+            name="name",
+            source="source",
+            command="command",
+        )
+    debug(parallel_task)
+    debug(task.parallelization_level)
+
+    task_list = [MockWorkflowTask(task=task)]
+    task_pars = TaskParameters(
+        input_paths=[tmp_path],
+        output_path=tmp_path,
+        metadata=dict(
+            component=["some_item"],
+        ),
+    )
+
+    def _wrong_submit_setup_call(*args, **kwargs):
+        """
+        A mock of `submit_setup_call`, which simply raises a given exception.
+        """
+        raise ValueError("custom error")
+
+    with ThreadPoolExecutor() as executor:
+        with pytest.raises(RuntimeError) as e:
+            execute_tasks(
+                executor=executor,
+                task_list=task_list,
+                task_pars=task_pars,
+                workflow_dir=tmp_path,
+                submit_setup_call=_wrong_submit_setup_call,
+                logger_name="logger",
+            )
+        debug(e.value)
+        assert "error in submit_setup_call=" in str(e.value)
+        assert 'ValueError("custom error")' in str(e.value)
+        assert "Original traceback" in str(e.value)
