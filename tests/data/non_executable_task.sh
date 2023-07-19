b6bfd8f110693aea2841f1f18b5adf744e72cc73
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-19 14:06:04 +0200

    Fix comment in dummy test task [skip ci]

--- tests/data/non_executable_task.sh
+++ tests/data/non_executable_task.sh
@@ -1,9 +1,5 @@
-# This script accepts two optional arguments, `--json`` and `--metadata-out`.
-# If both arguments are provided, the script copies the file specified
-# by the `--json` argument to the location specified by the `--metadata-out`
-# argument. If any other arguments are provided, the script raises an error.
-
-# used in `tests/test_full_workflow.py::test_non_python_task`
+# This script is a copy of `non_python_task_issue189.sh`, but it is used as a
+# non-executable task command (that should trigger a specific error).
 
 JSON_FILE=
 METADATA_OUT=
