1c76da22e534a900b7e1812585cc0633ebcb87bc
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-19 11:26:14 +0200

    Simplify f-string

--- fractal_server/app/runner/_slurm/_slurm_config.py
+++ fractal_server/app/runner/_slurm/_slurm_config.py
@@ -502,7 +502,7 @@ def get_slurm_config(
     """
 
     logger.debug(
-        "[get_slurm_config] WorkflowTask meta attribute: " f"{wftask.meta=}"
+        "[get_slurm_config] WorkflowTask meta attribute: {wftask.meta=}"
     )
 
     # Incorporate slurm_env.default_slurm_config
