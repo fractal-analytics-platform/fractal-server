1c13d3f91d808fa60ce24f593f110aa800338282
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-09-08 15:31:15 +0200

    Fix typo in property name

--- fractal_server/app/runner/history.py
+++ fractal_server/app/runner/history.py
@@ -74,7 +74,7 @@ def assemble_history_failed_job(
     # Part 3/A: Identify failed task, if needed
     if failed_wftask is None:
         first_task_index = job.first_task_index_integer
-        last_task_index = job.lasst_task_index_integer
+        last_task_index = job.last_task_index_integer
         job_wftasks = workflow.task_list[
             first_task_index : (last_task_index + 1)  # noqa
         ]
