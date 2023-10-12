31e248f1f6db12a7ed11c704f5d8278c24aada61
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-10-12 17:26:27 +0200

    Remove `_DatasetHistoryItem` from runner modules

--- fractal_server/app/runner/_slurm/__init__.py
+++ fractal_server/app/runner/_slurm/__init__.py
@@ -28,7 +28,6 @@ from ..common import set_start_and_last_task_index
 from ..common import TaskParameters
 from ._submit_setup import _slurm_submit_setup
 from .executor import FractalSlurmExecutor
-from fractal_server.app.schemas.dataset import _DatasetHistoryItem
 
 
 def _process_workflow(
@@ -37,7 +36,7 @@ def _process_workflow(
     input_paths: list[Path],
     output_path: Path,
     input_metadata: dict[str, Any],
-    input_history: list[_DatasetHistoryItem],
+    input_history: list[dict[str, Any]],
     logger_name: str,
     workflow_dir: Path,
     workflow_dir_user: Path,
@@ -105,7 +104,7 @@ async def process_workflow(
     input_paths: list[Path],
     output_path: Path,
     input_metadata: dict[str, Any],
-    input_history: list[_DatasetHistoryItem],
+    input_history: list[dict[str, Any]],
     logger_name: str,
     workflow_dir: Path,
     workflow_dir_user: Optional[Path] = None,
