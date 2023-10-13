c4a976bfdd6ae36aced0e5541fedfe7124680996
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-10-13 14:36:58 +0200

    Reset dataset serializers to `main` state

--- fractal_server/app/schemas/dataset.py
+++ fractal_server/app/schemas/dataset.py
@@ -95,7 +95,7 @@ class _DatasetBase(BaseModel):
     name: str
     type: Optional[str]
     meta: dict[str, Any] = Field(default={})
-    history: list[_DatasetHistoryItem]
+    history: list[_DatasetHistoryItem] = Field(default=[])
     read_only: bool = False
 
 
@@ -134,22 +134,15 @@ class DatasetRead(_DatasetBase):
     """
     Class for `Dataset` read from database.
 
-    Note: starting with 1.3.12, the `Dataset.history` column is always present.
-    We make `history` optional here to avoid errors when working on a database
-    entry created with previous versions.
-
     Attributes:
         id:
-        project_id:
         resource_list:
-        name:
-        meta:
-        history:
+        project_id:
+        read_only:
     """
 
     id: int
     resource_list: list[ResourceRead]
-    history: Optional[list[_DatasetHistoryItem]] = None
     project_id: int
     read_only: bool
 
