8816d3e1108de732b6f20dcd194b0bbb962e9051
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-10-17 13:17:16 +0200

    add dump columns to JobRead

--- fractal_server/app/schemas/applyworkflow.py
+++ fractal_server/app/schemas/applyworkflow.py
@@ -107,6 +107,8 @@ class ApplyWorkflowRead(_ApplyWorkflowBase):
     status: str
     log: Optional[str]
     workflow_dump: Optional[dict[str, Any]]
+    input_dataset_dump: Optional[dict[str, Any]]
+    output_dataset_dump: Optional[dict[str, Any]]
     working_dir: Optional[str]
     working_dir_user: Optional[str]
     first_task_index: Optional[int]
