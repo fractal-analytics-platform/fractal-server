80ce263e0ae165f9d883ab5bee8aea6709df23b1
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-11-08 12:19:10 +0100

    test monitoring

--- fractal_server/app/api/v1/monitoring.py
+++ fractal_server/app/api/v1/monitoring.py
@@ -1,8 +1,16 @@
+from datetime import datetime as DateTime
+from typing import Optional
+
 from fastapi import APIRouter
 from fastapi import Depends
+from sqlmodel import select
 
 from ...db import AsyncSession
 from ...db import get_db
+from ...models import ApplyWorkflow
+from ...models import Project
+from ...schemas import ApplyWorkflowRead
+from ...schemas import ProjectRead
 from ...security import current_active_superuser
 from ...security import User
 
@@ -11,10 +19,21 @@ router = APIRouter()
 
 @router.get("/project/")
 async def monitor_project(
+    id: Optional[int] = None,
     user: User = Depends(current_active_superuser),
     db: AsyncSession = Depends(get_db),
-):
-    pass
+) -> list[ProjectRead]:
+
+    stm = select(Project)
+
+    if id:
+        stm = stm.where(Project.id == id)
+
+    res = await db.execute(stm)
+    project_list = res.scalars().all()
+    await db.close()
+
+    return project_list
 
 
 @router.get("/workflow/")
@@ -35,7 +54,45 @@ async def monitor_dataset(
 
 @router.get("/job/")
 async def monitor_job(
+    id: Optional[int],
+    project_id: Optional[int],
+    input_dataset_id: Optional[int],
+    output_dataset_id: Optional[int],
+    workflow_id: Optional[int],
+    working_dir: Optional[str],
+    working_dir_user: Optional[str],
+    status: Optional[str],
+    start_timestamp: Optional[DateTime],
+    end_timestamp: Optional[DateTime],
     user: User = Depends(current_active_superuser),
     db: AsyncSession = Depends(get_db),
-):
-    pass
+) -> Optional[list[ApplyWorkflowRead]]:
+
+    stm = select(ApplyWorkflow)
+
+    if id:
+        stm = stm.where(ApplyWorkflow.id == id)
+    if project_id:
+        stm = stm.where(ApplyWorkflow.project_id == project_id)
+    if input_dataset_id:
+        stm = stm.where(ApplyWorkflow.input_dataset_id == input_dataset_id)
+    if output_dataset_id:
+        stm = stm.where(ApplyWorkflow.output_dataset_id == output_dataset_id)
+    if workflow_id:
+        stm = stm.where(ApplyWorkflow.workflow_id == workflow_id)
+    if working_dir:
+        stm = stm.where(ApplyWorkflow.working_dir == working_dir)
+    if working_dir_user:
+        stm = stm.where(ApplyWorkflow.working_dir_user == working_dir_user)
+    if status:
+        stm = stm.where(ApplyWorkflow.status == status)
+    if start_timestamp:
+        stm = stm.where(ApplyWorkflow.start_timestamp >= start_timestamp)
+    if end_timestamp:
+        stm = stm.where(ApplyWorkflow.end_timestamp <= end_timestamp)
+
+    res = await db.execute(stm)
+    job_list = res.scalars().all()
+    await db.close()
+
+    return job_list
