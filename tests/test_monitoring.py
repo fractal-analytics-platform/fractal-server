ce152b9885a9e7ffa04fd2587376cf48a4c84996
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-11-09 11:12:21 +0100

    test monitor dataset

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -79,12 +79,12 @@ async def test_monitor_workflow(
     async with MockCurrentUser(
         persist=True, user_kwargs={"is_superuser": True}
     ):
-        # get all workflow
+        # get all workflows
         res = await client.get(f"{PREFIX}/monitoring/workflow/")
         assert res.status_code == 200
         assert len(res.json()) == 3
 
-        # get workflow by id
+        # get workflows by id
         res = await client.get(
             f"{PREFIX}/monitoring/workflow/?id={workflow1a.id}"
         )
@@ -92,14 +92,14 @@ async def test_monitor_workflow(
         assert len(res.json()) == 1
         assert res.json()[0]["name"] == workflow1a.name
 
-        # get workflow by project_id
+        # get workflows by project_id
         res = await client.get(
             f"{PREFIX}/monitoring/workflow/?project_id={project1.id}"
         )
         assert res.status_code == 200
         assert len(res.json()) == 2
 
-        # get workflow by project_id and id
+        # get workflows by project_id and id
         res = await client.get(
             f"{PREFIX}/monitoring/workflow/"
             f"?project_id={project1.id}&id={workflow1b.id}"
@@ -115,7 +115,7 @@ async def test_monitor_workflow(
         assert res.status_code == 200
         assert len(res.json()) == 0
 
-        # get workflow by name
+        # get workflows by name
         res = await client.get(
             f"{PREFIX}/monitoring/workflow/?name={workflow2a.name}"
         )
@@ -126,3 +126,91 @@ async def test_monitor_workflow(
         res = await client.get(f"{PREFIX}/monitoring/workflow/?name=Workflow")
         assert res.status_code == 200
         assert len(res.json()) == 3
+
+
+async def test_monitor_dataset(
+    client, MockCurrentUser, project_factory, workflow_factory, dataset_factory
+):
+
+    async with MockCurrentUser(
+        persist=True, user_kwargs={"is_superuser": False}
+    ) as user:
+
+        project1 = await project_factory(user)
+
+        ds1a = await dataset_factory(
+            project_id=project1.id,
+            name="ds1a",
+            type="zarr",
+            read_only=False,
+        )
+        await dataset_factory(
+            project_id=project1.id,
+            name="ds1b",
+            type="image",
+            read_only=True,
+        )
+
+        project2 = await project_factory(user)
+
+        await dataset_factory(
+            project_id=project2.id,
+            name="ds2a",
+            type="zarr",
+            read_only=True,
+        )
+
+    async with MockCurrentUser(
+        persist=True, user_kwargs={"is_superuser": True}
+    ):
+        # get all datasets
+        res = await client.get(f"{PREFIX}/monitoring/dataset/")
+        assert res.status_code == 200
+        assert len(res.json()) == 3
+
+        # get datasets by id
+        res = await client.get(f"{PREFIX}/monitoring/dataset/?id={ds1a.id}")
+        assert res.status_code == 200
+        assert len(res.json()) == 1
+        assert res.json()[0]["name"] == ds1a.name
+        res = await client.get(f"{PREFIX}/monitoring/dataset/?id=123456789")
+        assert res.status_code == 200
+        assert len(res.json()) == 0
+
+        # get datasets by project_id
+        res = await client.get(
+            f"{PREFIX}/monitoring/dataset/?project_id={project1.id}"
+        )
+        assert res.status_code == 200
+        assert len(res.json()) == 2
+        res = await client.get(
+            f"{PREFIX}/monitoring/dataset/?project_id={project2.id}"
+        )
+        assert res.status_code == 200
+        assert len(res.json()) == 1
+
+        # get datasets by name
+        res = await client.get(
+            f"{PREFIX}/monitoring/dataset/?project_id={project1.id}&name=a"
+        )
+        assert res.status_code == 200
+        assert len(res.json()) == 1
+        assert res.json()[0]["name"] == ds1a.name
+        res = await client.get(
+            f"{PREFIX}/monitoring/dataset/?project_id={project1.id}&name=c"
+        )
+        assert res.status_code == 200
+        assert len(res.json()) == 0
+
+        # get datasets by type and read_only
+        res = await client.get(f"{PREFIX}/monitoring/dataset/?type=zarr")
+        assert res.status_code == 200
+        assert len(res.json()) == 2
+        res = await client.get(
+            f"{PREFIX}/monitoring/dataset/?type=zarr&read_only=true"
+        )
+        assert res.status_code == 200
+        assert len(res.json()) == 1
+        res = await client.get(f"{PREFIX}/monitoring/dataset/?read_only=true")
+        assert res.status_code == 200
+        assert len(res.json()) == 2
