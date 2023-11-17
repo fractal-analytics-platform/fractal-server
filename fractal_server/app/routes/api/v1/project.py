6d750e50b310d8656ace90bbe80bcad4d685736f
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-11-17 13:35:42 +0100

    Fix typo in comments [skip ci]

--- fractal_server/app/routes/api/v1/project.py
+++ fractal_server/app/routes/api/v1/project.py
@@ -150,7 +150,7 @@ async def delete_project(
         project_id=project_id, user_id=user.id, db=db
     )
 
-    # Fail if there exists jobs that are active (that is, pending or running)
+    # Fail if there exist jobs that are active (that is, pending or running)
     # and in relation with the current project.
     stm = _get_active_jobs_statement().where(
         ApplyWorkflow.project_id == project_id
