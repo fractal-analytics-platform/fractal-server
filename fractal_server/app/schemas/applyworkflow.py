d0335bc1ba313a142c0545a4cbe09838a5f56670
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-11-20 12:07:08 +0100

    Replace dict with dict[str,Any]

--- fractal_server/app/schemas/applyworkflow.py
+++ fractal_server/app/schemas/applyworkflow.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from enum import Enum
+from typing import Any
 from typing import Optional
 
 from pydantic import BaseModel
@@ -126,11 +127,11 @@ class ApplyWorkflowRead(_ApplyWorkflowBase):
     project_id: Optional[int]
     user_email: str
     workflow_id: Optional[int]
-    workflow_dump: Optional[dict]
+    workflow_dump: Optional[dict[str, Any]]
     input_dataset_id: Optional[int]
-    input_dataset_dump: Optional[dict]
+    input_dataset_dump: Optional[dict[str, Any]]
     output_dataset_id: Optional[int]
-    output_dataset_dump: Optional[dict]
+    output_dataset_dump: Optional[dict[str, Any]]
     start_timestamp: datetime
     end_timestamp: Optional[datetime]
     status: str
