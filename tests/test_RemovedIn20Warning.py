2d953448890851e0df01253a86f25f07faff563e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-11-20 09:40:03 +0100

    Add test_RemovedIn20Warning.py

---
+++ tests/test_RemovedIn20Warning.py
@@ -0,0 +1,20 @@
+from sqlalchemy import column
+from sqlalchemy import create_engine
+from sqlalchemy import select
+from sqlalchemy import table
+
+
+def test_RemovedIn20Warnings():
+    """
+    This is a dummy test, to make sure that we are catching the
+    RemovedIn20Warnings warning during the migration towards SQLAlchemy 2 - see
+    example in the docs:
+    https://docs.sqlalchemy.org/en/20/changelog/migration_20.html#migration-to-2-0-step-two-turn-on-removedin20warnings
+    """
+
+    engine = create_engine("sqlite://")
+    engine.execute("CREATE TABLE foo (id integer)")
+    engine.execute("INSERT INTO foo (id) VALUES (1)")
+    foo = table("foo", column("id"))
+    result = engine.execute(select([foo.c.id]))
+    print(result.fetchall())
