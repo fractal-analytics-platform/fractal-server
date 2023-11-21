5562112286523adbc85fefb7e382a4e3e345264b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-11-21 16:21:03 +0100

    Re-enable `test_failing_workflow_JobExecutionError`

--- tests/test_full_workflow.py
+++ tests/test_full_workflow.py
@@ -519,7 +519,6 @@ def _auxiliary_run(slurm_user, sleep_time):
     loop.close()
 
 
-@pytest.mark.skip(reason="This test is possibly causing issue #954")
 @pytest.mark.parametrize("backend", ["slurm"])
 async def test_failing_workflow_JobExecutionError(
     backend,
