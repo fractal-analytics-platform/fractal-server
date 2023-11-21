1599c80fba476d32ee6c2622bbead6fb0bb231db
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-11-21 14:46:38 +0100

    Remove `event_loop` fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import asyncio
 from os import environ
 from pathlib import Path
 
@@ -26,14 +25,6 @@ def pytest_configure(config):
     See https://docs.pytest.org/en/stable/how-to/mark.html#registering-marks
     """
     config.addinivalue_line("markers", "slow: marks tests as slow")
-
-
-@pytest.fixture(scope="session")
-def event_loop():
-    _event_loop = asyncio.new_event_loop()
-    _event_loop.set_debug(True)
-    yield _event_loop
-    _event_loop.close()
 
 
 @pytest.fixture(scope="session")
