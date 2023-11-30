0ae97bdec3ba8ecf278c797724cc8d1f0dce92cf
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-11-30 14:55:26 +0100

    Use strict models for dump attributes of `ApplyWorkflowRead` (close #998)q

--- fractal_server/app/schemas/applyworkflow.py
+++ fractal_server/app/schemas/applyworkflow.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from enum import Enum
-from typing import Any
 from typing import Optional
 
 from pydantic import BaseModel
 from pydantic import validator
 
 from ._validators import valstr
+from .dataset import DatasetRead
+from .workflow import WorkflowRead
 
 __all__ = (
     "_ApplyWorkflowBase",
@@ -127,11 +128,11 @@ class ApplyWorkflowRead(_ApplyWorkflowBase):
     project_id: Optional[int]
     user_email: str
     workflow_id: Optional[int]
-    workflow_dump: Optional[dict[str, Any]]
+    workflow_dump: Optional[WorkflowRead]
     input_dataset_id: Optional[int]
-    input_dataset_dump: Optional[dict[str, Any]]
+    input_dataset_dump: Optional[DatasetRead]
     output_dataset_id: Optional[int]
-    output_dataset_dump: Optional[dict[str, Any]]
+    output_dataset_dump: Optional[DatasetRead]
     start_timestamp: datetime
     end_timestamp: Optional[datetime]
     status: str
