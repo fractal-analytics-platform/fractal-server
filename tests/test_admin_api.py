5f6c3b56df28ed8a9434c9ba13d52b4cc9da34ca
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-12-04 11:42:32 +0100

    test download job logs

--- tests/test_admin_api.py
+++ tests/test_admin_api.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from pathlib import Path
+from zipfile import ZipFile
 
 import pytest
 from devtools import debug
@@ -504,3 +506,66 @@ async def test_stop_job(
             assert shutdown_file.exists()
         else:
             assert res.status_code == 422
+
+
+async def test_download_job_logs(
+    client,
+    MockCurrentUser,
+    project_factory,
+    dataset_factory,
+    workflow_factory,
+    job_factory,
+    task_factory,
+    db,
+    tmp_path,
+):
+    async with MockCurrentUser(user_kwargs={"id": 1111}) as user:
+        prj = await project_factory(user)
+        input_dataset = await dataset_factory(project_id=prj.id, name="input")
+        output_dataset = await dataset_factory(
+            project_id=prj.id, name="output"
+        )
+        workflow = await workflow_factory(project_id=prj.id)
+        task = await task_factory()
+        await workflow.insert_task(task_id=task.id, db=db)
+        working_dir = (tmp_path / "workflow_dir_for_zipping").as_posix()
+        job = await job_factory(
+            project_id=prj.id,
+            workflow_id=workflow.id,
+            working_dir=working_dir,
+            input_dataset_id=input_dataset.id,
+            output_dataset_id=output_dataset.id,
+        )
+
+    async with MockCurrentUser(user_kwargs={"id": 2222, "is_superuser": True}):
+
+        # Write a log file in working_dir
+        LOG_CONTENT = "This is a log\n"
+        LOG_FILE = "log.txt"
+        Path(working_dir).mkdir()
+        with (Path(working_dir) / LOG_FILE).open("w") as f:
+            f.write(LOG_CONTENT)
+
+        # Test that the endpoint returns a list with the new job
+        res = await client.get(f"{PREFIX}/job/{job.id}/download/")
+        assert res.status_code == 200
+        assert (
+            res.headers.get("content-type") == "application/x-zip-compressed"
+        )
+
+        # Write response into a zipped file
+        zipped_archive_path = tmp_path / "logs.zip"
+        debug(zipped_archive_path)
+        with zipped_archive_path.open("wb") as f:
+            f.write(res.content)
+
+        # Unzip the log archive
+        unzipped_archived_path = tmp_path / "unzipped_logs"
+        debug(unzipped_archived_path)
+        with ZipFile(zipped_archive_path, mode="r") as zipfile:
+            zipfile.extractall(path=unzipped_archived_path)
+
+        # Verify content of the unzipped log archive
+        with (unzipped_archived_path / LOG_FILE).open("r") as f:
+            actual_logs = f.read()
+        assert LOG_CONTENT in actual_logs
