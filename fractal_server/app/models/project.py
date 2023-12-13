9f6bdead5f1cfa98b5ea560785ec6a5df94ae0f6
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-12-13 13:42:44 +0100

    rm leftover

--- fractal_server/app/models/project.py
+++ fractal_server/app/models/project.py
@@ -34,7 +34,7 @@ class Project(_ProjectBase, SQLModel, table=True):
     workflow_list: list[Workflow] = Relationship(
         back_populates="project",
         sa_relationship_kwargs={
-            "lazy": "joined",
+            "lazy": "selectin",
             "cascade": "all, delete-orphan",
         },
     )
