512cb6c9888c3f0dea4826d9fc3233f00166f873
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-12-20 17:13:11 +0100

    improve test admin api

--- tests/api/test_admin_api.py
+++ tests/api/test_admin_api.py
@@ -53,6 +53,7 @@ async def test_view_project(client, MockCurrentUser, project_factory):
         prj1_id = project1.id
         await project_factory(user)
         user_id = user.id
+        TIMESTAMP_CREATED = project1.timestamp_created
 
     async with MockCurrentUser(user_kwargs={"is_superuser": True}):
         res = await client.get(f"{PREFIX}/project/")
@@ -69,6 +70,22 @@ async def test_view_project(client, MockCurrentUser, project_factory):
         )
         assert res.status_code == 200
         assert len(res.json()) == 1
+        res = await client.get(
+            f"{PREFIX}/project/?user_id={user_id}&id={prj1_id}"
+        )
+        assert res.status_code == 200
+        assert len(res.json()) == 1
+
+        res = await client.get(
+            f"{PREFIX}/project/?timestamp_created_min={TIMESTAMP_CREATED}"
+        )
+        assert res.status_code == 200
+        assert len(res.json()) == 2
+        res = await client.get(
+            f"{PREFIX}/project/?timestamp_created_max={TIMESTAMP_CREATED}"
+        )
+        assert res.status_code == 200
+        assert len(res.json()) == 2
 
 
 async def test_view_workflow(
