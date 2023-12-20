84baa54f5435c6d43564ec0cb3414595bf343cee
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-12-20 15:09:12 +0100

    Add more schema-based validation to `fix_db/current.py` (ref #1106)

--- scripts/fix_db/current.py
+++ scripts/fix_db/current.py
@@ -10,6 +10,7 @@ from sqlalchemy.exc import IntegrityError
 from fractal_server.app.db import get_sync_db
 from fractal_server.app.models.job import ApplyWorkflow
 from fractal_server.app.models.project import Project
+from fractal_server.app.schemas.applyworkflow import ApplyWorkflowRead
 from fractal_server.app.schemas.applyworkflow import ProjectDump
 
 with next(get_sync_db()) as db:
@@ -21,25 +22,35 @@ with next(get_sync_db()) as db:
 
     # Loop over jobs
     for row in sorted(rows, key=lambda x: x.id):
-        if row.project_dump == {}:  # protects from overriding
+        if row.project_dump != {}:
+            logging.warning(
+                f"[Job {row.id:4d}] project_dump attribute non-empty, skip"
+            )
+        else:
+            # protects from overriding
             if row.project_id is None:
-                logging.warning(f"Job with ID {row.id:4d} has project_id=None")
+                logging.warning(
+                    f"[Job {row.id:4d}] project_id=None, use dummy data"
+                )
                 project_dump = dict(
-                    id=-1, name="Not Available", read_only=True
-                )  # fake project_dump
+                    id=-1, name="__UNDEFINED__", read_only=True
+                )
             else:
                 project = db.get(Project, row.project_id)
                 if project is None:
                     raise IntegrityError(
-                        f"Job {row.id} has project_id={row.project_id}, "
-                        f"but the Project {row.project_id} does not exist"
+                        f"[Job {row.id:4d}] "
+                        f"project_id={row.project_id}, "
+                        f"but Project {row.project_id} does not exist"
                     )
                 project_dump = project.dict(exclude={"user_list"})
 
-            logging.warning(
-                f"Handling job with ID {row.id:4d}, setting {project_dump=}"
-            )
+            logging.warning(f"[Job {row.id:4d}] setting {project_dump=}")
             ProjectDump(**project_dump)
             row.project_dump = project_dump
             db.add(row)
             db.commit()
+
+            # Also validate that the row can be cast into ApplyWorkflowRead
+            db.expunge(row)
+            ApplyWorkflowRead(**row.dict())
