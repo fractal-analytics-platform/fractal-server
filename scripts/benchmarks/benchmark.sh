8e4e685208fa4f4f6f904b24eb45ca554ba20928
mfranzon <franzonmarco94@gmail.com>
2024-01-04 15:24:35 +0100

    try gunicorn in daemon mode

---
+++ scripts/benchmarks/benchmark.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+N_WORKERS=1
+BIND=0.0.0.0:8000
+WORKER_CLASS=uvicorn.workers.UvicornWorker
+
+fractalctl set-db
+
+gunicorn "fractal_server.main:app" \
+    --bind=$BIND \
+    --workers=$N_WORKERS \
+    --worker-class=$WORKER_CLASS \
+    --daemon
+
+poetry run python populate_db.py
+
+locust -f api_bench.py \
+--host http://0.0.0.0:8000 \
+--users 1 \
+--spawn-rate 10 \
+--run-time 10s \
+--headless \
+--html test.html
