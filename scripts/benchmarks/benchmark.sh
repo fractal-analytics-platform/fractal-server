69a3913b60c398eba321cb53908fc3b9d626524a
mfranzon <franzonmarco94@gmail.com>
2024-01-04 17:35:47 +0100

    check if server is running

--- scripts/benchmarks/benchmark.sh
+++ scripts/benchmarks/benchmark.sh
@@ -17,10 +17,12 @@ sleep 3
 
 python populate_db.py
 
-locust -f api_bench.py \
---host http://127.0.0.1:8000 \
---users 1 \
---spawn-rate 10 \
---run-time 10s \
---headless \
---html test.html
+curl -X GET http://127.0.0.1/api/alive/
+
+# locust -f api_bench.py \
+# --host http://127.0.0.1:8000 \
+# --users 1 \
+# --spawn-rate 10 \
+# --run-time 10s \
+# --headless \
+# --html test.html
