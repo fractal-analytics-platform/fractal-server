bd7b55605e63a36e4275f732d4c49d5c6c9c220f
mfranzon <franzonmarco94@gmail.com>
2024-01-04 17:44:28 +0100

    try to make two distinc steps

--- scripts/benchmarks/benchmark.sh
+++ scripts/benchmarks/benchmark.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-N_WORKERS=1
-BIND=127.0.0.1:8000
-WORKER_CLASS=uvicorn.workers.UvicornWorker
-
-fractalctl set-db
-
-gunicorn "fractal_server.main:app" \
-    --bind=$BIND \
-    --workers=$N_WORKERS \
-    --worker-class=$WORKER_CLASS \
-    --daemon
-
-# let gunicorn takes its time
-sleep 3
-
-python populate_db.py
-
 curl -X GET http://127.0.0.1:8000/api/alive/
 
 # locust -f api_bench.py \
