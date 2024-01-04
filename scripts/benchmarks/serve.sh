e16779d85e9c1d7ee0da8de2e772118b6b8ff981
mfranzon <franzonmarco94@gmail.com>
2024-01-04 17:44:28 +0100

    try to make two distinc steps

---
+++ scripts/benchmarks/serve.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+N_WORKERS=1
+BIND=127.0.0.1:8000
+WORKER_CLASS=uvicorn.workers.UvicornWorker
+
+fractalctl set-db
+
+gunicorn "fractal_server.main:app" \
+    --bind=$BIND \
+    --workers=$N_WORKERS \
+    --worker-class=$WORKER_CLASS \
+    --daemon
+
+# let gunicorn takes its time
+sleep 3
+
+python populate_db.py
