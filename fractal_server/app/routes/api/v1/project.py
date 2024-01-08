9874c86b0a26d3dc6a41c8ef374c445fc6a2799c
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-01-08 12:02:24 +0100

    Implement more cascade set-foreign-key-to-null in delete-project endpoint

--- fractal_server/app/routes/api/v1/project.py
+++ fractal_server/app/routes/api/v1/project.py
@@ -178,6 +178,16 @@ async def delete_project(
     res = await db.execute(stm)
     workflows = res.scalars().all()
     for wf in workflows:
+        # Cascade operations: set foreign-keys to null for jobs which are in
+        # relationship with the current workflow
+        stm = select(ApplyWorkflow).where(ApplyWorkflow.workflow_id == wf.id)
+        res = await db.execute(stm)
+        jobs = res.scalars().all()
+        for job in jobs:
+            job.workflow_id = None
+            await db.merge(job)
+        await db.commit()
+        # Delete workflow
         await db.delete(wf)
 
     # Dataset
