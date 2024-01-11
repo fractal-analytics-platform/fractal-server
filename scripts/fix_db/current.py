e4eaf0b1e9776fb46b6503525f5c667638efe5e6
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-01-11 11:38:50 +0100

    fix script

--- scripts/fix_db/current.py
+++ scripts/fix_db/current.py
@@ -17,7 +17,9 @@ from fractal_server.app.models.project import Project
 from fractal_server.app.models.workflow import Workflow
 from fractal_server.app.schemas.applyworkflow import ApplyWorkflowRead
 from fractal_server.app.schemas.dataset import DatasetRead
+from fractal_server.app.schemas.dumps import DatasetDump
 from fractal_server.app.schemas.dumps import ProjectDump
+from fractal_server.app.schemas.dumps import WorkflowDump
 from fractal_server.app.schemas.project import ProjectRead
 from fractal_server.app.schemas.workflow import WorkflowRead
 
@@ -65,6 +67,7 @@ with next(get_sync_db()) as db:
     stm = select(Workflow)
     workflows = db.execute(stm).scalars().all()
     for workflow in workflows:
+        # add timestamp_created to Workflows
         timestamp_created = workflow.timestamp_created
         if timestamp_created != REFERENCE_TIMESTAMP:
             logging.warning(
@@ -87,11 +90,24 @@ with next(get_sync_db()) as db:
             db.refresh(workflow)
             db.expunge(workflow)
             WorkflowRead(**workflow.model_dump())
+        # add timestamp_created to Jobs.workflow_dump
+        stm = select(ApplyWorkflow).where(
+            ApplyWorkflow.workflow_dump["id"] == workflow.id
+        )
+        jobs = db.execute(stm).scalars().all()
+        for job in jobs:
+            job.workflow_dump["timestamp_created"] = str(timestamp_created)
+            db.add(job)
+            db.commit()
+            db.refresh(job)
+            db.expunge(job)
+            WorkflowDump(**job.workflow_dump)
 
     # Dataset.timestamp_created
     stm = select(Dataset)
     datasets = db.execute(stm).scalars().all()
     for dataset in datasets:
+        # add timestamp_created to Datasets
         timestamp_created = dataset.timestamp_created
         if timestamp_created != REFERENCE_TIMESTAMP:
             logging.warning(
@@ -114,6 +130,35 @@ with next(get_sync_db()) as db:
             db.refresh(dataset)
             db.expunge(dataset)
             DatasetRead(**dataset.model_dump())
+
+        # add timestamp_created to Jobs.input_dataset_dump
+        stm = select(ApplyWorkflow).where(
+            ApplyWorkflow.input_dataset_dump["id"] == dataset.id
+        )
+        jobs = db.execute(stm).scalars().all()
+        for job in jobs:
+            job.input_dataset_dump["timestamp_created"] = str(
+                timestamp_created
+            )
+            db.add(job)
+            db.commit()
+            db.refresh(job)
+            db.expunge(job)
+            DatasetDump(**job.input_dataset_dump)
+        # add timestamp_created to Jobs.output_dataset_dump
+        stm = select(ApplyWorkflow).where(
+            ApplyWorkflow.output_dataset_dump["id"] == dataset.id
+        )
+        jobs = db.execute(stm).scalars().all()
+        for job in jobs:
+            job.output_dataset_dump["timestamp_created"] = str(
+                timestamp_created
+            )
+            db.add(job)
+            db.commit()
+            db.refresh(job)
+            db.expunge(job)
+            DatasetDump(**job.output_dataset_dump)
 
     # Get list of all jobs
     stm = select(ApplyWorkflow)
