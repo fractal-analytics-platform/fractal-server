ee547388b05e4a13e36adb3f0a9b3860f42103d9
mfranzon <franzonmarco94@gmail.com>
2024-01-18 15:50:10 +0100

    [skip ci] first version of populate client script

--- scripts/populate_db/populate_clients.py
+++ scripts/populate_db/populate_clients.py
@@ -1,5 +1,20 @@
 import requests
 
+from fractal_server.app.schemas import ApplyWorkflowCreate
+from fractal_server.app.schemas import ApplyWorkflowRead
+from fractal_server.app.schemas import DatasetCreate
+from fractal_server.app.schemas import DatasetRead
+from fractal_server.app.schemas import ProjectCreate
+from fractal_server.app.schemas import ProjectRead
+from fractal_server.app.schemas import ResourceCreate
+from fractal_server.app.schemas import ResourceRead
+from fractal_server.app.schemas import TaskCreate
+from fractal_server.app.schemas import TaskRead
+from fractal_server.app.schemas import WorkflowCreate
+from fractal_server.app.schemas import WorkflowRead
+from fractal_server.app.schemas import WorkflowTaskCreate
+from fractal_server.app.schemas import WorkflowTaskRead
+
 
 BASE_URL = "http://localhost:8000"
 CREDENTIALS = dict(username="admin@fractal.xy", password="1234")  # nosec
@@ -8,20 +23,18 @@ CREDENTIALS = dict(username="admin@fractal.xy", password="1234")  # nosec
 class SimpleHttpClient:
     base_url: str
 
-    def __init__(self, base_url):
+    def __init__(self, base_url, credentials):
         self.base_url = base_url
 
-    # login needs a custom func because the content type of this endpoint
-    # is not json but x-www-form-urlencoded,
-    # so it requires `data` attribute in requests.post
-    def login(self, credentials: str):
         response = requests.post(
             f"{self.base_url}/auth/token/login/",
             data=credentials,
         )
-        return response
 
-    def make_request(self, endpoint, method="GET", data=None, headers=None):
+        self.bearer_token = response.json().get("access_token")
+
+    def make_request(self, endpoint, method="GET", data=None):
+        headers = {"Authorization": f"Bearer {self.bearer_token}"}
         url = f"{self.base_url}/{endpoint}"
 
         try:
@@ -43,30 +56,99 @@ class SimpleHttpClient:
             return None
 
 
-class BearerHttpClient(SimpleHttpClient):
-    bearer_token: str
+class New:
+    def __init__(self, client: SimpleHttpClient):
+        self.client = client
 
-    def __init__(self, base_url: str, bearer_token: str):
-        super().__init__(base_url=base_url)
-        self.bearer_token = bearer_token
-        self.headers = {"Authorization": f"Bearer {bearer_token}"}
+    def detail(self, res):
+        if res.get("detail"):
+            raise ValueError(f"Attention: {res.get('detail')}")
 
-    def make_request(self, endpoint: str, method: str = "GET", data=None):
-        res = super().make_request(
-            endpoint=endpoint, method=method, data=data, headers=self.headers
+    def add_project(self, project: ProjectCreate):
+        res = self.client.make_request(
+            endpoint="api/v1/project/",
+            method="POST",
+            data=project.dict(),
         )
-        return res
+        self.detail(res.json())
+        return ProjectRead(**res.json())
 
+    def add_dataset(self, project_id, dataset: DatasetCreate):
+        res = self.client.make_request(
+            endpoint=f"api/v1/project/{project_id}/dataset/",
+            method="POST",
+            data=dataset.dict(),
+        )
+        self.detail(res.json())
+        return DatasetRead(**res.json())
 
-def auth_client(base_url: str, credentials: str) -> BearerHttpClient:
-    anon_client = SimpleHttpClient(base_url)
+    def add_resource(
+        self, project_id: int, dataset_id: int, resource: ResourceCreate
+    ):
+        res = self.client.make_request(
+            endpoint=f"api/v1/project/{project_id}/dataset/"
+            f"{dataset_id}/resource/",
+            method="POST",
+            data=resource.dict(),
+        )
+        self.detail(res.json())
 
-    res = anon_client.login(credentials=credentials)
+        return ResourceRead(**res.json())
 
-    bearer_token = res.json().get("access_token")
+    def add_workflow(self, project_id, workflow: WorkflowCreate):
+        res = self.client.make_request(
+            endpoint=f"api/v1/project/{project_id}/workflow/",
+            method="POST",
+            data=workflow.dict(),
+        )
+        self.detail(res.json())
 
-    bearer_client = BearerHttpClient(
-        base_url=base_url, bearer_token=bearer_token
-    )
+        return WorkflowRead(**res.json())
 
-    return bearer_client
+    def add_workflowtask(
+        self,
+        project_id: int,
+        workflow_id: int,
+        task_id: int,
+        wftask: WorkflowTaskCreate,
+    ):
+        res = self.client.make_request(
+            endpoint=f"api/v1/project/{project_id}/workflow/"
+            f"{workflow_id}/wftask/?{task_id=}",
+            method="POST",
+            data=wftask.dict(),
+        )
+        self.detail(res.json())
+
+        return WorkflowTaskRead(**res.json())
+
+    def add_task(self, task: TaskCreate):
+
+        res = self.client.make_request(
+            endpoint="api/v1/task/",
+            method="POST",
+            data=task.dict(exclude_none=True),
+        )
+        self.detail(res.json())
+
+        return TaskRead(**res.json())
+
+    def add_job(
+        self,
+        project_id: int,
+        workflow_id: int,
+        in_dataset_id: int,
+        out_dataset_id: int,
+        applyworkflow: ApplyWorkflowCreate,
+    ):
+        res = self.client.make_request(
+            endpoint=f"api/v1/project/{project_id}/"
+            f"workflow/{workflow_id}/apply/"
+            f"?input_dataset_id={in_dataset_id}"
+            f"&output_dataset_id={out_dataset_id}",
+            method="POST",
+            data=applyworkflow.dict(exclude_none=True),
+        )
+        self.detail(res.json())
+
+        return ApplyWorkflowRead(**res.json())
--- scripts/populate_db/populate_first_version.py
+++ scripts/populate_db/populate_first_version.py
@@ -1,98 +1,43 @@
 from devtools import debug
-from populate_clients import auth_client
 from populate_clients import BASE_URL
-from populate_clients import BearerHttpClient
 from populate_clients import CREDENTIALS
+from populate_clients import New
+from populate_clients import SimpleHttpClient
 
+from fractal_server.app.schemas import ApplyWorkflowCreate
 from fractal_server.app.schemas import DatasetCreate
-from fractal_server.app.schemas import DatasetRead
 from fractal_server.app.schemas import ProjectCreate
-from fractal_server.app.schemas import ProjectRead
+from fractal_server.app.schemas import ResourceCreate
 from fractal_server.app.schemas import TaskCreate
-from fractal_server.app.schemas import TaskRead
 from fractal_server.app.schemas import WorkflowCreate
-from fractal_server.app.schemas import WorkflowRead
-
-
-class New:
-    def __init__(self, bearer_client: BearerHttpClient):
-        self.bearer_client = bearer_client
-
-    def add_project(self, project_name):
-        res = self.bearer_client.make_request(
-            endpoint="api/v1/project/",
-            method="POST",
-            data=ProjectCreate(name=project_name).dict(),
-        )
-
-        return ProjectRead(**res.json())
-
-    def add_dataset(self, project_id, dataset_name, type):
-        res = self.bearer_client.make_request(
-            endpoint=f"api/v1/project/{project_id}/dataset/",
-            method="POST",
-            data=DatasetCreate(name=dataset_name, type=type).dict(),
-        )
-
-        return DatasetRead(**res.json())
-
-    def add_workflow(self, project_id, workflow_name):
-        res = self.bearer_client.make_request(
-            endpoint=f"api/v1/project/{project_id}/workflow/",
-            method="POST",
-            data=WorkflowCreate(name=workflow_name).dict(),
-        )
-
-        return WorkflowRead(**res.json())
-
-    def add_task(
-        self,
-        source: str,
-        name: str,
-        command: str,
-        input_type: str,
-        output_type: str,
-    ):
-
-        res = self.bearer_client.make_request(
-            endpoint="api/v1/task/",
-            method="POST",
-            data=TaskCreate(
-                source=source,
-                name=name,
-                command=command,
-                input_type=input_type,
-                output_type=output_type,
-            ).dict(
-                exclude={
-                    "meta",
-                    "args_schema",
-                    "docs_info",
-                    "docs_link",
-                    "version",
-                    "args_schema_version",
-                }
-            ),
-        )
-
-        return TaskRead(**res.json())
+from fractal_server.app.schemas import WorkflowTaskCreate
 
 
 if __name__ == "__main__":
-    bearer_client = auth_client(base_url=BASE_URL, credentials=CREDENTIALS)
+    client = SimpleHttpClient(base_url=BASE_URL, credentials=CREDENTIALS)
 
-    x = New(bearer_client=bearer_client)
-    p = x.add_project("test_p")
+    x = New(client=client)
+    p = x.add_project(ProjectCreate(name="test"))
     debug(p)
-    d = x.add_dataset(p.id, dataset_name="test_d", type="zarr")
+    d = x.add_dataset(p.id, DatasetCreate(name="test_ds", type="zarr"))
     debug(d)
-    w = x.add_workflow(p.id, workflow_name="test_w")
+    r = x.add_resource(
+        p.id, d.id, resource=ResourceCreate(path="/tmp")  # nosec
+    )
+    debug(r)
+    w = x.add_workflow(p.id, WorkflowCreate(name="test_wf"))
     debug(w)
     t = x.add_task(
-        source="..",
-        name="test_t",
-        command="ls",
-        input_type="zarr",
-        output_type="zarr",
+        TaskCreate(
+            source="/",
+            name="test_t",
+            command="ls",
+            input_type="zarr",
+            output_type="zarr",
+        )
     )
     debug(t)
+    wt = x.add_workflowtask(p.id, w.id, t.id, WorkflowTaskCreate(order=0))
+    debug(wt)
+    a = x.add_job(p.id, w.id, d.id, d.id, applyworkflow=ApplyWorkflowCreate())
+    debug(a)
