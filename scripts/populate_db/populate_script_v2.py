f36336342efe0a9fcb51fe77cbfc46b4b2bcd1ff
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-01-19 12:47:30 +0100

    Bulk addition of new resources

--- scripts/populate_db/populate_script_v2.py
+++ scripts/populate_db/populate_script_v2.py
@@ -1,4 +1,3 @@
-from devtools import debug
 from populate_clients import FractalClient
 from populate_clients import SimpleHttpClient
 
@@ -11,6 +10,25 @@ from fractal_server.app.schemas import WorkflowCreate
 from fractal_server.app.schemas import WorkflowTaskCreate
 
 
+def _create_user_client(_admin: FractalClient, _identifier) -> FractalClient:
+    email = f"user{_identifier}@example.org"
+    password = f"user{_identifier}-pwd"
+    slurm_user = f"user{_identifier}-slurm"
+    _admin.add_user(
+        UserCreate(
+            email=email,
+            password=password,
+            slurm_user=slurm_user,
+        )
+    )
+    user_client = SimpleHttpClient(
+        credentials=dict(username=email, password=password)
+    )
+    _user = FractalClient(client=user_client)
+    print(_user.whoami())
+    return _user
+
+
 if __name__ == "__main__":
     base_client = SimpleHttpClient()
     admin = FractalClient(client=base_client)
@@ -18,35 +36,31 @@ if __name__ == "__main__":
     working_task = admin.add_working_task()
     failing_task = admin.add_failing_task()
 
-    for ind_user in range(2):
-        email = f"user{ind_user}@example.org"
-        password = f"user{ind_user}-pwd"
-        slurm_user = f"user{ind_user}-slurm"
-        user_response = admin.add_user(
-            UserCreate(
-                email=email,
-                password=password,
-                slurm_user=slurm_user,
-            )
-        )
-        debug(user_response)
-        user_client = SimpleHttpClient(
-            credentials=dict(username=email, password=password)
-        )
-        user = FractalClient(client=user_client)
-        print(user.whoami())
+    num_users = 2
+    num_projects = 2
+    num_jobs = 10
 
-        p = user.add_project(ProjectCreate(name="test"))
-        d = user.add_dataset(p.id, DatasetCreate(name="test_ds", type="zarr"))
-        r = user.add_resource(
-            p.id, d.id, resource=ResourceCreate(path="/tmp")  # nosec
-        )
-        w = user.add_workflow(p.id, WorkflowCreate(name="test_wf"))
-        wt = user.add_workflowtask(
-            p.id, w.id, working_task.id, WorkflowTaskCreate()
-        )
-        a = user.apply_workflow(
-            p.id, w.id, d.id, d.id, applyworkflow=ApplyWorkflowCreate()
-        )
+    for ind_user in range(num_users):
+        user = _create_user_client(admin, ind_user)
+        for ind_p in range(num_projects):
+            p = user.add_project(ProjectCreate(name=f"proj-{ind_p}"))
+            for ind_job in range(num_jobs):
+                d = user.add_dataset(
+                    p.id, DatasetCreate(name=f"ds-{ind_job}", type="zarr")
+                )
+                r = user.add_resource(
+                    p.id,
+                    d.id,
+                    resource=ResourceCreate(path=f"/invalid_{ind_job}"),
+                )
+                w = user.add_workflow(
+                    p.id, WorkflowCreate(name=f"wf-{ind_job}")
+                )
+                user.add_workflowtask(
+                    p.id, w.id, working_task.id, WorkflowTaskCreate()
+                )
+                a = user.apply_workflow(
+                    p.id, w.id, d.id, d.id, applyworkflow=ApplyWorkflowCreate()
+                )
 
     admin.wait_for_all_jobs()
