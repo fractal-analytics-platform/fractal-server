29b646c763272b12a1c4b242312273f16b456e34
mfranzon <franzonmarco94@gmail.com>
2024-01-24 10:39:02 +0100

    add variable and commands for docker psql

--- scripts/populate_db/populate_db.sh
+++ scripts/populate_db/populate_db.sh
@@ -1,4 +1,12 @@
+# using local postgres database
 dropdb fractal_test
 createdb fractal_test
+
+# using container postgres created with
+# docker run --name postgres-main -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres
+
+# docker exec -it postgres-main dropdb -Upostgres fractal_test
+# docker exec -it postgres-main createdb -Upostgres fractal_test
+
 poetry run fractalctl set-db
 poetry run python populate_script_v2.py
