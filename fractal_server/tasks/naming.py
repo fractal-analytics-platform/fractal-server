6893f01ae29a589a095ae1842e9b0f8e7cd3cfcb
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-01-25 17:40:59 +0100

    Fix docstring

--- fractal_server/tasks/naming.py
+++ fractal_server/tasks/naming.py
@@ -7,7 +7,7 @@ def _normalize_package_name(name: str) -> str:
 
     The name should be lowercased with all runs of the characters `.`, `-`,
     or `_` replaced with a single `-` character. This can be implemented in
-    Python with the re module:e lookups and comparisons.
+    Python with the re module.
     (https://packaging.python.org/en/latest/specifications/name-normalization)
 
     Args:
