ea04e0967ebcec5139ab85046e8b17dc876f1b50
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-01-25 09:57:44 +0100

    back to DateTime

--- fractal_server/app/models/project.py
+++ fractal_server/app/models/project.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Optional
 
 from sqlalchemy import Column
-from sqlalchemy_utc import UtcDateTime
+from sqlalchemy.types import DateTime
 from sqlmodel import Field
 from sqlmodel import Relationship
 from sqlmodel import SQLModel
@@ -18,7 +18,7 @@ class Project(_ProjectBase, SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     timestamp_created: datetime = Field(
         default_factory=get_timestamp,
-        sa_column=Column(UtcDateTime(), nullable=False),
+        sa_column=Column(DateTime(timezone=True), nullable=False),
     )
 
     user_list: list[UserOAuth] = Relationship(
