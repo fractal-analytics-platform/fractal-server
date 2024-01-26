1c4fc5218bc4e5b65b944c48ff41af288f9b9b08
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-01-26 12:26:13 +0100

    Add test_get_absolute_venv_path

--- tests/07_task_collection/test_unit_task_utils.py
+++ tests/07_task_collection/test_unit_task_utils.py
@@ -1,2 +1,14 @@
-def test():
-    pass
+from pathlib import Path
+
+from fractal_server.tasks.utils import get_absolute_venv_path
+
+
+def test_get_absolute_venv_path(tmp_path, override_settings_factory):
+    FRACTAL_TASKS_DIR = tmp_path / "TASKS"
+    override_settings_factory(FRACTAL_TASKS_DIR=FRACTAL_TASKS_DIR)
+    absolute_path = tmp_path
+    relative_path = Path("somewhere/else/")
+    assert get_absolute_venv_path(absolute_path) == absolute_path
+    assert get_absolute_venv_path(relative_path) == (
+        FRACTAL_TASKS_DIR / relative_path
+    )
