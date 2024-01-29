771f391ddd7ed8ee2b2a9a00fa9bd15fe8f62056
mfranzon <franzonmarco94@gmail.com>
2024-01-29 18:04:54 +0100

    add custom benchmark profiler

--- benchmarks/serve.sh
+++ benchmarks/serve.sh
@@ -6,13 +6,10 @@ WORKER_CLASS=uvicorn.workers.UvicornWorker
 
 fractalctl set-db
 
+python ../scripts/populate_db/populate_script_v2.py
+
 gunicorn "fractal_server.main:app" \
     --bind=$BIND \
     --workers=$N_WORKERS \
     --worker-class=$WORKER_CLASS \
     --daemon
-
-# let gunicorn takes its time
-sleep 3
-
-python populate_db.py
