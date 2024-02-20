8463993ca10fa8f251fcc352e395773c7544fef4
mfranzon <franzonmarco94@gmail.com>
2024-02-20 15:52:56 +0100

    add removed_images ParallelTaskOutput

--- fractal_server/app/runner/v2/task_output.py
+++ fractal_server/app/runner/v2/task_output.py
@@ -53,6 +53,7 @@ class ParallelTaskOutput(BaseModel):
     new_images: Optional[list[SingleImage]] = None
     edited_images: Optional[list[SingleImage]] = None
     new_filters: Optional[FilterSet] = None  # FIXME
+    removed_images: Optional[list[SingleImage]] = None
 
 
 def merge_outputs(
