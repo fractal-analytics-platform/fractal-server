51872df91be5dcf1afdbcba1ecf607666760db1c
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-02-20 16:05:34 +0100

    move merge_outputs

--- fractal_server/app/runner/v2/runner_functions.py
+++ fractal_server/app/runner/v2/runner_functions.py
@@ -5,7 +5,6 @@ from .images import find_image_by_path
 from .models import DictStrAny
 from .models import SingleImage
 from .models import Task
-from .task_output import merge_outputs
 from .task_output import ParallelTaskOutput
 from .task_output import TaskOutput
 
@@ -47,6 +46,62 @@ def _run_non_parallel_task(
             task_output.new_images = final_new_images
 
     return TaskOutput(**task_output.dict())
+
+
+def merge_outputs(
+    task_outputs: list[ParallelTaskOutput],
+    new_old_image_mapping: dict[str, str],
+    old_dataset_images: list[SingleImage],
+) -> TaskOutput:
+
+    final_new_images = []
+    final_edited_images = []
+    final_removed_images = []
+    final_new_filters = None
+
+    for task_output in task_outputs:
+
+        if task_output.new_images:
+            for new_image in task_output.new_images:
+                old_image = find_image_by_path(
+                    images=old_dataset_images,
+                    path=new_old_image_mapping[new_image.path],
+                )
+                # Propagate old-image attributes to new-image
+                new_image.attributes = (
+                    old_image.attributes | new_image.attributes
+                )
+                final_new_images.append(new_image)
+
+        if task_output.edited_images:
+            for edited_image in task_output.edited_images:
+                final_edited_images.append(edited_image)
+
+        if task_output.removed_images:
+            for removed_image in task_output.removed_images:
+                final_removed_images.append(removed_image)
+
+        new_filters = task_output.new_filters
+        if new_filters:
+            if final_new_filters is None:
+                final_new_filters = new_filters
+            else:
+                if final_new_filters != new_filters:
+                    raise ValueError(
+                        f"{new_filters=} but {final_new_filters=}"
+                    )
+
+    final_output = TaskOutput()
+    if final_new_images:
+        final_output.new_images = final_new_images
+    if final_edited_images:
+        final_output.edited_images = final_edited_images
+    if final_new_filters:
+        final_output.new_filters = final_new_filters
+    if final_edited_images:
+        final_output.removed_images = final_removed_images
+
+    return final_output
 
 
 def _run_parallel_task(
