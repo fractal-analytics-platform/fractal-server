dfbe367b79041b0f28e84de9abcfe9fba027d693
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-02-26 16:49:55 +0100

    export v2 models

--- fractal_server/app/models/v2/__init__.py
+++ fractal_server/app/models/v2/__init__.py
@@ -6,9 +6,9 @@ from ..linkuserproject import LinkUserProject  # noqa F401
 from ..project import Project  # noqa F401
 from ..security import OAuthAccount  # noqa F401
 from ..security import UserOAuth  # noqa F401
+from ..state import State  # noqa F401
 from .dataset import DatasetV2  # noqa F401
 from .job import JobV2  # noqa F401
-from .state import State  # noqa F401
 from .task import TaskV2  # noqa F401
 from .workflow import WorkflowTaskV2  # noqa F401
 from .workflow import WorkflowTaskV2Legacy  # noqa F401
