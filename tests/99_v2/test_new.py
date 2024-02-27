45eb3173b7db8f95dce6381833d245611a5c9067
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-02-27 12:29:30 +0100

    test update and delete

--- tests/99_v2/test_new.py
+++ tests/99_v2/test_new.py
@@ -126,7 +126,7 @@ async def test_api_dataset_v2(client, MockCurrentUser):
         res = await client.post(
             f"api/v2/project/{p2_id}/dataset/",
             json=dict(
-                name="name",
+                name="dataset",
                 images=[
                     {
                         "path": "/tmp/xxx.yz",
@@ -145,23 +145,24 @@ async def test_api_dataset_v2(client, MockCurrentUser):
 
         # GET (3 different ones)
 
+        # 1
         res = await client.get("api/v2/dataset/")
         assert res.status_code == 200
         user_dataset_list = res.json()
 
+        # 2
         res = await client.get(f"api/v2/project/{p1_id}/dataset/")
         assert res.status_code == 400
-
         res = await client.get(f"api/v2/project/{p2_id}/dataset/")
         assert res.status_code == 200
         project_dataset_list = res.json()
 
+        # 3
         res = await client.get(
             f"api/v2/project/{p2_id}/dataset/{dataset1['id']}/"
         )
         assert res.status_code == 200
         ds1 = res.json()
-
         res = await client.get(
             f"api/v2/project/{p2_id}/dataset/{dataset2['id']}/"
         )
@@ -169,3 +170,25 @@ async def test_api_dataset_v2(client, MockCurrentUser):
         ds2 = res.json()
 
         assert user_dataset_list == project_dataset_list == [ds1, ds2]
+
+        # UPDATE
+
+        NEW_NAME = "new name"
+        res = await client.patch(
+            f"api/v2/project/{p2_id}/dataset/{dataset2['id']}/",
+            json=dict(name=NEW_NAME),
+        )
+        assert res.status_code == 200
+        res = await client.get(
+            f"api/v2/project/{p2_id}/dataset/{dataset2['id']}/"
+        )
+        assert res.json()["name"] == NEW_NAME
+
+        # DELETE
+
+        res = await client.delete(
+            f"api/v2/project/{p2_id}/dataset/{dataset2['id']}/"
+        )
+        assert res.status_code == 204
+        res = await client.get(f"api/v2/project/{p2_id}/dataset/")
+        assert len(res.json()) == 1
