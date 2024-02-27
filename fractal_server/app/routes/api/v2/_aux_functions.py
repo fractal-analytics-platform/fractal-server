9f5bbb1dddaa228b66b4990fa778bb156a09fa5e
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-02-27 11:12:26 +0100

    api v2 dataset

---
+++ fractal_server/app/routes/api/v2/_aux_functions.py
@@ -0,0 +1,436 @@
+"""
+Auxiliary functions to get object from the database or perform simple checks
+"""
+from typing import Any
+from typing import Literal
+from typing import Optional
+from typing import Union
+
+from fastapi import HTTPException
+from fastapi import status
+from sqlmodel import select
+from sqlmodel.sql.expression import SelectOfScalar
+
+from ....db import AsyncSession
+from ....models.v2 import DatasetV2
+from ....models.v2 import JobV2
+from ....models.v2 import LinkUserProject
+from ....models.v2 import Project
+from ....models.v2 import TaskV2
+from ....models.v2 import WorkflowTaskV2
+from ....models.v2 import WorkflowV2
+from ....schemas import JobStatusType
+from ....security import User
+
+
+async def _get_project_check_owner(
+    *,
+    project_id: int,
+    user_id: int,
+    db: AsyncSession,
+) -> Project:
+    """
+    Check that user is a member of project and return the project.
+
+    Args:
+        project_id:
+        user_id:
+        db:
+
+    Returns:
+        The project object
+
+    Raises:
+        HTTPException(status_code=403_FORBIDDEN):
+            If the user is not a member of the project
+        HTTPException(status_code=404_NOT_FOUND):
+            If the project does not exist
+    """
+    project = await db.get(Project, project_id)
+    link_user_project = await db.get(LinkUserProject, (project_id, user_id))
+    if not project:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Project not found"
+        )
+    if not link_user_project:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Not allowed on project {project_id}",
+        )
+    return project
+
+
+async def _get_workflow_check_owner(
+    *,
+    workflow_id: int,
+    project_id: int,
+    user_id: int,
+    db: AsyncSession,
+) -> WorkflowV2:
+    """
+    Get a workflow and a project, after access control on the project.
+
+    Args:
+        workflow_id:
+        project_id:
+        user_id:
+        db:
+
+    Returns:
+        The workflow object.
+
+    Raises:
+        HTTPException(status_code=404_NOT_FOUND):
+            If the workflow does not exist
+        HTTPException(status_code=422_UNPROCESSABLE_ENTITY):
+            If the workflow is not associated to the project
+    """
+
+    # Access control for project
+    project = await _get_project_check_owner(
+        project_id=project_id, user_id=user_id, db=db
+    )
+    # Get workflow
+    workflow = await db.get(WorkflowV2, workflow_id)
+    if not workflow:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Workflow not found"
+        )
+    if workflow.project_id != project.id:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=(f"Invalid {project_id=} for {workflow_id=}."),
+        )
+
+    # Refresh so that workflow.project relationship is loaded (see discussion
+    # in issue #1063)
+    await db.refresh(workflow)
+
+    return workflow
+
+
+async def _get_workflow_task_check_owner(
+    *,
+    project_id: int,
+    workflow_id: int,
+    workflow_task_id: int,
+    user_id: int,
+    db: AsyncSession,
+) -> tuple[WorkflowTaskV2, WorkflowV2]:
+    """
+    Check that user has access to Workflow and WorkflowTask.
+
+    Args:
+        project_id:
+        workflow_id:
+        workflow_task_id:
+        user_id:
+        db:
+
+    Returns:
+        Tuple of WorkflowTask and Workflow objects.
+
+    Raises:
+        HTTPException(status_code=404_NOT_FOUND):
+            If the WorkflowTask does not exist
+        HTTPException(status_code=422_UNPROCESSABLE_ENTITY):
+            If the WorkflowTask is not associated to the Workflow
+    """
+
+    # Access control for workflow
+    workflow = await _get_workflow_check_owner(
+        workflow_id=workflow_id, project_id=project_id, user_id=user_id, db=db
+    )
+
+    # If WorkflowTask is not in the db, exit
+    workflow_task = await db.get(WorkflowTaskV2, workflow_task_id)
+    if not workflow_task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="WorkflowTask not found",
+        )
+
+    # If WorkflowTask is not part of the expected Workflow, exit
+    if workflow_id != workflow_task.workflow_id:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Invalid {workflow_id=} for {workflow_task_id=}",
+        )
+
+    return workflow_task, workflow
+
+
+async def _check_workflow_exists(
+    *,
+    name: str,
+    project_id: int,
+    db: AsyncSession,
+) -> None:
+    """
+    Check that no other workflow of this project has the same name.
+
+    Args:
+        name: Workflow name
+        project_id: Project ID
+        db:
+
+    Raises:
+        HTTPException(status_code=422_UNPROCESSABLE_ENTITY):
+            If such a workflow already exists
+    """
+    stm = (
+        select(WorkflowV2)
+        .where(WorkflowV2.name == name)
+        .where(WorkflowV2.project_id == project_id)
+    )
+    res = await db.execute(stm)
+    if res.scalars().all():
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Workflow with {name=} and {project_id=} already exists.",
+        )
+
+
+async def _check_project_exists(
+    *,
+    project_name: str,
+    user_id: int,
+    db: AsyncSession,
+) -> None:
+    """
+    Check that no other project with this name exists for this user.
+
+    Args:
+        project_name: Project name
+        user_id: User ID
+        db:
+
+    Raises:
+        HTTPException(status_code=422_UNPROCESSABLE_ENTITY):
+            If such a project already exists
+    """
+    stm = (
+        select(Project)
+        .join(LinkUserProject)
+        .where(Project.name == project_name)
+        .where(LinkUserProject.user_id == user_id)
+    )
+    res = await db.execute(stm)
+    if res.scalars().all():
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Project name ({project_name}) already in use",
+        )
+
+
+async def _get_dataset_check_owner(
+    *,
+    project_id: int,
+    dataset_id: int,
+    user_id: int,
+    db: AsyncSession,
+) -> dict[Literal["dataset", "project"], Union[DatasetV2, Project]]:
+    """
+    Get a dataset and a project, after access control on the project
+
+    Args:
+        project_id:
+        dataset_id:
+        user_id:
+        db:
+
+    Returns:
+        Dictionary with the dataset and project objects (keys: `dataset`,
+            `project`).
+
+    Raises:
+        HTTPException(status_code=422_UNPROCESSABLE_ENTITY):
+            If the dataset is not associated to the project
+    """
+
+    # Access control for project
+    project = await _get_project_check_owner(
+        project_id=project_id, user_id=user_id, db=db
+    )
+    # Get dataset
+    dataset = await db.get(DatasetV2, dataset_id)
+    if not dataset:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Dataset not found"
+        )
+    if dataset.project_id != project_id:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Invalid {project_id=} for {dataset_id=}",
+        )
+
+    # Refresh so that dataset.project relationship is loaded (see discussion
+    # in issue #1063)
+    await db.refresh(dataset)
+
+    return dict(dataset=dataset, project=project)
+
+
+async def _get_job_check_owner(
+    *,
+    project_id: int,
+    job_id: int,
+    user_id: int,
+    db: AsyncSession,
+) -> dict[Literal["job", "project"], Union[JobV2, Project]]:
+    """
+    Get a job and a project, after access control on the project
+
+    Args:
+        project_id:
+        job_id:
+        user_id:
+        db:
+
+    Returns:
+        Dictionary with the job and project objects (keys: `job`,
+            `project`).
+
+    Raises:
+        HTTPException(status_code=422_UNPROCESSABLE_ENTITY):
+            If the job is not associated to the project
+    """
+    # Access control for project
+    project = await _get_project_check_owner(
+        project_id=project_id, user_id=user_id, db=db
+    )
+    # Get dataset
+    job = await db.get(JobV2, job_id)
+    if not job:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Job not found"
+        )
+    if job.project_id != project_id:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Invalid {project_id=} for {job_id=}",
+        )
+    return dict(job=job, project=project)
+
+
+async def _get_task_check_owner(
+    *,
+    task_id: int,
+    user: User,
+    db: AsyncSession,
+) -> TaskV2:
+    """
+    Get a task, after access control.
+
+    This check constitutes a preliminary version of access control:
+    if the current user is not a superuser and differs from the task owner
+    (including when `owner is None`), we raise an 403 HTTP Exception.
+
+    Args:
+        task_id:
+        user:
+        db:
+
+    Returns:
+        The task object.
+
+    Raises:
+        HTTPException(status_code=404_NOT_FOUND):
+            If the task does not exist
+        HTTPException(status_code=403_FORBIDDEN):
+            If the user does not have rights to edit this task.
+    """
+    task = await db.get(TaskV2, task_id)
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Task {task_id} not found.",
+        )
+
+    if not user.is_superuser:
+        if task.owner is None:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=(
+                    "Only a superuser can modify a Task with `owner=None`."
+                ),
+            )
+        else:
+            owner = user.username or user.slurm_user
+            if owner != task.owner:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail=(
+                        f"Current user ({owner}) cannot modify Task {task.id} "
+                        f"with different owner ({task.owner})."
+                    ),
+                )
+    return task
+
+
+def _get_submitted_jobs_statement() -> SelectOfScalar:
+    """
+    Returns:
+        A sqlmodel statement that selects all `ApplyWorkflow`s with
+        `ApplyWorkflow.status` equal to `submitted`.
+    """
+    stm = select(JobV2).where(JobV2.status == JobStatusType.SUBMITTED)
+    return stm
+
+
+async def _workflow_insert_task(
+    *,
+    workflow_id: int,
+    task_id: int,
+    args: Optional[dict[str, Any]] = None,
+    meta: Optional[dict[str, Any]] = None,
+    order: Optional[int] = None,
+    db: AsyncSession,
+) -> WorkflowTaskV2:
+    """
+    Insert a new WorkflowTask into Workflow.task_list
+
+    Args:
+        task_id: TBD
+        args: TBD
+        meta: TBD
+        order: TBD
+        db: TBD
+    """
+    db_workflow = await db.get(WorkflowV2, workflow_id)
+    if db_workflow is None:
+        raise ValueError(f"Workflow {workflow_id} does not exist")
+
+    if order is None:
+        order = len(db_workflow.task_list)
+
+    # Get task from db, and extract default arguments via a Task property
+    # method
+    db_task = await db.get(TaskV2, task_id)
+    if db_task is None:
+        raise ValueError(f"Task {task_id} does not exist")
+
+    default_args = db_task.default_args_from_args_schema
+    # Override default_args with args
+    actual_args = default_args.copy()
+    if args is not None:
+        for k, v in args.items():
+            actual_args[k] = v
+    if not actual_args:
+        actual_args = None
+
+    # Combine meta (higher priority) and db_task.meta (lower priority)
+    wt_meta = (db_task.meta or {}).copy()
+    wt_meta.update(meta or {})
+    if not wt_meta:
+        wt_meta = None
+
+    # Create DB entry
+    wf_task = WorkflowTaskV2(task_id=task_id, args=actual_args, meta=wt_meta)
+    db.add(wf_task)
+    db_workflow.task_list.insert(order, wf_task)
+    db_workflow.task_list.reorder()  # type: ignore
+    await db.commit()
+    await db.refresh(wf_task)
+
+    return wf_task
---
+++ fractal_server/app/routes/api/v2/dataset.py
@@ -0,0 +1,380 @@
+from typing import Optional
+
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Response
+from fastapi import status
+from sqlmodel import and_
+from sqlmodel import select
+
+from ....db import AsyncSession
+from ....db import get_async_db
+from ....models import Project
+from ....models.v2 import DatasetV2
+from ....models.v2 import JobV2
+from ....schemas.v2 import DatasetCreateV2
+from ....schemas.v2 import DatasetReadV2
+from ....schemas.v2 import DatasetUpdateV2
+from ....security import current_active_user
+from ....security import User
+from ._aux_functions import _get_dataset_check_owner
+from ._aux_functions import _get_project_check_owner
+from ._aux_functions import _get_submitted_jobs_statement
+
+
+router = APIRouter()
+
+
+@router.post(
+    "/project/{project_id}/dataset/",
+    response_model=DatasetReadV2,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_dataset(
+    project_id: int,
+    dataset: DatasetCreateV2,
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_async_db),
+) -> Optional[DatasetReadV2]:
+    """
+    Add new dataset to current project
+    """
+    await _get_project_check_owner(
+        project_id=project_id, user_id=user.id, db=db
+    )
+    db_dataset = DatasetV2(project_id=project_id, **dataset.dict())
+    db.add(db_dataset)
+    await db.commit()
+    await db.refresh(db_dataset)
+    await db.close()
+
+    return db_dataset
+
+
+@router.get(
+    "/project/{project_id}/dataset/",
+    response_model=list[DatasetReadV2],
+)
+async def read_dataset_list(
+    project_id: int,
+    history: bool = True,
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_async_db),
+) -> Optional[list[DatasetReadV2]]:
+    """
+    Get dataset list for given project
+    """
+    # Access control
+    project = await _get_project_check_owner(
+        project_id=project_id, user_id=user.id, db=db
+    )
+    # Find datasets of the current project. Note: this select/where approach
+    # has much better scaling than refreshing all elements of
+    # `project.dataset_list` - ref
+    # https://github.com/fractal-analytics-platform/fractal-server/pull/1082#issuecomment-1856676097.
+    stm = select(DatasetV2).where(DatasetV2.project_id == project.id)
+    res = await db.execute(stm)
+    dataset_list = res.scalars().all()
+    await db.close()
+    if not history:
+        for ds in dataset_list:
+            setattr(ds, "history", [])
+    return dataset_list
+
+
+@router.get(
+    "/project/{project_id}/dataset/{dataset_id}/",
+    response_model=DatasetReadV2,
+)
+async def read_dataset(
+    project_id: int,
+    dataset_id: int,
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_async_db),
+) -> Optional[DatasetReadV2]:
+    """
+    Get info on a dataset associated to the current project
+    """
+    output = await _get_dataset_check_owner(
+        project_id=project_id,
+        dataset_id=dataset_id,
+        user_id=user.id,
+        db=db,
+    )
+    dataset = output["dataset"]
+    await db.close()
+    return dataset
+
+
+@router.patch(
+    "/project/{project_id}/dataset/{dataset_id}/",
+    response_model=DatasetReadV2,
+)
+async def update_dataset(
+    project_id: int,
+    dataset_id: int,
+    dataset_update: DatasetUpdateV2,
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_async_db),
+) -> Optional[DatasetReadV2]:
+    """
+    Edit a dataset associated to the current project
+    """
+
+    output = await _get_dataset_check_owner(
+        project_id=project_id,
+        dataset_id=dataset_id,
+        user_id=user.id,
+        db=db,
+    )
+    db_dataset = output["dataset"]
+
+    for key, value in dataset_update.dict(exclude_unset=True).items():
+        setattr(db_dataset, key, value)
+
+    await db.commit()
+    await db.refresh(db_dataset)
+    await db.close()
+    return db_dataset
+
+
+@router.delete(
+    "/project/{project_id}/dataset/{dataset_id}/",
+    status_code=204,
+)
+async def delete_dataset(
+    project_id: int,
+    dataset_id: int,
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_async_db),
+) -> Response:
+    """
+    Delete a dataset associated to the current project
+    """
+    output = await _get_dataset_check_owner(
+        project_id=project_id,
+        dataset_id=dataset_id,
+        user_id=user.id,
+        db=db,
+    )
+    dataset = output["dataset"]
+
+    # Fail if there exist jobs that are submitted and in relation with the
+    # current dataset.
+    stm = _get_submitted_jobs_statement().where(JobV2.dataset_id == dataset_id)
+    res = await db.execute(stm)
+    jobs = res.scalars().all()
+    if jobs:
+        string_ids = str([job.id for job in jobs])[1:-1]
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=(
+                f"Cannot delete dataset {dataset.id} because it "
+                f"is linked to active job(s) {string_ids}."
+            ),
+        )
+
+    # Cascade operations: set foreign-keys to null for jobs which are in
+    # relationship with the current dataset
+    stm = select(JobV2).where(JobV2.input_dataset_id == dataset_id)
+    res = await db.execute(stm)
+    jobs = res.scalars().all()
+    for job in jobs:
+        job.dataset_id = None
+        await db.merge(job)
+    await db.commit()
+
+    # Delete dataset
+    await db.delete(dataset)
+    await db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.get("/dataset/", response_model=list[DatasetReadV2])
+async def get_user_datasets(
+    history: bool = True,
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_async_db),
+) -> list[DatasetReadV2]:
+    """
+    Returns all the datasets of the current user
+    """
+    stm = select(DatasetV2)
+    stm = stm.join(Project).where(
+        and_(
+            Project.user_list.any(User.id == user.id), Project.version == "v2"
+        )
+    )
+    res = await db.execute(stm)
+    dataset_list = res.scalars().all()
+    await db.close()
+    if not history:
+        for ds in dataset_list:
+            setattr(ds, "history", [])
+    return dataset_list
+
+
+# ADVANCED ENDPOINTS
+
+# @router.get(
+#     "/project/{project_id}/dataset/{dataset_id}/status/",
+#     response_model=DatasetStatusRead,
+# )
+# async def get_workflowtask_status(
+#     project_id: int,
+#     dataset_id: int,
+#     user: User = Depends(current_active_user),
+#     db: AsyncSession = Depends(get_async_db),
+# ) -> Optional[DatasetStatusRead]:
+#     """
+#     Extract the status of all `WorkflowTask`s that ran on a given `Dataset`.
+#     """
+#     # Get the dataset DB entry
+#     output = await _get_dataset_check_owner(
+#         project_id=project_id,
+#         dataset_id=dataset_id,
+#         user_id=user.id,
+#         db=db,
+#     )
+#     dataset = output["dataset"]
+
+#     # Check whether there exists a job such that
+#     # 1. `job.output_dataset_id == dataset_id`, and
+#     # 2. `job.status` is either submitted or running.
+#     # If one such job exists, it will be used later. If there are multiple
+#     # jobs, raise an error.
+#     # Note: see
+#   # https://sqlmodel.tiangolo.com/tutorial/where/#type-annotations-and-errors
+#     # regarding the type-ignore in this code block
+#     stm = _get_submitted_jobs_statement().where(
+#         ApplyWorkflow.output_dataset_id == dataset_id
+#     )
+#     res = await db.execute(stm)
+#     running_jobs = res.scalars().all()
+#     if len(running_jobs) == 0:
+#         running_job = None
+#     elif len(running_jobs) == 1:
+#         running_job = running_jobs[0]
+#     else:
+#         string_ids = str([job.id for job in running_jobs])[1:-1]
+#         raise HTTPException(
+#             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+#             detail=(
+#                 f"Cannot get WorkflowTask statuses as dataset {dataset.id} "
+#                 f"is linked to multiple active jobs: {string_ids}"
+#             ),
+#         )
+
+#     # Initialize empty dictionary for workflowtasks status
+#     workflow_tasks_status_dict: dict = {}
+
+#   # Lowest priority: read status from DB, which corresponds to jobs that are
+#     # not running
+#     history = dataset.history
+#     for history_item in history:
+#         wftask_id = history_item["workflowtask"]["id"]
+#         wftask_status = history_item["status"]
+#         workflow_tasks_status_dict[wftask_id] = wftask_status
+
+#     # If a job is running, then gather more up-to-date information
+#     if running_job is not None:
+#         # Get the workflow DB entry
+#         running_workflow = await _get_workflow_check_owner(
+#             project_id=project_id,
+#             workflow_id=running_job.workflow_id,
+#             user_id=user.id,
+#             db=db,
+#         )
+#       # Mid priority: Set all WorkflowTask's that are part of the running job
+#         # as "submitted"
+#         start = running_job.first_task_index
+#         end = running_job.last_task_index + 1
+#         for wftask in running_workflow.task_list[start:end]:
+#             workflow_tasks_status_dict[wftask.id] = "submitted"
+
+#         # Highest priority: Read status updates coming from the running-job
+#         # temporary file. Note: this file only contains information on
+#         # WorkflowTask's that ran through successfully
+#         tmp_file = Path(running_job.working_dir) / HISTORY_FILENAME
+#         try:
+#             with tmp_file.open("r") as f:
+#                 history = json.load(f)
+#         except FileNotFoundError:
+#             history = []
+#         for history_item in history:
+#             wftask_id = history_item["workflowtask"]["id"]
+#             wftask_status = history_item["status"]
+#             workflow_tasks_status_dict[wftask_id] = wftask_status
+
+#     response_body = DatasetStatusRead(status=workflow_tasks_status_dict)
+#     return response_body
+
+
+# @router.get(
+#     "/project/{project_id}/dataset/{dataset_id}/export_history/",
+#     response_model=WorkflowExport,
+# )
+# async def export_history_as_workflow(
+#     project_id: int,
+#     dataset_id: int,
+#     user: User = Depends(current_active_user),
+#     db: AsyncSession = Depends(get_async_db),
+# ) -> Optional[WorkflowExport]:
+#     """
+#     Extract a reproducible workflow from the dataset history.
+#     """
+#     # Get the dataset DB entry
+#     output = await _get_dataset_check_owner(
+#         project_id=project_id,
+#         dataset_id=dataset_id,
+#         user_id=user.id,
+#         db=db,
+#     )
+#     dataset = output["dataset"]
+
+#     # Check whether there exists a submitted job such that
+#     # `job.output_dataset_id==dataset_id`.
+#     # If at least one such job exists, then this endpoint will fail.
+#   # We do not support the use case of exporting a reproducible workflow when
+#     # job execution is in progress; this may change in the future.
+#     stm = _get_submitted_jobs_statement().where(
+#         ApplyWorkflow.output_dataset_id == dataset_id
+#     )
+#     res = await db.execute(stm)
+#     jobs = res.scalars().all()
+#     if jobs:
+#         string_ids = str([job.id for job in jobs])[1:-1]
+#         raise HTTPException(
+#             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+#             detail=(
+#                 f"Cannot export history because dataset {dataset.id} "
+#                 f"is linked to active job(s) {string_ids}."
+#             ),
+#         )
+
+#   # It such a job does not exist, continue with the endpoint. Note that this
+#     # means that the history in the DB is up-to-date.
+
+#     # Read history from DB
+#     history = dataset.history
+
+#     # Construct reproducible workflow
+#     task_list = []
+#     for history_item in history:
+#         wftask = history_item["workflowtask"]
+#         wftask_status = history_item["status"]
+#         if wftask_status == "done":
+#             task_list.append(WorkflowTaskExport(**wftask))
+
+#     def _slugify_dataset_name(_name: str) -> str:
+#         _new_name = _name
+#         for char in (" ", ".", "/", "\\"):
+#             _new_name = _new_name.replace(char, "_")
+#         return _new_name
+
+#     name = f"history_{_slugify_dataset_name(dataset.name)}"
+
+#     workflow = WorkflowExport(name=name, task_list=task_list)
+#     return workflow
