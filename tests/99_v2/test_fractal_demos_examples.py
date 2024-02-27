002af15786edaca1f2393e5c7b604dfc64c8d5e4
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-02-27 10:43:00 +0100

    Add second version of fractal-demos example 01

--- tests/99_v2/test_fractal_demos_examples.py
+++ tests/99_v2/test_fractal_demos_examples.py
@@ -15,7 +15,164 @@ def _assert_image_data_exist(image_list: list[dict]):
 
 
 def test_fractal_demos_01(tmp_path: Path):
-    """ """
+    """
+    Mock of fractal-demos/examples/01.
+    """
+    zarr_dir = (tmp_path / "zarr_dir").as_posix().rstrip("/")
+    dataset = Dataset(id=1)
+    dataset = execute_tasks_v2(
+        wf_task_list=[
+            WorkflowTask(
+                task=TASK_LIST["create_ome_zarr"],
+                args=dict(image_dir="/tmp/input_images", zarr_dir=zarr_dir),
+            )
+        ],
+        dataset=dataset,
+    )
+
+    assert dataset.history == [
+        "create_ome_zarr",
+    ]
+    assert dataset.filters == {
+        "plate": "my_plate.zarr",
+        "data_dimensionality": "3",
+    }
+    assert dataset.image_paths == [
+        f"{zarr_dir}/my_plate.zarr/A/01/0",
+        f"{zarr_dir}/my_plate.zarr/A/02/0",
+    ]
+
+    dataset = execute_tasks_v2(
+        wf_task_list=[
+            WorkflowTask(
+                task=TASK_LIST["yokogawa_to_zarr"],
+                args=dict(),
+            )
+        ],
+        dataset=dataset,
+    )
+
+    debug(dataset)
+
+    assert dataset.history == [
+        "create_ome_zarr",
+        "yokogawa_to_zarr",
+    ]
+    assert dataset.filters == {
+        "plate": "my_plate.zarr",
+        "data_dimensionality": "3",
+    }
+    _assert_image_data_exist(dataset.images)
+
+    dataset = execute_tasks_v2(
+        wf_task_list=[
+            WorkflowTask(
+                task=TASK_LIST["illumination_correction"],
+                args=dict(overwrite_input=True),
+            )
+        ],
+        dataset=dataset,
+    )
+
+    assert dataset.history == [
+        "create_ome_zarr",
+        "yokogawa_to_zarr",
+        "illumination_correction",
+    ]
+    assert dataset.filters == {
+        "plate": "my_plate.zarr",
+        "data_dimensionality": "3",
+        "illumination_correction": True,
+    }
+    assert set(dataset.image_paths) == {
+        f"{zarr_dir}/my_plate.zarr/A/01/0",
+        f"{zarr_dir}/my_plate.zarr/A/02/0",
+    }
+
+    img = find_image_by_path(
+        path=f"{zarr_dir}/my_plate.zarr/A/01/0", images=dataset.images
+    )
+    assert img.dict() == {
+        "path": f"{zarr_dir}/my_plate.zarr/A/01/0",
+        "attributes": {
+            "well": "A_01",
+            "plate": "my_plate.zarr",
+            "data_dimensionality": "3",
+            "illumination_correction": True,
+        },
+    }
+    _assert_image_data_exist(dataset.images)
+
+    dataset = execute_tasks_v2(
+        wf_task_list=[
+            WorkflowTask(
+                task=TASK_LIST["new_ome_zarr"],
+                args=dict(suffix="mip"),
+            )
+        ],
+        dataset=dataset,
+    )
+
+    assert dataset.history == [
+        "create_ome_zarr",
+        "yokogawa_to_zarr",
+        "illumination_correction",
+        "new_ome_zarr",
+    ]
+    assert dataset.filters == {
+        "plate": "my_plate_mip.zarr",
+        "data_dimensionality": "2",
+        "illumination_correction": True,
+    }
+    assert set(dataset.image_paths) == {
+        f"{zarr_dir}/my_plate.zarr/A/01/0",
+        f"{zarr_dir}/my_plate.zarr/A/02/0",
+        f"{zarr_dir}/my_plate_mip.zarr/A/01/0",
+        f"{zarr_dir}/my_plate_mip.zarr/A/02/0",
+    }
+
+    dataset = execute_tasks_v2(
+        wf_task_list=[
+            WorkflowTask(
+                task=TASK_LIST["maximum_intensity_projection"],
+                args=dict(),
+            )
+        ],
+        dataset=dataset,
+    )
+
+    assert dataset.history == [
+        "create_ome_zarr",
+        "yokogawa_to_zarr",
+        "illumination_correction",
+        "new_ome_zarr",
+        "maximum_intensity_projection",
+    ]
+    assert dataset.filters == {
+        "plate": "my_plate_mip.zarr",
+        "data_dimensionality": "2",
+        "illumination_correction": True,
+    }
+    img = find_image_by_path(
+        path=f"{zarr_dir}/my_plate_mip.zarr/A/01/0", images=dataset.images
+    )
+    assert img.dict() == {
+        "path": f"{zarr_dir}/my_plate_mip.zarr/A/01/0",
+        "attributes": {
+            "well": "A_01",
+            "plate": "my_plate_mip.zarr",
+            "data_dimensionality": "2",
+            "illumination_correction": True,
+        },
+    }
+    _assert_image_data_exist(dataset.images)
+
+
+def test_fractal_demos_01_no_overwrite(tmp_path: Path):
+    """
+    Similar to fractal-demos/examples/01, but illumination
+    correction task does not override its input images.
+    """
     zarr_dir = (tmp_path / "zarr_dir").as_posix().rstrip("/")
     dataset = Dataset(id=1)
     dataset = execute_tasks_v2(
