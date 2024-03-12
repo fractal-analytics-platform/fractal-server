d8cb45b0f3fe0609693fc4a653f97325f45383e9
mfranzon <franzonmarco94@gmail.com>
2024-03-12 14:13:30 +0100

    refactor _custom_jobs_finished function

--- fractal_server/app/runner/_slurm/_check_jobs_status.py
+++ fractal_server/app/runner/_slurm/_check_jobs_status.py
@@ -1,6 +1,6 @@
-from subprocess import CalledProcessError  # nosec
-from subprocess import PIPE  # nosec
 from subprocess import run  # nosec
+
+from ....logger import set_logger
 
 STATES_FINISHED = {  # https://slurm.schedmd.com/squeue.html#lbAG
     "BOOT_FAIL",
@@ -15,64 +15,61 @@ STATES_FINISHED = {  # https://slurm.schedmd.com/squeue.html#lbAG
     "TIMEOUT",
 }
 
+logger = set_logger(__name__)
 
-def _custom_jobs_finished(job_ids, logger):
-    """Check which ones of the given Slurm jobs already finished"""
+
+def run_squeue(job_ids):
+    res = run(  # nosec
+        [
+            "squeue",
+            "--noheader",
+            "--format='%i %T'",
+            "--jobs",
+            ",".join([str(j) for j in job_ids]),
+            "--states=all",
+        ],
+        capture_output=True,
+        encoding="utf-8",
+        check=False,
+    )
+    if res.returncode != 0:
+        logger.warning(
+            f"squeue command with {job_ids}"
+            f" failed with:\n{res.stderr=}\n{res.stdout=}"
+        )
+
+    return res
+
+
+def _custom_jobs_finished(job_ids) -> set[str]:
+    """
+    Check which ones of the given Slurm jobs already finished
+
+    The function is copied from clusterfutures 0.5.
+    Original Copyright: 2022 Adrian Sampson,
+    released under the MIT licence
+    """
 
     # If there is no Slurm job to check, return right away
     if not job_ids:
         return set()
+    id_to_state = dict()
 
-    try:
-        res = run(  # nosec
-            [
-                "squeue",
-                "--noheader",
-                "--format=%i %T",
-                "--jobs",
-                ",".join([str(j) for j in job_ids]),
-                "--states=all",
-            ],
-            stdout=PIPE,
-            stderr=PIPE,
-            encoding="utf-8",
-            check=True,
-        )
-
-        id_to_state = dict(
-            [
-                stdout.strip().partition(" ")[::2]
-                for stdout in res.stdout.splitlines()
-            ]
-        )
-    except CalledProcessError:
+    res = run_squeue(job_ids)
+    if res.returncode == 0:
+        id_to_state = {
+            out.split()[0]: out.split()[1] for out in res.stdout.splitlines()
+        }
+    else:
         id_to_state = dict()
         for j in job_ids:
-            res = run(  # nosec
-                [
-                    "squeue",
-                    "--noheader",
-                    "--format=%i %T",
-                    "--jobs",
-                    str(j),
-                    "--states=all",
-                ],
-                stdout=PIPE,
-                stderr=PIPE,
-                encoding="utf-8",
-                check=False,
-            )
+            res = run_squeue([j])
             if res.returncode != 0:
-                detail = run(  # nosec
-                    ["sacct", "-j", str(j)],
-                    stdout=PIPE,
-                    stderr=PIPE,
-                    encoding="utf-8",
+                id_to_state.update({str(j): "COMPLETED"})
+            else:
+                id_to_state.update(
+                    {res.stdout.split()[0]: res.stdout.split()[1]}
                 )
-                logger.info(f"{detail}")
-
-                id_to_state.update(j="COMPLETED")
-            id_to_state.update(res.stdout.strip().partition(" ")[::2])
 
     # Finished jobs only stay in squeue for a few mins (configurable). If
     # a job ID isn't there, we'll assume it's finished.
