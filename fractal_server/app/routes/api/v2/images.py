232629240c2523dc254193d3e8c2218697c5386d
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-03-13 12:10:41 +0100

    delete images endpoint

--- fractal_server/app/routes/api/v2/images.py
+++ fractal_server/app/routes/api/v2/images.py
@@ -3,7 +3,10 @@ from typing import Optional
 
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Query
 from fastapi import Request
+from fastapi import Response
 from fastapi import status
 from pydantic import BaseModel
 from pydantic import validator
@@ -61,7 +64,7 @@ async def get_dataset_images(
     output = await _get_dataset_check_owner(
         project_id=project_id, dataset_id=dataset_id, user_id=user.id, db=db
     )
-    images: DatasetV2 = output["dataset"].images
+    images: list[SingleImage] = output["dataset"].images
 
     # Query parameters casting
     query = ImageQuery(
@@ -97,3 +100,41 @@ async def get_dataset_images(
             )
         ),
     )
+
+
+@router.delete(
+    "/project/{project_id}/dataset/{dataset_id}/images/",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_dataset_images(
+    project_id: int,
+    dataset_id: int,
+    path: list[str] = Query(...),
+    user: User = Depends(current_active_user),
+    db: AsyncSession = Depends(get_async_db),
+) -> Response:
+
+    output = await _get_dataset_check_owner(
+        project_id=project_id, dataset_id=dataset_id, user_id=user.id, db=db
+    )
+
+    dataset: DatasetV2 = output["dataset"]
+
+    if not all(p in dataset.image_paths for p in path):
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=(
+                f"The list of paths you provided ({path}) is not a sublist of "
+                f"{dataset.image_paths}."
+            ),
+        )
+
+    new_images = [
+        image for image in dataset.images if not image["path"] in path
+    ]
+
+    dataset.images = new_images
+
+    await db.merge(dataset)
+    await db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
