042a07bbe15f28b82df9126db9abc6cd0598b8b8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-03-13 16:21:10 +0100

    Remove obsolete test

--- tests/v2/04_runner/test_unit_aux_functions_v2.py
+++ tests/v2/04_runner/test_unit_aux_functions_v2.py
@@ -1,33 +1,4 @@
-import pytest
-
-from fractal_server.app.runner.v2.runner import (
-    _validate_parallelization_list_valid,
-)
 from fractal_server.images import deduplicate_list
-
-
-def test_validate_parallelization_list():
-
-    # Missing path
-    PARALLELIZATION_LIST = [dict()]
-    CURRENT_IMAGE_PATHS = []
-    with pytest.raises(ValueError):
-        _validate_parallelization_list_valid(
-            parallelization_list=PARALLELIZATION_LIST,
-            current_image_paths=CURRENT_IMAGE_PATHS,
-        )
-
-    # Valid call
-    PARALLELIZATION_LIST = [
-        dict(path="asd", parameter=1),
-        dict(path="asd", parameter=2),
-        dict(path="asd", parameter=3),
-    ]
-    CURRENT_IMAGE_PATHS = ["asd"]
-    _validate_parallelization_list_valid(
-        parallelization_list=PARALLELIZATION_LIST,
-        current_image_paths=CURRENT_IMAGE_PATHS,
-    )
 
 
 def test_deduplicate_list_of_dicts():
