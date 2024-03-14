d5b9ffdbb1db73451b091c532e1436e9906d1da8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-03-14 17:01:12 +0100

    Comment out obsolete lines in runner v2

--- fractal_server/app/runner/v2/runner_functions.py
+++ fractal_server/app/runner/v2/runner_functions.py
@@ -2,12 +2,13 @@ from concurrent.futures import ThreadPoolExecutor
 from copy import copy
 from typing import Any
 
-from ....images import find_image_by_path
 from ....images import SingleImage
 from .models import DictStrAny
 from .models import Task
 from .task_output import InitTaskOutput
 from .task_output import TaskOutput
+
+# from ....images import find_image_by_path
 
 MAX_PARALLELIZATION_LIST_SIZE = 200
 
@@ -30,11 +31,11 @@ def _run_non_parallel_task(
         _wrapper_expand_kwargs, function_kwargs
     ).result()
 
-    task_output = update_task_output_added_images(
-        task_output=task_output,
-        old_dataset_images=old_dataset_images,
-        task_input_paths=function_kwargs["paths"],
-    )
+    # task_output = update_task_output_added_images(
+    #     task_output=task_output,
+    #     old_dataset_images=old_dataset_images,
+    #     task_input_paths=function_kwargs["paths"],
+    # )
 
     return TaskOutput(**task_output.dict())
 
@@ -60,38 +61,38 @@ def _run_non_parallel_task_init(
     return InitTaskOutput(**task_output.dict())
 
 
-def update_task_output_added_images(
-    task_output: TaskOutput,
-    task_input_paths: list[str],
-    old_dataset_images: list[SingleImage],
-):
-    """
-    Update task_output.added_images (if set)
-    by propagating old images attributes
-    """
-    if task_output.added_images is None:
-        return task_output
+# def update_task_output_added_images(
+#     task_output: TaskOutput,
+#     task_input_paths: list[str],
+#     old_dataset_images: list[SingleImage],
+# ):
+#     """
+#     Update task_output.added_images (if set)
+#     by propagating old images attributes
+#     """
+#     if task_output.added_images is None:
+#         return task_output
 
-    added_image_paths = [
-        added_image.path for added_image in task_output.added_images
-    ]
-    if len(task_input_paths) == len(added_image_paths):
-        final_added_images = []
-        for ind, added_image in enumerate(task_output.added_images):
-            old_image = find_image_by_path(
-                images=old_dataset_images,
-                path=task_input_paths[ind],
-            )
-            added_image.attributes = (
-                old_image.attributes | added_image.attributes
-            )
-            final_added_images.append(added_image)
-        task_output.added_images = final_added_images
-    elif len(task_input_paths) == 0 or len(added_image_paths) == 0:
-        pass
-    else:
-        print("WARNING: If lenghts are different we don't know how to proceed")
-    return task_output
+#     added_image_paths = [
+#         added_image.path for added_image in task_output.added_images
+#     ]
+#     if len(task_input_paths) == len(added_image_paths):
+#         final_added_images = []
+#         for ind, added_image in enumerate(task_output.added_images):
+#             old_image = find_image_by_path(
+#                 images=old_dataset_images,
+#                 path=task_input_paths[ind],
+#             )
+#             added_image.attributes = (
+#                 old_image.attributes | added_image.attributes
+#             )
+#             final_added_images.append(added_image)
+#         task_output.added_images = final_added_images
+#     elif len(task_input_paths) == 0 or len(added_image_paths) == 0:
+#         pass
+#     else:
+#         print("WARNING: If lenghts are different we don't know how to ..")
+#     return task_output
 
 
 def merge_outputs(
