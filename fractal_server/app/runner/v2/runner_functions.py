b70a84cb2d90595507c024b3269457975c66c136
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-03-14 17:03:23 +0100

    Remove commented-out obsolete lines in runner v2

--- fractal_server/app/runner/v2/runner_functions.py
+++ fractal_server/app/runner/v2/runner_functions.py
@@ -8,7 +8,6 @@ from .models import Task
 from .task_output import InitTaskOutput
 from .task_output import TaskOutput
 
-# from ....images import find_image_by_path
 
 MAX_PARALLELIZATION_LIST_SIZE = 200
 
@@ -30,12 +29,6 @@ def _run_non_parallel_task(
     task_output = executor.submit(
         _wrapper_expand_kwargs, function_kwargs
     ).result()
-
-    # task_output = update_task_output_added_images(
-    #     task_output=task_output,
-    #     old_dataset_images=old_dataset_images,
-    #     task_input_paths=function_kwargs["paths"],
-    # )
 
     return TaskOutput(**task_output.dict())
 
@@ -61,44 +54,8 @@ def _run_non_parallel_task_init(
     return InitTaskOutput(**task_output.dict())
 
 
-# def update_task_output_added_images(
-#     task_output: TaskOutput,
-#     task_input_paths: list[str],
-#     old_dataset_images: list[SingleImage],
-# ):
-#     """
-#     Update task_output.added_images (if set)
-#     by propagating old images attributes
-#     """
-#     if task_output.added_images is None:
-#         return task_output
-
-#     added_image_paths = [
-#         added_image.path for added_image in task_output.added_images
-#     ]
-#     if len(task_input_paths) == len(added_image_paths):
-#         final_added_images = []
-#         for ind, added_image in enumerate(task_output.added_images):
-#             old_image = find_image_by_path(
-#                 images=old_dataset_images,
-#                 path=task_input_paths[ind],
-#             )
-#             added_image.attributes = (
-#                 old_image.attributes | added_image.attributes
-#             )
-#             final_added_images.append(added_image)
-#         task_output.added_images = final_added_images
-#     elif len(task_input_paths) == 0 or len(added_image_paths) == 0:
-#         pass
-#     else:
-#         print("WARNING: If lenghts are different we don't know how to ..")
-#     return task_output
-
-
 def merge_outputs(
     task_outputs: list[TaskOutput],
-    # new_old_image_mapping: dict[str, str],
-    # old_dataset_images: list[SingleImage],
 ) -> TaskOutput:
 
     final_added_images = []
@@ -109,16 +66,6 @@ def merge_outputs(
 
         if task_output.added_images:
             for added_image in task_output.added_images:
-                # Propagate old-image attributes to new-image
-                # (if old image exists)
-                # old_image = find_image_by_path(
-                #     images=old_dataset_images,
-                #     path=new_old_image_mapping[added_image.path],
-                # )
-                # if old_image is not None:
-                #     added_image.attributes = (
-                #         old_image.attributes | added_image.attributes
-                #     )
                 final_added_images.append(added_image)
 
         if task_output.edited_images:
@@ -151,7 +98,6 @@ def merge_outputs(
 def _run_parallel_task(
     task: Task,
     list_function_kwargs: list[DictStrAny],
-    # old_dataset_images: list[SingleImage],
     executor: ThreadPoolExecutor,
 ) -> TaskOutput:
 
@@ -173,26 +119,7 @@ def _run_parallel_task(
     results = executor.map(_wrapper_expand_kwargs, list_function_kwargs)
     task_outputs = list(results)
 
-    # new_old_image_mapping = {}
-
-    # for ind, task_output in enumerate(task_outputs):
-    #     if task_output.added_images is not None:
-    #         for added_image in task_output.added_images:
-    #             new_key = added_image.path
-    #             new_value = list_function_kwargs[ind]["path"]
-    #             old_value = new_old_image_mapping.get(new_key)
-    #             if old_value is not None and old_value != new_value:
-    #                 raise ValueError(
-    #                     "The same `added_image.path` corresponds to "
-    #                     "multiple `path` function arguments. This means "
-    #                     "that two tasks with different `path` input created "
-    #                     "the same `added_image` entry."
-    #                 )
-    #             new_old_image_mapping[new_key] = new_value
-
     merged_output = merge_outputs(
         task_outputs,
-        # new_old_image_mapping,
-        # old_dataset_images,
     )
     return merged_output
