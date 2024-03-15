4edfbc09ad715e78e1db3fcdd741d2234e571dda
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-03-15 13:52:42 +0100

    Adapt to new `patched_run_squeue` fixture, and remove it when possible

--- tests/05_backend/test_unit_slurm_jobs_finished.py
+++ tests/05_backend/test_unit_slurm_jobs_finished.py
@@ -1,15 +1,15 @@
 from fractal_server.app.runner._slurm._check_jobs_status import (
-    _custom_jobs_finished,
+    _jobs_finished,
 )
 
 
 def test_jobs_finished(monkey_slurm):
 
-    jobs_finished = _custom_jobs_finished([1, 2, 4])
+    jobs_finished = _jobs_finished([1, 2, 4])
     assert jobs_finished == {1, 2, 4}
 
-    jobs_finished = _custom_jobs_finished([1])
+    jobs_finished = _jobs_finished([1])
     assert jobs_finished == {1}
 
-    jobs_finished = _custom_jobs_finished(["a"])
+    jobs_finished = _jobs_finished(["a"])
     assert jobs_finished == {"a"}
