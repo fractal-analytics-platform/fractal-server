ad1ca45264e0c6da61a6c10db27d1a514ff9fb59
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-03-18 13:42:42 +0100

    Exclude None from valid attribute types

--- fractal_server/images/__init__.py
+++ fractal_server/images/__init__.py
@@ -15,9 +15,9 @@ T = TypeVar("T")
 def val_scalar_dict(attribute: str):
     def val(
         dict_str_any: dict[str, Any],
-    ) -> dict[str, Union[int, float, str, bool, None]]:
+    ) -> dict[str, Union[int, float, str, bool]]:
         for key, value in dict_str_any.items():
-            if not isinstance(value, (int, float, str, bool, type(None))):
+            if not isinstance(value, (int, float, str, bool)):
                 raise ValueError(
                     f"{attribute}[{key}] must be a scalar (int, float, str, "
                     f"bool, or None). Given {value} ({type(value)})"
