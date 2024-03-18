1325d853f4c04cf1a858eaa7a09898e90b3c812f
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-03-18 13:42:48 +0100

    Update tests

--- tests/v2/05_images/test_filters.py
+++ tests/v2/05_images/test_filters.py
@@ -8,7 +8,7 @@ from fractal_server.images import val_scalar_dict
 IMAGES = [
     SingleImage(
         path="plate.zarr/A/01/0",
-        flags=dict(data_dimensionality=3),
+        flags=dict(has_z=True),
         attributes=dict(
             plate="plate.zarr",
             well="A01",
@@ -16,7 +16,7 @@ IMAGES = [
     ),
     SingleImage(
         path="plate.zarr/A/02/0",
-        flags=dict(data_dimensionality=3),
+        flags=dict(has_z=True),
         attributes=dict(
             plate="plate.zarr",
             well="A02",
@@ -25,7 +25,7 @@ IMAGES = [
     SingleImage(
         path="plate.zarr/A/01/0_corr",
         flags=dict(
-            data_dimensionality=3,
+            has_z=True,
             illumination_correction=True,
         ),
         attributes=dict(
@@ -36,7 +36,7 @@ IMAGES = [
     SingleImage(
         path="plate.zarr/A/02/0_corr",
         flags=dict(
-            data_dimensionality=3,
+            has_z=True,
             illumination_correction=True,
         ),
         attributes=dict(
@@ -47,7 +47,7 @@ IMAGES = [
     SingleImage(
         path="plate_2d.zarr/A/01/0_corr",
         flags=dict(
-            data_dimensionality=2,
+            has_z=False,
             illumination_correction=True,
         ),
         attributes=dict(
@@ -58,7 +58,7 @@ IMAGES = [
     SingleImage(
         path="plate_2d.zarr/A/02/0_corr",
         flags=dict(
-            data_dimensionality=2,
+            has_z=False,
             illumination_correction=True,
         ),
         attributes=dict(
@@ -75,6 +75,7 @@ def test_filter_validation():
         {"d": "i", "c": "t"},
         {"s", "e", "t"},
         ("t", "u", "p", "l", "e"),
+        None,
         bool,  # type
         lambda x: x,  # function
     ]
@@ -83,51 +84,48 @@ def test_filter_validation():
         with pytest.raises(ValueError):
             val_scalar_dict("")(filters)
 
-    valid = ["string", -7, 3.14, True, None]
+    valid = ["string", -7, 3.14, True]
     for item in valid:
         filters = dict(key=item)
         assert val_scalar_dict("")(filters) == filters
 
 
 @pytest.mark.parametrize(
-    "attribute_filters,type_filters,expected_number",
+    "attribute_filters,flag_filters,expected_number",
     [
         # No filter
         (None, None, 6),
         # Key is not part of attribute keys
         ({"missing_key": "whatever"}, None, 0),
-        # Key is not part of type keys
-        (None, {"missing_key": "whatever"}, 0),
-        # Key is part of type keys, but value is missing
-        (None, {"data_dimensionality": 0}, 0),
+        # Key is not part of flag keys (default is False)
+        (None, {"missing_key": True}, 0),
+        (None, {"missing_key": False}, 6),
         # Key is part of attribute keys, but value is missing
         ({"plate": "missing_plate.zarr"}, None, 0),
         # Meaning of None for attributes: skip a given filter
         ({"plate": None}, None, 6),
-        # Meaning of None for types: require that a value is actually None
-        (None, {"data_dimensionality": None}, 0),
-        # Single type filter
-        (None, {"data_dimensionality": 3}, 4),
-        # Single type filter
-        (None, {"data_dimensionality": 2}, 2),
-        # Two type filters
-        (None, {"data_dimensionality": 3, "illumination_correction": True}, 2),
-        # Both attribute and type filters
+        # Single flag filter
+        (None, {"has_z": True}, 4),
+        # Single flag filter
+        (None, {"has_z": False}, 2),
+        # Two flag filters
+        (None, {"has_z": True, "illumination_correction": True}, 2),
+        # Both attribute and flag filters
         (
             {"plate": "plate.zarr"},
-            {"data_dimensionality": 3, "illumination_correction": True},
+            {"has_z": True, "illumination_correction": True},
             2,
         ),
-        # Both attribute and type filters
+        # Both attribute and flag filters
         (
             {"plate": "plate_2d.zarr"},
-            {"data_dimensionality": 3, "illumination_correction": True},
+            {"has_z": True, "illumination_correction": True},
             0,
         ),
-        # Both attribute and type filters
+        # Both attribute and flag filters
         (
             {"plate": "plate.zarr", "well": "A01"},
-            {"data_dimensionality": 3, "illumination_correction": True},
+            {"has_z": True, "illumination_correction": True},
             1,
         ),
         # Single attribute filter
@@ -136,16 +134,16 @@ def test_filter_validation():
 )
 def test_filter_image_list(
     attribute_filters,
-    type_filters,
+    flag_filters,
     expected_number,
 ):
     filtered_list = _filter_image_list(
         images=IMAGES,
         attribute_filters=attribute_filters,
-        flag_filters=type_filters,
+        flag_filters=flag_filters,
     )
 
     debug(attribute_filters)
-    debug(type_filters)
+    debug(flag_filters)
     debug(filtered_list)
     assert len(filtered_list) == expected_number
