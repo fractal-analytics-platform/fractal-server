9bf51f6307f95d5a7fdbdb9a369f9813778abe0d
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-03-25 13:32:35 +0100

    Move heavy dependencies to extras, in mock package

--- tests/v2/fractal_tasks_mock/update_package_manifest_and_wheel.sh
+++ tests/v2/fractal_tasks_mock/update_package_manifest_and_wheel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if [ ! -d "venv" ]; then
-    python -m venv venv
+    python3 -m venv venv
 fi
 source venv/bin/activate
-python -m pip install -e .
+python -m pip install -e ".[dev,extra]"
 python create_manifest.py
 python -m build
+deactivate
