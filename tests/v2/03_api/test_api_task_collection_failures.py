75948b4969aea88172d3f0af740f24a6c1b1f61b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-03-25 15:09:56 +0100

    Improve tests for task-collection failures

--- tests/v2/03_api/test_api_task_collection_failures.py
+++ tests/v2/03_api/test_api_task_collection_failures.py
@@ -1,4 +1,3 @@
-import shutil
 from pathlib import Path
 
 from devtools import debug  # noqa
@@ -7,36 +6,57 @@ from devtools import debug  # noqa
 PREFIX = "api/v2/task"
 
 
-async def test_failed_get_collection_info(client, MockCurrentUser):
-    """
-    Get task-collection info for non-existing collection.
-    """
+async def test_failed_API_calls(
+    client, MockCurrentUser, tmp_path, testdata_path, task_factory_v2
+):
+    # Missing state ID
     invalid_state_id = 99999
     async with MockCurrentUser():
         res = await client.get(f"{PREFIX}/collect/{invalid_state_id}/")
-    debug(res)
-    assert res.status_code == 404
+        assert res.status_code == 404
 
-
-async def test_collection_non_verified_user(client, MockCurrentUser):
-    """
-    Test that non-verified users are not authorized to make calls
-    to `/api/v1/task/collect/pip/`.
-    """
+    # Task collection triggered by non-verified user
     async with MockCurrentUser(user_kwargs=dict(is_verified=False)):
         res = await client.post(
             f"{PREFIX}/collect/pip/", json={"package": "fractal-tasks-core"}
         )
         assert res.status_code == 401
 
+    # Missing wheel file
+    async with MockCurrentUser(user_kwargs=dict(is_verified=True)):
+        res = await client.post(
+            f"{PREFIX}/collect/pip/",
+            json=dict(package=str(tmp_path / "missing_file.whl")),
+        )
+        assert res.status_code == 422
+        assert "does not exist" in str(res.json())
 
-async def test_failed_collection_invalid_manifest(
+    # Task collection fails if a task with the same source already exists
+    # (see issue 866)
+    await task_factory_v2(
+        source="pip_local:fractal_tasks_mock:0.0.1:::create_ome_zarr_compound"
+    )
+    async with MockCurrentUser(user_kwargs=dict(is_verified=True)):
+        wheel_path = (
+            testdata_path.parent
+            / "v2/fractal_tasks_mock/dist"
+            / "fractal_tasks_mock-0.0.1-py3-none-any.whl"
+        )
+        res = await client.post(
+            f"{PREFIX}/collect/pip/",
+            json=dict(package=wheel_path.as_posix()),
+        )
+        assert res.status_code == 422
+        assert "Task with source" in res.json()["detail"]
+        assert "already exists in the database" in res.json()["detail"]
+
+
+async def test_invalid_manifest(
     client,
-    dummy_task_package_invalid_manifest,
-    dummy_task_package_missing_manifest,
     MockCurrentUser,
     override_settings_factory,
     tmp_path: Path,
+    testdata_path: Path,
 ):
     """
     GIVEN a package with invalid/missing manifest
@@ -44,50 +64,39 @@ async def test_failed_collection_invalid_manifest(
     THEN it returns 422 (Unprocessable Entity) with an informative message
     """
 
-    override_settings_factory(
-        FRACTAL_TASKS_DIR=(
-            tmp_path / "test_failed_collection_invalid_manifest"
-        )
-    )
+    override_settings_factory(FRACTAL_TASKS_DIR=tmp_path)
+    debug(tmp_path)
 
-    task_collection = dict(
-        package=dummy_task_package_invalid_manifest.as_posix()
+    # Invalid manifest
+    wheel_path = (
+        testdata_path.parent
+        / "v2/fractal_tasks_fail/invalid_manifest"
+        / "dist/fractal_tasks_mock-0.0.1-py3-none-any.whl"
     )
-    debug(dummy_task_package_invalid_manifest)
     async with MockCurrentUser(user_kwargs=dict(is_verified=True)):
-        res = await client.post(f"{PREFIX}/collect/pip/", json=task_collection)
+        res = await client.post(
+            f"{PREFIX}/collect/pip/", json=dict(package=wheel_path.as_posix())
+        )
         debug(res.json())
         assert res.status_code == 422
         assert "not supported" in res.json()["detail"]
 
-    task_collection = dict(
-        package=dummy_task_package_missing_manifest.as_posix()
+    # Missing manifest
+    wheel_path = (
+        testdata_path.parent
+        / "v2/fractal_tasks_fail/missing_manifest"
+        / "dist/fractal_tasks_mock-0.0.1-py3-none-any.whl"
     )
-    debug(dummy_task_package_missing_manifest)
     async with MockCurrentUser(user_kwargs=dict(is_verified=True)):
-        res = await client.post(f"{PREFIX}/collect/pip/", json=task_collection)
+        res = await client.post(
+            f"{PREFIX}/collect/pip/", json=dict(package=wheel_path.as_posix())
+        )
         debug(res.json())
         assert res.status_code == 422
         assert "does not include" in res.json()["detail"]
 
 
-async def test_failed_collection_missing_wheel_file(
-    client,
-    MockCurrentUser,
-    tmp_path: Path,
-):
-    async with MockCurrentUser(user_kwargs=dict(is_verified=True)):
-        res = await client.post(
-            f"{PREFIX}/collect/pip/",
-            json=dict(package=str(tmp_path / "missing_file.whl")),
-        )
-        debug(res)
-        debug(res.json())
-        assert res.status_code == 422
-        assert "does not exist" in str(res.json())
-
-
-async def test_failed_collection_missing_task_file(
+async def test_missing_task_executable(
     client,
     MockCurrentUser,
     override_settings_factory,
@@ -99,96 +108,28 @@ async def test_failed_collection_missing_task_file(
     includes a task for which there does not exist the python script), and
     handle failure.
     """
+    override_settings_factory(FRACTAL_TASKS_DIR=tmp_path)
 
-    override_settings_factory(
-        FRACTAL_TASKS_DIR=(
-            tmp_path / "test_failed_collection_missing_task_file"
-        )
+    wheel_path = (
+        testdata_path.parent
+        / "v2/fractal_tasks_fail/missing_executable"
+        / "dist/fractal_tasks_mock-0.0.1-py3-none-any.whl"
     )
-    debug(tmp_path)
-
-    path = str(
-        testdata_path
-        / "my-tasks-fail/dist/my_tasks_fail-0.1.0-py3-none-any.whl"
-    )
-    task_collection = dict(package=path)
-    debug(task_collection)
 
     async with MockCurrentUser(user_kwargs=dict(is_verified=True)):
-        res = await client.post(f"{PREFIX}/collect/pip/", json=task_collection)
-        debug(res.json())
+        # Trigger collection
+        res = await client.post(
+            f"{PREFIX}/collect/pip/", json=dict(package=wheel_path.as_posix())
+        )
         assert res.status_code == 201
         assert res.json()["data"]["status"] == "pending"
-        state = res.json()
-        data = state["data"]
-        assert "my-tasks-fail" in data["venv_path"]
-
-        res = await client.get(f"{PREFIX}/collect/{state['id']}/?verbose=True")
+        state_id = res.json()["id"]
+        # Inspect collection outcome
+        res = await client.get(f"{PREFIX}/collect/{state_id}/?verbose=True")
         debug(res.json())
-
         assert res.status_code == 200
-        state = res.json()
-        data = state["data"]
-
+        data = res.json()["data"]
         assert "Cannot find executable" in data["info"]
         assert data["status"] == "fail"
         assert data["log"]  # This is because of verbose=True
         assert "fail" in data["log"]
-
-
-async def test_failed_collection_existing_db_tasks(
-    client,
-    MockCurrentUser,
-    dummy_task_package,
-    override_settings_factory,
-    tmp_path: Path,
-):
-    """
-    Catch issue 866:
-
-    * collect tasks
-    * remove venv folders
-    * collect tasks again
-    """
-
-    _FRACTAL_TASKS_DIR = (
-        tmp_path / "test_collection_api_local_package_with_extras"
-    )
-    override_settings_factory(FRACTAL_TASKS_DIR=_FRACTAL_TASKS_DIR)
-
-    async with MockCurrentUser(user_kwargs=dict(is_verified=True)):
-        # First task collection
-        res = await client.post(
-            f"{PREFIX}/collect/pip/",
-            json=dict(
-                package=dummy_task_package.as_posix(),
-                package_extras="my_extra",
-            ),
-        )
-        collection_state_id = res.json()["id"]
-        venv_path = res.json()["data"]["venv_path"]
-        debug(collection_state_id)
-        debug(venv_path)
-        assert res.status_code == 201
-
-        # Check that task collection is complete
-        res = await client.get(f"{PREFIX}/collect/{collection_state_id}/")
-        assert res.status_code == 200
-        debug(res.json())
-        assert res.json()["data"]["status"] == "OK"
-
-        # Remove task folder from disk
-        shutil.rmtree(_FRACTAL_TASKS_DIR / venv_path)
-
-        # Second task collection
-        res = await client.post(
-            f"{PREFIX}/collect/pip/",
-            json=dict(
-                package=dummy_task_package.as_posix(),
-                package_extras="my_extra",
-            ),
-        )
-        assert res.status_code == 422
-        debug(res.json())
-        assert "Task with source" in res.json()["detail"]
-        assert "already exists in the database" in res.json()["detail"]
