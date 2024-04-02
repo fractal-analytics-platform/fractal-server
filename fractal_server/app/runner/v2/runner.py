ec248c72f1f160a90f6e632e63fbdeae84d9166e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-04-02 09:51:45 +0200

    Re-implement on-disk caching of temporary dataset attributes in v2 runner, and its handling upon failures

--- fractal_server/app/runner/v2/runner.py
+++ fractal_server/app/runner/v2/runner.py
@@ -1,3 +1,4 @@
+import json
 from concurrent.futures import ThreadPoolExecutor
 from copy import copy
 from copy import deepcopy
@@ -10,6 +11,9 @@ from ....images import SingleImage
 from ....images.tools import _filter_image_list
 from ....images.tools import find_image_by_path
 from ....images.tools import match_filter
+from ..filenames import FILTERS_FILENAME
+from ..filenames import HISTORY_FILENAME
+from ..filenames import IMAGES_FILENAME
 from .runner_functions import no_op_submit_setup_call
 from .runner_functions import run_v1_task_parallel
 from .runner_functions import run_v2_task_compound
@@ -33,10 +37,14 @@ def execute_tasks_v2(
     submit_setup_call: Callable = no_op_submit_setup_call,
 ) -> DatasetV2:
 
-    if not workflow_dir.exists():
+    if not workflow_dir.exists():  # FIXME: this should have already happened
         workflow_dir.mkdir()
 
-    tmp_dataset = deepcopy(dataset)
+    # Initialize local dataset attributes
+    zarr_dir = dataset.zarr_dir
+    tmp_images = deepcopy(dataset.images)
+    tmp_filters = deepcopy(dataset.filters)
+    tmp_history = []
 
     for wftask in wf_task_list:
         task = wftask.task
@@ -44,18 +52,18 @@ def execute_tasks_v2(
         # PRE TASK EXECUTION
 
         # Get filtered images
-        pre_type_filters = copy(dataset.filters["types"])
+        pre_type_filters = copy(tmp_filters["types"])
         pre_type_filters.update(wftask.input_filters["types"])
-        pre_attribute_filters = copy(dataset.filters["attributes"])
+        pre_attribute_filters = copy(tmp_filters["attributes"])
         pre_attribute_filters.update(wftask.input_filters["attributes"])
         filtered_images = _filter_image_list(
-            images=tmp_dataset.images,
+            images=tmp_images,
             filters=Filters(
                 types=pre_type_filters,
                 attributes=pre_attribute_filters,
             ),
         )
-        # Verify that filtered images comply with output types
+        # Verify that filtered images comply with task input_types
         for image in filtered_images:
             if not match_filter(image, Filters(types=task.input_types)):
                 raise ValueError(
@@ -63,12 +71,12 @@ def execute_tasks_v2(
                     f"not comply with {task.input_types=}."
                 )
 
-        # TASK EXECUTION (V2 or V1)
+        # TASK EXECUTION (V2)
         if not wftask.is_legacy_task:
             if task.type == "non_parallel":
                 current_task_output = run_v2_task_non_parallel(
                     images=filtered_images,
-                    zarr_dir=tmp_dataset.zarr_dir,
+                    zarr_dir=zarr_dir,
                     wftask=wftask,
                     task=wftask.task,
                     workflow_dir=workflow_dir,
@@ -91,7 +99,7 @@ def execute_tasks_v2(
             elif task.type == "compound":
                 current_task_output = run_v2_task_compound(
                     images=filtered_images,
-                    zarr_dir=tmp_dataset.zarr_dir,
+                    zarr_dir=zarr_dir,
                     wftask=wftask,
                     task=wftask.task,
                     workflow_dir=workflow_dir,
@@ -102,9 +110,8 @@ def execute_tasks_v2(
                 )
             else:
                 raise ValueError(f"Invalid {task.type=}.")
-
+        # TASK EXECUTION (V1)
         else:
-
             current_task_output = run_v1_task_parallel(
                 images=filtered_images,
                 wftask=wftask,
@@ -121,7 +128,7 @@ def execute_tasks_v2(
         for image_obj in current_task_output.image_list_updates:
             image = image_obj.dict()
             # Edit existing image
-            if image["path"] in tmp_dataset.image_paths:
+            if image["path"] in [_image["path"] for _image in tmp_images]:
                 if (
                     image["origin"] is not None
                     and image["origin"] != image["path"]
@@ -131,11 +138,14 @@ def execute_tasks_v2(
                         f"and {image['origin']=}."
                     )
                 image_search = find_image_by_path(
-                    images=tmp_dataset.images,
+                    images=tmp_images,
                     path=image["path"],
                 )
                 if image_search is None:
-                    raise ValueError("This should have not happened")
+                    raise ValueError(
+                        f"Image with path {image['path']} not found, while "
+                        "updating image list."
+                    )
                 original_img = image_search["image"]
                 original_index = image_search["index"]
                 updated_attributes = copy(original_img["attributes"])
@@ -147,16 +157,14 @@ def execute_tasks_v2(
                 updated_types.update(task.output_types)
 
                 # Update image in the dataset image list
-                tmp_dataset.images[original_index][
-                    "attributes"
-                ] = updated_attributes
-                tmp_dataset.images[original_index]["types"] = updated_types
+                tmp_images[original_index]["attributes"] = updated_attributes
+                tmp_images[original_index]["types"] = updated_types
             # Add new image
             else:
                 # Check that image['path'] is relative to zarr_dir
-                if not image["path"].startswith(tmp_dataset.zarr_dir):
+                if not image["path"].startswith(zarr_dir):
                     raise ValueError(
-                        f"{tmp_dataset.zarr_dir} is not a parent directory of "
+                        f"{zarr_dir} is not a parent directory of "
                         f"{image['path']}"
                     )
                 # Propagate attributes and types from `origin` (if any)
@@ -164,7 +172,7 @@ def execute_tasks_v2(
                 updated_types = {}
                 if image["origin"] is not None:
                     image_search = find_image_by_path(
-                        images=tmp_dataset.images,
+                        images=tmp_images,
                         path=image["origin"],
                     )
                     if image_search is not None:
@@ -182,26 +190,26 @@ def execute_tasks_v2(
                     types=updated_types,
                 )
                 # Add image into the dataset image list
-                tmp_dataset.images.append(new_image.dict())
+                tmp_images.append(new_image.dict())
 
-        # Remove images from Dataset.images
+        # Remove images from tmp_images
         for image in current_task_output.image_list_removals:
             image_search = find_image_by_path(
-                images=tmp_dataset.images, path=image["path"]
+                images=tmp_images, path=image["path"]
             )
             if image_search["index"] is None:
                 raise
             else:
-                tmp_dataset.images.pop(image_search["index"])
+                tmp_images.pop(image_search["index"])
 
-        # Update Dataset.filters.attributes:
+        # Update filters.attributes:
         # current + (task_output: not really, in current examples..)
         if current_task_output.filters is not None:
-            tmp_dataset.filters["attributes"].update(
+            tmp_filters["attributes"].update(
                 current_task_output.filters.attributes
             )
 
-        # Update Dataset.filters.types: current + (task_output + task_manifest)
+        # Update filters.types: current + (task_output + task_manifest)
         if wftask.is_legacy_task:
             types_from_manifest = {}
         else:
@@ -219,19 +227,38 @@ def execute_tasks_v2(
                 "Both task and task manifest did set the same"
                 f"output type. Overlapping keys: {overlap}."
             )
-        # Update Dataset.filters.types
-        tmp_dataset.filters["types"].update(types_from_manifest)
-        tmp_dataset.filters["types"].update(types_from_task)
+        # Update filters.types
+        tmp_filters["types"].update(types_from_manifest)
+        tmp_filters["types"].update(types_from_task)
 
-        # Update Dataset.history (based on _DatasetHistoryItemV2)
+        # Update history (based on _DatasetHistoryItemV2)
         history_item = _DatasetHistoryItemV2(
             workflowtask=wftask,
             status=WorkflowTaskStatusTypeV2.DONE,
             parallelization=dict(
-                task_type=wftask.task.type,
+                # task_type=wftask.task.type,  # FIXME: breaks for V1 tasks
                 # component_list=fil, #FIXME
             ),
         ).dict()
-        tmp_dataset.history.append(history_item)
+        tmp_history.append(history_item)
 
-    return tmp_dataset
+        # Write current dataset attributes (history, images, filters) into
+        # temporary files which can be used (1) to retrieve the latest state
+        # when the job fails, (2) from within endpoints that need up-to-date
+        # information
+        with open(workflow_dir / HISTORY_FILENAME, "w") as f:
+            json.dump(tmp_history, f, indent=2)
+        with open(workflow_dir / FILTERS_FILENAME, "w") as f:
+            json.dump(tmp_filters, f, indent=2)
+        with open(workflow_dir / IMAGES_FILENAME, "w") as f:
+            json.dump(tmp_images, f, indent=2)
+
+    # NOTE: tmp_history only contains the newly-added history items (to be
+    # appended to the original history), while tmp_filters and tmp_images
+    # represent the new attributes (to replace the original ones)
+    result = dict(
+        history=tmp_history,
+        filters=tmp_filters,
+        images=tmp_images,
+    )
+    return result
