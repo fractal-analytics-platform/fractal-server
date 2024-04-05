664e013fd109f03b8efbee0d1b8aa7887aaf9812
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-04-05 10:35:25 +0200

    Add test_dummy_remove_images

--- tests/v2/04_runner/test_dummy_examples.py
+++ tests/v2/04_runner/test_dummy_examples.py
@@ -13,12 +13,12 @@ from fractal_server.app.runner.v2.runner import execute_tasks_v2
 def test_dummy_insert_single_image(
     tmp_path: Path, executor: Executor, fractal_tasks_mock_venv
 ):
+    # Preliminary setup
     execute_tasks_v2_args = dict(
         executor=executor,
         workflow_dir=tmp_path / "job_dir",
         workflow_dir_user=tmp_path / "job_dir",
     )
-
     zarr_dir = (tmp_path / "zarr_dir").as_posix().rstrip("/")
 
     # Run successfully on an empty dataset
@@ -66,3 +66,60 @@ def test_dummy_insert_single_image(
     error_msg = str(e.value)
     assert "is not a parent directory" in error_msg
     assert zarr_dir in error_msg
+
+
+def test_dummy_remove_images(
+    tmp_path: Path, executor: Executor, fractal_tasks_mock_venv
+):
+    # Preliminary setup
+    execute_tasks_v2_args = dict(
+        executor=executor,
+        workflow_dir=tmp_path / "job_dir",
+        workflow_dir_user=tmp_path / "job_dir",
+    )
+    zarr_dir = (tmp_path / "zarr_dir").as_posix().rstrip("/")
+
+    # Run successfully on a dataset which includes the images to be removed
+    dataset_pre = DatasetV2Mock(
+        name="dataset",
+        zarr_dir=zarr_dir,
+        images=[
+            dict(path=Path(zarr_dir, str(index)).as_posix())
+            for index in [0, 1, 2]
+        ],
+    )
+    dataset_attrs = execute_tasks_v2(
+        wf_task_list=[
+            WorkflowTaskV2Mock(
+                task=fractal_tasks_mock_venv["dummy_remove_images"],
+                id=0,
+                order=0,
+            )
+        ],
+        dataset=dataset_pre,
+        **execute_tasks_v2_args,
+    )
+    debug(dataset_attrs)
+
+    # Fail when removing images that do not exist
+    dataset_pre = DatasetV2Mock(
+        name="dataset",
+        zarr_dir=zarr_dir,
+    )
+    with pytest.raises(ValueError) as e:
+        execute_tasks_v2(
+            wf_task_list=[
+                WorkflowTaskV2Mock(
+                    task=fractal_tasks_mock_venv["dummy_remove_images"],
+                    id=1,
+                    order=1,
+                    args_non_parallel=dict(
+                        more_paths=[Path(zarr_dir, "missing-image").as_posix()]
+                    ),
+                )
+            ],
+            dataset=dataset_pre,
+            **execute_tasks_v2_args,
+        )
+    error_msg = str(e.value)
+    assert "Cannot remove missing image" in error_msg
