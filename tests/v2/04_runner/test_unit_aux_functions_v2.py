b4da5dae3f0d884a152d1dc1cb80a88c9ca63334
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-04-08 14:32:56 +0200

    Add `test_check_zarr_urls_are_unique`

--- tests/v2/04_runner/test_unit_aux_functions_v2.py
+++ tests/v2/04_runner/test_unit_aux_functions_v2.py
@@ -1,7 +1,9 @@
+import pytest
 from devtools import debug
 
 from fractal_server.app.runner.v2.deduplicate_list import deduplicate_list
 from fractal_server.app.runner.v2.task_interface import InitArgsModel
+from fractal_server.app.runner.v2.task_interface import TaskOutput
 
 
 def test_deduplicate_list_of_dicts():
@@ -23,3 +25,21 @@ def test_deduplicate_list_of_dicts():
     new = deduplicate_list(old)
     debug(new)
     assert len(new) == 2
+
+
+def test_check_zarr_urls_are_unique():
+    t = TaskOutput(image_list_updates=[dict(zarr_url="a"), dict(zarr_url="b")])
+    t.check_zarr_urls_are_unique()
+
+    t = TaskOutput(image_list_updates=[dict(zarr_url="a"), dict(zarr_url="a")])
+    with pytest.raises(ValueError) as e:
+        t.check_zarr_urls_are_unique()
+    debug(str(e.value))
+
+    t = TaskOutput(
+        image_list_updates=[dict(zarr_url="a"), dict(zarr_url="b")],
+        image_list_removals=["a"],
+    )
+    with pytest.raises(ValueError) as e:
+        t.check_zarr_urls_are_unique()
+    debug(str(e.value))
