8f11afbb52656af3408d0111e8bb9c1cf56f5390
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-04-09 10:31:34 +0200

    test

--- tests/v2/01_schemas/test_unit_json_schemas.py
+++ tests/v2/01_schemas/test_unit_json_schemas.py
@@ -1,8 +1,8 @@
 import json
 from pathlib import Path
 
-import fractal_server.app.schemas.v1 as v1
-from fractal_server.app.schemas.v1.manifest import ManifestV1
+import fractal_server.app.schemas.v2 as v2
+from fractal_server.app.schemas.v2.manifest import ManifestV2
 
 
 def test_ManifestV1_jsonschema():
@@ -11,10 +11,10 @@ def test_ManifestV1_jsonschema():
     with the one currently present in the repository.
     """
     json_schema_path = (
-        Path(v1.__file__).parent.parent.parent.parent
-        / "json_schemas/manifest_v1.json"
+        Path(v2.__file__).parent.parent.parent.parent
+        / "json_schemas/manifest_v2.json"
     )
     with json_schema_path.open("r") as f:
         current_schema = json.load(f)
-    new_schema = ManifestV1.schema()
+    new_schema = ManifestV2.schema()
     assert new_schema == current_schema
