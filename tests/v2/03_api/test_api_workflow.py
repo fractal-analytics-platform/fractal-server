7fdde73e641f86a4edc94a3f0818c3aa3d3ee18b
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-04-10 11:12:48 +0200

    test patch without args schema

--- tests/v2/03_api/test_api_workflow.py
+++ tests/v2/03_api/test_api_workflow.py
@@ -554,7 +554,8 @@ async def test_patch_workflow_task(
         assert workflow["task_list"][0]["args_non_parallel"] is None
 
         payload = dict(
-            args_non_parallel={"a": 123, "d": 321},
+            args_non_parallel={"a": 111, "b": 222},
+            args_parallel={"c": 333, "d": 444},
             meta_parallel={"executor": "cpu-low"},
             input_filters={
                 "attributes": {"a": "b", "c": "d"},
@@ -574,6 +575,9 @@ async def test_patch_workflow_task(
             == payload["args_non_parallel"]
         )
         assert (
+            patched_workflow_task["args_parallel"] == payload["args_parallel"]
+        )
+        assert (
             patched_workflow_task["meta_parallel"] == payload["meta_parallel"]
         )
         assert (
@@ -581,7 +585,10 @@ async def test_patch_workflow_task(
         )
         assert res.status_code == 200
 
-        payload_up = dict(args_non_parallel={"a": {"c": 43}, "b": 123})
+        payload_up = dict(
+            args_non_parallel={"a": {"b": 43}, "c": 2},
+            args_parallel={"x": "y"},
+        )
         res = await client.patch(
             f"{PREFIX}/project/{project.id}/workflow/{workflow['id']}/"
             f"wftask/{workflow['task_list'][0]['id']}/",
@@ -591,6 +598,10 @@ async def test_patch_workflow_task(
         assert (
             patched_workflow_task_up["args_non_parallel"]
             == payload_up["args_non_parallel"]
+        )
+        assert (
+            patched_workflow_task_up["args_parallel"]
+            == payload_up["args_parallel"]
         )
         assert res.status_code == 200
 
