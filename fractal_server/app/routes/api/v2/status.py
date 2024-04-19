6386f9f394b2496eab13ed07f4ed5088b50c1c8e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-04-19 08:47:37 +0200

    Clean up status endpoint docstring/comments

--- fractal_server/app/routes/api/v2/status.py
+++ fractal_server/app/routes/api/v2/status.py
@@ -34,7 +34,13 @@ async def get_workflowtask_status(
     db: AsyncSession = Depends(get_async_db),
 ) -> Optional[StatusReadV2]:
     """
-    Extract the status of all `WorkflowTask`s that ran on a given `DatasetV2`.
+    Extract the status of all `WorkflowTaskV2` of a given `WorkflowV2` that ran
+    on a given `DatasetV2`.
+
+    *NOTE*: the current endpoint is not guaranteed to provide consistent
+    results if the workflow task list is modified in a non-trivial way
+    (that is, by adding intermediate tasks, removing tasks, or changing their
+    order). See fractal-server GitHub issues: 793, 1083.
     """
     # Get the dataset DB entry
     output = await _get_dataset_check_owner(
@@ -52,10 +58,6 @@ async def get_workflowtask_status(
         user_id=user.id,
         db=db,
     )
-    if len(dataset.history) > 0:
-        last_valid_wftask_id = dataset.history[-1]["workflowtask"]["id"]
-    else:
-        last_valid_wftask_id = None
 
     # Check whether there exists a submitted job associated to this
     # workflow/dataset pair. If it does exist, it will be used later.
@@ -90,10 +92,15 @@ async def get_workflowtask_status(
         wftask_status = history_item["status"]
         workflow_tasks_status_dict[wftask_id] = wftask_status
 
-    # If a job is running, then gather more up-to-date information
-    if running_job is not None:
-
-        # FIXME use running_job.workflow_dump
+    if running_job is None:
+        # If no job is running, the chronological-last history item is also the
+        # positional-last workflow task to be included in the response.
+        if len(dataset.history) > 0:
+            last_valid_wftask_id = dataset.history[-1]["workflowtask"]["id"]
+        else:
+            last_valid_wftask_id = None
+    else:
+        # If a job is running, then gather more up-to-date information
 
         # Mid priority: Set all WorkflowTask's that are part of the running job
         # as "submitted"
@@ -104,9 +111,8 @@ async def get_workflowtask_status(
                 wftask.id
             ] = WorkflowTaskStatusTypeV2.SUBMITTED
 
-        # debug(end)
-        # debug(workflow.task_list)
-
+        # The last workflow task that is included in the submitted job is also
+        # the positional-last workflow task to be included in the response.
         last_valid_wftask_id = workflow.task_list[end - 1]
 
         # Highest priority: Read status updates coming from the running-job
@@ -123,23 +129,22 @@ async def get_workflowtask_status(
             wftask_status = history_item["status"]
             workflow_tasks_status_dict[wftask_id] = wftask_status
 
+    # Based on previously-gathered information, clean up the response body
     clean_workflow_tasks_status_dict = {}
-
     for wf_task in workflow.task_list:
-
         wf_task_status = workflow_tasks_status_dict.get(wf_task.id)
-
         if wf_task_status is None:
+            # If a wftask ID was not found, ignore it and continue
             continue
-
         clean_workflow_tasks_status_dict[wf_task.id] = wf_task_status
-
         if wf_task_status == WorkflowTaskStatusTypeV2.FAILED:
+            # Starting from the beginning of `workflow.task_list`, stop the
+            # first time that you hit a failed job
             break
-
         if wf_task.id == last_valid_wftask_id:
+            # Starting from the beginning of `workflow.task_list`, stop the
+            # first time that you hit `last_valid_wftask_id``
             break
 
     response_body = StatusReadV2(status=clean_workflow_tasks_status_dict)
-
     return response_body
