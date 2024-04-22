cd2a77d2bc4e6fdd36749122704f1a2b097d001a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-04-22 14:42:21 +0200

    Re-export project models from main folder

--- fractal_server/app/models/__init__.py
+++ fractal_server/app/models/__init__.py
@@ -2,3 +2,8 @@
 `models` module
 """
 from .security import *  # noqa: F401, F403
+from .v1 import Project  # noqa: F401
+from .v2 import ProjectV2  # noqa: F401
+
+# We include the project models to avoid issues with LinkUserProject
+# (sometimes taking place in alembic autogenerate)
