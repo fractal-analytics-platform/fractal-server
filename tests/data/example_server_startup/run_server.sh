9d5bbb7329f33e8662c238ec6e203ca73de00087
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-04-30 16:31:04 +0200

    Include gunicorn command in test script

--- tests/data/example_server_startup/run_server.sh
+++ tests/data/example_server_startup/run_server.sh
@@ -5,3 +5,4 @@ poetry run fractalctl set-db
 
 # Start the server
 poetry run fractalctl start --port 8000
+# poetry run gunicorn fractal_server.main:app --workers 8 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --access-logfile fractal-server.out --error-logfile fractal-server.err
