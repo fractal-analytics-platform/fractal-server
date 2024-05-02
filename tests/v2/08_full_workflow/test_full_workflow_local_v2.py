6466468f137279199edcfc2c041bdb3b917d1f83
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-05-02 10:59:23 +0200

    splits tests full_workflow into local/slurm files

---
+++ tests/v2/08_full_workflow/test_full_workflow_local_v2.py
@@ -0,0 +1,128 @@
+import pytest
+from common.common import failing_workflow_UnknownError
+from common.common import full_workflow
+from common.common import full_workflow_TaskExecutionError
+from common.common import non_executable_task_command_local
+
+
+async def test_full_workflow_local(
+    client,
+    MockCurrentUser,
+    tmp777_path,
+    project_factory_v2,
+    dataset_factory_v2,
+    workflow_factory_v2,
+    override_settings_factory,
+    tmp_path_factory,
+    fractal_tasks_mock,  # needed
+):
+    # Use a session-scoped FRACTAL_TASKS_DIR folder
+    override_settings_factory(
+        FRACTAL_RUNNER_BACKEND="local",
+        FRACTAL_RUNNER_WORKING_BASE_DIR=tmp777_path / "artifacts-local",
+        FRACTAL_TASKS_DIR=tmp_path_factory.getbasetemp() / "FRACTAL_TASKS_DIR",
+    )
+    await full_workflow(
+        MockCurrentUser=MockCurrentUser,
+        project_factory_v2=project_factory_v2,
+        dataset_factory_v2=dataset_factory_v2,
+        workflow_factory_v2=workflow_factory_v2,
+        client=client,
+    )
+
+
+async def test_full_workflow_TaskExecutionError(
+    client,
+    MockCurrentUser,
+    tmp777_path,
+    project_factory_v2,
+    dataset_factory_v2,
+    workflow_factory_v2,
+    override_settings_factory,
+    tmp_path_factory,
+    fractal_tasks_mock,  # needed
+):
+    """ "
+    Run a workflow made of three tasks, two successful tasks and one
+    that raises an error.
+    """
+
+    # Use a session-scoped FRACTAL_TASKS_DIR folder
+    override_settings_factory(
+        FRACTAL_RUNNER_BACKEND="local",
+        FRACTAL_RUNNER_WORKING_BASE_DIR=tmp777_path / "artifacts-local",
+        FRACTAL_TASKS_DIR=tmp_path_factory.getbasetemp() / "FRACTAL_TASKS_DIR",
+    )
+    await full_workflow_TaskExecutionError(
+        MockCurrentUser=MockCurrentUser,
+        project_factory_v2=project_factory_v2,
+        dataset_factory_v2=dataset_factory_v2,
+        workflow_factory_v2=workflow_factory_v2,
+        client=client,
+    )
+
+
+async def test_non_executable_task_command_local(
+    client,
+    MockCurrentUser,
+    testdata_path,
+    tmp777_path,
+    task_factory_v2,
+    project_factory_v2,
+    dataset_factory_v2,
+    workflow_factory_v2,
+    override_settings_factory,
+):
+    """
+    Execute a workflow with a task which has an invalid `command` (i.e. it is
+    not executable).
+    """
+    override_settings_factory(
+        FRACTAL_RUNNER_BACKEND="local",
+        FRACTAL_RUNNER_WORKING_BASE_DIR=tmp777_path / "artifacts-local",
+    )
+    await non_executable_task_command_local(
+        MockCurrentUser=MockCurrentUser,
+        client=client,
+        testdata_path=testdata_path,
+        project_factory_v2=project_factory_v2,
+        workflow_factory_v2=workflow_factory_v2,
+        dataset_factory_v2=dataset_factory_v2,
+        task_factory_v2=task_factory_v2,
+    )
+
+
+@pytest.mark.parametrize("legacy", [False, True])
+async def test_failing_workflow_UnknownError_local(
+    legacy: bool,
+    client,
+    MockCurrentUser,
+    tmp777_path,
+    project_factory_v2,
+    dataset_factory_v2,
+    workflow_factory_v2,
+    task_factory_v2,
+    task_factory,
+    monkeypatch,
+    override_settings_factory,
+):
+    """
+    Submit a workflow that fails with some unrecognized exception (due
+    to a monkey-patched function in the runner).
+    """
+
+    override_settings_factory(
+        FRACTAL_RUNNER_BACKEND="local",
+        FRACTAL_RUNNER_WORKING_BASE_DIR=tmp777_path / "artifacts-local",
+    )
+    await failing_workflow_UnknownError(
+        MockCurrentUser=MockCurrentUser,
+        client=client,
+        monkeypatch=monkeypatch,
+        legacy=legacy,
+        project_factory_v2=project_factory_v2,
+        dataset_factory_v2=dataset_factory_v2,
+        workflow_factory_v2=workflow_factory_v2,
+        task_factory=task_factory,
+        task_factory_v2=task_factory_v2,
+    )
