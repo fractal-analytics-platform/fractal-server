e65578d7565f5155b03bb634ca67cda4ff39d773
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-05-03 08:26:01 +0200

    Fix update-db script, to include both v1 and v2 tables

--- fractal_server/data_migrations/current.py
+++ fractal_server/data_migrations/current.py
@@ -6,11 +6,10 @@ from sqlalchemy.orm.attributes import flag_modified
 
 import fractal_server
 from fractal_server.app.db import get_sync_db
+from fractal_server.app.models.v1 import ApplyWorkflow
 from fractal_server.app.models.v2 import JobV2
+from fractal_server.app.schemas.v1 import ApplyWorkflowReadV1
 from fractal_server.app.schemas.v2 import JobReadV2
-
-
-KEYS = ["images", "history"]
 
 
 def fix_db():
@@ -31,15 +30,39 @@ def fix_db():
 
     with next(get_sync_db()) as db:
 
+        # V1 jobs
+        stm = select(ApplyWorkflow)
+        jobs_v1 = db.execute(stm).scalars().all()
+        for job_v1 in sorted(jobs_v1, key=lambda x: x.id):
+            for KEY in ["history"]:
+                logger.warning(
+                    f"Now removing {KEY} from `input/output_dataset_dump`, "
+                    f"for appplyworkflow.id={job_v1.id}."
+                )
+                if KEY in job_v1.input_dataset_dump.keys():
+                    job_v1.input_dataset_dump.pop(KEY)
+                if KEY in job_v1.output_dataset_dump.keys():
+                    job_v1.output_dataset_dump.pop(KEY)
+            flag_modified(job_v1)
+            db.add(job_v1)
+            db.commit()
+            db.refresh(job_v1)
+            db.expunge(job_v1)
+            logger.warning(
+                f"Now validating obv2.id={job_v1.id} with ApplyWorkflowReadV1."
+            )
+            ApplyWorkflowReadV1(**job_v1.model_dump())
+
+        # V2 jobs
         stm = select(JobV2)
         jobs_v2 = db.execute(stm).scalars().all()
         for job_v2 in sorted(jobs_v2, key=lambda x: x.id):
-            for KEY in KEYS:
+            for KEY in ["history", "images"]:
                 logger.warning(
                     f"Now removing {KEY} from `dataset_dump`, "
                     f"for jobv2.id={job_v2.id}."
                 )
-                if KEY in job_v2.input_dataset_dump.keys():
+                if KEY in job_v2.dataset_dump.keys():
                     job_v2.dataset_dump.pop(KEY)
             flag_modified(job_v2)
             db.add(job_v2)
