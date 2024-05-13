07320564497ad7eb3128fc4a983dfb06a1872c30
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-05-13 17:48:37 +0200

    expose LinkUserProject/V2 from v1/v2 modules

--- fractal_server/app/models/v1/__init__.py
+++ fractal_server/app/models/v1/__init__.py
@@ -1,12 +1,13 @@
 """
 `models` module
 """
-from .dataset import Dataset  # noqa: F401
-from .dataset import Resource  # noqa: F401
-from .job import ApplyWorkflow  # noqa: F403, F401
-from .job import JobStatusTypeV1  # noqa: F401, F403
-from .project import Project  # noqa: F403, F401
-from .state import State  # noqa: F403, F401
-from .task import Task  # noqa: F403, F401
-from .workflow import Workflow  # noqa: F401, F403
-from .workflow import WorkflowTask  # noqa: F401, F403
+from ..linkuserproject import LinkUserProject  # noqa F401
+from .dataset import Dataset  # noqa F401
+from .dataset import Resource  # noqa F401
+from .job import ApplyWorkflow  # noqa F401
+from .job import JobStatusTypeV1  # noqa F401
+from .project import Project  # noqa F401
+from .state import State  # noqa F401
+from .task import Task  # noqa F401
+from .workflow import Workflow  # noqa F401
+from .workflow import WorkflowTask  # noqa F401
