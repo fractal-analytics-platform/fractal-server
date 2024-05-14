a24e1d4cd5a37eeca7bcb10e56605d1432c3eac1
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-05-14 12:22:19 +0200

    dot in imports

--- scripts/client/__init__.py
+++ scripts/client/__init__.py
@@ -1,2 +1,2 @@
-from client import DEFAULT_CREDENTIALS  # noqa F401
-from client import FractalClient  # noqa F401
+from .client import DEFAULT_CREDENTIALS  # noqa F401
+from .client import FractalClient  # noqa F401
