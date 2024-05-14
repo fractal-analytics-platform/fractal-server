f8682b9513cde965dc53754df742cd92ddec26b8
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-05-14 12:17:12 +0200

    fix imports

--- examples/example_1.py
+++ examples/example_1.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 sys.path.append((Path(__file__).parents[1] / "scripts/populate_db").as_posix())
 
-from fractal_client import DEFAULT_CREDENTIALS
-from fractal_client import FractalClient
+from scripts.client import DEFAULT_CREDENTIALS
+from scripts.client import FractalClient
 from passlib.context import CryptContext
 import os
 
