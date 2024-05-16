6a920b878b5274f1bf3addc5c4549de4d3f1db21
mfranzon <franzonmarco94@gmail.com>
2024-05-16 15:42:42 +0200

    remove global variable from app

--- scripts/mvp_gunicorn/main.py
+++ scripts/mvp_gunicorn/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
+from fastapi import Request
 
 app = FastAPI()
-app.state.jobs = []
 
 
 @app.get("/status/")
-async def get_status():
-    return {"jobs": app.state.jobs}
+async def get_status(request: Request):
+
+    if not hasattr(request.app.state, "jobs"):
+        jobs = []
+    else:
+        jobs = request.app.state.jobs
+
+    return {"jobs": jobs}
 
 
 @app.post("/add-status/")
-async def post_status(id: int):
-    app.state.jobs.append(id)
+async def post_status(id: int, request: Request):
+
+    if not hasattr(request.app.state, "jobs"):
+        request.app.state.jobs = []
+        app.state.jobs.append(id)
+    else:
+        app.state.jobs.append(id)
+
     return {"jobs": app.state.jobs}
