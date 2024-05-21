2368c1cd1e603e579f067788e24cdadac5c38ef8
mfranzon <franzonmarco94@gmail.com>
2024-05-21 14:37:44 +0200

    [skip ci] add sigabrt handling

--- scripts/mvp_gunicorn/main.py
+++ scripts/mvp_gunicorn/main.py
@@ -1,19 +1,28 @@
 import json
+import os
+import signal
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi import Request
 
+file_path = "dump.txt"
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    print("STARTUP")
+    if not os.path.isfile(file_path):
+        with open(file_path, "w") as f:
+            db = f.write(json.dumps({}))
     app.state.jobs = {}
     yield
-    with open("dump.txt", "r") as f:
+    print("SHUTDOWN")
+    with open(file_path, "r") as f:
         db = json.loads(f.read())
     for key in app.state.jobs.keys():
         db.update({key: "FAILED"})
-    with open("dump.txt", "w") as f:
+    with open(file_path, "w") as f:
         f.write(json.dumps(db))
 
 
@@ -23,10 +32,7 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/status/")
 async def get_status(request: Request):
 
-    if not hasattr(request.app.state, "jobs"):
-        jobs = {}
-    else:
-        jobs = request.app.state.jobs
+    jobs = request.app.state.jobs
 
     return {"jobs": jobs}
 
@@ -34,28 +40,24 @@ async def get_status(request: Request):
 @app.post("/add-status/")
 async def post_status(id: int, request: Request):
 
-    if not hasattr(request.app.state, "jobs"):
-        request.app.state.jobs = {}
-        request.app.state.jobs[id] = "RUNNING"
+    request.app.state.jobs[id] = "RUNNING"
 
-        with open("dump.txt", "r") as f:
-            db = json.loads(f.read())
+    with open(file_path, "r") as f:
+        db = json.loads(f.read())
+    print(db)
 
-        db.update({id: request.app.state.jobs[id]})
+    db.update({id: request.app.state.jobs[id]})
 
-        with open("dump.txt", "w") as f:
-            f.write(json.dumps(db))
-
-    else:
-        request.app.state.jobs[id] = "RUNNING"
-
-        with open("dump.txt", "r") as f:
-            db = json.loads(f.read())
-        print(db)
-
-        db.update({id: request.app.state.jobs[id]})
-
-        with open("dump.txt", "w") as f:
-            f.write(json.dumps(db))
+    with open(file_path, "w") as f:
+        f.write(json.dumps(db))
 
     return {"jobs": app.state.jobs}
+
+
+# SIGABORT HANDLING
+def handle_timeout(signum, frame):
+    print(f"RECEIVERD SIGNAL {signum}")
+    os.kill(os.getpid(), signal.SIGTERM)
+
+
+signal.signal(signal.SIGABRT, handle_timeout)
