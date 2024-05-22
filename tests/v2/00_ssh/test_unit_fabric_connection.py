1d418ff4cde784c9c26f4a8ced804fb07df334b8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-05-22 15:12:48 +0200

    Remove old debugging from test

--- tests/v2/00_ssh/test_unit_fabric_connection.py
+++ tests/v2/00_ssh/test_unit_fabric_connection.py
@@ -1,44 +1,16 @@
 import io
-import shlex
-import subprocess
 
 from fabric.connection import Connection
 
 
-def _run_locally(cmd):
-    print("COMMAND:")
-    print(cmd)
-    res = subprocess.run(
-        shlex.split(cmd),
-        capture_output=True,
-        encoding="utf-8",
-    )
-    print("RETCODE:")
-    print(res.returncode)
-    print("STDOUT:")
-    print(res.stdout)
-    print("STDERR:")
-    print(res.stderr)
-    print("subprocess SSH - end")
-
-
 def test_unit_fabric_connection(
-    slurmlogin_ip, slurmlogin_container, monkeypatch
+    slurmlogin_ip, ssh_alive, slurmlogin_container, monkeypatch
 ):
     """
     Test both the pytest-docker setup and the use of a `fabric` connection, by
     running the `hostname` over SSH.
     """
     print(f"{slurmlogin_ip=}")
-
-    cmd = f"docker exec --user root {slurmlogin_container} ip a"
-    _run_locally(cmd)
-    cmd = f"docker exec --user root {slurmlogin_container} service ssh status"
-    _run_locally(cmd)
-    cmd = f"docker exec --user root {slurmlogin_container} lsof -i -n -P"
-    _run_locally(cmd)
-    cmd = f"docker exec --user root {slurmlogin_container} grep Port /etc/ssh/sshd_config"
-    _run_locally(cmd)
 
     command = "hostname"
     print(f"Now run {command=} at {slurmlogin_ip=}")
