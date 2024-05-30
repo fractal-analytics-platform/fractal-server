6b3acae67699aec69584ec563b2dd23b0e6cda95
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-05-30 15:20:06 +0200

    Fix use of wrong `app.state` attribute

--- fractal_server/app/routes/api/v1/project.py
+++ fractal_server/app/routes/api/v1/project.py
@@ -260,7 +260,7 @@ async def apply_workflow(
         new_jobs_list = await clean_app_job_list_v1(
             db, request.app.state.jobsV1
         )
-        request.app.state.jobs = new_jobs_list
+        request.app.state.jobsV1 = new_jobs_list
 
     output = await _get_dataset_check_owner(
         project_id=project_id,
