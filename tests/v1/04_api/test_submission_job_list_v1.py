e8b44320c641db9a69d6ad0db4665d6db1649e4a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-05-31 09:18:21 +0200

    Fix and clean up tests

--- tests/v1/04_api/test_submission_job_list_v1.py
+++ tests/v1/04_api/test_submission_job_list_v1.py
@@ -11,62 +11,61 @@ async def test_clean_app_job_list_v1(
     db,
     app,
     client,
+    task_factory,
     project_factory,
     workflow_factory,
     dataset_factory,
     resource_factory,
     job_factory,
-    task_factory,
     override_settings_factory,
 ):
+
+    # Check that app fixture starts in a clean state
+    assert app.state.jobsV1 == []
+    assert app.state.jobsV2 == []
+
+    # Set this to 0 so that the endpoint also calls the clean-up function
     override_settings_factory(FRACTAL_API_MAX_JOB_LIST_LENGTH=0)
 
     async with MockCurrentUser(user_kwargs=dict(is_verified=True)) as user:
-        task = await task_factory(name="task", source="task_source")
-        project = await project_factory(user)
-        workflow1 = await workflow_factory(project_id=project.id)
-        workflow2 = await workflow_factory(project_id=project.id)
-        await _workflow_insert_task(
-            workflow_id=workflow1.id, task_id=task.id, db=db
-        )
-        await _workflow_insert_task(
-            workflow_id=workflow2.id, task_id=task.id, db=db
-        )
-        dataset1_in = await dataset_factory(project_id=project.id, type="zarr")
-        dataset1_out = await dataset_factory(
-            project_id=project.id, type="zarr"
-        )
-        dataset2_in = await dataset_factory(project_id=project.id, type="zarr")
-        dataset2_out = await dataset_factory(
-            project_id=project.id, type="zarr"
-        )
-        await resource_factory(dataset1_in)
-        await resource_factory(dataset1_out)
-        await resource_factory(dataset2_in)
-        await resource_factory(dataset2_out)
 
+        # Create DB objects
+        task = await task_factory(name="task", command="echo")
+        project = await project_factory(user)
+        workflow = await workflow_factory(project_id=project.id)
+        await _workflow_insert_task(
+            workflow_id=workflow.id, task_id=task.id, db=db
+        )
+        dataset1 = await dataset_factory(project_id=project.id, type="zarr")
+        dataset2 = await dataset_factory(project_id=project.id, type="zarr")
+        await resource_factory(dataset1)
+        await resource_factory(dataset2)
+
+        # Create job with submitted status
         job1 = await job_factory(
             project_id=project.id,
-            workflow_id=workflow1.id,
-            input_dataset_id=dataset1_in.id,
-            output_dataset_id=dataset1_out.id,
+            workflow_id=workflow.id,
+            input_dataset_id=dataset1.id,
+            output_dataset_id=dataset1.id,
             status="submitted",
             working_dir="/somewhere",
         )
         job1_id = job1.id
-        app.state.jobsV2.append(job1_id)
+        app.state.jobsV1.append(job1_id)
 
+        # Submit a second job via API
         res = await client.post(
-            f"/api/v1/project/{project.id}/workflow/{workflow2.id}/apply/"
-            f"?input_dataset_id={dataset2_in.id}"
-            f"&output_dataset_id={dataset2_out.id}",
+            f"/api/v1/project/{project.id}/workflow/{workflow.id}/apply/"
+            f"?input_dataset_id={dataset2.id}"
+            f"&output_dataset_id={dataset2.id}",
             json={},
         )
         assert res.status_code == 202
         job2_id = res.json()["id"]
 
         # Before clean-up, both jobs are listed
-        assert app.state.jobsV2 == [job1_id, job2_id]
+        assert app.state.jobsV1 == [job1_id, job2_id]
+
         # After clean-up, only the submitted job is left
         jobs_list = await clean_app_job_list_v1(db, app.state.jobsV1)
         assert jobs_list == [job1_id]
