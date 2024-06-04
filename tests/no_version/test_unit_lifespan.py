dca68fe07c79fad8ae695090e2116be1e6fddf61
mfranzon <franzonmarco94@gmail.com>
2024-06-04 17:09:29 +0200

    add job v1

--- tests/no_version/test_unit_lifespan.py
+++ tests/no_version/test_unit_lifespan.py
@@ -4,8 +4,11 @@ from fastapi import FastAPI
 from sqlmodel import select
 
 from fractal_server.app.models.security import UserOAuth
+from fractal_server.app.routes.api.v1._aux_functions import (
+    _workflow_insert_task as _workflow_insert_task_v1,
+)
 from fractal_server.app.routes.api.v2._aux_functions import (
-    _workflow_insert_task,
+    _workflow_insert_task as _workflow_insert_task_v2,
 )
 from fractal_server.app.runner.filenames import SHUTDOWN_FILENAME
 from fractal_server.main import lifespan
@@ -19,6 +22,12 @@ async def test_app_with_lifespan(
     workflow_factory_v2,
     dataset_factory_v2,
     job_factory_v2,
+    project_factory,
+    workflow_factory,
+    dataset_factory,
+    job_factory,
+    task_factory,
+    tmp_path,
 ):
 
     app = FastAPI()
@@ -38,12 +47,11 @@ async def test_app_with_lifespan(
         project = await project_factory_v2(user[0])
         workflow = await workflow_factory_v2(project_id=project.id)
         dataset1 = await dataset_factory_v2(project_id=project.id, name="ds-1")
-        await _workflow_insert_task(
+        await _workflow_insert_task_v2(
             workflow_id=workflow.id, task_id=task.id, db=db
         )
-
-        # Create job with submitted status
-        job = await job_factory_v2(
+        # Create jobv2 with submitted status
+        jobv2 = await job_factory_v2(
             project_id=project.id,
             workflow_id=workflow.id,
             dataset_id=dataset1.id,
@@ -51,10 +59,32 @@ async def test_app_with_lifespan(
             working_dir="/tmp",
             last_task_index=0,
         )
-        db.add(job)
+        db.add(jobv2)
         await db.commit()
-        await db.refresh(job)
+        await db.refresh(jobv2)
         # append submitted job to jobsV2 status
-        app.state.jobsV2.append(job.id)
+        app.state.jobsV2.append(jobv2.id)
+
+        # create jobv1
+        projectv1 = await project_factory(user[0])
+        workflowv1 = await workflow_factory(project_id=projectv1.id)
+        taskv1 = await task_factory(name="task", source="task_source")
+        await _workflow_insert_task_v1(
+            workflow_id=workflowv1.id, task_id=taskv1.id, db=db
+        )
+        datasetv1 = await dataset_factory(project_id=projectv1.id)
+        jobv1 = await job_factory(
+            working_dir=tmp_path.as_posix(),
+            project_id=project.id,
+            input_dataset_id=datasetv1.id,
+            output_dataset_id=datasetv1.id,
+            workflow_id=workflow.id,
+        )
+        db.add(jobv1)
+        await db.commit()
+        await db.refresh(jobv1)
+        # append submitted job to jobsV2 status
+        app.state.jobsV1.append(jobv1.id)
+
     # verify that the shutdown file was created during the lifespan cleanup
-    assert os.path.exists(f"{job.working_dir}/{SHUTDOWN_FILENAME}")
+    assert os.path.exists(f"{jobv2.working_dir}/{SHUTDOWN_FILENAME}")
