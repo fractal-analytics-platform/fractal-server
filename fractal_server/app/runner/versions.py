741545688d63704b2d13ce9d5d7430d2c81b01b5
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-06-04 09:16:23 +0200

    Clean up runner/versions module

--- fractal_server/app/runner/versions.py
+++ fractal_server/app/runner/versions.py
@@ -1,12 +1,30 @@
-if __name__ == "__main__":
+import json
+import sys
+from typing import Union
 
-    import sys
-    import cloudpickle
-    import fractal_server
+import cloudpickle
 
+import fractal_server
+
+
+def get_versions() -> dict[str, Union[list[int], str]]:
+    """
+    Extract versions of Python, fractal-server and cloudpickle.
+
+    This information is useful to check compatibility of two Python
+    interpreters when running tasks: the current interpreter (which prepares
+    the input pickles and orchestrates workflow execution) and a remote
+    interpreter (e.g. the one defined in the `FRACTAL_SLURM_WORKER_PYTHON`
+    configuration variable) that executes the tasks.
+    """
     versions = dict(
-        python=sys.version_info[:3],
+        python=list(sys.version_info[:3]),
         cloudpickle=cloudpickle.__version__,
         fractal_server=fractal_server.__VERSION__,
     )
-    print(versions)
+    return versions
+
+
+if __name__ == "__main__":
+    versions = get_versions()
+    print(json.dumps(versions))
