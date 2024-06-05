0e8056266210652c2616d5b4e7b5387793bf1687
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-06-05 15:06:26 +0200

    Revert "copy test_unit_aux_functions and adapt to v2"
    This reverts commit 70d90325f1e6f68575e03dabbe1ba444db85c10a.

--- tests/v2/04_runner/test_unit_aux_functions_v2.py
+++ tests/v2/04_runner/test_unit_aux_functions_v2.py
@@ -1,366 +1,141 @@
+from pathlib import Path
+
 import pytest
-from fastapi import HTTPException
+from devtools import debug
+from pydantic import ValidationError
 
-from fractal_server.app.routes.api.v2._aux_functions import (
-    _check_workflow_exists,
+from fractal_server.app.runner.exceptions import JobExecutionError
+from fractal_server.app.runner.v2.deduplicate_list import deduplicate_list
+from fractal_server.app.runner.v2.runner_functions import (
+    _cast_and_validate_InitTaskOutput,
 )
-from fractal_server.app.routes.api.v2._aux_functions import (
-    _get_dataset_check_owner,
+from fractal_server.app.runner.v2.runner_functions import (
+    _cast_and_validate_TaskOutput,
 )
-from fractal_server.app.routes.api.v2._aux_functions import (
-    _get_job_check_owner,
-)
-from fractal_server.app.routes.api.v2._aux_functions import (
-    _get_project_check_owner,
-)
-from fractal_server.app.routes.api.v2._aux_functions import (
-    _get_submitted_jobs_statement,
-)
-from fractal_server.app.routes.api.v2._aux_functions import (
-    _get_task_check_owner,
-)
-from fractal_server.app.routes.api.v2._aux_functions import (
-    _get_workflow_check_owner,
-)
-from fractal_server.app.routes.api.v2._aux_functions import (
-    _get_workflow_task_check_owner,
-)
-from fractal_server.app.routes.api.v2._aux_functions import (
-    _workflow_insert_task,
-)
+from fractal_server.app.runner.v2.task_interface import InitArgsModel
+from fractal_server.app.runner.v2.task_interface import TaskOutput
+from fractal_server.app.runner.v2.v1_compat import convert_v2_args_into_v1
 
 
-async def test_get_project_check_owner(
-    MockCurrentUser,
-    project_factory_v2,
-    db,
-):
-    async with MockCurrentUser() as other_user:
-        other_project = await project_factory_v2(other_user)
+def test_deduplicate_list_of_dicts():
+    #
+    old = [
+        InitArgsModel(zarr_url="/asd", init_args=dict(a=1)),
+        InitArgsModel(zarr_url="/asd", init_args=dict(a=2)),
+    ]
+    new = deduplicate_list(old)
+    assert len(new) == 2
 
-    async with MockCurrentUser() as user:
-        project = await project_factory_v2(user)
-
-        # Test success
-        await _get_project_check_owner(
-            project_id=project.id, user_id=user.id, db=db
-        )
-
-        # Test fail 1
-        with pytest.raises(HTTPException) as err:
-            await _get_project_check_owner(
-                project_id=project.id + 1, user_id=user.id, db=db
-            )
-        assert err.value.status_code == 404
-        assert err.value.detail == "Project not found"
-
-        # Test fail 2
-        with pytest.raises(HTTPException) as err:
-            await _get_project_check_owner(
-                project_id=other_project.id, user_id=user.id, db=db
-            )
-        assert err.value.status_code == 403
-        assert err.value.detail == f"Not allowed on project {other_project.id}"
+    #
+    old = [
+        InitArgsModel(zarr_url="/asd", init_args=dict(a=1)),
+        InitArgsModel(zarr_url="/asd", init_args=dict(a=1)),
+        InitArgsModel(zarr_url="/asd", init_args=dict(a=2)),
+    ]
+    debug(old)
+    new = deduplicate_list(old)
+    debug(new)
+    assert len(new) == 2
 
 
-async def test_get_workflow_check_owner(
-    MockCurrentUser,
-    project_factory_v2,
-    workflow_factory_v2,
-    db,
-):
-    async with MockCurrentUser() as other_user:
-        other_project = await project_factory_v2(other_user)
-        other_workflow = await workflow_factory_v2(project_id=other_project.id)
-
-    async with MockCurrentUser() as user:
-        project = await project_factory_v2(user)
-        workflow = await workflow_factory_v2(project_id=project.id)
-
-        # Test success
-        await _get_workflow_check_owner(
-            project_id=project.id,
-            workflow_id=workflow.id,
-            user_id=user.id,
-            db=db,
-        )
-
-        # Test fail 1
-        with pytest.raises(HTTPException) as err:
-            await _get_workflow_check_owner(
-                project_id=project.id,
-                workflow_id=workflow.id + 1,
-                user_id=user.id,
-                db=db,
-            )
-        assert err.value.status_code == 404
-        assert err.value.detail == "Workflow not found"
-
-        # Test fail 2
-        with pytest.raises(HTTPException) as err:
-            await _get_workflow_check_owner(
-                project_id=project.id,
-                workflow_id=other_workflow.id,
-                user_id=user.id,
-                db=db,
-            )
-        assert err.value.status_code == 422
-        assert err.value.detail == (
-            f"Invalid project_id={project.id} "
-            f"for workflow_id={other_workflow.id}."
-        )
-
-
-async def test_get_workflow_task_check_owner(
-    MockCurrentUser,
-    project_factory_v2,
-    workflow_factory_v2,
-    task_factory_v2,
-    workflowtask_factory_v2,
-    db,
-):
-    async with MockCurrentUser() as user:
-        project = await project_factory_v2(user)
-        workflow = await workflow_factory_v2(project_id=project.id)
-        task = await task_factory_v2(source="A")
-        wftask = await workflowtask_factory_v2(
-            workflow_id=workflow.id, task_id=task.id
-        )
-        other_workflow = await workflow_factory_v2(project_id=project.id)
-        other_task = await task_factory_v2(source="B")
-        other_wftask = await workflowtask_factory_v2(
-            workflow_id=other_workflow.id, task_id=other_task.id
-        )
-
-        # Test success
-        await _get_workflow_task_check_owner(
-            project_id=project.id,
-            workflow_id=workflow.id,
-            workflow_task_id=wftask.id,
-            user_id=user.id,
-            db=db,
-        )
-        # Test fail 1
-        with pytest.raises(HTTPException) as err:
-            await _get_workflow_task_check_owner(
-                project_id=project.id,
-                workflow_id=workflow.id,
-                workflow_task_id=wftask.id + other_wftask.id,
-                user_id=user.id,
-                db=db,
-            )
-        assert err.value.status_code == 404
-        assert err.value.detail == "WorkflowTask not found"
-
-        # Test fail 2
-        with pytest.raises(HTTPException) as err:
-            await _get_workflow_task_check_owner(
-                project_id=project.id,
-                workflow_id=workflow.id,
-                workflow_task_id=other_wftask.id,
-                user_id=user.id,
-                db=db,
-            )
-        assert err.value.status_code == 422
-        assert err.value.detail == (
-            f"Invalid workflow_id={workflow.id} "
-            f"for workflow_task_id={other_wftask.id}"
-        )
-
-
-async def test_check_workflow_exists(
-    MockCurrentUser,
-    project_factory_v2,
-    workflow_factory_v2,
-    db,
-):
-    async with MockCurrentUser() as user:
-        project = await project_factory_v2(user)
-        workflow = await workflow_factory_v2(project_id=project.id)
-
-    # Test success
-    await _check_workflow_exists(
-        name=workflow.name + "abc",
-        project_id=project.id,
-        db=db,
+def test_task_output():
+    # Test 'check_zarr_urls_are_unique'
+    t = TaskOutput(
+        image_list_updates=[dict(zarr_url="/a"), dict(zarr_url="/b")]
     )
-    await _check_workflow_exists(
-        name=workflow.name,
-        project_id=project.id + 1,
-        db=db,
+    t.check_zarr_urls_are_unique()
+
+    t = TaskOutput(
+        image_list_updates=[dict(zarr_url="/a"), dict(zarr_url="/a")]
+    )
+    with pytest.raises(ValueError) as e:
+        t.check_zarr_urls_are_unique()
+    debug(str(e.value))
+
+    t = TaskOutput(
+        image_list_updates=[dict(zarr_url="/a"), dict(zarr_url="/b")],
+        image_list_removals=["/a"],
+    )
+    with pytest.raises(ValueError) as e:
+        t.check_zarr_urls_are_unique()
+    debug(str(e.value))
+    # Test 'normalize_paths'
+    assert TaskOutput(
+        image_list_removals=["/a/b/../c/", "/tmp//"]
+    ).image_list_removals == ["/a/c", "/tmp"]
+    with pytest.raises(ValidationError):
+        TaskOutput(image_list_removals=["s3://BUCKET"])
+    with pytest.raises(ValidationError):
+        TaskOutput(image_list_removals=["http://url.json"])
+
+
+def test_convert_v2_args_into_v1(tmp_path: Path):
+    kwargs_v2 = dict(
+        zarr_url=(tmp_path / "input_path/plate.zarr/B/03/0").as_posix(),
+        something="else",
+        metadata="this will be overwritten",
+        component="this will be overwritten",
     )
 
-    # Test fail
-    with pytest.raises(HTTPException) as err:
-        await _check_workflow_exists(
-            name=workflow.name,
-            project_id=project.id,
-            db=db,
+    # Image
+    kwargs_v1 = convert_v2_args_into_v1(
+        kwargs_v2, parallelization_level="image"
+    )
+    PATH = (tmp_path / "input_path").as_posix()
+    assert kwargs_v1 == {
+        "input_paths": [PATH],
+        "output_path": PATH,
+        "metadata": {},
+        "component": "plate.zarr/B/03/0",
+        "something": "else",
+    }
+
+    # Well
+    kwargs_v1 = convert_v2_args_into_v1(
+        kwargs_v2, parallelization_level="well"
+    )
+    PATH = (tmp_path / "input_path").as_posix()
+    assert kwargs_v1 == {
+        "input_paths": [PATH],
+        "output_path": PATH,
+        "metadata": {},
+        "component": "plate.zarr/B/03",
+        "something": "else",
+    }
+
+    # Plate
+    kwargs_v1 = convert_v2_args_into_v1(
+        kwargs_v2, parallelization_level="plate"
+    )
+    PATH = (tmp_path / "input_path").as_posix()
+    assert kwargs_v1 == {
+        "input_paths": [PATH],
+        "output_path": PATH,
+        "metadata": {},
+        "component": "plate.zarr",
+        "something": "else",
+    }
+
+    # None
+    with pytest.raises(ValueError):
+        convert_v2_args_into_v1(kwargs_v2, parallelization_level=None)
+
+
+def test_cast_and_validate_functions():
+
+    _cast_and_validate_TaskOutput(
+        dict(filters={}, image_list_updates=[dict(zarr_url="/some/image")])
+    )
+
+    with pytest.raises(JobExecutionError):
+        _cast_and_validate_TaskOutput(dict(invalid=True))
+
+    _cast_and_validate_InitTaskOutput(
+        dict(
+            parallelization_list=[
+                dict(zarr_url="/tmp", init_args=dict(key="value"))
+            ]
         )
-        assert err.value.status_code == 404
-        assert err.value.detail == (
-            f"Workflow with name={workflow.name} and project_id={project.id} "
-            "already in use"
-        )
-
-
-async def test_get_dataset_check_owner(
-    MockCurrentUser,
-    project_factory_v2,
-    dataset_factory_v2,
-    db,
-):
-    async with MockCurrentUser() as user:
-        project = await project_factory_v2(user)
-        other_project = await project_factory_v2(user)
-        dataset = await dataset_factory_v2(project_id=project.id)
-
-        # Test success
-        await _get_dataset_check_owner(
-            project_id=project.id,
-            dataset_id=dataset.id,
-            user_id=user.id,
-            db=db,
-        )
-
-        # Test fail 1
-        with pytest.raises(HTTPException) as err:
-            await _get_dataset_check_owner(
-                project_id=project.id,
-                dataset_id=dataset.id + 1,
-                user_id=user.id,
-                db=db,
-            )
-        assert err.value.status_code == 404
-        assert err.value.detail == "Dataset not found"
-
-        # Test fail 2
-        with pytest.raises(HTTPException) as err:
-            await _get_dataset_check_owner(
-                project_id=other_project.id,
-                dataset_id=dataset.id,
-                user_id=user.id,
-                db=db,
-            )
-        assert err.value.status_code == 422
-        assert err.value.detail == (
-            f"Invalid project_id={other_project.id} "
-            f"for dataset_id={dataset.id}"
-        )
-
-
-async def test_get_job_check_owner(
-    MockCurrentUser,
-    project_factory_v2,
-    workflow_factory_v2,
-    dataset_factory_v2,
-    job_factory_v2,
-    task_factory_v2,
-    db,
-    tmp_path,
-):
-    async with MockCurrentUser() as user:
-        project = await project_factory_v2(user, id=1)
-        other_project = await project_factory_v2(user, id=2)
-
-        workflow = await workflow_factory_v2(project_id=project.id)
-        t = await task_factory_v2()
-        await _workflow_insert_task(
-            workflow_id=workflow.id, task_id=t.id, db=db
-        )
-
-        dataset = await dataset_factory_v2(project_id=project.id)
-
-        job = await job_factory_v2(
-            project_id=project.id,
-            dataset_id=dataset.id,
-            workflow_id=workflow.id,
-            working_dir=tmp_path,
-        )
-
-        # Test success
-        await _get_job_check_owner(
-            project_id=project.id, job_id=job.id, user_id=user.id, db=db
-        )
-
-        # Test fail 1
-        with pytest.raises(HTTPException) as err:
-            await _get_job_check_owner(
-                project_id=project.id,
-                job_id=job.id + 1,
-                user_id=user.id,
-                db=db,
-            )
-        assert err.value.status_code == 404
-        assert err.value.detail == "Job not found"
-
-        # Test fail 2
-        with pytest.raises(HTTPException) as err:
-            await _get_job_check_owner(
-                project_id=other_project.id,
-                job_id=job.id,
-                user_id=user.id,
-                db=db,
-            )
-        assert err.value.status_code == 422
-        assert err.value.detail == (
-            f"Invalid project_id={other_project.id} for job_id={job.id}"
-        )
-
-
-async def test_get_task_check_owner(MockCurrentUser, task_factory_v2, db):
-
-    async with MockCurrentUser(user_kwargs={"username": "alice"}) as user:
-        taskA = await task_factory_v2(source="A", owner=user.username)
-        taskB = await task_factory_v2(source="B")
-
-        # Test fail 1: 404 NOT FOUND
-        with pytest.raises(HTTPException) as err:
-            await _get_task_check_owner(
-                task_id=taskA.id + 999, user=user, db=db
-            )
-        assert err.value.status_code == 404
-        assert err.value.detail == f"TaskV2 {taskA.id + 999} not found."
-
-        # Test success
-        _task = await _get_task_check_owner(task_id=taskA.id, user=user, db=db)
-        assert _task.id == taskA.id
-
-        # Test fail 2: 403 FORBIDDEN
-        with pytest.raises(HTTPException) as err:
-            await _get_task_check_owner(task_id=taskB.id, user=user, db=db)
-        assert err.value.status_code == 403
-        assert err.value.detail == (
-            "Only a superuser can modify a TaskV2 with `owner=None`."
-        )
-
-    async with MockCurrentUser(user_kwargs={"username": "bob"}) as user:
-        # Test fail 3: 403 FORBIDDEN
-        with pytest.raises(HTTPException) as err:
-            await _get_task_check_owner(task_id=taskA.id, user=user, db=db)
-        assert err.value.status_code == 403
-        assert err.value.detail == (
-            f"Current user ({user.username}) cannot modify TaskV2 {taskA.id} "
-            f"with different owner ({taskA.owner})."
-        )
-
-    async with MockCurrentUser(
-        user_kwargs={"username": "boss", "is_superuser": True}
-    ) as superuser:
-        # Test success
-        _task = await _get_task_check_owner(
-            task_id=taskA.id, user=superuser, db=db
-        )
-        assert _task.id == taskA.id
-        _task = await _get_task_check_owner(
-            task_id=taskB.id, user=superuser, db=db
-        )
-        assert _task.id == taskB.id
-
-
-async def test_get_submitted_jobs_statement():
-
-    stm = _get_submitted_jobs_statement()
-    from sqlmodel.sql.expression import SelectOfScalar
-
-    assert type(stm) is SelectOfScalar
+    )
+    with pytest.raises(JobExecutionError):
+        _cast_and_validate_InitTaskOutput(dict(invalid=True))
