b0ee885857486dbce962047cc73210045e06efd5
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-06-06 12:55:18 +0200

    Replace aux module (from sudo to ssh)

---
+++ fractal_server/app/runner/executors/slurm_ssh/_run_through_ssh.py
@@ -0,0 +1,73 @@
+import time
+
+from fabric.connection import Connection
+from invoke import UnexpectedExit
+
+from .....logger import set_logger
+from .....syringe import Inject
+from ...exceptions import JobExecutionError
+from fractal_server.config import get_settings
+
+logger = set_logger(__name__)
+
+
+def _run_command_over_ssh(
+    *,
+    cmd: str,
+    connection: Connection,
+) -> str:
+    """
+    Run a command within an open SSH connection.
+
+    Args:
+        cmd: Command to be run
+        connection: Fabric connection object
+
+    Returns:
+        Standard output of the command, if successful.
+    """
+    t_0 = time.perf_counter()
+    logger.debug(f"START running '{cmd}' over SSH.")
+    try:
+        res = connection.run(cmd, hide=True)
+    except UnexpectedExit as e:
+        error_msg = (
+            f"Running command `{cmd}` over SSH failed.\n"
+            f"Original error:\n{str(e)}."
+        )
+        logger.error(error_msg)
+        # FIXME switch to JobExecutionError
+        raise ValueError(error_msg)
+        raise JobExecutionError(info=error_msg)
+
+    t_1 = time.perf_counter()
+    logger.info(f"END   running '{cmd}' over SSH, elapsed {t_1-t_0:.3f}")
+    logger.debug(f"STDOUT: {res.stdout}")
+    logger.debug(f"STDERR: {res.stderr}")
+    return res.stdout
+
+
+def _mkdir_over_ssh(*, folder: str, parents: bool = True) -> None:
+    """
+    Create a folder remotely via SSH.
+
+    Args:
+        folder:
+    """
+
+    if parents:
+        cmd = f"mkdir -p {folder}"
+    else:
+        cmd = f"mkdir {folder}"
+
+    settings = Inject(get_settings)
+    timeout = 3
+    with Connection(
+        host=settings.FRACTAL_SLURM_SSH_HOST,
+        user=settings.FRACTAL_SLURM_SSH_USER,
+        connect_kwargs={
+            "key_filename": settings.FRACTAL_SLURM_SSH_PRIVATE_KEY_PATH
+        },
+        connect_timeout=timeout,
+    ) as connection:
+        connection.run(cmd, hide=True)
