cd06bf73906d0d3231e955cc9c86a3538c04cd8d
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-06-11 15:13:13 +0200

    Improve compress_folder module

--- fractal_server/app/runner/compress_folder.py
+++ fractal_server/app/runner/compress_folder.py
@@ -1,6 +1,30 @@
 import sys
 import tarfile
+import time
 from pathlib import Path
+from typing import Optional
+
+
+def _filter(info: tarfile.TarInfo) -> Optional[tarfile.TarInfo]:
+    # print(f"START FILTERING {info.name}")
+    if info.name.endswith(".pickle"):
+        filename = info.name.split("/")[-1]
+        parts = filename.split("_")
+        if len(parts) == 3 and parts[1] == "in":
+            print(f"SKIP {info.name=}")
+            return None
+        elif len(parts) == 5 and parts[3] == "in":
+            print(f"SKIP {info.name=}")
+            return None
+    elif info.name.endswith(".args.json"):
+        print(f"SKIP {info.name=}")
+        return None
+    elif info.name.endswith("slurm_submit.sbatch"):
+        print(f"SKIP {info.name=}")
+        return None
+    print(f"OK {info.name=}")
+    return info
+
 
 if __name__ == "__main__":
     help_msg = (
@@ -15,10 +39,19 @@ if __name__ == "__main__":
         )
 
     subfolder_path = Path(sys.argv[1])
+    t_0 = time.perf_counter()
+    print("[compress_folder.py] START")
     print(f"[compress_folder.py] {subfolder_path=}")
     job_folder = subfolder_path.parent
     subfolder_name = subfolder_path.name
     tarfile_path = job_folder / f"{subfolder_name}.tar.gz"
     with tarfile.open(tarfile_path, "w:gz") as tar:
-        tar.add(subfolder_path, arcname=subfolder_name, recursive=True)
+        tar.add(
+            subfolder_path,
+            arcname=subfolder_name,
+            recursive=True,
+            filter=_filter,
+        )
     print(f"[compress_folder.py] {tarfile_path=}")
+    t_1 = time.perf_counter()
+    print(f"[compress_folder] END - elapsed {t_1 - t_0:.3f} seconds")
