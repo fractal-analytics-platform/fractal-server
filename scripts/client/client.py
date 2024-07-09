8b5f46c1146e54ca5cbe3076d64d2057745ea481
mfranzon <franzonmarco94@gmail.com>
2024-07-09 12:31:36 +0200

    add state fractal_ssh

--- scripts/client/client.py
+++ scripts/client/client.py
@@ -32,6 +32,7 @@ wsgi_app = ASGIMiddleware(app)
 wsgi_app.app.state.jobsV1 = []
 wsgi_app.app.state.jobsV2 = []
 wsgi_app.app.state.connection = None
+wsgi_app.app.state.fractal_ssh = None
 
 
 class FractalClient:
