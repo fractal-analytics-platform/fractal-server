ba07f01f8324923b0b06d3cb0a2b8a022873fa5b
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-07-12 11:26:21 +0200

    adapt old fixture

--- tests/v2/04_runner/fixtures_mocks.py
+++ tests/v2/04_runner/fixtures_mocks.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import pytest
 
+from fractal_server.app.models.v2 import TaskV2
 from fractal_server.app.runner.v2._local import FractalThreadPoolExecutor
 
 
@@ -34,58 +35,27 @@ def _run_cmd(*, cmd: str, label: str) -> str:
 
 
 @pytest.fixture
-def fractal_tasks_mock_venv(testdata_path, tmp_path_factory) -> dict:
+def fractal_tasks_mock_venv(
+    fractal_tasks_mock_venv_new: Path,
+    fractal_tasks_mock_collection_new: list[TaskV2],
+) -> dict:
     from v2_mock_models import TaskV2Mock
 
-    basetemp = tmp_path_factory.getbasetemp()
-    venv_name = "venv_fractal_tasks_mock"
-    venv_path = (basetemp / venv_name).as_posix()
-    python_bin = (basetemp / venv_name / "bin/python").as_posix()
-
-    if not os.path.isdir(venv_path):
-        logging.debug(f"venv does not exists ({venv_path=})")
-        # Create venv
-        cmd = f"{sys.executable} -m venv {venv_path}"
-        _run_cmd(cmd=cmd, label="create-venv")
-        # Install fractal-tasks-mock from wheel
-        wheel_file = (
-            testdata_path.parent
-            / "v2/fractal_tasks_mock"
-            / "dist/fractal_tasks_mock-0.0.1-py3-none-any.whl"
-        ).as_posix()
-        cmd = f"{python_bin} -m pip install {wheel_file}"
-        _run_cmd(cmd=cmd, label="install-fractal-tasks-mock")
-    else:
-        logging.info("venv already exists")
-
-    # Extract installed-package folder
-    cmd = f"{python_bin} -m pip show fractal_tasks_mock"
-    out = _run_cmd(cmd=cmd, label="extract-pkg-dir")
-    location = next(
-        line for line in out.split("\n") if line.startswith("Location:")
-    )
-    location = location.replace("Location: ", "")
-    src_dir = Path(location) / "fractal_tasks_mock/"
-
-    # Construct TaskV2Mock objects, and store them as a key-value pairs
-    # (indexed by their names)
-    with (src_dir / "__FRACTAL_MANIFEST__.json").open("r") as f:
-        manifest = json.load(f)
     task_dict = {}
-    for ind, task in enumerate(manifest["task_list"]):
+    for ind, task in enumerate(fractal_tasks_mock_collection_new):
         task_attributes = dict(
             id=ind,
-            name=task["name"],
-            source=task["name"].replace(" ", "_"),
+            name=task.name,
+            source=task.name.replace(" ", "_"),
         )
-        if task["name"] == "MIP_compound":
+        if task.name == "MIP_compound":
             task_attributes.update(
                 dict(
                     input_types={"3D": True},
                     output_types={"3D": False},
                 )
             )
-        elif task["name"] in [
+        elif task.name in [
             "illumination_correction",
             "illumination_correction_compound",
         ]:
@@ -95,28 +65,29 @@ def fractal_tasks_mock_venv(testdata_path, tmp_path_factory) -> dict:
                     output_types={"illumination_correction": True},
                 )
             )
-        elif task["name"] == "apply_registration_to_image":
+        elif task.name == "apply_registration_to_image":
             task_attributes.update(
                 dict(
                     input_types={"registration": False},
                     output_types={"registration": True},
                 )
             )
-        elif task["name"] == "generic_task_parallel":
+        elif task.name == "generic_task_parallel":
             task_attributes.update(
                 dict(
                     input_types={"my_type": False},
                     output_types={"my_type": True},
                 )
             )
-
         for step in ["non_parallel", "parallel"]:
-            key = f"executable_{step}"
-            if key in task.keys():
-                task_path = (src_dir / task[key]).as_posix()
-                task_attributes[
-                    f"command_{step}"
-                ] = f"{python_bin} {task_path}"
+            key = f"command_{step}"
+            if task.model_dump().get(key) is not None:
+                task_attributes[f"command_{step}"] = (
+                    fractal_tasks_mock_venv_new.parent
+                    / "lib/python3.10/site-packages/fractal_tasks_mock"
+                    / task.model_dump()[key]
+                ).as_posix()
+
         t = TaskV2Mock(**task_attributes)
         task_dict[t.name] = t
 
