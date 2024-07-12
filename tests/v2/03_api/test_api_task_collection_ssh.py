f183fd0d21c91d5ee39ccda868e7261cca856dff
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-07-12 13:23:32 +0200

    Working version of test_api_task_collection_ssh

--- tests/v2/03_api/test_api_task_collection_ssh.py
+++ tests/v2/03_api/test_api_task_collection_ssh.py
@@ -1,18 +1,12 @@
-import logging
-import sys
 from pathlib import Path
 
-from devtools import debug  # noqa
+import pytest
 
 from fractal_server.app.schemas.v2 import CollectionStatusV2
-from fractal_server.config import get_settings
 from fractal_server.ssh._fabric import FractalSSH
-from fractal_server.syringe import Inject
 
 
 PREFIX = "api/v2/task"
-INFO = sys.version_info
-CURRENT_PYTHON = f"{INFO.major}.{INFO.minor}"
 
 
 async def test_task_collection_ssh_from_pypi(
@@ -22,50 +16,83 @@ async def test_task_collection_ssh_from_pypi(
     MockCurrentUser,
     override_settings_factory,
     tmp_path: Path,
+    tmp777_path: Path,
     fractal_ssh: FractalSSH,
 ):
-    # Note 1: Use function-scoped `FRACTAL_TASKS_DIR` to avoid sharing state.
-    # Note 2: Set logging level to CRITICAL, and then make sure that
-    # task-collection logs are included
-    override_settings_factory(
-        FRACTAL_TASKS_DIR=(tmp_path / "FRACTAL_TASKS_DIR"),
-        FRACTAL_LOGGING_LEVEL=logging.CRITICAL,
-        FRACTAL_TASKS_PYTHON_DEFAULT_VERSION=CURRENT_PYTHON,
-        FRACTAL_RUNNER_BACKEND="slurm_ssh",
-        FRACTAL_SLURM_SSH_WORKING_BASE_DIR="/tmp/SLURM_SSH_WORKING_BASE_DIR",
-    )
-    settings = Inject(get_settings)
 
-    fractal_ssh.mkdir(
-        folder=settings.FRACTAL_RUNNER_WORKING_BASE_DIR.as_posix()
-    )
+    # Define and create remote working directory
+    WORKING_BASE_DIR = (tmp777_path / "working_dir").as_posix()
+    fractal_ssh.mkdir(folder=WORKING_BASE_DIR)
 
+    # Assign FractalSSH object to app state
     app.state.fractal_ssh = fractal_ssh
 
-    # Prepare and validate payload
-    PACKAGE_VERSION = "1.0.2"
-    PYTHON_VERSION = settings.FRACTAL_TASKS_PYTHON_DEFAULT_VERSION
-    payload = dict(
-        package="fractal-tasks-core",
-        package_version=PACKAGE_VERSION,
-        python_version=PYTHON_VERSION,
+    # Override settins with Python/SSH configurations
+    override_settings_factory(
+        FRACTAL_TASKS_PYTHON_DEFAULT_VERSION="3.9",
+        FRACTAL_TASKS_PYTHON_3_9="/usr/bin/python3.9",
+        FRACTAL_RUNNER_BACKEND="slurm_ssh",
+        FRACTAL_SLURM_SSH_WORKING_BASE_DIR=WORKING_BASE_DIR,
     )
-    debug(payload)
 
     async with MockCurrentUser(user_kwargs=dict(is_verified=True)):
+
+        # CASE 1: successful collection
+
         # Trigger task collection
+        PACKAGE_VERSION = "1.0.2"
         res = await client.post(
             f"{PREFIX}/collect/pip/",
-            json=payload,
+            json=dict(
+                package="fractal-tasks-core",
+                package_version=PACKAGE_VERSION,
+                python_version="3.9",
+            ),
         )
         assert res.status_code == 201
         assert res.json()["data"]["status"] == CollectionStatusV2.PENDING
-        state = res.json()
-        state_id = state["id"]
+        state_id = res.json()["id"]
 
         # Get collection info
         res = await client.get(f"{PREFIX}/collect/{state_id}/")
         assert res.status_code == 200
-        state = res.json()
-        data = state["data"]
-        debug(data)
+        data = res.json()["data"]
+        assert data["status"] == CollectionStatusV2.OK
+        assert f"fractal-tasks-core=={PACKAGE_VERSION}" in data["freeze"]
+        remote_folder = (
+            Path(WORKING_BASE_DIR)
+            / ".fractal"
+            / f"fractal-tasks-core{PACKAGE_VERSION}"
+        ).as_posix()
+        fractal_ssh.run_command(cmd=f"ls {remote_folder}")
+
+        # CASE 2: Failure due to invalid version
+
+        # Trigger task collection
+        PACKAGE_VERSION = "9.9.9"
+        res = await client.post(
+            f"{PREFIX}/collect/pip/",
+            json=dict(
+                package="fractal-tasks-core",
+                package_version=PACKAGE_VERSION,
+                python_version="3.9",
+            ),
+        )
+        assert res.status_code == 201
+        assert res.json()["data"]["status"] == CollectionStatusV2.PENDING
+        state_id = res.json()["id"]
+
+        # Get collection info
+        res = await client.get(f"{PREFIX}/collect/{state_id}/")
+        assert res.status_code == 200
+        data = res.json()["data"]
+        assert data["status"] == CollectionStatusV2.FAIL
+        assert "No matching distribution found" in data["log"]
+        assert f"fractal-tasks-core=={PACKAGE_VERSION}" in data["log"]
+        remote_folder = (
+            Path(WORKING_BASE_DIR)
+            / ".fractal"
+            / f"fractal-tasks-core{PACKAGE_VERSION}"
+        ).as_posix()
+        with pytest.raises(RuntimeError, match="No such file or directory"):
+            fractal_ssh.run_command(cmd=f"ls {remote_folder}")
