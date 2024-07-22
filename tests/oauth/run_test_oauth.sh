8580e1f57dc132b36c91917ee05e2e68d340de8e
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-07-22 11:50:49 +0200

    working script with recursive curl

--- tests/oauth/run_test_oauth.sh
+++ tests/oauth/run_test_oauth.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 export FRACTAL_TASKS_DIR=/tmp/fractal/task
 export FRACTAL_RUNNER_WORKING_BASE_DIR=/tmp/fractal/wbd
 export FRACTAL_RUNNER_BACKEND=local
@@ -12,10 +12,7 @@ export SQLITE_PATH=test_oauth.sql
 export OAUTH_TEST_CLIENT_ID=client_test_id
 export OAUTH_TEST_CLIENT_SECRET=client_test_secret
 export OAUTH_TEST_REDIRECT_URL=http://localhost:8001/auth/test/callback/
-export PORT=8001
-export DEX_PORT=5556
-
-export OAUTH_TEST_OIDC_CONFIGURATION_ENDPOINT=http://127.0.0.1:${DEX_PORT}/dex/.well-known/openid-configuration
+export OAUTH_TEST_OIDC_CONFIGURATION_ENDPOINT=http://127.0.0.1:5556/dex/.well-known/openid-configuration
 
 
 cleanup() {
@@ -26,41 +23,53 @@ cleanup() {
 trap cleanup EXIT
 
 docker compose up -d
+sleep 2
 
 poetry run fractalctl set-db
-poetry run fractalctl start -p $PORT &
-
+poetry run fractalctl start -p 8001 &
 sleep 5
 
-# Get admin token
-ADMIN_TOKEN=$(
-    wget -qO- --header="Content-Type: application/x-www-form-urlencoded" \
-    --post-data="username=admin@fractal.xy&password=1234" \
-    http://127.0.0.1:${PORT}/auth/token/login/ \
-    | jq -r ".access_token"
-)
+# ----
 
-# Assert that there is only ONE user
-USER_LIST=$(
-    wget -qO- --header="Authorization: Bearer ${ADMIN_TOKEN}" \
-    http://127.0.0.1:${PORT}/auth/users/
-)
-if [ $(echo $USER_LIST | jq '. | length') != "1" ]; then
-    exit 1
-fi
-
-# Authenticate (and register) another user with OAuth
-AUTHORIZATION_URL=$(wget -qO- \
-    http://127.0.0.1:${PORT}/auth/test/authorize/ \
+AUTHORIZATION_URL=$(curl \
+    http://127.0.0.1:8001/auth/test/authorize/ \
     | jq -r ".authorization_url"
 )
-wget -qO- $AUTHORIZATION_URL
-
-# Assert that there are TWO users
-USER_LIST=$(
-    wget -qO- --header="Authorization: Bearer ${ADMIN_TOKEN}" \
-    http://127.0.0.1:${PORT}/auth/users/
+TOKEN=$(
+    curl -L --silent --output /dev/null --cookie-jar - $AUTHORIZATION_URL \
+    | grep "fastapiusersauth" | awk '{print $NF}'
 )
-if [ $(echo $USER_LIST | jq '. | length') != "2" ]; then
-    exit 2
-fi
+WHOAMI=$(
+    curl -H "Authorization: Bearer $TOKEN" \
+    http://127.0.0.1:8001/auth/current-user/
+)
+
+# ----
+
+assert_equal() {
+  if [ "$1" != "$2" ]; then
+    echo "Error: $1 != $2"
+    exit 1
+  fi
+}
+
+USER_ID=$(echo $WHOAMI | jq -r ".id")
+EMAIL=$(echo $WHOAMI | jq -r ".email")
+IS_ACTIVE=$(echo $WHOAMI | jq -r ".is_active")
+IS_SUPERUSER=$(echo $WHOAMI | jq -r ".is_superuser")
+IS_VERIFIED=$(echo $WHOAMI | jq -r ".is_verified")
+SLURM_USER=$(echo $WHOAMI | jq -r ".slurm_user")
+CACHE_DIR=$(echo $WHOAMI | jq -r ".cache_dir")
+USER_NAME=$(echo $WHOAMI | jq -r ".user_name")
+SLURM_ACCOUNTS=$(echo $WHOAMI | jq -r ".slurm_accounts")
+
+
+assert_equal $USER_ID "2"
+assert_equal $EMAIL "kilgore@kilgore.trout"
+assert_equal $IS_ACTIVE "true"
+assert_equal $IS_SUPERUSER "false"
+assert_equal $IS_VERIFIED "false"
+assert_equal $SLURM_USER "null"
+assert_equal $CACHE_DIR "null"
+assert_equal $USER_NAME "null"
+assert_equal $SLURM_ACCOUNTS "[]"
