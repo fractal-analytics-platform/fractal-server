060680e35d8fe74311464ab2f280fb11a4cd8d0f
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-07-23 16:43:39 +0200

    Rename `app.runner.v2._slurm` into `app.runner.v2._slurm_sudo`

--- tests/v2/09_backends/test_slurm_config.py
+++ tests/v2/09_backends/test_slurm_config.py
@@ -13,11 +13,11 @@ from pydantic import root_validator
 from fractal_server.app.runner.executors.slurm._slurm_config import (
     SlurmConfigError,
 )
-from fractal_server.app.runner.v2._slurm._submit_setup import (
-    _slurm_submit_setup,
-)
 from fractal_server.app.runner.v2._slurm_common.get_slurm_config import (
     get_slurm_config,
+)
+from fractal_server.app.runner.v2._slurm_sudo._submit_setup import (
+    _slurm_submit_setup,
 )
 
 
