73ba5d82352d4bae2f5519c1703a7e4b7a9fce7a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-09-09 15:43:45 +0200

    BROKEN - initialize `tests/no_version/test_unit_auth.py` - cc @ychiucco - cc @mfranzon

---
+++ tests/no_version/test_unit_auth.py
@@ -0,0 +1,53 @@
+import pytest
+from devtools import debug
+from fastapi import HTTPException
+from sqlmodel import select
+
+from fractal_server.app.models import UserOAuth
+from fractal_server.app.routes.auth._aux_auth import (
+    _get_single_group_with_user_ids,
+)
+from fractal_server.app.routes.auth._aux_auth import (
+    _get_single_user_with_group_ids,
+)
+from fractal_server.app.routes.auth._aux_auth import (
+    _get_single_user_with_group_names,
+)
+from fractal_server.app.routes.auth._aux_auth import _user_or_404
+from fractal_server.app.security import _create_first_group  # noqa
+from fractal_server.app.security import _create_first_user
+
+
+async def _get_first_user(db):
+    stm = select(UserOAuth)
+    res = await db.execute(stm)
+    return res.scalars().unique().all()[0]
+
+
+async def test_user_or_404(db):
+    with pytest.raises(HTTPException) as exc_info:
+        await _user_or_404(user_id=9999, db=db)
+    debug(exc_info.value)
+    await _create_first_user(email="test1@fractal.com", password="xxxx")
+    user = await _get_first_user(db)
+    await _user_or_404(user_id=user.id, db=db)
+
+
+async def test_get_single_group_with_user_ids(db):
+    with pytest.raises(HTTPException) as exc_info:
+        await _get_single_group_with_user_ids(group_id=9999, db=db)
+    debug(exc_info.value)
+
+
+async def test_get_single_user_with_group_names(db):
+    await _create_first_user(email="test1@fractal.com", password="xxxx")
+    user = await _get_first_user(db)
+    res = await _get_single_user_with_group_names(user=user, db=db)
+    debug(res)
+
+
+async def test_get_single_user_with_group_ids(db):
+    await _create_first_user(email="test1@fractal.com", password="xxxx")
+    user = await _get_first_user(db)
+    res = await _get_single_user_with_group_ids(user=user, db=db)
+    debug(res)
