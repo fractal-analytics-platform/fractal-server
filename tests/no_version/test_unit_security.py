42892f312af92c61a42e31f80bfb739ebf17a435
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-09-12 15:07:13 +0200

    Remove assertsion about logs

--- tests/no_version/test_unit_security.py
+++ tests/no_version/test_unit_security.py
@@ -1,5 +1,3 @@
-import logging
-
 from sqlmodel import select
 
 from fractal_server.app.models import UserGroup
@@ -18,7 +16,7 @@ async def count_groups(db):
     return len(res.unique().scalars().all())
 
 
-async def test_unit_create_first_user(db, caplog):
+async def test_unit_create_first_user(db):
     assert await count_users(db) == 0
 
     # Calls that do create a new user
@@ -44,30 +42,21 @@ async def test_unit_create_first_user(db, caplog):
     # Calls that do not create new users
 
     # Cannot create two users with the same email
-    with caplog.at_level(logging.WARNING):
-        await _create_first_user(email="test2@fractal.com", password="xxxx")
-    assert "User test2@fractal.com already exists" in caplog.text
+    await _create_first_user(email="test2@fractal.com", password="xxxx")
     assert await count_users(db) == 4
-    caplog.clear()
 
     # Cannot create more than one superuser
-    with caplog.at_level(logging.INFO):
-        await _create_first_user(
-            email="test5@fractal.com", password="xxxx", is_superuser=True
-        )
-    assert "superuser already exists, skip creation" in caplog.text
+    await _create_first_user(
+        email="test5@fractal.com", password="xxxx", is_superuser=True
+    )
     assert await count_users(db) == 4
-    caplog.clear()
 
 
-async def test_unit_create_first_group(db, caplog):
+async def test_unit_create_first_group(db):
     assert await count_groups(db) == 0
     # First call is effective
     _create_first_group()
     assert await count_groups(db) == 1
     # Second call is a no-op
-    caplog.set_level(level=0)
-    caplog.clear()
     _create_first_group()
     assert await count_groups(db) == 1
-    assert "Group All already exists, skip." in caplog.text
