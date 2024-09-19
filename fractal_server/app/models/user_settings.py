ef30633ffae7eb75f48be713e348c65c28d3fc3e
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-09-19 12:15:09 +0200

    new user settings columns

--- fractal_server/app/models/user_settings.py
+++ fractal_server/app/models/user_settings.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from sqlalchemy import Column
+from sqlalchemy.types import JSON
 from sqlmodel import Field
 from sqlmodel import SQLModel
 
@@ -9,9 +11,17 @@ class UserSettings(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
 
-    # Actual settings columns
+    # SSH
     ssh_host: Optional[str] = None
     ssh_username: Optional[str] = None
     ssh_private_key_path: Optional[str] = None
     ssh_tasks_dir: Optional[str] = None
     ssh_jobs_dir: Optional[str] = None
+
+    # Slurm
+    slurm_user: Optional[str] = None
+    slurm_accounts: list[str] = Field(
+        sa_column=Column(JSON, server_default="[]", nullable=False)
+    )
+    # Cache
+    cache_dir: Optional[str] = None
--- fractal_server/app/schemas/user_settings.py
+++ fractal_server/app/schemas/user_settings.py
@@ -2,10 +2,12 @@ from typing import Optional
 
 from pydantic import BaseModel
 from pydantic import validator
+from pydantic.types import StrictStr
 
 from ._validators import val_absolute_path
+from ._validators import val_unique_list
 from ._validators import valstr
-
+from fractal_server.string_tools import validate_cmd
 
 __all__ = (
     "UserSettingsRead",
@@ -17,23 +19,39 @@ __all__ = (
 
 class UserSettingsRead(BaseModel):
     id: int
+    # SSH
     ssh_host: Optional[str]
     ssh_username: Optional[str]
     ssh_private_key_path: Optional[str]
     ssh_tasks_dir: Optional[str]
     ssh_jobs_dir: Optional[str]
+    # Slurm
+    slurm_user: Optional[str]
+    slurm_accounts: list[str]
+    # Cache
+    cache_dir: Optional[str]
 
 
 class UserSettingsReadStrict(BaseModel):
-    id: int
+    # Slurm
+    slurm_user: Optional[str]
+    slurm_accounts: list[str]
+    # Cache
+    cache_dir: Optional[str]
 
 
 class UserSettingsUpdate(BaseModel):
+    # SSH
     ssh_host: Optional[str]
     ssh_username: Optional[str]
     ssh_private_key_path: Optional[str]
     ssh_tasks_dir: Optional[str]
     ssh_jobs_dir: Optional[str]
+    # Slurm
+    slurm_user: Optional[str]
+    slurm_accounts: Optional[list[StrictStr]]
+    # Cache
+    cache_dir: Optional[str]
 
     _ssh_host = validator("ssh_host", allow_reuse=True)(valstr("ssh_host"))
     _ssh_username = validator("ssh_username", allow_reuse=True)(
@@ -50,6 +68,34 @@ class UserSettingsUpdate(BaseModel):
         val_absolute_path("ssh_jobs_dir")
     )
 
+    _slurm_user = validator("slurm_user", allow_reuse=True)(
+        valstr("slurm_user")
+    )
+    _slurm_accounts = validator("slurm_accounts", allow_reuse=True)(
+        val_unique_list("slurm_accounts")
+    )
+
+    @validator("cache_dir")
+    def cache_dir_validator(cls, value):
+        validate_cmd(value)
+        return val_absolute_path("cache_dir")(value)
+
 
 class UserSettingsUpdateStrict(BaseModel):
-    pass
+    # Slurm
+    slurm_user: Optional[str]
+    slurm_accounts: Optional[list[StrictStr]]
+    # Cache
+    cache_dir: Optional[str]
+
+    _slurm_user = validator("slurm_user", allow_reuse=True)(
+        valstr("slurm_user")
+    )
+    _slurm_accounts = validator("slurm_accounts", allow_reuse=True)(
+        val_unique_list("slurm_accounts")
+    )
+
+    @validator("cache_dir")
+    def cache_dir_validator(cls, value):
+        validate_cmd(value)
+        return val_absolute_path("cache_dir")(value)
