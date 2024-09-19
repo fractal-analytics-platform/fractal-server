5e8bc77907bc977c553ee0a28041545762b22b35
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-09-19 11:43:09 +0200

    validators

--- fractal_server/app/schemas/user_settings.py
+++ fractal_server/app/schemas/user_settings.py
@@ -1,6 +1,10 @@
 from typing import Optional
 
 from pydantic import BaseModel
+from pydantic import validator
+
+from ._validators import val_absolute_path
+from ._validators import valstr
 
 
 __all__ = (
@@ -30,6 +34,21 @@ class UserSettingsUpdate(BaseModel):
     ssh_private_key_path: Optional[str]
     ssh_tasks_dir: Optional[str]
     ssh_jobs_dir: Optional[str]
+
+    _ssh_host = validator("ssh_host", allow_reuse=True)(valstr("ssh_host"))
+    _ssh_username = validator("ssh_username", allow_reuse=True)(
+        valstr("ssh_username")
+    )
+    _ssh_private_key_path = validator(
+        "ssh_private_key_path", allow_reuse=True
+    )(valstr("ssh_private_key_path"))
+
+    _ssh_tasks_dir = validator("ssh_tasks_dir", allow_reuse=True)(
+        val_absolute_path("ssh_tasks_dir")
+    )
+    _ssh_jobs_dir = validator("ssh_jobs_dir", allow_reuse=True)(
+        val_absolute_path("ssh_jobs_dir")
+    )
 
 
 class UserSettingsUpdateStrict(BaseModel):
