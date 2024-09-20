81280675ab6381e01a6c6bc330facac2f9ea4288
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-09-20 10:30:16 +0200

    ssh settings in data migration script

--- fractal_server/data_migrations/2_6_0.py
+++ fractal_server/data_migrations/2_6_0.py
@@ -5,7 +5,9 @@ from sqlalchemy import select
 from fractal_server.app.db import get_sync_db
 from fractal_server.app.models import UserOAuth
 from fractal_server.app.models import UserSettings
+from fractal_server.config import get_settings
 from fractal_server.data_migrations.tools import _check_current_version
+from fractal_server.syringe import Inject
 
 
 def fix_db():
@@ -13,9 +15,24 @@ def fix_db():
     logger.warning("START execution of fix_db function")
     _check_current_version("2.6.0")
 
+    global_settings = Inject(get_settings)
+
     with next(get_sync_db()) as db:
         for user in db.execute(select(UserOAuth)).scalars().unique().all():
             user.settings = UserSettings(
+                # SSH
+                ssh_host=global_settings.FRACTAL_SLURM_SSH_HOST,
+                ssh_username=global_settings.FRACTAL_SLURM_SSH_USER,
+                ssh_private_key_path=(
+                    global_settings.FRACTAL_SLURM_SSH_PRIVATE_KEY_PATH
+                ),
+                ssh_tasks_dir=(
+                    global_settings.FRACTAL_SLURM_SSH_WORKING_BASE_DIR
+                ),
+                ssh_jobs_dir=(
+                    global_settings.FRACTAL_SLURM_SSH_WORKING_BASE_DIR
+                ),
+                # SUDO
                 slurm_user=user.slurm_user,
                 slurm_accounts=user.slurm_accounts,
                 cache_dir=user.cache_dir,
