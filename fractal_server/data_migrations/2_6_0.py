b387ed9520e61efee6bef2c5b91dc862607f5ed0
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-09-20 10:17:14 +0200

    move _check_current_version

--- fractal_server/data_migrations/2_6_0.py
+++ fractal_server/data_migrations/2_6_0.py
@@ -1,32 +1,17 @@
 import logging
 
-from packaging.version import parse
 from sqlalchemy import select
 
-import fractal_server
 from fractal_server.app.db import get_sync_db
 from fractal_server.app.models import UserOAuth
 from fractal_server.app.models import UserSettings
-
-
-def _check_current_version(*, expected_version: str):
-    # Check that this module matches with the current version
-    module_version = parse(expected_version)
-    current_version = parse(fractal_server.__VERSION__)
-    if (
-        current_version.major != module_version.major
-        or current_version.minor != module_version.minor
-        or current_version.micro != module_version.micro
-    ):
-        raise RuntimeError(
-            f"{fractal_server.__VERSION__=} not matching with {__file__=}"
-        )
+from fractal_server.data_migrations.tools import _check_current_version
 
 
 def fix_db():
     logger = logging.getLogger("fix_db")
     logger.warning("START execution of fix_db function")
-    _check_current_version(expected_version="2.6.0")
+    _check_current_version("2.6.0")
 
     with next(get_sync_db()) as db:
         for user in db.execute(select(UserOAuth)).scalars().unique().all():
