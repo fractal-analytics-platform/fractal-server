93f810e81dda9ac83549c83656251e6b90b88b0a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-09-25 15:35:22 +0200

    Remove obsolete v1 tests

--- tests/v1/04_api/test_task_api.py
+++ tests/v1/04_api/test_task_api.py
@@ -4,19 +4,6 @@ from devtools import debug
 PREFIX = "/api/v1/task"
 
 
-async def test_non_verified_user(client, MockCurrentUser):
-    """
-    Test that a non-verified user is not authorized to make POST/PATCH task
-    cals.
-    """
-    async with MockCurrentUser(user_kwargs=dict(is_verified=False)):
-        res = await client.post(f"{PREFIX}/", json={})
-        assert res.status_code == 401
-
-        res = await client.patch(f"{PREFIX}/123/", json={})
-        assert res.status_code == 401
-
-
 async def test_task_get_list(db, client, task_factory, MockCurrentUser):
     t0 = await task_factory(name="task0", source="source0")
     t1 = await task_factory(name="task1", source="source1")
--- tests/v1/04_api/test_task_collection_api.py
+++ tests/v1/04_api/test_task_collection_api.py
@@ -12,15 +12,3 @@ async def test_failed_get_collection_info(client, MockCurrentUser):
         res = await client.get(f"{PREFIX}/collect/{invalid_state_id}/")
     debug(res)
     assert res.status_code == 404
-
-
-async def test_collection_non_verified_user(client, MockCurrentUser):
-    """
-    Test that non-verified users are not authorized to make calls
-    to `/api/v1/task/collect/pip/`.
-    """
-    async with MockCurrentUser(user_kwargs=dict(is_verified=False)):
-        res = await client.post(
-            f"{PREFIX}/collect/pip/", json={"package": "fractal-tasks-core"}
-        )
-        assert res.status_code == 401
