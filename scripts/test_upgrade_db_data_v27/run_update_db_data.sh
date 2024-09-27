e7c9d0b09919428ce365f0c68c70b79705af65d6
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-09-27 12:29:32 +0200

    Create scripts/test_upgrade_db_data_v27

---
+++ scripts/test_upgrade_db_data_v27/run_update_db_data.sh
@@ -0,0 +1,8 @@
+dropdb --if-exists fractal_test_v27
+createdb fractal_test_v27
+
+PGPASSWORD=postgres psql -U postgres -h localhost -d fractal_test_v27 -f ../../tests/data/testing_databases//clean_db_fractal_2.6.0.sql
+
+poetry run fractalctl set-db
+
+echo -e "yes\nyes\nyes\nyes\nyes"| poetry run fractalctl update-db-data
