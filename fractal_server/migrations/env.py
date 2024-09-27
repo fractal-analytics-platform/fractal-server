54a5da453aedb69ebcfd1c3d222698b3ea57e9b8
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-09-27 11:26:03 +0200

    Revert "move import"
    This reverts commit 926529ecbe145b46a6aca33004962b9b3ee23e52.

--- fractal_server/migrations/env.py
+++ fractal_server/migrations/env.py
@@ -5,6 +5,7 @@ from alembic import context
 from sqlalchemy.engine import Connection
 from sqlmodel import SQLModel
 
+from fractal_server.app import models  # noqa
 from fractal_server.config import get_settings
 from fractal_server.migrations.naming_convention import NAMING_CONVENTION
 from fractal_server.syringe import Inject
@@ -26,7 +27,6 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = SQLModel.metadata
 target_metadata.naming_convention = NAMING_CONVENTION
-from fractal_server.app import models  # noqa
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
