1b6505d83c6816aff6803c23f713ab8c77eb3606
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-09-27 16:17:04 +0200

    Keep updating scripts/test_upgrade_db_data_v27

--- scripts/test_upgrade_db_data_v27/run_update_db_data.sh
+++ scripts/test_upgrade_db_data_v27/run_update_db_data.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Create empty fractal_test_v27 db
 dropdb --if-exists fractal_test_v27
 createdb fractal_test_v27
 
-PGPASSWORD=postgres psql -U postgres -h localhost -d fractal_test_v27 -f ../../tests/data/testing_databases//clean_db_fractal_2.6.0.sql
+# Load existing db into fractal_test_v27
+PGPASSWORD=postgres psql -U postgres -h localhost -d fractal_test_v27 -f "$1"
+
+# Apply 2.6.0 schema migrations
+./venv2.6.0/bin/fractalctl set-db
+
+# Apply 2.6.0 date migrations
+echo -e "yes\nyes\nyes\nyes\nyes" | ./venv2.6.0/bin/fractalctl update-db-data
+
 
 poetry run fractalctl set-db
 
-echo -e "yes\nyes\nyes\nyes\nyes"| poetry run fractalctl update-db-data
+echo -e "yes\nyes\nyes\nyes\nyes" | poetry run fractalctl update-db-data --dry-run
