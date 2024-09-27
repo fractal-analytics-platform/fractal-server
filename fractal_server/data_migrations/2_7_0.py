de5f426f489c222748ff0facf8fcff9c5465961b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-09-27 13:14:37 +0200

    Update-db-data for v2.7.0

--- fractal_server/data_migrations/2_7_0.py
+++ fractal_server/data_migrations/2_7_0.py
@@ -5,14 +5,18 @@ from sqlalchemy import select
 from fractal_server.app.db import get_sync_db
 from fractal_server.app.models import TaskGroupV2
 from fractal_server.app.models import TaskV2
+from fractal_server.app.models import UserGroup
 from fractal_server.app.models import UserOAuth
 from fractal_server.app.models import UserSettings
+from fractal_server.app.security import FRACTAL_DEFAULT_GROUP_NAME
 from fractal_server.data_migrations.tools import _check_current_version
 
 logger = logging.getLogger("fix_db")
 
 
 def _check_users(db):
+    logger.warning("START _check_users")
+
     stm_users = select(UserOAuth).order_by(UserOAuth.id)
     users = db.execute(stm_users).scalars().unique().all()
     list_username_or_slurm_user = []
@@ -47,9 +51,60 @@ def _check_users(db):
             "Some user doesn't have either `username` or `slurm_user`."
             "Manually edit database until this check passes."
         )
+    logger.warning("END _check_users")
+    print()
 
 
-def _create_task_groups_v0():
+def _default_user_group_id(db):
+    stm = select(UserGroup.id).where(
+        UserGroup.name == FRACTAL_DEFAULT_GROUP_NAME
+    )
+    res = db.execute(stm)
+    default_group_id = res.scalars().one_or_none()
+    if default_group_id is None:
+        raise RuntimeError("Default user group is missing.")
+    else:
+        return default_group_id
+
+
+def _create_task_groups_v0(db, dry_run: bool = True):
+    stm_tasks = select(TaskV2).order_by(TaskV2.id)
+    tasks = db.execute(stm_tasks).scalars().unique().all()
+    logger.warning("START _create_task_groups_v0")
+
+    for task in tasks:
+        logger.warning(f"START handling task {task.id}: '{task.name}'")
+        if task.taskgroupv2_id is not None:
+            logger.warning(
+                f"This task is already associated to {task.taskgroupv2_id=}, "
+                "skip."
+            )
+        else:
+            # FIXME: identify user_id
+            task_group = TaskGroupV2(
+                user_id=1,
+                user_group_id=_default_user_group_id(db),
+                task_list=[task],
+            )
+            db.add(task_group)
+            db.commit()
+            db.refresh(task_group)
+            db.refresh(task)
+            logger.warning(f"Created task group {task_group}")
+            logger.warning(
+                f"This task is now associated to {task.taskgroupv2_id=}."
+            )
+
+        logger.warning(f"END   handling task {task.id}: '{task.name}'")
+        print()
+    logger.warning("END   _create_task_groups_v0")
+    print()
+
+
+def _create_task_groups_v1(db, dry_run: bool = True):
+    """
+    Finds associations based on source.
+    """
     pass
 
 
