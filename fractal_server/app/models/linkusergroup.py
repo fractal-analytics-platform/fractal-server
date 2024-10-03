ca865c53c4e01ff08cbd419b2fa6b98827b95a0a
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-10-03 14:01:07 +0200

    add timestamp_created to LinkUserGroup

--- fractal_server/app/models/linkusergroup.py
+++ fractal_server/app/models/linkusergroup.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
+from sqlalchemy import Column
+from sqlalchemy.types import DateTime
 from sqlmodel import Field
 from sqlmodel import SQLModel
+
+from fractal_server.utils import get_timestamp
 
 
 class LinkUserGroup(SQLModel, table=True):
@@ -9,3 +15,8 @@ class LinkUserGroup(SQLModel, table=True):
 
     group_id: int = Field(foreign_key="usergroup.id", primary_key=True)
     user_id: int = Field(foreign_key="user_oauth.id", primary_key=True)
+
+    timestamp_created: datetime = Field(
+        default_factory=get_timestamp,
+        sa_column=Column(DateTime(timezone=True), nullable=False),
+    )
