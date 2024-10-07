3fb1a07d3ea49fe5ab5fab029683838b2e6d90e0
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-10-07 15:50:12 +0200

    Add `get_package_version_from_pypi` auxiliary function

--- fractal_server/tasks/v2/endpoint_operations.py
+++ fractal_server/tasks/v2/endpoint_operations.py
@@ -5,17 +5,24 @@ from typing import Optional
 from typing import Union
 from zipfile import ZipFile
 
+from fastapi import HTTPException
+from fastapi import status
+from httpx import AsyncClient
+from httpx import TimeoutException
+
 from ._TaskCollectPip import _TaskCollectPip
 from .utils import _parse_wheel_filename
 from .utils import get_python_interpreter_v2
 from fractal_server.app.schemas.v2 import ManifestV2
 from fractal_server.config import get_settings
 from fractal_server.logger import get_logger
+from fractal_server.logger import set_logger
 from fractal_server.syringe import Inject
 from fractal_server.utils import execute_command
 
 
 FRACTAL_PUBLIC_TASK_SUBDIR = ".fractal"
+logger = set_logger(__name__)
 
 
 async def download_package(
@@ -134,3 +141,38 @@ def create_package_dir_pip(
     if create:
         venv_path.mkdir(exist_ok=False, parents=True)
     return venv_path
+
+
+async def get_package_version_from_pypi(name: str) -> str:
+    """
+    Make a GET call to PyPI JSON API and get latest package version.
+
+    Ref https://warehouse.pypa.io/api-reference/json.html.
+
+    Arguments:
+        name: Package name.
+    """
+
+    url = f"https://pypi.org/pypi/{name}/json"
+    hint = f"Hint: specify the required version for '{name}'."
+    try:
+        async with AsyncClient(timeout=5.0) as client:
+            res = await client.get(url)
+            if res.status_code != 200:
+                raise HTTPException(
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                    detail=(
+                        f"Could not get {url} (status_code {res.status_code})."
+                        f"\n{hint}"
+                    ),
+                )
+            version = res.json()["info"]["version"]
+            return version
+    except (TypeError, KeyError, TimeoutException) as e:
+        logger.warning(
+            f"An error occurred while getting {url}. Original error: {str(e)}."
+        )
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=(f"An error occurred while getting {url}.\n{hint}"),
+        )
