a6e8ad17b8808370dbaf10def6831773cb156f2b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-10-07 13:09:46 +0200

    Update scripts/test_upgrade_db_data_v27

--- scripts/test_upgrade_db_data_v27/run_update_db_data.sh
+++ scripts/test_upgrade_db_data_v27/run_update_db_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Create empty fractal_test_v27 db
 dropdb --if-exists fractal_test_v27
 createdb fractal_test_v27
@@ -8,12 +10,16 @@ createdb fractal_test_v27
 PGPASSWORD=postgres psql -U postgres -h localhost -d fractal_test_v27 -f "$1"
 
 # Apply 2.6.0 schema migrations
-./venv2.6.0/bin/fractalctl set-db
+./venv260/bin/fractalctl set-db
 
-# Apply 2.6.0 date migrations
-echo -e "yes\nyes\nyes\nyes\nyes" | ./venv2.6.0/bin/fractalctl update-db-data
+# Apply 2.6.0 data migrations
+echo -e "yes\nyes\nyes\nyes\nyes" | ./venv260/bin/fractalctl update-db-data
 
-
+# Apply 2.7.0 schema migrations
 poetry run fractalctl set-db
 
+# Specific fix
+psql -d fractal_test_v27 -c "update user_oauth set username = 'admin_123' where id=21;"
+
+# Apply 2.7.0 data migrations
 echo -e "yes\nyes\nyes\nyes\nyes" | poetry run fractalctl update-db-data --dry-run
