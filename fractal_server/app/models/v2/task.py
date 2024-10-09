29a62ed95bb5ef90e87b84254b9beb95d388753d
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-10-09 15:44:13 +0200

    drop unique

--- fractal_server/app/models/v2/task.py
+++ fractal_server/app/models/v2/task.py
@@ -21,7 +21,7 @@ class TaskV2(SQLModel, table=True):
     type: str
     command_non_parallel: Optional[str] = None
     command_parallel: Optional[str] = None
-    source: str = Field(unique=True)
+    source: str
 
     meta_non_parallel: dict[str, Any] = Field(
         sa_column=Column(JSON, server_default="{}", default={}, nullable=False)
