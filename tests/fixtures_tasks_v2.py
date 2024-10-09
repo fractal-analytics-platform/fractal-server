4e54bab674847b1beb579bdc6371186a079f3ad8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-10-09 17:33:02 +0200

    Fix `fractal_tasks_mock_db` fixture

--- tests/fixtures_tasks_v2.py
+++ tests/fixtures_tasks_v2.py
@@ -12,6 +12,7 @@ from sqlalchemy.orm import Session as DBSyncSession
 
 from fractal_server.app.models import UserGroup
 from fractal_server.app.models import UserOAuth
+from fractal_server.app.models.v2 import TaskGroupV2
 from fractal_server.app.models.v2 import TaskV2
 from fractal_server.app.schemas.v2 import ManifestV2
 from fractal_server.app.schemas.v2 import TaskCreateV2
@@ -91,11 +92,21 @@ def fractal_tasks_mock_db(
     default_user_group: UserGroup,
 ) -> dict[str, TaskV2]:
 
-    task_group = create_db_tasks_and_update_task_group(
-        task_list=fractal_tasks_mock_collection["task_list"],
-        task_group_obj=TaskGroupCreateV2(origin="other", pkg_name="mock"),
+    task_group_obj = TaskGroupCreateV2(
+        origin="other",
+        pkg_name="fractal_tasks_mock",
         user_id=first_user.id,
         user_group_id=default_user_group.id,
+    )
+    task_group = TaskGroupV2(**task_group_obj.dict())
+    db_sync.add(task_group)
+    db_sync.commit()
+    db_sync.refresh(task_group)
+    db_sync.expunge(task_group)
+
+    task_group = create_db_tasks_and_update_task_group(
+        task_group_id=task_group.id,
+        task_list=fractal_tasks_mock_collection["task_list"],
         db=db_sync,
     )
     return {task.name: task for task in task_group.task_list}
