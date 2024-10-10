1f3e5d7b95cbde017e5b64d1533afb7c35ade4e8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-10-10 09:40:15 +0200

    Add `test_task_collection_ssh_from_wheel` (close #1864)

--- tests/v2/03_api/test_api_task_collection_ssh.py
+++ tests/v2/03_api/test_api_task_collection_ssh.py
@@ -61,7 +61,7 @@ async def test_task_collection_ssh_from_pypi(
         user_kwargs=dict(is_verified=True),
         user_settings_dict=user_settings_dict,
     ) as user:
-        # SUCCESS
+        # SUCCESSFUL COLLECTION
         package_version = "1.0.2"
         res = await client.post(
             f"{PREFIX}/collect/pip/",
@@ -177,3 +177,102 @@ async def test_task_collection_ssh_from_pypi(
         venv_path = Path(state_data["venv_path"])
         assert not venv_path.exists()
         assert not venv_path.parent.exists()
+
+
+async def test_task_collection_ssh_from_wheel(
+    db,
+    app,
+    client,
+    MockCurrentUser,
+    override_settings_factory,
+    tmp777_path: Path,
+    fractal_ssh_list: FractalSSHList,
+    current_py_version: str,
+    slurmlogin_ip,
+    ssh_keys,
+    testdata_path: Path,
+):
+    CURRENT_FRACTAL_MAX_PIP_VERSION = "21.0"
+    credentials = dict(
+        host=slurmlogin_ip,
+        user=SLURM_USER,
+        key_path=ssh_keys["private"],
+    )
+
+    assert not fractal_ssh_list.contains(**credentials)
+    fractal_ssh = fractal_ssh_list.get(**credentials)
+
+    # Define and create remote working directory
+    TASKS_BASE_DIR = (tmp777_path / "tasks").as_posix()
+    fractal_ssh.mkdir(folder=TASKS_BASE_DIR)
+
+    # Assign FractalSSH object to app state
+    app.state.fractal_ssh_list = fractal_ssh_list
+
+    # Override settins with Python/SSH configurations
+    current_py_version_underscore = current_py_version.replace(".", "_")
+    PY_KEY = f"FRACTAL_TASKS_PYTHON_{current_py_version_underscore}"
+    settings_overrides = {
+        "FRACTAL_TASKS_PYTHON_DEFAULT_VERSION": current_py_version,
+        PY_KEY: f"/usr/bin/python{current_py_version}",
+        "FRACTAL_RUNNER_BACKEND": "slurm_ssh",
+        "FRACTAL_MAX_PIP_VERSION": CURRENT_FRACTAL_MAX_PIP_VERSION,
+    }
+    override_settings_factory(**settings_overrides)
+
+    user_settings_dict = dict(
+        ssh_host=slurmlogin_ip,
+        ssh_username=SLURM_USER,
+        ssh_private_key_path=ssh_keys["private"],
+        ssh_tasks_dir=TASKS_BASE_DIR,
+        ssh_jobs_dir=(tmp777_path / "jobs").as_posix(),
+    )
+
+    # Copy wheel file to remote path
+    local_wheel_path = (
+        testdata_path.parent
+        / "v2/fractal_tasks_mock/dist"
+        / "fractal_tasks_mock-0.0.1-py3-none-any.whl"
+    ).as_posix()
+    remote_wheel_path = (tmp777_path / Path(local_wheel_path).name).as_posix()
+    fractal_ssh.send_file(
+        local=local_wheel_path,
+        remote=remote_wheel_path,
+    )
+
+    async with MockCurrentUser(
+        user_kwargs=dict(is_verified=True),
+        user_settings_dict=user_settings_dict,
+    ):
+        # SUCCESSFUL COLLECTION
+        res = await client.post(
+            f"{PREFIX}/collect/pip/",
+            json=dict(
+                package=remote_wheel_path,
+                python_version=current_py_version,
+            ),
+        )
+        assert res.status_code == 201
+        assert res.json()["data"]["status"] == CollectionStatusV2.PENDING
+        state_id = res.json()["id"]
+        # Get collection info
+        res = await client.get(f"{PREFIX}/collect/{state_id}/")
+        assert res.status_code == 200
+        state_data = res.json()["data"]
+        debug(state_data["log"])
+        assert state_data["status"] == CollectionStatusV2.OK
+        # Check remote venv folder exists
+        remote_folder = state_data["venv_path"]
+        fractal_ssh.run_command(cmd=f"ls {remote_folder}")
+
+        # API FAILURE: wheel file and version set
+        res = await client.post(
+            f"{PREFIX}/collect/pip/",
+            json=dict(
+                package=remote_wheel_path,
+                version="1.2.3",
+                python_version=current_py_version,
+            ),
+        )
+        assert res.status_code == 422
+        debug(res.json())
