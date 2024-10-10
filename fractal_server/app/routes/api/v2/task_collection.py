321404e3937b025b8746e481a5bac192ed910e93
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-10-10 13:16:05 +0200

    Assign foreign-key to CollectionStateV2

--- fractal_server/app/routes/api/v2/task_collection.py
+++ fractal_server/app/routes/api/v2/task_collection.py
@@ -221,7 +221,9 @@ async def collect_tasks_pip(
         venv_path=task_group_attrs["venv_path"],
         package=task_collect.package,
     )
-    state = CollectionStateV2(data=collection_status)
+    state = CollectionStateV2(
+        data=collection_status, taskgroupv2_id=task_group.id
+    )
     db.add(state)
     await db.commit()
     await db.refresh(state)
