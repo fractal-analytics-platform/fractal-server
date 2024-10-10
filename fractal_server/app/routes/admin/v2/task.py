a43dabdcec066700183a2f57ccfbe8ee3e875047
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-10-10 15:41:50 +0200

    Remove `kind=common/users` query parameters from admin endpoint

--- fractal_server/app/routes/admin/v2/task.py
+++ fractal_server/app/routes/admin/v2/task.py
@@ -1,4 +1,3 @@
-from typing import Literal
 from typing import Optional
 
 from fastapi import APIRouter
@@ -62,7 +61,6 @@ async def query_tasks(
     version: Optional[str] = None,
     name: Optional[str] = None,
     owner: Optional[str] = None,
-    kind: Optional[Literal["common", "users"]] = None,
     max_number_of_results: int = 25,
     user: UserOAuth = Depends(current_active_superuser),
     db: AsyncSession = Depends(get_async_db),
@@ -78,8 +76,6 @@ async def query_tasks(
         version: If not `None`, query for matching `task.version`.
         name: If not `None`, query for contained case insensitive `task.name`.
         owner: If not `None`, query for matching `task.owner`.
-        kind: If not `None`, query for TaskV2s that have (`users`) or don't
-            have (`common`) a `task.owner`.
         max_number_of_results: The maximum length of the response.
     """
 
@@ -95,11 +91,6 @@ async def query_tasks(
         stm = stm.where(TaskV2.name.icontains(name))
     if owner is not None:
         stm = stm.where(TaskV2.owner == owner)
-
-    if kind == "common":
-        stm = stm.where(TaskV2.owner == None)  # noqa E711
-    elif kind == "users":
-        stm = stm.where(TaskV2.owner != None)  # noqa E711
 
     res = await db.execute(stm)
     task_list = res.scalars().all()
