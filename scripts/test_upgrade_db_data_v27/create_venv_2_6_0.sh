41165296bb88fb8b53a87815ff06d5c62a03d122
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-10-25 12:29:11 +0200

    Drop `postgres-psycopg-binary`

--- scripts/test_upgrade_db_data_v27/create_venv_2_6_0.sh
+++ scripts/test_upgrade_db_data_v27/create_venv_2_6_0.sh
@@ -1,4 +1,4 @@
 python3 -m venv venv260
 source venv260/bin/activate
-python -m pip install "fractal-server[gunicorn,postgres-psycopg-binary]==2.6.0"
+python -m pip install "fractal-server[gunicorn]==2.6.0"
 deactivate
