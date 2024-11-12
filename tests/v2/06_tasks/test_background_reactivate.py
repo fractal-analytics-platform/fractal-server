1cac29e36d4406a3eb714a659d9d3a872de2c0e3
mfranzon <franzonmarco94@gmail.com>
2024-11-12 15:50:46 +0100

    add tests for reactivate

---
+++ tests/v2/06_tasks/test_background_reactivate.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+
+from devtools import debug
+
+from fractal_server.app.models.v2 import TaskGroupActivityV2
+from fractal_server.app.models.v2 import TaskGroupV2
+from fractal_server.app.schemas.v2 import TaskGroupActivityStatusV2
+from fractal_server.app.schemas.v2.task_group import TaskGroupActivityActionV2
+from fractal_server.tasks.v2.local import reactivate_local
+
+
+def test_unit_missing_objects(db, caplog):
+    """
+    Test a branch which is in principle unreachable.
+    """
+    caplog.clear()
+    reactivate_local(
+        task_group_activity_id=9999,
+        task_group_id=9999,
+    )
+    assert "Cannot find database rows" in caplog.text
+
+    caplog.clear()
+    assert caplog.text == ""
+
+    # not implemented
+    # collect_package_ssh(
+    #     task_group_activity_id=9999,
+    #     task_group_id=9999,
+    #     fractal_ssh=None,
+    #     tasks_base_dir="/invalid",
+    # )
+    # assert "Cannot find database rows" in caplog.text
+
+
+async def test_deactivate_venv_path(tmp_path, db, first_user):
+    # Prepare db objects
+    path = tmp_path / "something"
+    task_group = TaskGroupV2(
+        pkg_name="pkg",
+        version="1.2.3",
+        origin="pypi",
+        path=path.as_posix(),
+        venv_path=f"{tmp_path}/i_am_here",
+        user_id=first_user.id,
+    )
+    db.add(task_group)
+    await db.commit()
+    await db.refresh(task_group)
+    db.expunge(task_group)
+    task_group_activity = TaskGroupActivityV2(
+        user_id=first_user.id,
+        taskgroupv2_id=task_group.id,
+        status=TaskGroupActivityStatusV2.PENDING,
+        action=TaskGroupActivityActionV2.DEACTIVATE,
+        pkg_name="pkg",
+        version="1.0.0",
+    )
+    db.add(task_group_activity)
+    await db.commit()
+    await db.refresh(task_group_activity)
+    db.expunge(task_group_activity)
+    # create venv_path
+    Path(task_group.venv_path).mkdir()
+    # background task
+    reactivate_local(
+        task_group_id=task_group.id,
+        task_group_activity_id=task_group_activity.id,
+    )
+
+    # Verify that reactivate failed
+    task_group_activity_v2 = await db.get(
+        TaskGroupActivityV2, task_group_activity.id
+    )
+    debug(task_group_activity_v2)
+    assert task_group_activity_v2.status == "failed"
+    assert "already exists" in task_group_activity_v2.log
