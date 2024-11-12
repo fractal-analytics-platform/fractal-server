f1d260e046e9687f55ca2f0cd127eb4b35c6791b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-11-12 10:52:19 +0100

    Expand `test_deactivate_task_group` [skip ci]

--- tests/v2/03_api/test_api_task_lifecycle.py
+++ tests/v2/03_api/test_api_task_lifecycle.py
@@ -1,4 +1,8 @@
+from devtools import debug  # noqa
+
 from fractal_server.app.models.v2 import TaskGroupV2
+from fractal_server.app.schemas.v2 import TaskGroupActivityActionV2
+from fractal_server.app.schemas.v2 import TaskGroupActivityStatusV2
 
 
 async def test_deactivate_task_group(
@@ -17,21 +21,27 @@ async def test_deactivate_task_group(
         )
 
     async with MockCurrentUser() as user:
+        # Create mock task groups
         non_active_task = await task_factory_v2(user_id=user.id, name="task")
+        task_other = await task_factory_v2(user_id=user.id, name="task")
+        task_pypi = await task_factory_v2(
+            user_id=user.id, name="task", version="1.2.3"
+        )
         non_active_task_group = await db.get(
             TaskGroupV2, non_active_task.taskgroupv2_id
         )
+        task_group_other = await db.get(TaskGroupV2, task_other.taskgroupv2_id)
+        task_group_pypi = await db.get(TaskGroupV2, task_pypi.taskgroupv2_id)
         non_active_task_group.active = False
+        task_group_other.origin = "other"
+        task_group_pypi.origin = "pypi"
         db.add(non_active_task_group)
+        db.add(task_group_other)
+        db.add(task_group_pypi)
         await db.commit()
         await db.refresh(non_active_task_group)
-
-        task_other = await task_factory_v2(user_id=user.id, name="task")
-        task_group_other = await db.get(TaskGroupV2, task_other.taskgroupv2_id)
-        task_group_other.origin = "other"
-        db.add(task_group_other)
-        await db.commit()
         await db.refresh(task_group_other)
+        await db.refresh(task_group_pypi)
 
         # API failure: Not full access to another user's task group
         res = await client.post(
@@ -52,3 +62,20 @@ async def test_deactivate_task_group(
         activity = res.json()
         assert res.status_code == 202
         assert activity["version"] == "N/A"
+        assert activity["status"] == TaskGroupActivityStatusV2.OK
+        assert activity["action"] == TaskGroupActivityActionV2.DEACTIVATE
+        await db.refresh(task_group_other)
+        assert task_group_other.active is False
+
+        # API success with `origin="pypi"`
+        res = await client.post(
+            f"api/v2/task-group/{task_group_pypi.id}/deactivate/"
+        )
+        activity = res.json()
+        assert res.status_code == 202
+        debug(activity)
+        assert activity["version"] == task_group_pypi.version
+        assert activity["status"] == TaskGroupActivityStatusV2.PENDING
+        assert activity["action"] == TaskGroupActivityActionV2.DEACTIVATE
+        await db.refresh(task_group_pypi)
+        assert task_group_pypi.active is False
