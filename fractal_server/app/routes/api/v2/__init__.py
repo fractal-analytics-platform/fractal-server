d9615d684f331c0382bf0af6fd69649947e5bd59
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-11-12 12:30:43 +0100

    Add new router to API

--- fractal_server/app/routes/api/v2/__init__.py
+++ fractal_server/app/routes/api/v2/__init__.py
@@ -13,6 +13,7 @@ from .task import router as task_router_v2
 from .task_collection import router as task_collection_router_v2
 from .task_collection_custom import router as task_collection_router_v2_custom
 from .task_group import router as task_group_router_v2
+from .task_group_lifecycle import router as task_group_lifecycle_router_v2
 from .workflow import router as workflow_router_v2
 from .workflow_import import router as workflow_import_router_v2
 from .workflowtask import router as workflowtask_router_v2
@@ -31,13 +32,21 @@ router_api_v2.include_router(submit_job_router_v2, tags=["V2 Job"])
 
 settings = Inject(get_settings)
 router_api_v2.include_router(
-    task_collection_router_v2, prefix="/task", tags=["V2 Task Collection"]
+    task_collection_router_v2,
+    prefix="/task",
+    tags=["V2 Task Lifecycle"],
 )
 router_api_v2.include_router(
     task_collection_router_v2_custom,
     prefix="/task",
-    tags=["V2 Task Collection"],
+    tags=["V2 Task Lifecycle"],
 )
+router_api_v2.include_router(
+    task_group_lifecycle_router_v2,
+    prefix="/task-group",
+    tags=["V2 Task Lifecycle"],
+)
+
 router_api_v2.include_router(task_router_v2, prefix="/task", tags=["V2 Task"])
 router_api_v2.include_router(
     task_group_router_v2, prefix="/task-group", tags=["V2 TaskGroup"]
