0feb454aa175d3bf2d05728e397e8cdfc2a27124
mfranzon <franzonmarco94@gmail.com>
2024-11-12 15:21:21 +0100

    add test for invalid wheel path

--- tests/v2/06_tasks/test_background_deactivate.py
+++ tests/v2/06_tasks/test_background_deactivate.py
@@ -47,3 +47,45 @@ async def test_deactivate_no_venv_path(tmp_path, db, first_user):
     debug(task_group_activity_v2)
     assert task_group_activity_v2.status == "failed"
     assert "does not exist" in task_group_activity_v2.log
+
+
+async def test_deactivate_wheel_no_wheel_path(tmp_path, db, first_user):
+    # Prepare db objects
+    path = tmp_path / "something"
+    task_group = TaskGroupV2(
+        pkg_name="pkg",
+        version="1.2.3",
+        origin="wheel",
+        wheel_path="/invalid",
+        path=path.as_posix(),
+        venv_path=(path / "venv").as_posix(),
+        user_id=first_user.id,
+    )
+    db.add(task_group)
+    await db.commit()
+    await db.refresh(task_group)
+    db.expunge(task_group)
+    task_group_activity = TaskGroupActivityV2(
+        user_id=first_user.id,
+        taskgroupv2_id=task_group.id,
+        status=TaskGroupActivityStatusV2.PENDING,
+        action=TaskGroupActivityActionV2.DEACTIVATE,
+        pkg_name="pkg",
+        version="1.0.0",
+    )
+    db.add(task_group_activity)
+    await db.commit()
+    await db.refresh(task_group_activity)
+    db.expunge(task_group_activity)
+    # background task
+    deactivate_local(
+        task_group_id=task_group.id,
+        task_group_activity_id=task_group_activity.id,
+    )
+    # Verify that collection failed
+    task_group_activity_v2 = await db.get(
+        TaskGroupActivityV2, task_group_activity.id
+    )
+    debug(task_group_activity_v2)
+    assert task_group_activity_v2.status == "failed"
+    assert "does not exist" in task_group_activity_v2.log
