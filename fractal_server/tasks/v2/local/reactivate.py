4ad4f1422de8bce1c58b0e328f198c794b8dba2a
mfranzon <franzonmarco94@gmail.com>
2024-11-12 10:35:37 +0100

    remove redundant check on venv path

--- fractal_server/tasks/v2/local/reactivate.py
+++ fractal_server/tasks/v2/local/reactivate.py
@@ -58,27 +58,7 @@ def reactivate_local(
 
             # Check that the (local) task_group path does exist
             if not Path(task_group.venv_path).exists():
-                error_msg = f"{task_group.venv_path} not exists."
-                logger.error(error_msg)
-                fail_and_cleanup(
-                    task_group=task_group,
-                    task_group_activity=activity,
-                    logger_name=LOGGER_NAME,
-                    log_file_path=log_file_path,
-                    exception=FileNotFoundError(error_msg),
-                    db=db,
-                )
-                return
-
-            if task_group.venv_path is None:
-
-                logging.error(
-                    "Cannot find task_group venv_path with "
-                    f"{task_group_id=} :\n"
-                    f"{task_group=}\n. Exit."
-                )
-
-                error_msg = f"{task_group=} venv_path not exists."
+                error_msg = f"{task_group} venv_path not exists."
                 logger.error(error_msg)
                 fail_and_cleanup(
                     task_group=task_group,
