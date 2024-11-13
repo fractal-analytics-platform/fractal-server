279589f84da235933649295d50daa232ee0aedce
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-11-13 08:45:37 +0100

    coverage

--- tests/v2/06_tasks/test_background_reactivate.py
+++ tests/v2/06_tasks/test_background_reactivate.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pytest
 from devtools import debug
 
 from fractal_server.app.models.v2 import TaskGroupActivityV2
@@ -33,7 +34,7 @@ def test_unit_missing_objects(db, caplog):
     # assert "Cannot find database rows" in caplog.text
 
 
-async def test_deactivate_venv_path(tmp_path, db, first_user):
+async def test_reactivate_venv_path(tmp_path, db, first_user):
     # Prepare db objects
     path = tmp_path / "something"
     task_group = TaskGroupV2(
@@ -77,73 +78,75 @@ async def test_deactivate_venv_path(tmp_path, db, first_user):
     assert "already exists" in task_group_activity_v2.log
 
 
-async def test_collect_pip_local_fail_rmtree(
+@pytest.mark.parametrize("make_rmtree_fail", [True, False])
+async def test_deactivate_local_fail(
     tmp_path,
     db,
     first_user,
     current_py_version,
     monkeypatch,
+    make_rmtree_fail: bool,
 ):
 
-    import fractal_server.tasks.v2.local.reactivate
+    if make_rmtree_fail:
+        import fractal_server.tasks.v2.local.reactivate
 
-    def patched_function(*args, **kwargs):
-        raise RuntimeError("Broken rm")
+        def patched_function(*args, **kwargs):
+            raise RuntimeError("Broken rm")
 
-    monkeypatch.setattr(
-        fractal_server.tasks.v2.local.reactivate.shutil,
-        "rmtree",
-        patched_function,
-    )
+        monkeypatch.setattr(
+            fractal_server.tasks.v2.local.reactivate.shutil,
+            "rmtree",
+            patched_function,
+        )
 
-    # Prepare db objects
+    # Prepare task group that will make `pip install` fail
     path = tmp_path / "rmtree-error"
     task_group = TaskGroupV2(
-        pkg_name="fractal-tasks-mock",
-        version="0.0.1",
+        pkg_name="invalid-package-name",
+        version="11.11.11",
         origin="pypi",
         python_version=current_py_version,
         path=path.as_posix(),
         venv_path=(path / "venv").as_posix(),
         user_id=first_user.id,
-        pip_freeze="wrong-package",
+        pip_freeze="something==99.99.99",
     )
     debug(task_group)
     db.add(task_group)
     await db.commit()
     await db.refresh(task_group)
-    db.expunge(task_group)
     task_group_activity = TaskGroupActivityV2(
         user_id=first_user.id,
         taskgroupv2_id=task_group.id,
         status=TaskGroupActivityStatusV2.PENDING,
-        action=TaskGroupActivityActionV2.COLLECT,
-        pkg_name="pkg",
-        version="1.0.0",
+        action=TaskGroupActivityActionV2.REACTIVATE,
+        pkg_name=task_group.pkg_name,
+        version=task_group.version,
     )
     await db.commit()
     db.add(task_group_activity)
     await db.commit()
     await db.refresh(task_group_activity)
-    db.expunge(task_group_activity)
+    db.expunge_all()
+
     # create path
     Path(task_group.path).mkdir()
+
     # Run background task
-    try:
-        reactivate_local(
-            task_group_id=task_group.id,
-            task_group_activity_id=task_group_activity.id,
-        )
-    except RuntimeError as e:
-        print(
-            f"Caught exception {e} within the test, which is taking place in "
-            "the `rmtree` call that cleans up `tmpdir`. Safe to ignore."
-        )
-    # Verify that collection failed
-    task_group_activity_v2 = await db.get(
-        TaskGroupActivityV2, task_group_activity.id
+    reactivate_local(
+        task_group_id=task_group.id,
+        task_group_activity_id=task_group_activity.id,
     )
-    debug(task_group_activity_v2)
-    assert task_group_activity_v2.status == "failed"
-    assert "Broken rm" in task_group_activity_v2.log
-    assert path.exists()
+    # Verify that collection failed
+    activity = await db.get(TaskGroupActivityV2, task_group_activity.id)
+    debug(activity.status)
+    debug(activity.log)
+    assert activity.status == "failed"
+
+    assert Path(task_group.path).exists()
+    if make_rmtree_fail:
+        assert "Broken rm" in activity.log
+        assert Path(task_group.venv_path).exists()
+    else:
+        assert not Path(task_group.venv_path).exists()
