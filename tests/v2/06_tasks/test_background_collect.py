db925a67f0780449d5e4ba38a42ef2cfae13517e
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-11-15 15:08:46 +0100

    Split test module into two

--- tests/v2/06_tasks/test_background_collect.py
+++ tests/v2/06_tasks/test_background_collect.py
@@ -1,8 +1,4 @@
-from typing import Optional
-
-import pytest
 from devtools import debug
-from pydantic import BaseModel
 
 from fractal_server.app.models.v2 import TaskGroupActivityV2
 from fractal_server.app.models.v2 import TaskGroupV2
@@ -11,56 +7,6 @@ from fractal_server.app.schemas.v2 import (
 )
 from fractal_server.app.schemas.v2.task_group import TaskGroupActivityActionV2
 from fractal_server.tasks.v2.local import collect_local
-from fractal_server.tasks.v2.local._utils import (
-    check_task_files_exist,
-)
-from fractal_server.tasks.v2.utils_database import _get_task_type
-
-
-class _MockTaskCreateV2(BaseModel):
-    name: str = "task_name"
-    command_non_parallel: Optional[str] = None
-    command_parallel: Optional[str] = None
-
-
-def test_get_task_type():
-
-    task = _MockTaskCreateV2(command_non_parallel="x")
-    assert _get_task_type(task) == "non_parallel"
-    task = _MockTaskCreateV2(command_parallel="x")
-    assert _get_task_type(task) == "parallel"
-    task = _MockTaskCreateV2(command_parallel="x", command_non_parallel="y")
-    assert _get_task_type(task) == "compound"
-
-
-def test_check_task_files_exist(tmp_path):
-    existing_path = tmp_path / "existing.py"
-    missing_path = tmp_path / "missing.py"
-    existing_path.touch()
-    existing_path = existing_path.as_posix()
-    missing_path = missing_path.as_posix()
-    # Success
-    check_task_files_exist(
-        task_list=[
-            _MockTaskCreateV2(command_non_parallel=f"py {existing_path}"),
-            _MockTaskCreateV2(command_parallel=f"py {existing_path}"),
-        ]
-    )
-    # Failures
-    with pytest.raises(FileNotFoundError) as e:
-        check_task_files_exist(
-            task_list=[
-                _MockTaskCreateV2(command_non_parallel=f"py {missing_path}")
-            ]
-        )
-    assert "missing file" in str(e.value)
-    with pytest.raises(FileNotFoundError) as e:
-        check_task_files_exist(
-            task_list=[
-                _MockTaskCreateV2(command_parallel=f"py {missing_path}")
-            ]
-        )
-    assert "missing file" in str(e.value)
 
 
 async def test_collect_pip_existing_file(tmp_path, db, first_user):
@@ -114,7 +60,6 @@ async def test_collect_pip_local_fail_rmtree(
     current_py_version,
     monkeypatch,
 ):
-
     import fractal_server.tasks.v2.local.collect
 
     def patched_function(*args, **kwargs):
