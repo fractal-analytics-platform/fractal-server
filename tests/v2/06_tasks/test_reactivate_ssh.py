ca166b782ec5582310101c68f0fdb6eb881c9fe8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-11-18 09:07:38 +0100

    Add SSH reactivation tests

---
+++ tests/v2/06_tasks/test_reactivate_ssh.py
@@ -0,0 +1,162 @@
+from pathlib import Path
+
+import pytest
+from devtools import debug
+
+from fractal_server.app.models.v2 import TaskGroupActivityV2
+from fractal_server.app.models.v2 import TaskGroupV2
+from fractal_server.app.schemas.v2 import TaskGroupActivityStatusV2
+from fractal_server.app.schemas.v2.task_group import TaskGroupActivityActionV2
+from fractal_server.ssh._fabric import FractalSSH
+from fractal_server.tasks.v2.ssh import reactivate_ssh
+
+
+async def test_reactivate_ssh_venv_exists(
+    tmp777_path,
+    db,
+    first_user,
+    fractal_ssh: FractalSSH,
+):
+    path = tmp777_path / "package"
+    task_group = TaskGroupV2(
+        pkg_name="pkg",
+        version="1.2.3",
+        origin="pypi",
+        path=path.as_posix(),
+        venv_path=f"{tmp777_path}/i_am_here",
+        user_id=first_user.id,
+    )
+    db.add(task_group)
+    await db.commit()
+    await db.refresh(task_group)
+    task_group_activity = TaskGroupActivityV2(
+        user_id=first_user.id,
+        taskgroupv2_id=task_group.id,
+        status=TaskGroupActivityStatusV2.PENDING,
+        action=TaskGroupActivityActionV2.DEACTIVATE,
+        pkg_name=task_group.pkg_name,
+        version=task_group.version,
+    )
+    db.add(task_group_activity)
+    await db.commit()
+    await db.refresh(task_group_activity)
+    db.expunge_all()
+
+    # create venv_path
+    fractal_ssh.mkdir(folder=task_group.venv_path)
+
+    # background task
+    reactivate_ssh(
+        task_group_id=task_group.id,
+        task_group_activity_id=task_group_activity.id,
+        fractal_ssh=fractal_ssh,
+        tasks_base_dir=tmp777_path.as_posix(),
+    )
+
+    # Verify that reactivate failed
+    task_group_activity_v2 = await db.get(
+        TaskGroupActivityV2, task_group_activity.id
+    )
+    debug(task_group_activity_v2)
+    assert task_group_activity_v2.status == "failed"
+    assert "already exists" in task_group_activity_v2.log
+
+
+@pytest.mark.parametrize("make_rmtree_fail", [False, True])
+async def test_reactivate_ssh_fail(
+    tmp777_path,
+    db,
+    first_user,
+    monkeypatch,
+    make_rmtree_fail: bool,
+    fractal_ssh: FractalSSH,
+    override_settings_factory,
+    current_py_version,
+):
+    """
+    Make reactivation fail (due to wrong pip-freeze data), in two cases:
+    1. The removal of the venv path works.
+    2. The removal of the venv path fails.
+    """
+
+    # Setup remote Python interpreter
+    current_py_version_underscore = current_py_version.replace(".", "_")
+    key = f"FRACTAL_TASKS_PYTHON_{current_py_version_underscore}"
+    value = f"/.venv{current_py_version}/bin/python{current_py_version}"
+    override_settings_factory(**{key: value})
+
+    if make_rmtree_fail:
+        import fractal_server.tasks.v2.ssh.reactivate
+
+        FAILED_RMTREE_MESSAGE = "Broken rm"
+
+        def patched_rmtree(*args, **kwargs):
+            raise RuntimeError(FAILED_RMTREE_MESSAGE)
+
+        monkeypatch.setattr(
+            fractal_server.tasks.v2.ssh.reactivate.FractalSSH,
+            "remove_folder",
+            patched_rmtree,
+        )
+
+    # Prepare task group that will make `pip install` fail
+    path = tmp777_path / f"make-rmtree-fail-{make_rmtree_fail}"
+    task_group = TaskGroupV2(
+        pkg_name="invalid-package-name",
+        version="11.11.11",
+        origin="pypi",
+        python_version=current_py_version,
+        path=path.as_posix(),
+        venv_path=(path / "venv").as_posix(),
+        user_id=first_user.id,
+        pip_freeze="something==99.99.99",
+    )
+    debug(task_group)
+    db.add(task_group)
+    await db.commit()
+    await db.refresh(task_group)
+    task_group_activity = TaskGroupActivityV2(
+        user_id=first_user.id,
+        taskgroupv2_id=task_group.id,
+        status=TaskGroupActivityStatusV2.PENDING,
+        action=TaskGroupActivityActionV2.REACTIVATE,
+        pkg_name=task_group.pkg_name,
+        version=task_group.version,
+    )
+    db.add(task_group_activity)
+    await db.commit()
+    await db.refresh(task_group_activity)
+    db.expunge_all()
+
+    # Create path
+    fractal_ssh.mkdir(folder=task_group.path)
+
+    # Run background task
+    try:
+        reactivate_ssh(
+            task_group_id=task_group.id,
+            task_group_activity_id=task_group_activity.id,
+            fractal_ssh=fractal_ssh,
+            tasks_base_dir=tmp777_path.as_posix(),
+        )
+    except RuntimeError as e:
+        print(
+            f"Caught exception {e} within the test, which is taking place in "
+            "the `rmtree` call that cleans up `tmpdir`. Safe to ignore."
+        )
+
+    # Verify that collection failed
+    activity = await db.get(TaskGroupActivityV2, task_group_activity.id)
+    debug(activity.status)
+    debug(activity.log)
+    assert activity.status == "failed"
+    return
+    MSG = "Could not find a version that satisfies the requirement something==99.99.99"  # noqa
+    assert MSG in activity.log
+    assert Path(task_group.path).exists()
+
+    if make_rmtree_fail:
+        assert FAILED_RMTREE_MESSAGE in activity.log
+        assert fractal_ssh.remote_exists(task_group.venv_path)
+    else:
+        assert not fractal_ssh.remote_exists(task_group.venv_path)
