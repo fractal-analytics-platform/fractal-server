bc3e659d11dbb509ede19aad2cfe9b2243bc4e48
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-11-19 11:39:42 +0100

    remove Optional

--- fractal_server/app/routes/aux/__init__.py
+++ fractal_server/app/routes/aux/__init__.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-from typing import Optional
 
 from fastapi import HTTPException
 from fastapi import status
 
 
-def _raise_if_naive_datetime(*timestamps: tuple[Optional[datetime]]) -> None:
+def _raise_if_naive_datetime(*timestamps: tuple[datetime | None]) -> None:
     """
     Raise 422 if any not-null argument is a naive `datetime` object:
     https://docs.python.org/3/library/datetime.html#determining-if-an-object-is-aware-or-naive
