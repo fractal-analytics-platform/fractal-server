4c322600fa756568fa5290d4a69676d6a52847b0
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-01-09 12:51:57 +0100

    Update `validate_attribute_filters` (accepting `values=None`)

--- fractal_server/app/schemas/_validators.py
+++ fractal_server/app/schemas/_validators.py
@@ -112,22 +112,31 @@ def validate_type_filters(
 
 
 def validate_attribute_filters(
-    attribute_filters: Optional[dict[str, list[Any]]]
-) -> Optional[dict[str, list[Any]]]:
+    attribute_filters: Optional[dict[str, Optional[list[Any]]]],
+) -> Optional[dict[str, Optional[list[Any]]]]:
     if attribute_filters is not None:
         attribute_filters = valdict_keys("attribute_filters")(
             attribute_filters
         )
         for key, values in attribute_filters.items():
-            if values:
+            if values is None:
+                # values=None corresponds to not applying any filter for
+                # attribute `key`
+                pass
+            elif values == []:
+                # WARNING: in this case, no image can match with the current
+                # filter. In the future we may deprecate this possibility.
+                pass
+            else:
+                # values is a non-empty list, and its items must all be of the
+                # same scalar non-None type
                 _type = type(values[0])
                 if not all(isinstance(value, _type) for value in values):
                     raise ValueError(
                         f"attribute_filters[{key}] has values with "
                         f"non-homogeneous types: {values}."
                     )
-                if _type not in (int, float, str, bool, type(None)):
-                    # FIXME: Review whether None is accepted
+                if _type not in (int, float, str, bool):
                     raise ValueError(
                         f"attribute_filters[{key}] has values with "
                         f"invalid types: {values}."
