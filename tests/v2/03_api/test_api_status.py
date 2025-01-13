6a10497367ca4226fd3acb6687485cd4ac15bf5c
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-01-13 15:07:45 +0100

    style

--- tests/v2/03_api/test_api_status.py
+++ tests/v2/03_api/test_api_status.py
@@ -1,4 +1,3 @@
-from devtools import debug
 from sqlalchemy.orm.attributes import flag_modified
 
 from fractal_server.app.routes.api.v2._aux_functions import (
@@ -7,7 +6,7 @@ from fractal_server.app.routes.api.v2._aux_functions import (
 from fractal_server.app.schemas.v2.dataset import WorkflowTaskStatusTypeV2
 
 
-async def test_workflowtask_status_no_history_no_job(
+async def test_status_no_history_no_running_job(
     db,
     MockCurrentUser,
     project_factory_v2,
@@ -17,8 +16,8 @@ async def test_workflowtask_status_no_history_no_job(
     client,
 ):
     """
-    Test the status endpoint when there is information in the DB and no running
-    job associated to a given dataset/workflow pair.
+    GIVEN A database with no jobs and no history
+    THEN The status-endpoint response is empty
     """
     async with MockCurrentUser() as user:
         project = await project_factory_v2(user)
@@ -40,7 +39,7 @@ async def test_workflowtask_status_no_history_no_job(
         assert res.json() == {"status": {}}
 
 
-async def test_workflowtask_status_history_no_job(
+async def test_status_yes_history_no_running_job(
     db,
     MockCurrentUser,
     project_factory_v2,
@@ -50,8 +49,8 @@ async def test_workflowtask_status_history_no_job(
     client,
 ):
     """
-    Test the status endpoint when there is a non-empty history in the DB but
-    no running job associated to a given dataset/workflow pair.
+    GIVEN A database with non-empty dataset.history and no running jobs
+    THEN The status-endpoint response is empty
     """
     async with MockCurrentUser() as user:
         project = await project_factory_v2(user)
@@ -136,7 +135,7 @@ async def test_workflowtask_status_history_no_job(
         assert res.json() == {"status": {"3": "done"}}
 
 
-async def test_workflowtask_status_history_job(
+async def test_status_yes_history_yes_running_job(
     db,
     MockCurrentUser,
     tmp_path,
@@ -152,17 +151,9 @@ async def test_workflowtask_status_history_job(
     there is a running job associated to a given dataset/workflow pair.
     """
     working_dir = tmp_path / "working_dir"
-    history = [
-        dict(
-            workflowtask=dict(id=1),
-            status=WorkflowTaskStatusTypeV2.SUBMITTED,
-        )
-    ]
     async with MockCurrentUser() as user:
         project = await project_factory_v2(user)
-        dataset = await dataset_factory_v2(
-            project_id=project.id, history=history
-        )
+        dataset = await dataset_factory_v2(project_id=project.id, history=[])
         task = await task_factory_v2(
             user_id=user.id, name="task1", source="task1"
         )
@@ -182,6 +173,15 @@ async def test_workflowtask_status_history_job(
         )
 
     # CASE 1: first submitted
+    dataset.history = [
+        dict(
+            workflowtask=dict(id=workflow.task_list[0].id),
+            status=WorkflowTaskStatusTypeV2.SUBMITTED,
+        ),
+    ]
+    flag_modified(dataset, "history")
+    await db.merge(dataset)
+    await db.commit()
     res = await client.get(
         (
             f"api/v2/project/{project.id}/status/?"
@@ -213,6 +213,29 @@ async def test_workflowtask_status_history_job(
     )
     assert res.status_code == 200
     assert res.json()["status"] == {"1": "done", "2": "submitted"}
+
+    # CASE 3: no "SUBMITTED" in the task list
+    dataset.history = [
+        dict(
+            workflowtask=dict(id=workflow.task_list[0].id),
+            status=WorkflowTaskStatusTypeV2.DONE,
+        ),
+        dict(
+            workflowtask=dict(id=workflow.task_list[1].id),
+            status=WorkflowTaskStatusTypeV2.FAILED,
+        ),
+    ]
+    flag_modified(dataset, "history")
+    await db.merge(dataset)
+    await db.commit()
+    res = await client.get(
+        (
+            f"api/v2/project/{project.id}/status/?"
+            f"dataset_id={dataset.id}&workflow_id={workflow.id}"
+        )
+    )
+    assert res.status_code == 200
+    assert res.json()["status"] == {"1": "submitted", "2": "submitted"}
 
 
 async def test_workflowtask_status_two_jobs(
@@ -248,14 +271,12 @@ async def test_workflowtask_status_two_jobs(
                 dataset_id=dataset.id,
                 working_dir=str(working_dir),
             )
-
     res = await client.get(
         (
             f"api/v2/project/{project.id}/status/?"
             f"dataset_id={dataset.id}&workflow_id={workflow.id}"
         )
     )
-    debug(res.json())
     assert res.status_code == 422
 
 
@@ -272,8 +293,8 @@ async def test_workflowtask_status_modified_workflow(
 ):
     """
     Test the status endpoint when there is a running job associated to a given
-    dataset/workflow pair, but the workflow is modified after the job was
-    created.
+    dataset/workflow pair, but the workflow has been modified after the job
+    was created.
     """
     working_dir = tmp_path / "working_dir"
     async with MockCurrentUser() as user:
