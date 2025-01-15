2e748131ae4bb6d500e732b4518f5631097acf0b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-01-15 09:46:05 +0100

    Add some validation tests

--- tests/v2/01_schemas/test_schemas_dataset.py
+++ tests/v2/01_schemas/test_schemas_dataset.py
@@ -1,4 +1,5 @@
 import pytest
+from devtools import debug
 from pydantic import ValidationError
 
 from fractal_server.app.models.v2 import DatasetV2
@@ -8,6 +9,51 @@ from fractal_server.app.schemas.v2 import DatasetImportV2
 from fractal_server.app.schemas.v2 import DatasetReadV2
 from fractal_server.app.schemas.v2 import DatasetUpdateV2
 from fractal_server.urls import normalize_url
+
+
+VALID_ATTRIBUTE_FILTERS = (
+    {},
+    {"key1": []},
+    {"key1": ["A"]},
+    {"key1": ["A", "B"]},
+    {"key1": [1, 2]},
+    {"key1": [True, False]},
+    {"key1": [1.5, -1.2]},
+    {"key1": None},
+    {"key1": [1, 2], "key2": ["A", "B"]},
+)
+
+INVALID_ATTRIBUTE_FILTERS = (
+    {True: ["value"]},  # non-string key
+    {1: ["value"]},  # non-string key
+    {"key1": [1], " key1": [1]},  # non-unique normalized keys
+    {"key1": [None]},  # None value
+    # {"key1": [1, True]},  # non-homogeneous types - FIXME unsupported
+    {"key1": [1, 1.0]},  # non-homogeneous types - FIXME unsupported
+    {"key1": [1, "a"]},  # non-homogeneous types
+    {"key1": [[1, 2], [1, 2]]},  # non-scalar type
+)
+
+
+@pytest.mark.parametrize("attribute_filters", VALID_ATTRIBUTE_FILTERS)
+def test_valid_attribute_filters(attribute_filters: dict):
+    DatasetCreateV2(
+        name="x",
+        zarr_dir="/x",
+        attribute_filters=attribute_filters,
+    )
+
+
+@pytest.mark.parametrize("attribute_filters", INVALID_ATTRIBUTE_FILTERS)
+def test_invalid_attribute_filters(attribute_filters: dict):
+    debug(attribute_filters)
+    with pytest.raises(ValueError) as e:
+        DatasetCreateV2(
+            name="x",
+            zarr_dir="/x",
+            attribute_filters=attribute_filters,
+        )
+    debug(e.value)
 
 
 async def test_schemas_dataset_v2():
