c18194c3e97c75997cff8a9a5c19fa0625546a52
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-01-15 10:50:50 +0100

    refactor merge_outputs

--- fractal_server/app/runner/v2/merge_outputs.py
+++ fractal_server/app/runner/v2/merge_outputs.py
@@ -1,5 +1,3 @@
-from copy import copy
-
 from fractal_server.app.runner.v2.deduplicate_list import deduplicate_list
 from fractal_server.app.runner.v2.task_interface import TaskOutput
 
@@ -8,35 +6,31 @@ def merge_outputs(task_outputs: list[TaskOutput]) -> TaskOutput:
 
     final_image_list_updates = []
     final_image_list_removals = []
-    last_new_type_filters = None
 
-    for ind, task_output in enumerate(task_outputs):
+    for task_output in task_outputs:
 
         final_image_list_updates.extend(task_output.image_list_updates)
         final_image_list_removals.extend(task_output.image_list_removals)
 
-        # Check that all filters are the same
-        current_new_type_filters = task_output.type_filters
-        if ind == 0:
-            last_new_type_filters = copy(current_new_type_filters)
-        if current_new_type_filters != last_new_type_filters:
+        # Check that all type_filters are the same
+        if task_output.type_filters != task_outputs[0].type_filters:
             raise ValueError(
-                f"{current_new_type_filters=} but {last_new_type_filters=}"
+                f"{task_output.type_filters=} "
+                f"but {task_outputs[0].type_filters=}"
             )
-        last_new_type_filters = copy(current_new_type_filters)
 
     final_image_list_updates = deduplicate_list(final_image_list_updates)
 
-    if last_new_type_filters is None:
+    if task_outputs:
         final_output = TaskOutput(
             image_list_updates=final_image_list_updates,
             image_list_removals=final_image_list_removals,
+            type_filters=task_outputs[0].type_filters,
         )
     else:
         final_output = TaskOutput(
             image_list_updates=final_image_list_updates,
             image_list_removals=final_image_list_removals,
-            type_filters=last_new_type_filters,
         )
 
     return final_output
