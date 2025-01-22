4a3e8c99e4feba2515d448312b12fb86843d0517
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-01-22 16:31:57 +0100

    Minor changes to data-migration script

--- fractal_server/data_migrations/2_11_0.py
+++ fractal_server/data_migrations/2_11_0.py
@@ -19,19 +19,21 @@ logger.setLevel(logging.INFO)
 
 
 def dict_values_to_list(
-    input_dict: dict[str, Union[int, float, bool, str, None]], identifier: str
+    input_dict: dict[str, Union[int, float, bool, str, None]],
+    identifier: str,
 ) -> dict[str, list[Union[int, float, bool, str]]]:
     for k, v in input_dict.items():
         if not isinstance(v, (int, float, bool, str, type(None))):
             error_msg = (
-                f"Attribute '{k}' from '{identifier}' has type '{type(v)}'."
+                f"Attribute '{k}' from '{identifier}' "
+                f"has invalid type '{type(v)}'."
             )
             logger.error(error_msg)
             raise RuntimeError(error_msg)
         elif v is None:
             logger.warning(
-                f"Attribute '{k}' from '{identifier}' is None and it will be "
-                "removed."
+                f"Attribute '{k}' from '{identifier}' is None and it "
+                "will be removed."
             )
         else:
             input_dict[k] = [v]
@@ -39,74 +41,74 @@ def dict_values_to_list(
 
 
 def fix_db():
-
     logger.info("START execution of fix_db function")
 
     with next(get_sync_db()) as db:
-
         # DatasetV2.filters
         # DatasetV2.history[].workflowtask.input_filters
         stm = select(DatasetV2).order_by(DatasetV2.id)
         datasets = db.execute(stm).scalars().all()
         for ds in datasets:
-            try:
-                ds.attribute_filters = dict_values_to_list(
-                    ds.filters["attributes"],
-                    f"Dataset[{ds.id}].filters.attributes",
-                )
-                ds.type_filters = ds.filters["types"]
-                ds.filters = None
-                for i, h in enumerate(ds.history):
-                    ds.history[i]["workflowtask"]["type_filters"] = h[
-                        "workflowtask"
-                    ]["input_filters"]["types"]
-                flag_modified(ds, "history")
-                DatasetReadV2(
-                    project=ProjectReadV2(**ds.project.model_dump()),
-                    **ds.model_dump(),
-                )
-                db.add(ds)
-                logger.info(f"Fixed filters in DatasetV2[{ds.id}]")
-            except RuntimeError as e:
-                logger.info(f"Skipping DatasetV2[{ds.id}]. Error: '{e}'.")
+            logger.info(f"DatasetV2[{ds.id}] START")
+            ds.attribute_filters = dict_values_to_list(
+                ds.filters["attributes"],
+                f"Dataset[{ds.id}].filters.attributes",
+            )
+            ds.type_filters = ds.filters["types"]
+            ds.filters = None
+            for i, h in enumerate(ds.history):
+                ds.history[i]["workflowtask"]["type_filters"] = h[
+                    "workflowtask"
+                ]["input_filters"]["types"]
+            flag_modified(ds, "history")
+            DatasetReadV2(
+                **ds.model_dump(),
+                project=ProjectReadV2(**ds.project.model_dump()),
+            )
+            db.add(ds)
+            logger.info(f"DatasetV2[{ds.id}] END - fixed filters")
+
+        logger.info("------ switch from dataset to workflowtasks ------")
 
         # WorkflowTaskV2.input_filters
         stm = select(WorkflowTaskV2).order_by(WorkflowTaskV2.id)
         wftasks = db.execute(stm).scalars().all()
         for wft in wftasks:
+            logger.info(f"WorkflowTaskV2[{wft.id}] START")
             wft.type_filters = wft.input_filters["types"]
             if wft.input_filters["attributes"]:
                 logger.warning(
-                    f"Removing WorkflowTaskV2[{wft.id}].input_filters"
-                    f"['attributes'] = {wft.input_filters['attributes']}"
+                    "Removing input_filters['attributes']. "
+                    f"(previous value: {wft.input_filters['attributes']})"
                 )
             wft.input_filters = None
             flag_modified(wft, "input_filters")
             WorkflowTaskReadV2(
-                task=TaskReadV2(**wft.task.model_dump()), **wft.model_dump()
+                **wft.model_dump(),
+                task=TaskReadV2(**wft.task.model_dump()),
             )
             db.add(wft)
-            logger.info(f"Fixed filters in WorkflowTaskV2[{wft.id}]")
+            logger.info(f"WorkflowTaskV2[{wft.id}] END - fixed filters")
+
+        logger.info("------ switch from workflowtasks to jobs ------")
 
         # JOBS V2
         stm = select(JobV2).order_by(JobV2.id)
         jobs = db.execute(stm).scalars().all()
         for job in jobs:
-            try:
-                job.dataset_dump["type_filters"] = job.dataset_dump["filters"][
-                    "types"
-                ]
-                job.dataset_dump["attribute_filters"] = dict_values_to_list(
-                    job.dataset_dump["filters"]["attributes"],
-                    f"JobV2[{job.id}].dataset_dump.filters.attributes",
-                )
-                job.dataset_dump.pop("filters")
-                flag_modified(job, "dataset_dump")
-                JobReadV2(**job.model_dump())
-                db.add(job)
-                logger.info(f"Fixed filters in JobV2[{job.id}].datasetdump")
-            except RuntimeError as e:
-                logger.info(f"Skipping JobV2[{job.id}]. Error: '{e}'.")
+            logger.info(f"JobV2[{job.id}] START")
+            job.dataset_dump["type_filters"] = job.dataset_dump["filters"][
+                "types"
+            ]
+            job.dataset_dump["attribute_filters"] = dict_values_to_list(
+                job.dataset_dump["filters"]["attributes"],
+                f"JobV2[{job.id}].dataset_dump.filters.attributes",
+            )
+            job.dataset_dump.pop("filters")
+            flag_modified(job, "dataset_dump")
+            JobReadV2(**job.model_dump())
+            db.add(job)
+            logger.info(f"JobV2[{job.id}] END - fixed filters")
 
         db.commit()
         logger.info("Changes committed.")
