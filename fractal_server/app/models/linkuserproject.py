d2e79ef1fdeab3036b7fe0166ebb50bd085376ad
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-01-29 15:36:16 +0100

    Empty out v1 models

--- fractal_server/app/models/linkuserproject.py
+++ fractal_server/app/models/linkuserproject.py
@@ -1,14 +1,11 @@
+from typing import Optional
+
 from sqlmodel import Field
 from sqlmodel import SQLModel
 
 
 class LinkUserProject(SQLModel, table=True):
-    """
-    Crossing table between User and Project
-    """
-
-    project_id: int = Field(foreign_key="project.id", primary_key=True)
-    user_id: int = Field(foreign_key="user_oauth.id", primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
 
 
 class LinkUserProjectV2(SQLModel, table=True):
