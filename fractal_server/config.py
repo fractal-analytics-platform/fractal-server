74691a467b18c284202b7db93757a70babd41c0b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-01-29 13:57:10 +0100

    Remove some more v1 code from fractal-server

--- fractal_server/config.py
+++ fractal_server/config.py
@@ -496,26 +496,12 @@ class Settings(BaseSettings):
     `JobExecutionError`.
     """
 
-    FRACTAL_API_SUBMIT_RATE_LIMIT: int = 2
-    """
-    Interval to wait (in seconds) to be allowed to call again
-    `POST api/v1/{project_id}/workflow/{workflow_id}/apply/`
-    with the same path and query parameters.
-    """
-
     FRACTAL_RUNNER_TASKS_INCLUDE_IMAGE: str = (
         "Copy OME-Zarr structure;Convert Metadata Components from 2D to 3D"
     )
     """
     `;`-separated list of names for task that require the `metadata["image"]`
     attribute in their input-arguments JSON file.
-    """
-
-    FRACTAL_API_V1_MODE: Literal[
-        "include", "include_read_only", "exclude"
-    ] = "include"
-    """
-    Whether to include the v1 API.
     """
 
     FRACTAL_PIP_CACHE_DIR: Optional[str] = None
--- fractal_server/main.py
+++ fractal_server/main.py
@@ -39,20 +39,11 @@ def collect_routers(app: FastAPI) -> None:
             The application to register the routers to.
     """
     from .app.routes.api import router_api
-    from .app.routes.api.v1 import router_api_v1
     from .app.routes.api.v2 import router_api_v2
-    from .app.routes.admin.v1 import router_admin_v1
     from .app.routes.admin.v2 import router_admin_v2
     from .app.routes.auth.router import router_auth
 
-    settings = Inject(get_settings)
-
     app.include_router(router_api, prefix="/api")
-    if settings.FRACTAL_API_V1_MODE.startswith("include"):
-        app.include_router(router_api_v1, prefix="/api/v1")
-        app.include_router(
-            router_admin_v1, prefix="/admin/v1", tags=["V1 Admin area"]
-        )
     app.include_router(router_api_v2, prefix="/api/v2")
     app.include_router(
         router_admin_v2, prefix="/admin/v2", tags=["V2 Admin area"]
