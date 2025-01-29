5a1f647a7376679244c4077ab440076ceb479807
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-01-29 15:22:18 +0100

    Drop V1 tables / migration

--- fractal_server/migrations/env.py
+++ fractal_server/migrations/env.py
@@ -67,6 +67,7 @@ def do_run_migrations(connection: Connection) -> None:
     context.configure(
         connection=connection,
         target_metadata=target_metadata,
+        dialect_opts={"paramstyle": "named"},
         render_as_batch=True,
     )
 
