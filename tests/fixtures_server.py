e049e14799395c0918a85f5d2facf1d2123da0a8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-01-29 14:00:04 +0100

    Remove `FRACTAL_API_SUBMIT_RATE_LIMIT`

--- tests/fixtures_server.py
+++ tests/fixtures_server.py
@@ -42,7 +42,6 @@ def get_patched_settings(temp_path: Path):
     settings.FRACTAL_RUNNER_WORKING_BASE_DIR = temp_path / "artifacts"
     settings.FRACTAL_RUNNER_WORKING_BASE_DIR.mkdir(parents=True, exist_ok=True)
     settings.FRACTAL_RUNNER_WORKING_BASE_DIR.chmod(0o755)
-    settings.FRACTAL_API_SUBMIT_RATE_LIMIT = 0
     settings.FRACTAL_API_MAX_JOB_LIST_LENGTH = 1
     settings.FRACTAL_GRACEFUL_SHUTDOWN_TIME = 1
 
