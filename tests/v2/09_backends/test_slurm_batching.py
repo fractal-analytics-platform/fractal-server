a191ce7ded06340c282c05d89e0437ee6ca8ec48
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-02-03 09:00:48 +0100

    Improve test

--- tests/v2/09_backends/test_slurm_batching.py
+++ tests/v2/09_backends/test_slurm_batching.py
@@ -1,18 +1,22 @@
-from devtools import debug
-
 from fractal_server.app.runner.executors.slurm._batching import heuristics
 
 
-def test_heuristics():
-    res = heuristics(
-        tot_tasks=10000,
+def test_heuristics_many_tasks():
+    """
+    Test a specific case of slurm-job batching.
+    """
+
+    NUM_TASKS = 10000
+    tasks_per_job, parallel_tasks_per_job = heuristics(
+        tot_tasks=NUM_TASKS,
         cpus_per_task=1,
-        mem_per_task=1,
+        mem_per_task=100,
         target_cpus_per_job=1,
-        max_cpus_per_job=10,
-        target_mem_per_job=1,
-        max_mem_per_job=10,
-        target_num_jobs=100,
+        max_cpus_per_job=1,
+        target_mem_per_job=100,
+        max_mem_per_job=100,
         max_num_jobs=1,
+        target_num_jobs=10,
     )
-    debug(res)
+    assert tasks_per_job == NUM_TASKS
+    assert parallel_tasks_per_job == 1
