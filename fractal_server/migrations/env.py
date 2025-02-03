4d159f2e763c15b649f9b9c38482375881e2ac82
mfranzon <43796979+mfranzon@users.noreply.github.com>
2025-02-03 10:05:01 +0100

    remove run_migrations_offline, make run_migrations_online sync

--- fractal_server/migrations/env.py
+++ fractal_server/migrations/env.py
@@ -1,13 +1,10 @@
-import asyncio
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy.engine import Connection
 from sqlmodel import SQLModel
 
-from fractal_server.config import get_settings
 from fractal_server.migrations.naming_convention import NAMING_CONVENTION
-from fractal_server.syringe import Inject
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -37,32 +34,6 @@ from fractal_server.app import models  # noqa
 # ... etc.
 
 
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    settings = Inject(get_settings)
-    settings.check_db()
-    context.configure(
-        url=settings.DATABASE_ASYNC_URL,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-        render_as_batch=True,
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
 def do_run_migrations(connection: Connection) -> None:
     context.configure(
         connection=connection,
@@ -74,7 +45,7 @@ def do_run_migrations(connection: Connection) -> None:
         context.run_migrations()
 
 
-async def run_migrations_online() -> None:
+def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
@@ -83,14 +54,11 @@ async def run_migrations_online() -> None:
     """
     from fractal_server.app.db import DB
 
-    engine = DB.engine_async()
-    async with engine.connect() as connection:
-        await connection.run_sync(do_run_migrations)
+    engine = DB.engine_sync()
+    with engine.connect() as connection:
+        do_run_migrations(connection=connection)
 
-    await engine.dispose()
+    engine.dispose()
 
 
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    asyncio.run(run_migrations_online())
+run_migrations_online()
