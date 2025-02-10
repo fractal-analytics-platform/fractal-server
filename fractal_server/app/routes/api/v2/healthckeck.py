2aeac5178fa4553397c4fd2657633efcfc0d9c74
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-02-10 10:02:55 +0100

    precommit

--- fractal_server/app/routes/api/v2/healthckeck.py
+++ fractal_server/app/routes/api/v2/healthckeck.py
@@ -1,21 +1,19 @@
-from devtools import debug
 from random import randint
 
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import status
 
-from .project import create_project
-from .dataset import create_dataset
-from .workflow import create_workflow
-
-from ...auth import current_active_user
 from ....db import AsyncSession
 from ....db import get_async_db
 from ....models import UserOAuth
-from ....schemas.v2 import ProjectCreateV2
 from ....schemas.v2 import DatasetCreateV2
+from ....schemas.v2 import ProjectCreateV2
 from ....schemas.v2 import WorkflowCreateV2
+from ...auth import current_active_user
+from .dataset import create_dataset
+from .project import create_project
+from .workflow import create_workflow
 
 router = APIRouter()
 
@@ -25,7 +23,7 @@ async def run_healthcheck(
     user: UserOAuth = Depends(current_active_user),
     db: AsyncSession = Depends(get_async_db),
 ):
-    random_integer = randint(10**9, 10**10 - 1)
+    random_integer = randint(10**9, 10**10 - 1)  # nosec B311
 
     # Create a project
     project = await create_project(
@@ -35,22 +33,20 @@ async def run_healthcheck(
     )
 
     # Create a dataset
-    dataset = await create_dataset(
+    await create_dataset(
         project_id=project.id,
         dataset=DatasetCreateV2(
             name=f"dataset_{random_integer}",
-            zarr_dir="/invalid/zarr/dir/not/to/be/used/"
+            zarr_dir="/invalid/zarr/dir/not/to/be/used/",
         ),
         user=user,
         db=db,
     )
 
     # Create a workflow;
-    workflow = await create_workflow(
+    await create_workflow(
         project_id=project.id,
         workflow=WorkflowCreateV2(name=f"workflow_{random_integer}"),
         user=user,
         db=db,
     )
-
-    debug(project, dataset, workflow)
--- tests/v2/test_03_api/test_api_healthcheck.py
+++ tests/v2/test_03_api/test_api_healthcheck.py
@@ -3,7 +3,7 @@ from devtools import debug
 
 async def test_healthcheck(client, db, MockCurrentUser):
 
-    async with MockCurrentUser() as userA:
+    async with MockCurrentUser():
         res = await client.post("/api/v2/healthcheck/")
         debug(res)
         assert res.status_code == 200
