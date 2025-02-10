b0b583b5f400fd34c114665432121f86f8b341a8
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-02-10 18:03:39 +0100

    complete endpoint

--- fractal_server/app/routes/api/v2/healthckeck.py
+++ fractal_server/app/routes/api/v2/healthckeck.py
@@ -1,28 +1,48 @@
 from random import randint
 
 from fastapi import APIRouter
+from fastapi import BackgroundTasks
 from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Request
 from fastapi import status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from sqlmodel import select
 
 from ....db import AsyncSession
 from ....db import get_async_db
+from ....models import TaskGroupV2
 from ....models import UserOAuth
 from ....schemas.v2 import DatasetCreateV2
+from ....schemas.v2 import JobCreateV2
 from ....schemas.v2 import ProjectCreateV2
+from ....schemas.v2 import TaskCreateV2
 from ....schemas.v2 import WorkflowCreateV2
+from ....schemas.v2 import WorkflowTaskCreateV2
 from ...auth import current_active_user
 from .dataset import create_dataset
 from .project import create_project
+from .submit import apply_workflow
+from .task import create_task
 from .workflow import create_workflow
+from .workflowtask import create_workflowtask
 
 router = APIRouter()
 
 
+class HealthCheck(BaseModel):
+    zarr_dir: str
+
+
 @router.post("/", status_code=status.HTTP_200_OK)
 async def run_healthcheck(
+    payload: HealthCheck,
+    request: Request,
     user: UserOAuth = Depends(current_active_user),
     db: AsyncSession = Depends(get_async_db),
-):
+) -> JSONResponse:
+
     random_integer = randint(10**9, 10**10 - 1)  # nosec B311
 
     # Create a project
@@ -33,20 +53,64 @@ async def run_healthcheck(
     )
 
     # Create a dataset
-    await create_dataset(
+    dataset = await create_dataset(
         project_id=project.id,
         dataset=DatasetCreateV2(
             name=f"dataset_{random_integer}",
-            zarr_dir="/invalid/zarr/dir/not/to/be/used/",
+            zarr_dir=payload.zarr_dir,
         ),
         user=user,
         db=db,
     )
 
     # Create a workflow;
-    await create_workflow(
+    workflow = await create_workflow(
         project_id=project.id,
         workflow=WorkflowCreateV2(name=f"workflow_{random_integer}"),
         user=user,
         db=db,
     )
+
+    # Create (or get) a task
+    try:
+        task = await create_task(
+            task=TaskCreateV2(
+                name="__TEST_ECHO_TASK__",
+                version="9.9.9",
+                command_non_parallel="echo",
+            ),
+            user=user,
+            db=db,
+        )
+    except HTTPException as e:
+        if "already owns a task" not in e.detail:
+            raise e
+        res = await db.execute(
+            select(TaskGroupV2)
+            .where(TaskGroupV2.user_id == user.id)
+            .where(TaskGroupV2.pkg_name == "__TEST_ECHO_TASK__")
+            .where(TaskGroupV2.version == "9.9.9")
+        )
+        task_group = res.scalar_one()
+        task = task_group.task_list[0]
+
+    await create_workflowtask(
+        project_id=project.id,
+        workflow_id=workflow.id,
+        task_id=task.id,
+        wftask=WorkflowTaskCreateV2(),
+        user=user,
+        db=db,
+    )
+
+    job = await apply_workflow(
+        project_id=project.id,
+        workflow_id=workflow.id,
+        dataset_id=dataset.id,
+        job_create=JobCreateV2(),
+        background_tasks=BackgroundTasks(),
+        request=request,
+        user=user,
+        db=db,
+    )
+    return JSONResponse(content=job.id, status_code=200)
--- tests/v2/test_03_api/test_api_healthcheck.py
+++ tests/v2/test_03_api/test_api_healthcheck.py
@@ -1,9 +1,7 @@
-from devtools import debug
-
-
-async def test_healthcheck(client, db, MockCurrentUser):
+async def test_healthcheck(client, db, MockCurrentUser, tmp_path):
 
     async with MockCurrentUser():
-        res = await client.post("/api/v2/healthcheck/")
-        debug(res)
+        res = await client.post(
+            "/api/v2/healthcheck/", json={"zarr_dir": tmp_path.as_posix()}
+        )
         assert res.status_code == 200
