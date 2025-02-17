f33301a764d8b10c4d6555aeae4ab83cf049471a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-02-17 15:46:38 +0100

    Cleanup endpoint and test

--- fractal_server/app/routes/admin/v2/accounting.py
+++ fractal_server/app/routes/admin/v2/accounting.py
@@ -4,10 +4,12 @@ from typing import Optional
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
+from fastapi import Query
 from fastapi import status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from pydantic.types import AwareDatetime
+from sqlmodel import func
 from sqlmodel import select
 
 from fractal_server.app.db import AsyncSession
@@ -40,56 +42,48 @@ router = APIRouter()
 async def query_accounting(
     query: AccountingQuery,
     # pagination
-    page: int = 1,
-    page_size: Optional[int] = None,
+    page: int = Query(default=1, ge=1),
+    page_size: Optional[int] = Query(default=None, ge=1),
     # dependencies
     superuser: UserOAuth = Depends(current_active_superuser),
     db: AsyncSession = Depends(get_async_db),
 ) -> AccountingPage:
 
-    _raise_if_naive_datetime(query.timestamp_min, query.timestamp_max)
-
-    if page < 1:
+    if page_size is None and page > 1:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=f"Invalid pagination parameter: page={page} < 1",
+            detail=(f"Invalid pagination parameters: {page=}, {page_size=}."),
         )
 
-    stm = select(AccountingRecord)
+    stm = select(AccountingRecord).order_by(AccountingRecord.id)
+    stm_count = select(func.count(AccountingRecord.id))
     if query.user_id is not None:
         stm = stm.where(AccountingRecord.user_id == query.user_id)
+        stm_count = stm_count.where(AccountingRecord.user_id == query.user_id)
     if query.timestamp_min is not None:
         stm = stm.where(AccountingRecord.timestamp >= query.timestamp_min)
+        stm_count = stm_count.where(
+            AccountingRecord.timestamp >= query.timestamp_min
+        )
     if query.timestamp_max is not None:
         stm = stm.where(AccountingRecord.timestamp <= query.timestamp_max)
-
-    if page_size is None:
-        if page > 1:
-            raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=(
-                    f"Invalid pagination parameters: {page=}, {page_size=}."
-                ),
-            )
-    else:
-        if page_size <= 0:
-            raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=(
-                    f"Invalid pagination parameter: page_size={page_size} <= 0"
-                ),
-            )
+        stm_count = stm_count.where(
+            AccountingRecord.timestamp <= query.timestamp_max
+        )
+    if page_size is not None:
         stm = stm.offset((page - 1) * page_size).limit(page_size)
 
     res = await db.execute(stm)
-    accounting_list = res.scalars().all()
+    records = res.scalars().all()
+    res_total_count = await db.execute(stm_count)
+    total_count = res_total_count.scalar()
 
-    actual_page_size = page_size or len(accounting_list)
+    actual_page_size = page_size or len(records)
     return AccountingPage(
-        total_count=len(accounting_list),
+        total_count=total_count,
         page_size=actual_page_size,
         current_page=page,
-        records=[record.model_dump() for record in accounting_list],
+        records=[record.model_dump() for record in records],
     )
 
 
--- tests/v2/test_03_api/admin/test_admin_accounting.py
+++ tests/v2/test_03_api/admin/test_admin_accounting.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from devtools import debug
 
 from fractal_server.app.models.v2 import AccountingRecord
@@ -13,16 +15,16 @@ async def test_accounting(
         db.add(
             AccountingRecord(
                 user_id=user1.id,
-                num_new_images=10,
-                num_tasks=20,
+                num_new_images=1,
+                num_tasks=1,
             )
         )
         intermediate_time = get_timestamp()
         db.add(
             AccountingRecord(
                 user_id=user1.id,
-                num_new_images=10,
-                num_tasks=20,
+                num_new_images=2,
+                num_tasks=2,
             )
         )
         await db.commit()
@@ -30,12 +32,13 @@ async def test_accounting(
         db.add(
             AccountingRecord(
                 user_id=user2.id,
-                num_new_images=10,
-                num_tasks=20,
+                num_new_images=3,
+                num_tasks=3,
             )
         )
 
     async with MockCurrentUser(user_kwargs=dict(is_superuser=True)):
+        # Test timestamp/user filters
         res = await client.post(
             "/admin/v2/accounting/",
             json=dict(
@@ -48,12 +51,58 @@ async def test_accounting(
         debug(accounting_list)
         assert res.json()["total_count"] == 1
 
+        # Test pagination
+        res = await client.post("/admin/v2/accounting/?page_size=1", json={})
+        assert res.status_code == 200
+        assert res.json()["total_count"] == 3
+        assert res.json()["page_size"] == 1
+        assert res.json()["current_page"] == 1
+        assert res.json()["records"][0]["num_tasks"] == 1
+        res = await client.post(
+            "/admin/v2/accounting/?page_size=1&page=2", json={}
+        )
+        assert res.status_code == 200
+        assert res.json()["total_count"] == 3
+        assert res.json()["page_size"] == 1
+        assert res.json()["current_page"] == 2
+        assert res.json()["records"][0]["num_tasks"] == 2
+        res = await client.post(
+            "/admin/v2/accounting/?page_size=1&page=3", json={}
+        )
+        assert res.status_code == 200
+        assert res.json()["total_count"] == 3
+        assert res.json()["page_size"] == 1
+        assert res.json()["current_page"] == 3
+        assert res.json()["records"][0]["num_tasks"] == 3
 
-async def test_accounting_non_superuser(
+
+async def test_accounting_api_failure(
     db,
     client,
     MockCurrentUser,
 ):
+    # Non admin
     async with MockCurrentUser(user_kwargs=dict(is_superuser=False)):
         res = await client.post("/admin/v2/accounting/", json={})
         assert res.status_code == 401
+
+    # Naive datetime
+    naive_datetime = datetime.now().isoformat()
+    async with MockCurrentUser(user_kwargs=dict(is_superuser=True)):
+        res = await client.post(
+            "/admin/v2/accounting/",
+            json=dict(
+                timestamp_min=naive_datetime,
+            ),
+        )
+        assert res.status_code == 422
+        assert "Input should have timezone info" in str(res.json())
+
+    # Pagination error
+    async with MockCurrentUser(user_kwargs=dict(is_superuser=True)):
+        res = await client.post(
+            "/admin/v2/accounting/?page=2",
+            json={},
+        )
+        assert res.status_code == 422
+        assert "Invalid pagination parameters" in str(res.json())
