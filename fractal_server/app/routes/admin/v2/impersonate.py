1ffb79e7fec91a5b973691ac11388065ed4543d9
mfranzon <43796979+mfranzon@users.noreply.github.com>
2025-02-17 16:20:05 +0100

    fix lifetime, use user_or_404

--- fractal_server/app/routes/admin/v2/impersonate.py
+++ fractal_server/app/routes/admin/v2/impersonate.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from fastapi import Depends
-from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 from fastapi_users.authentication import JWTStrategy
 
@@ -8,6 +7,7 @@ from fractal_server.app.db import AsyncSession
 from fractal_server.app.db import get_async_db
 from fractal_server.app.models import UserOAuth
 from fractal_server.app.routes.auth import current_active_superuser
+from fractal_server.app.routes.auth._aux_auth import _user_or_404
 from fractal_server.config import get_settings
 from fractal_server.syringe import Inject
 
@@ -20,14 +20,12 @@ async def impersonate_user(
     superuser: UserOAuth = Depends(current_active_superuser),
     db: AsyncSession = Depends(get_async_db),
 ) -> JSONResponse:
-    user = await db.get(UserOAuth, user_id)
-    if user is None:
-        raise HTTPException(404, detail="User not found")
+    user = await _user_or_404(user_id, db)
 
     settings = Inject(get_settings)
     jwt_strategy = JWTStrategy(
         secret=settings.JWT_SECRET_KEY,  # type: ignore
-        lifetime_seconds=120,  # 2 hours
+        lifetime_seconds=7200,  # 2 hours
     )
     token = await jwt_strategy.write_token(user)
 
