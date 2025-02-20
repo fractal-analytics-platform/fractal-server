14d5c99394d39045aed8a023fcec4316518ef202
mfranzon <43796979+mfranzon@users.noreply.github.com>
2025-02-20 11:52:14 +0100

    delete historyitem when deleting dataset

--- fractal_server/app/routes/api/v2/dataset.py
+++ fractal_server/app/routes/api/v2/dataset.py
@@ -48,7 +48,6 @@ async def create_dataset(
     )
 
     if dataset.zarr_dir is None:
-
         if user.settings.project_dir is None:
             raise HTTPException(
                 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
@@ -228,11 +227,19 @@ async def delete_dataset(
 
     # Cascade operations: set foreign-keys to null for history items which are
     # in relationship with the current dataset
+
+    # Version 1, pythonic way
     stm = select(HistoryItemV2).where(HistoryItemV2.dataset_id == dataset_id)
     res = await db.execute(stm)
     history_items = res.scalars().all()
     for history_item in history_items:
-        history_item.dataset_id = None
+        await db.delete(history_item)
+    # Version 2, SQL like
+    # from sqlalchemy import delete
+
+    # stm = delete(HistoryItemV2).where(HistoryItemV2.dataset_id == dataset_id)
+    # await db.execute(stm)
+    # await db.commit()
 
     # Delete dataset
     await db.delete(dataset)
