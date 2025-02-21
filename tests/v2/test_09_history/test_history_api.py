481b4b8fe2b491cbb0fe42335bd94cc035a96dbc
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-02-21 10:23:09 +0100

    test failing

--- tests/v2/test_09_history/test_history_api.py
+++ tests/v2/test_09_history/test_history_api.py
@@ -10,15 +10,16 @@ async def test_delete_workflow_associated_to_history(
     db,
     client,
     MockCurrentUser,
-    tmp_path,
 ):
     async with MockCurrentUser() as user:
 
-        # Create project
+        task = await task_factory_v2(user_id=user.id)
         project = await project_factory_v2(user)
         dataset = await dataset_factory_v2(project_id=project.id)
+
+        # Delete Workflow
+
         workflow = await workflow_factory_v2(project_id=project.id)
-        task = await task_factory_v2(user_id=user.id)
         wftask = await workflowtask_factory_v2(
             workflow_id=workflow.id, task_id=task.id
         )
@@ -41,6 +42,37 @@ async def test_delete_workflow_associated_to_history(
 
         await client.delete(
             f"/api/v2/project/{project.id}/workflow/{workflow.id}/"
+        )
+
+        await db.refresh(history)
+        assert history.workflowtask_id is None
+
+        # Delete WorkflowTask
+
+        workflow = await workflow_factory_v2(project_id=project.id)
+        wftask = await workflowtask_factory_v2(
+            workflow_id=workflow.id, task_id=task.id
+        )
+
+        history = HistoryItemV2(
+            dataset_id=dataset.id,
+            workflowtask_id=wftask.id,
+            worfklowtask_dump={},
+            task_group_dump={},
+            parameters_hash="abc",
+            num_available_images=0,
+            num_current_images=0,
+            images={},
+        )
+        db.add(history)
+        await db.commit()
+
+        await db.refresh(history)
+        assert history.workflowtask_id == wftask.id
+
+        await client.delete(
+            "/api/v2/project/"
+            f"{project.id}/workflow/{workflow.id}/wftask/{wftask.id}/"
         )
 
         await db.refresh(history)
