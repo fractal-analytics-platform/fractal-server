2bd37153d92ae499a4202d7ba28b7136b2409482
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-02-26 17:21:43 +0100

    Drop legacy status router

--- fractal_server/app/routes/api/v2/__init__.py
+++ fractal_server/app/routes/api/v2/__init__.py
@@ -8,7 +8,6 @@ from .history import router as history_router_v2
 from .images import router as images_routes_v2
 from .job import router as job_router_v2
 from .project import router as project_router_v2
-from .status import router as status_router_v2
 from .submit import router as submit_job_router_v2
 from .task import router as task_router_v2
 from .task_collection import router as task_collection_router_v2
@@ -58,4 +57,3 @@ router_api_v2.include_router(
     workflow_import_router_v2, tags=["V2 Workflow Import"]
 )
 router_api_v2.include_router(workflowtask_router_v2, tags=["V2 WorkflowTask"])
-router_api_v2.include_router(status_router_v2, tags=["V2 Status"])
