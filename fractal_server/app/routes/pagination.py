374226ce8362c6936a35121741c36c2c13181218
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-02-28 15:51:46 +0100

    fix dependency

--- fractal_server/app/routes/pagination.py
+++ fractal_server/app/routes/pagination.py
@@ -2,9 +2,11 @@ from typing import Generic
 from typing import Optional
 from typing import TypeVar
 
+from fastapi import HTTPException
 from pydantic import BaseModel
 from pydantic import Field
 from pydantic import model_validator
+from pydantic import ValidationError
 
 T = TypeVar("T")
 
@@ -26,7 +28,22 @@ class Pagination(BaseModel):
 def get_pagination_params(
     page: int = 1, page_size: Optional[int] = None
 ) -> Pagination:
-    return Pagination(page=page, page_size=page_size)
+    if page_size is None and page > 1:
+        raise HTTPException(
+            status_code=422,
+            detail=(
+                "Invalid pagination parameters: "
+                "'page_size' is None but page is greater than 1."
+            ),
+        )
+    try:
+        pagination = Pagination(page=page, page_size=page_size)
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=422,
+            detail=f"Invalid pagination parameters. Original error: '{e}'.",
+        )
+    return pagination
 
 
 class Page(BaseModel, Generic[T]):
