db2d869d70b036ec8d23a5d203a60fef2aa2605e
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-02-28 15:22:02 +0100

    use pagination in accounting api

--- fractal_server/app/routes/admin/v2/accounting.py
+++ fractal_server/app/routes/admin/v2/accounting.py
@@ -4,7 +4,6 @@ from typing import Optional
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
-from fastapi import Query
 from fastapi import status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -18,6 +17,9 @@ from fractal_server.app.models import UserOAuth
 from fractal_server.app.models.v2 import AccountingRecord
 from fractal_server.app.models.v2 import AccountingRecordSlurm
 from fractal_server.app.routes.auth import current_active_superuser
+from fractal_server.app.routes.pagination import get_pagination_params
+from fractal_server.app.routes.pagination import Page
+from fractal_server.app.routes.pagination import Pagination
 from fractal_server.app.schemas.v2 import AccountingRecordRead
 
 
@@ -27,26 +29,19 @@ class AccountingQuery(BaseModel):
     timestamp_max: Optional[AwareDatetime] = None
 
 
-class AccountingPage(BaseModel):
-    total_count: int
-    page_size: int
-    current_page: int
-    records: list[AccountingRecordRead]
-
-
 router = APIRouter()
 
 
-@router.post("/", response_model=AccountingPage)
+@router.post("/", response_model=Page[AccountingRecordRead])
 async def query_accounting(
     query: AccountingQuery,
-    # pagination
-    page: int = Query(default=1, ge=1),
-    page_size: Optional[int] = Query(default=None, ge=1),
     # dependencies
+    pagination: Pagination = Depends(get_pagination_params),
     superuser: UserOAuth = Depends(current_active_superuser),
     db: AsyncSession = Depends(get_async_db),
-) -> AccountingPage:
+) -> Page[AccountingRecordRead]:
+    page = pagination.page
+    page_size = pagination.page_size
 
     if page_size is None and page > 1:
         raise HTTPException(
@@ -82,11 +77,12 @@ async def query_accounting(
     records = res.scalars().all()
 
     actual_page_size = page_size or len(records)
-    return AccountingPage(
+
+    return Page[AccountingRecordRead](
         total_count=total_count,
         page_size=actual_page_size,
         current_page=page,
-        records=[record.model_dump() for record in records],
+        items=[record.model_dump() for record in records],
     )
 
 
