ae9d55a1a0db0a6b430ef89860a93efd51eee19a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-03-04 08:39:58 +0100

    Sort subset-endpoint reponse items

--- fractal_server/app/routes/api/v2/history.py
+++ fractal_server/app/routes/api/v2/history.py
@@ -140,21 +140,27 @@ async def get_per_workflowtask_subsets_aggregated_info(
         .group_by(ImageStatus.parameters_hash)
     )
     res = await db.execute(stm)
-    hash_statuses = res.all()
+    hash_to_statuses = res.all()
 
-    result = []
-    for _hash, statuses in hash_statuses:
-        dump = await db.execute(
-            select(HistoryItemV2.workflowtask_dump)
+    subsets = []
+    for parameters_hash, statuses in hash_to_statuses:
+        # Get the oldest HistoryItemV2 matching with `parameters_hash`
+        stm = (
+            select(HistoryItemV2)
             .where(HistoryItemV2.workflowtask_id == workflowtask_id)
             .where(HistoryItemV2.dataset_id == dataset_id)
-            .where(HistoryItemV2.parameters_hash == _hash)
+            .where(HistoryItemV2.parameters_hash == parameters_hash)
+            .order_by(HistoryItemV2.timestamp_started)
             .limit(1)
         )
-        result.append(
+        res = await db.execute(stm)
+        oldest_history_item = res.scalar_one()
+
+        subsets.append(
             {
-                "workflowtask_dump": dump.scalar_one(),
-                "parameters_hash": _hash,
+                "_timestamp": oldest_history_item.timestamp_started,
+                "workflowtask_dump": oldest_history_item.workflowtask_dump,
+                "parameters_hash": parameters_hash,
                 "info": {
                     "num_done_images": statuses.count(
                         HistoryItemImageStatus.DONE
@@ -169,7 +175,11 @@ async def get_per_workflowtask_subsets_aggregated_info(
             }
         )
 
-    return JSONResponse(content=result, status_code=200)
+    # Use `_timestamp` values for sorting, and then drop them from the response
+    sorted_results = sorted(subsets, key=lambda obj: obj["_timestamp"])
+    [item.pop("_timestamp") for item in sorted_results]
+
+    return JSONResponse(content=sorted_results, status_code=200)
 
 
 @router.get("/project/{project_id}/status/images/")
