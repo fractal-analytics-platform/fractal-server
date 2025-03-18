bdc46fc30638ff05af12d39d5e28b55b753c8f69
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-03-18 14:05:39 +0100

    unit test

--- tests/v2/test_09_history/test_unit_api_aux_functions.py
+++ tests/v2/test_09_history/test_unit_api_aux_functions.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import pytest
@@ -12,14 +13,6 @@ from fractal_server.app.routes.api.v2._aux_functions_history import (
 )
 
 
-class MockTaskV2(BaseModel):
-    name: str = "task-name"
-
-
-class MockWorkflowTaskV2(BaseModel):
-    task: MockTaskV2
-
-
 async def test_get_history_unit_or_404(db):
     with pytest.raises(
         HTTPException,
@@ -32,19 +25,30 @@ async def test_get_history_unit_or_404(db):
 
 
 def test_read_log_file(tmp_path: Path):
+    class MockTaskV2(BaseModel):
+        name: str = "task-name"
+
+    class MockWorkflowTaskV2(BaseModel):
+        task: MockTaskV2
+
     wftask = MockWorkflowTaskV2(task=MockTaskV2())
-    dataset_id = 1
 
-    log = read_log_file(logfile=None, wftask=wftask, dataset_id=dataset_id)
+    # Case 1: Undefined logfile
+    log = read_log_file(logfile=None, wftask=wftask, dataset_id=1)
     assert "not available" in log
 
+    # Case 2: File does not exist
     logfile = (tmp_path / "logs.txt").as_posix()
-
-    log = read_log_file(logfile=logfile, wftask=wftask, dataset_id=dataset_id)
+    log = read_log_file(logfile=logfile, wftask=wftask, dataset_id=1)
     assert "not available" in log
 
+    # Case 3: File exists and can be read
     with open(logfile, "w") as f:
         f.write("some keyword\n")
-
-    log = read_log_file(logfile=logfile, wftask=wftask, dataset_id=dataset_id)
+    log = read_log_file(logfile=logfile, wftask=wftask, dataset_id=1)
     assert "some keyword" in log
+
+    # Case 4: File exists but cannot be read
+    os.chmod(logfile, 0o000)
+    log = read_log_file(logfile=logfile, wftask=wftask, dataset_id=1)
+    assert "Permission denied" in log
