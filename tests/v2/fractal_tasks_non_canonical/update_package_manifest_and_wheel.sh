9181306cd34e97cf5aac8c6291d9e250f544cdaa
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-03-20 09:03:32 +0100

    Update wheel with non-canonical name

--- tests/v2/fractal_tasks_non_canonical/update_package_manifest_and_wheel.sh
+++ tests/v2/fractal_tasks_non_canonical/update_package_manifest_and_wheel.sh
@@ -10,3 +10,5 @@ python3 -m pip install -e ".[dev]"
 fractal-manifest create --package fractal-tasks-non-canonical
 python3 -m build
 deactivate
+
+mv dist/fractal_tasks_non_canonical-0.0.1-py3-none-any.whl dist/FrAcTaL_TaSkS_NoN_CaNoNiCaL-0.0.1-py3-none-any.whl
