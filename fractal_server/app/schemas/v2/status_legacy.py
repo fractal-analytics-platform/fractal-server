84305d9eb0b36c1ea8081c19ea584e54ec98e60a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-03-24 15:07:42 +0100

    Mark old status endpoint/schemas as legacy

--- fractal_server/app/schemas/v2/status_legacy.py
+++ fractal_server/app/schemas/v2/status_legacy.py
@@ -4,7 +4,7 @@ from pydantic import Field
 from .workflowtask import WorkflowTaskStatusTypeV2
 
 
-class StatusReadV2(BaseModel):
+class LegacyStatusReadV2(BaseModel):
     """
     Response type for the
     `/project/{project_id}/status/` endpoint
