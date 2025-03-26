813e9541566fcbcf6874bd658451754e80f999ad
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-03-26 08:38:51 +0100

    Drop `DatasetV2.type_filters`, add `JobV2.type_filters`

--- fractal_server/app/models/v2/dataset.py
+++ fractal_server/app/models/v2/dataset.py
@@ -34,16 +34,11 @@ class DatasetV2(SQLModel, table=True):
         sa_column=Column(DateTime(timezone=True), nullable=False),
     )
 
-    # New in V2
-
     zarr_dir: str
     images: list[dict[str, Any]] = Field(
         sa_column=Column(JSON, server_default="[]", nullable=False)
     )
 
-    type_filters: dict[str, bool] = Field(
-        sa_column=Column(JSON, nullable=False, server_default="{}")
-    )
     attribute_filters: AttributeFiltersType = Field(
         sa_column=Column(JSON, nullable=False, server_default="{}")
     )
