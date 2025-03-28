5e6b78580cb12aa7996c363086c3b4ddbb5d0640
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-03-28 11:37:02 +0100

    Use fractal-task-tools task wrapper in mock tasks

--- tests/v2/fractal_tasks_mock/src/fractal_tasks_mock/utils.py
+++ tests/v2/fractal_tasks_mock/src/fractal_tasks_mock/utils.py
@@ -1,9 +1,4 @@
-import json
-import logging
 import os
-import sys
-from argparse import ArgumentParser
-from pathlib import Path
 
 
 def _extract_common_root(zarr_urls: list[str]) -> dict[str, str]:
@@ -47,36 +42,3 @@ def _group_zarr_urls_by_well(zarr_urls: list[str]) -> dict[str, list[str]]:
         else:
             well_to_zarr_urls[well] = [zarr_url]
     return well_to_zarr_urls
-
-
-def run_fractal_task(task_function: callable) -> None:
-    # Parse command-line-interface arguments
-    parser = ArgumentParser()
-    parser.add_argument(
-        "--args-json", help="JSON file with task arguments", required=True
-    )
-    parser.add_argument(
-        "--out-json",
-        help="JSON file for task output metadata.",
-        required=True,
-    )
-    cli_args = parser.parse_args()
-
-    # Preliminary check
-    if Path(cli_args.out_json).exists():
-        logging.error(f"Output file {cli_args.out_json} already exists, exit.")
-        sys.exit(1)
-
-    # Read parameters dictionary
-    with open(cli_args.args_json, "r") as f:
-        task_args = json.load(f)
-
-    # Run task
-    logging.info(f"START {task_function.__name__} task")
-    metadata_update = task_function(**task_args)
-    logging.info(f"END {task_function.__name__} task")
-
-    # Write output metadata to file
-    if metadata_update not in [None, {}]:
-        with open(cli_args.out_json, "w") as fout:
-            json.dump(metadata_update, fout, indent=2)
