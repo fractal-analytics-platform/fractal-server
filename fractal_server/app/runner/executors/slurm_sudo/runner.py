e07a86a53c4f71e6e8cddcf229a60502097b78ba
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-03-28 16:41:55 +0100

    add logs

--- fractal_server/app/runner/executors/slurm_sudo/runner.py
+++ fractal_server/app/runner/executors/slurm_sudo/runner.py
@@ -719,12 +719,13 @@ class RunnerSlurmSudo(BaseRunner):
 
         # FIXME
         jobs_that_started = set()
-        while len(jobs_that_started) == len(self.jobs):
+        while len(jobs_that_started) == len(self.job_ids):
             res = run_squeue(self.job_ids)
             new_jobs = set(out.split()[0] for out in res.stdout.splitlines())
             jobs_that_started.union(new_jobs)
             logger.debug(f"{new_jobs=}")
             logger.debug(f"{len(jobs_that_started)=}")
+
         # Retrieval phase
         while len(self.jobs) > 0:
             if self.is_shutdown():
