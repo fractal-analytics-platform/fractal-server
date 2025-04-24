0728d8d30f01e4640504f44f3b95243efadfc9ac
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-04-24 13:52:54 +0200

    Trim fractal_tasks_non_canonical package (ref #2250)

--- tests/v2/fractal_tasks_non_canonical/src/fractal_tasks_non_canonical/generic_task.py
+++ tests/v2/fractal_tasks_non_canonical/src/fractal_tasks_non_canonical/generic_task.py
@@ -1,16 +1,10 @@
 import logging
-import time
-
-from pydantic import validate_call
 
 
-@validate_call
 def generic_task(
     *,
     zarr_urls: list[str],
     zarr_dir: str,
-    sleep_time: int = 1,
-    raise_error: bool = False,
 ) -> dict:
     """
     Dummy task description.
@@ -18,21 +12,9 @@ def generic_task(
     Arguments:
         zarr_urls: description
         zarr_dir: description
-        sleep_time: Time to sleep, in seconds
-        raise_error: If true, the task raises a ValueError
+
     """
-
     logging.info("[generic_task] START")
-    logging.info(f"[generic_task] {sleep_time=}")
-    logging.info(f"[generic_task] {raise_error=}")
-
-    logging.info(f"[generic_task] Now sleep for {sleep_time} seconds")
-    time.sleep(sleep_time)
-    logging.info(f"[generic_task] I slept for {sleep_time} seconds")
-
-    if raise_error:
-        logging.error("[generic_task] I will now raise an error!")
-        raise ValueError("This is the error message")
     logging.info("[generic_task] END")
 
 
