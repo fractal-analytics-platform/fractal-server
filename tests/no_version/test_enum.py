3030cfbffe67f2e0f487a5eec2dbefc20f626da8
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-05-05 11:40:51 +0200

    unit tests enum in db

---
+++ tests/no_version/test_enum.py
@@ -0,0 +1,40 @@
+from enum import Enum
+
+from sqlmodel import select
+
+from fractal_server.app.models.v2 import ProjectV2
+
+
+class FakeEnum(str, Enum):
+    FOO = "foo"
+    BAR = "bar"
+
+
+async def test_enum_in_db_queries(db):
+    db.add_all(
+        [
+            ProjectV2(name="foo"),
+            ProjectV2(name="foo"),
+            ProjectV2(name="bar"),
+            ProjectV2(name="name"),
+        ]
+    )
+    await db.commit()
+
+    res = await db.execute(
+        select(ProjectV2).where(ProjectV2.name == FakeEnum.FOO)
+    )
+    assert len(res.scalars().all()) == 2
+    res = await db.execute(
+        select(ProjectV2).where(ProjectV2.name != FakeEnum.FOO)
+    )
+    assert len(res.scalars().all()) == 2
+
+    res = await db.execute(
+        select(ProjectV2).where(ProjectV2.name == FakeEnum.BAR)
+    )
+    assert len(res.scalars().all()) == 1
+    res = await db.execute(
+        select(ProjectV2).where(ProjectV2.name != FakeEnum.BAR)
+    )
+    assert len(res.scalars().all()) == 3
