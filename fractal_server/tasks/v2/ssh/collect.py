1d7ba6fd42cfa7f0044440d645f018dadd77bdd9
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-05-05 11:18:59 +0200

    some .value

--- fractal_server/tasks/v2/ssh/collect.py
+++ fractal_server/tasks/v2/ssh/collect.py
@@ -166,7 +166,7 @@ def collect_ssh(
                     script_dir_remote=script_dir_remote,
                     prefix=(
                         f"{int(time.time())}_"
-                        f"{TaskGroupActivityActionV2.COLLECT}"
+                        f"{TaskGroupActivityActionV2.COLLECT.value}"
                     ),
                     fractal_ssh=fractal_ssh,
                     logger_name=LOGGER_NAME,
