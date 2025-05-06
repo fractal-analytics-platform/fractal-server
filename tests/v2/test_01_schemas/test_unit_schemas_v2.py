c6723d091c7f73f70fe55731585911f36ae43829
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-05-06 13:02:36 +0200

    Expand test with trivial addition

--- tests/v2/test_01_schemas/test_unit_schemas_v2.py
+++ tests/v2/test_01_schemas/test_unit_schemas_v2.py
@@ -90,6 +90,7 @@ def test_job_create():
     JobCreateV2()
     JobCreateV2(last_task_index=None)
     JobCreateV2(last_task_index=0)
+    JobCreateV2(last_task_index=1)
     with pytest.raises(ValidationError):
         JobCreateV2(last_task_index=-1)
 
