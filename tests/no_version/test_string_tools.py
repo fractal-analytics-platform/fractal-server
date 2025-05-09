d15931593ce236d6c606b73aad68d5c5ee3d055c
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-05-09 10:31:19 +0200

    remove test string tools

--- tests/no_version/test_string_tools.py
+++ tests/no_version/test_string_tools.py
@@ -1,10 +1,7 @@
-from functools import cmp_to_key
-
 import pytest
 
 from fractal_server.string_tools import __NOT_ALLOWED_FOR_COMMANDS__
 from fractal_server.string_tools import __SPECIAL_CHARACTERS__
-from fractal_server.string_tools import is_version_greater_than
 from fractal_server.string_tools import sanitize_string
 from fractal_server.string_tools import validate_cmd
 
@@ -31,53 +28,3 @@ def test_unit_validate_cmd():
         match="MyAttribute must not contain",
     ):
         validate_cmd("; rm", attribute_name="MyAttribute")
-
-
-def test_is_version_greater_than_than():
-
-    versions = [
-        "2",
-        "0.10.0c0",
-        "0.10.0b4",
-        "0.10.0",
-        "0.10.0alpha3",
-        "0.10.0a2",
-        "1.0.0",
-        "0.10.0a0",
-        "1.0.0rc4.dev7",
-        "0.10.0beta5",
-        "0.10.0alpha0",
-        "0.1.2",
-        "0.1.dev27+g1458b59",
-        "0.2.0a0",
-    ]
-
-    # FIXME this order differs from
-    # https://github.com/fractal-analytics-platform/fractal-web/blob/edfd62660ac20220b14d516eb3f67d826d217b45/components/__tests__/version.test.js
-    expected_sorted_versions = [
-        "0.1.dev27+g1458b59",
-        "0.1.2",
-        "0.2.0a0",
-        "0.10.0a0",
-        "0.10.0alpha0",
-        "0.10.0a2",
-        "0.10.0alpha3",
-        "0.10.0b4",
-        "0.10.0beta5",
-        "0.10.0c0",
-        "0.10.0",
-        "1.0.0rc4.dev7",
-        "1.0.0",
-        "2",
-    ]
-
-    def compare_versions(a, b):
-        if is_version_greater_than(a, b):
-            return 1
-        elif is_version_greater_than(b, a):
-            return -1
-        else:
-            return 0
-
-    sorted_versions = sorted(versions, key=cmp_to_key(compare_versions))
-    assert sorted_versions == expected_sorted_versions
