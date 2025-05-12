b0db6be74372ca5e77500837d46a13a5e0c7ed6d
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-05-12 16:08:59 +0200

    Update and extend `test_task_version_update.py`

--- tests/v2/test_03_api/test_task_version_update.py
+++ tests/v2/test_03_api/test_task_version_update.py
@@ -1,4 +1,4 @@
-from fractal_server.app.routes.api.v2.task_version_update import TaskVersion
+from packaging.version import parse
 
 
 async def test_get_workflow_version_update_candidates(
@@ -15,40 +15,85 @@ async def test_get_workflow_version_update_candidates(
         project = await project_factory_v2(user)
         workflow = await workflow_factory_v2(project_id=project.id)
 
+        # Matching tasks (0, 1, 2)
         task0 = await task_factory_v2(
             user_id=user.id,
+            version="1",
+            task_group_kwargs={"pkg_name": "my_pkg", "version": "1"},
             name="my_task",
             args_schema_parallel={"foo": "bar"},
-            task_group_kwargs={"pkg_name": "my_pkg", "version": "1.0.0a0"},
         )
         task1 = await task_factory_v2(
             user_id=user.id,
+            version="2",
+            task_group_kwargs={"pkg_name": "my_pkg", "version": "2"},
             name="my_task",
             args_schema_parallel={"foo": "bar"},
-            task_group_kwargs={"pkg_name": "my_pkg", "version": "1.0.0a1"},
         )
         task2 = await task_factory_v2(
             user_id=user.id,
+            version="3",
+            task_group_kwargs={"pkg_name": "my_pkg", "version": "3"},
             name="my_task",
             args_schema_parallel={"foo": "bar"},
-            task_group_kwargs={"pkg_name": "my_pkg", "version": "1.0.0"},
         )
         task3 = await task_factory_v2(
             user_id=user.id,
+            version="4",
+            task_group_kwargs={"pkg_name": "my_pkg", "version": "4"},
             name="my_task",
-            # no 'args_schema_parallel'
-            task_group_kwargs={"pkg_name": "my_pkg", "version": "1.0.1"},
+            args_schema_parallel={"foo": "bar"},
         )
+        # Task with no args schemas
         task4 = await task_factory_v2(
+            user_id=user.id,
+            version="5",
+            task_group_kwargs={"pkg_name": "my_pkg", "version": "5"},
+            name="my_task",
+        )
+        # Task with non-parsable version
+        task5 = await task_factory_v2(
             user_id=user.id,
             name="my_task",
             args_schema_parallel={"foo": "bar"},
-            task_group_kwargs={"pkg_name": "my_pkg", "version": "2.0.0rc4"},
+            version="non-parsable-version",
+            task_group_kwargs={
+                "pkg_name": "my_pkg",
+                "version": "non-parsable-version",
+            },
         )
+        # Task with non-matching pkg_name
+        task6 = await task_factory_v2(
+            user_id=user.id,
+            version="6",
+            task_group_kwargs={"pkg_name": "another-one", "version": "6"},
+            name="my_task",
+            type="converter_compound",
+            args_schema_parallel={"foo": "bar"},
+        )
+        # Task with non-compatible type
+        task7 = await task_factory_v2(
+            user_id=user.id,
+            version="6",
+            task_group_kwargs={"pkg_name": "my_pkg", "version": "6"},
+            type="parallel",
+            name="my_task",
+            args_schema_parallel={"foo": "bar"},
+        )
+        assert task0.type != task7.type
 
-        for task_id in [task0.id, task1.id, task2.id, task3.id, task4.id]:
+        for task in [
+            task0,
+            task1,
+            task2,
+            task3,
+            task4,
+            task5,
+            task6,
+            task7,
+        ]:
             await workflowtask_factory_v2(
-                workflow_id=workflow.id, task_id=task_id
+                workflow_id=workflow.id, task_id=task.id
             )
 
         await db.refresh(workflow)
@@ -60,20 +105,25 @@ async def test_get_workflow_version_update_candidates(
 
         assert res.status_code == 200
         assert len(res.json()) == len(workflow.task_list)
-        assert res.json()[0] == [
-            {"task_id": task1.id, "version": "1.0.0a1"},
-            {"task_id": task2.id, "version": "1.0.0"},
-            {"task_id": task4.id, "version": "2.0.0rc4"},
+        assert res.json() == [
+            [
+                {"task_id": task1.id, "version": task1.version},
+                {"task_id": task2.id, "version": task2.version},
+                {"task_id": task3.id, "version": task3.version},
+            ],
+            [
+                {"task_id": task2.id, "version": task2.version},
+                {"task_id": task3.id, "version": task3.version},
+            ],
+            [
+                {"task_id": task3.id, "version": task3.version},
+            ],
+            [],
+            [],
+            [],
+            [],
+            [],
         ]
-        assert res.json()[1] == [
-            {"task_id": task2.id, "version": "1.0.0"},
-            {"task_id": task4.id, "version": "2.0.0rc4"},
-        ]
-        assert res.json()[2] == [
-            {"task_id": task4.id, "version": "2.0.0rc4"},
-        ]
-        assert res.json()[3] == []
-        assert res.json()[4] == []
 
 
 async def test_get_workflow_version_update_candidates_ordering():
@@ -93,10 +143,10 @@ async def test_get_workflow_version_update_candidates_ordering():
         "0.1.dev27+g1458b59",
         "0.2.0a0",
     ]
-    unordered = [
-        TaskVersion(task_id=0, version=version) for version in versions
-    ]
-    ordered = sorted(unordered)
+    ordered_versions = sorted(
+        versions,
+        key=lambda _version: parse(_version),
+    )
 
     # FIXME this order differs from
     # https://github.com/fractal-analytics-platform/fractal-web/blob/edfd62660ac20220b14d516eb3f67d826d217b45/components/__tests__/version.test.js
@@ -117,4 +167,4 @@ async def test_get_workflow_version_update_candidates_ordering():
         "2",
     ]
 
-    assert [task.version for task in ordered] == expected_sorted_versions
+    assert ordered_versions == expected_sorted_versions
