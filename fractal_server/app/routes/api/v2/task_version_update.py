ec94e0d8020a029a28aa3181e2522a74b830ed05
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-05-15 16:01:56 +0200

    handle meta=None ref #2577

--- fractal_server/app/routes/api/v2/task_version_update.py
+++ fractal_server/app/routes/api/v2/task_version_update.py
@@ -55,22 +55,36 @@ def _is_version_parsable(version: str) -> bool:
 
 def _get_new_workflow_task_meta(
     *,
-    old_task_meta: dict,
+    old_task_meta: dict | None,
     old_workflow_task_meta: dict | None,
-    new_task_meta: dict,
+    new_task_meta: dict | None,
 ) -> dict:
 
-    old_wft_meta = old_workflow_task_meta or {}
+    if old_task_meta is None:
+        # all the contents of old_workflow_task_meta are user-made,
+        # and this should be the output.
+        return old_workflow_task_meta
+
+    if old_workflow_task_meta is None:
+        return new_task_meta
+
+    if new_task_meta is None:
+        new_task_meta = {}
+
     additions = {
-        k: v for k, v in old_wft_meta.items() if v != old_task_meta.get(k)
+        k: v
+        for k, v in old_workflow_task_meta.items()
+        if v != old_task_meta.get(k)
     }
-    removals = old_task_meta.keys() - old_wft_meta.keys()
+    removals = old_task_meta.keys() - old_workflow_task_meta.keys()
 
-    new_wft_meta = new_task_meta | additions
-    for key in removals:
-        new_wft_meta.pop(key, None)
+    new_workflowtask_meta = {
+        k: v
+        for k, v in (new_task_meta | additions).items()
+        if k not in removals
+    }
 
-    return new_wft_meta
+    return new_workflowtask_meta
 
 
 class TaskVersion(BaseModel):
