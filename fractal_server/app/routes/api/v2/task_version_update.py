47c6523b490e07b41cfc8b386ddf39d317936a2b
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-05-15 10:00:48 +0200

    fix typo

--- fractal_server/app/routes/api/v2/task_version_update.py
+++ fractal_server/app/routes/api/v2/task_version_update.py
@@ -66,7 +66,7 @@ def _get_new_workflow_task_meta(
     new_wft_meta = new_task_meta | meta_patch
 
     for key in old_task_meta.keys() - old_wft_meta.keys():
-        new_task_meta.pop(key)
+        new_wft_meta.pop(key)
 
     return new_wft_meta
 
