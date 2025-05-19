4a55affa611f8eea8c777c2692f5e830f087e005
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-05-19 15:11:23 +0200

    docstring/comments

--- fractal_server/app/routes/api/v2/_aux_functions_task_version_update.py
+++ fractal_server/app/routes/api/v2/_aux_functions_task_version_update.py
@@ -1,28 +1,38 @@
+from typing import Any
+
+
 def get_new_workflow_task_meta(
     *,
-    old_task_meta: dict | None,
     old_workflow_task_meta: dict | None,
+    old_task_meta: dict | None,
     new_task_meta: dict | None,
-) -> dict:
+) -> dict[str, Any]:
+    """
+    Prepare new meta field based on old/new tasks and old workflow task.
+    """
 
+    # When the whole `old_workflow_task_meta` is user-provided, use it
+    # as the outcome
     if old_task_meta is None:
-        # all the contents of old_workflow_task_meta are user-made,
-        # and this should be the output.
         return old_workflow_task_meta
 
+    # When `old_workflow_task_meta` is unset, use the new-task meta as default.
     if old_workflow_task_meta is None:
         return new_task_meta
 
     if new_task_meta is None:
         new_task_meta = {}
 
+    # Find properties that were added to the old defaults
     additions = {
         k: v
         for k, v in old_workflow_task_meta.items()
         if v != old_task_meta.get(k)
     }
+    # Find properties that were removed from the old defaults
     removals = old_task_meta.keys() - old_workflow_task_meta.keys()
 
+    # Add `additions` and remove `removals`.
     new_workflowtask_meta = {
         k: v
         for k, v in (new_task_meta | additions).items()
