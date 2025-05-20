37eb632e430ee58e778adcdf3ed4217814b05b1f
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-05-20 16:14:51 +0200

    fix typo [skip ci]

--- fractal_server/app/routes/api/v2/task_group.py
+++ fractal_server/app/routes/api/v2/task_group.py
@@ -42,7 +42,7 @@ def _version_sort_key(
     task_group: TaskGroupV2,
 ) -> tuple[int, Version | str | None]:
     """
-    Return a tuple used as (reverse) ordering key for TaskGroups in
+    Returns a tuple used as (reverse) ordering key for TaskGroups in
     `get_task_group_list`.
     The TaskGroups with a parsable versions are the first in order,
     sorted according to the sorting rules of packaging.version.Version.
