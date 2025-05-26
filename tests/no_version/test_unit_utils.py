c1ee0f9e956952a712af4bf4c807a175b6ddc6e7
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-05-26 11:50:48 +0200

    Drop unused function `execute_command_async`
    cc @mfranzon

--- tests/no_version/test_unit_utils.py
+++ tests/no_version/test_unit_utils.py
@@ -1,10 +1,7 @@
 from datetime import timezone
-from pathlib import Path
 
-import pytest
 from devtools import debug
 
-from fractal_server.utils import execute_command_async
 from fractal_server.utils import get_timestamp
 
 
@@ -18,24 +15,3 @@ def test_timestamp():
     debug(ts)
     assert ts
     assert ts.tzinfo is timezone.utc
-
-
-async def test_execute_command(tmp_path):
-    """
-    GIVEN the `pwd` command and a path
-    WHEN executed via _execute_command with the path as working directory
-    THEN the command returns the full path
-    """
-    res = await execute_command_async(cwd=tmp_path, command="pwd")
-    assert res.strip() == tmp_path.as_posix()
-
-
-async def test_execute_command_fail():
-    """
-    GIVEN a unix command that fails
-    WHEN the command is executed via _execute_command
-    THEN an error is raised
-    """
-    with pytest.raises(RuntimeError) as e:
-        await execute_command_async(cwd=Path("/tmp"), command="ls __NOEXIST__")
-    debug(e.value)
