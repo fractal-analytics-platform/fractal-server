cff11a4862ac7c52137f1825c418642cb42dab30
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-05-27 16:06:05 +0200

    update template

--- fractal_server/tasks/v2/pixi_local/pixi_template.sh
+++ fractal_server/tasks/v2/pixi_local/pixi_template.sh
@@ -8,7 +8,11 @@ TOML="${PACKAGE_ENV_DIR}/pyproject.toml"
 
 mkdir -p ${PACKAGE_ENV_DIR}
 tar -xzfv ${TAR_GZ_PATH} -C ${PACKAGE_ENV_DIR}
-
 pixi install --manifest-path ${TOML}
-pixi run --manifest-path ${TOML} python \
-    -c "import ${PACKAGE_NAME}, os; print(os.path.dirname(${PACKAGE_NAME}.__file__))"
+
+TASK_DIR=$(
+    pixi run --manifest-path ${TOML} python \
+    -c "import ${PACKAGE_NAME} as p, os; print(os.path.dirname(p.__file__))"
+)
+ENV_DISK_USAGE=$(du -sk "${PACKAGE_ENV_DIR}" | cut -f1)
+ENV_FILE_NUMBER=$(find "${PACKAGE_ENV_DIR}" -type f | wc -l)
