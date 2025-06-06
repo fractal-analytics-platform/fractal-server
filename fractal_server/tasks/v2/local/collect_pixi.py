4b7a8a1f71b8f1a4b1514db17d9c3df1c05fe394
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-06-06 10:42:36 +0200

    Do not create `SCRIPTS_SUBFOLDER` twice

--- fractal_server/tasks/v2/local/collect_pixi.py
+++ fractal_server/tasks/v2/local/collect_pixi.py
@@ -95,9 +95,9 @@ def collect_local_pixi(
                 Path(task_group.path).mkdir(parents=True)
                 logger.info(f"Created {task_group.path}")
 
-                # FIXME: move to the correct place
-                Path(task_group.path, SCRIPTS_SUBFOLDER).mkdir(parents=True)
-                logger.info(f"Created {task_group.path}/{SCRIPTS_SUBFOLDER}")
+                # FIXME: review whether this is needed
+                # Path(task_group.path, SCRIPTS_SUBFOLDER).mkdir(parents=True)
+                # logger.info(f"Created {task_group.path}/{SCRIPTS_SUBFOLDER}")
 
                 archive_path = Path(
                     task_group.path, tar_gz_file.filename
