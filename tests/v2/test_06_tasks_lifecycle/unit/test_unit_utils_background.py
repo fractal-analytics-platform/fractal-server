28cac23bc0248de700ebf0dd367459d10f518678
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-06-10 13:02:59 +0200

    Fix test

--- tests/v2/test_06_tasks_lifecycle/unit/test_unit_utils_background.py
+++ tests/v2/test_06_tasks_lifecycle/unit/test_unit_utils_background.py
@@ -10,18 +10,11 @@ def test_prepare_tasks_metadata_failures():
             package_manifest=None,
             package_root="",
             python_bin="some",
-            pixi_bin="some",
+            project_python_wrapper="some",
         )
 
     with pytest.raises(UnreachableBranchError):
         prepare_tasks_metadata(
             package_manifest=None,
             package_root="",
-        )
-
-    with pytest.raises(UnreachableBranchError):
-        prepare_tasks_metadata(
-            package_manifest=None,
-            package_root="",
-            pixi_bin="some",
         )
