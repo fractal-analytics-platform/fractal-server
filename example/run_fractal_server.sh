db6bb1de3bec47716523653468f6e42e560fde74
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-07-24 09:19:16 +0200

    quote in bash script

--- example/run_fractal_server.sh
+++ example/run_fractal_server.sh
@@ -6,9 +6,9 @@ poetry run fractalctl set-db
 
 # Start the server
 poetry run gunicorn fractal_server.main:app \
-    --workers 4 \
-    --worker-class fractal_server.gunicorn_fractal.FractalWorker \
-    --bind 0.0.0.0:8000 \
+    --workers 2 \
+    --bind "0.0.0.0:8000" \
     --access-logfile - \
     --error-logfile - \
+    --worker-class fractal_server.gunicorn_fractal.FractalWorker \
     --logger-class fractal_server.gunicorn_fractal.FractalGunicornLogger \
