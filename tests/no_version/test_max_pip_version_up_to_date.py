578acdb06c9bed5d1276c477dd1eb3a505f0c49a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-09-05 08:36:44 +0200

    Bump FRACTAL_MAX_PIP_VERSION to 25.2

--- tests/no_version/test_max_pip_version_up_to_date.py
+++ tests/no_version/test_max_pip_version_up_to_date.py
@@ -7,4 +7,8 @@ from fractal_server.config import Settings
 async def test_check_pip_latest():
     current_max_pip_version = Settings().FRACTAL_MAX_PIP_VERSION
     pypi_latest_pip_version = await get_package_version_from_pypi("pip")
-    assert current_max_pip_version == pypi_latest_pip_version
+    hint_msg = (
+        "You should update `fractal_server.config.Settings."
+        f"FRACTAL_MAX_PIP_VERSION` to '{pypi_latest_pip_version}'"
+    )
+    assert current_max_pip_version == pypi_latest_pip_version, hint_msg
