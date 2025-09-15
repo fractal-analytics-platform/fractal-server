e8f7b7b034b3eec9e57e0e6ea3649179a37e1487
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-09-15 15:38:57 +0200

    Improve MRE

--- tests/no_version/test_2775.py
+++ tests/no_version/test_2775.py
@@ -1,18 +1,13 @@
 import pytest
 
 
-@pytest.fixture(scope="session")
-async def async_fixture():
-    pass
-
-
 @pytest.fixture
-def sync_fixture(async_fixture):
-    pass
+async def async_fixture():
+    yield 99
 
 
 async def test_2775(
     request,
     async_fixture,  # uncomment to make test pass
 ):
-    print(request.getfixturevalue("sync_fixture"))
+    print(request.getfixturevalue("async_fixture"))
