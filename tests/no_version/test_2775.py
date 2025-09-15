56f9f2ba2d397b10bce309adfd19f9ea3cea76cd
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-09-15 15:12:53 +0200

    Improve test

--- tests/no_version/test_2775.py
+++ tests/no_version/test_2775.py
@@ -1,9 +1,19 @@
 import pytest
 
 
+@pytest.fixture
+def intermediate(testdata_path):
+    pass
+
+
+@pytest.fixture
+def syncfixture(intermediate):
+    yield "This is the sync fixture"
+
+
 @pytest.mark.container
-async def test_admin_delete_task_group_api(
+async def test_2775(
     request,
     # testdata_path,   # CHANGE THIS TO CHANGE BEHAVIOR
 ):
-    request.getfixturevalue("fractal_ssh_list")
+    print(request.getfixturevalue("syncfixture"))
