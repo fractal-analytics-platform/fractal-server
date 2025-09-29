c0700d0366da0d861cf6e8972bacfda4f6fe61f8
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-09-29 18:10:11 +0200

    improve test

--- tests/v2/test_02_models/test_my_table.py
+++ tests/v2/test_02_models/test_my_table.py
@@ -1,5 +1,4 @@
 import pytest
-from devtools import debug
 from sqlalchemy.exc import IntegrityError
 
 from fractal_server.app.models.v2 import MyTable
@@ -11,11 +10,23 @@ async def test_my_table_unique_contraint(db):
 
     db.add(MyTable(a=1, b=False))
     await db.commit()
+    db.add(MyTable(a=1, b=False))
+    await db.commit()
 
     db.add(MyTable(a=2, b=True))
     await db.commit()
 
-    with pytest.raises(IntegrityError) as e:
+    with pytest.raises(IntegrityError):
         db.add(MyTable(a=1, b=True))
         await db.commit()
-    debug(e.value)
+    await db.rollback()
+
+    db.add(MyTable(a=1, b=False))
+    await db.commit()
+    db.add(MyTable(a=1, b=False))
+    await db.commit()
+
+    with pytest.raises(IntegrityError):
+        db.add(MyTable(a=1, b=True))
+        await db.commit()
+    await db.rollback()
