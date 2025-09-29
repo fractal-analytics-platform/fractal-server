4ab2c19a0f05d7ebb7464986ef90f991d86dd0dd
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-09-29 16:52:55 +0200

    modify __table_args__

--- fractal_server/app/models/v2/fake.py
+++ fractal_server/app/models/v2/fake.py
@@ -1,4 +1,6 @@
+from sqlalchemy import text
 from sqlmodel import Field
+from sqlmodel import Index
 from sqlmodel import SQLModel
 
 
@@ -6,3 +8,12 @@ class FakeTable(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     a: int
     b: bool
+
+    __table_args__ = (
+        Index(
+            "fancy_name",
+            "a",
+            unique=True,
+            postgresql_where=text("b IS TRUE"),
+        ),
+    )
