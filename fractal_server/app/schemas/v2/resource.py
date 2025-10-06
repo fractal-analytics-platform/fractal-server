ab621b20df8920135975368f49816aa3526a8342
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-10-06 14:44:49 +0200

    refactor pixi/python config

--- fractal_server/app/schemas/v2/resource.py
+++ fractal_server/app/schemas/v2/resource.py
@@ -1,194 +1,16 @@
-from pathlib import Path
-from typing import Annotated
 from typing import Any
 from typing import Literal
 from typing import Self
 
-from pydantic import AfterValidator
 from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic import model_validator
-from pydantic import PositiveInt
+from pydantic import ValidationError
 
+from fractal_server.tasks.config import TaskPythonSettings
+from fractal_server.tasks.config import TasksPixiSettings
 from fractal_server.types import AbsolutePathStr
-from fractal_server.types import DictStrStr
 from fractal_server.types import NonEmptyStr
-
-
-class TaskPythonSettings(BaseModel):
-    """
-    FIXME
-    """
-
-    versions: dict[
-        Literal[
-            "3.9",
-            "3.10",
-            "3.11",
-            "3.12",
-            "3.13",
-            "3.14",
-        ],
-        AbsolutePathStr,
-    ]
-    """
-    Dictionary mapping Python versions to the corresponding interpreters.
-    """
-    default_version: NonEmptyStr
-    """
-    Default task-collection Python version (must be a key of `self.versions`).
-    """
-
-    @model_validator(mode="after")
-    def check_python_version(self) -> Self:
-        if self.default_version not in self.versions:
-            raise ValueError(
-                f"Default Python version '{self.default_version}' not in "
-                f"available version {list(self.versions.keys())}."
-            )
-
-        return self
-
-
-def _check_pixi_slurm_memory(mem: str) -> str:
-    if mem[-1] not in ["K", "M", "G", "T"]:
-        raise ValueError(
-            f"Invalid memory requirement {mem=} for `pixi`, "
-            "please set a K/M/G/T units suffix."
-        )
-    return mem
-
-
-class PixiSLURMConfig(BaseModel):
-    """
-    Parameters that are passed directly to a `sbatch` command.
-
-    See https://slurm.schedmd.com/sbatch.html.
-    """
-
-    partition: NonEmptyStr
-    """
-    `-p, --partition=<partition_names>`
-    """
-    cpus: PositiveInt
-    """
-    `-c, --cpus-per-task=<ncpus>
-    """
-    mem: Annotated[NonEmptyStr, AfterValidator(_check_pixi_slurm_memory)]
-    """
-    `--mem=<size>[units]` (examples: `"10M"`, `"10G"`).
-    From `sbatch` docs: Specify the real memory required per node. Default
-    units are megabytes. Different units can be specified using the suffix
-    [K|M|G|T].
-    """
-    time: NonEmptyStr
-    """
-    `-t, --time=<time>`.
-    From `sbatch` docs: "A time limit of zero requests that no time limit be
-    imposed. Acceptable time formats include "minutes", "minutes:seconds",
-    "hours:minutes:seconds", "days-hours", "days-hours:minutes" and
-    "days-hours:minutes:seconds".
-    """
-
-
-class PixiSettings(BaseModel):
-    """
-    Configuration for Pixi Task collection.
-
-    In order to use Pixi for Task collection, you must have one or more Pixi
-    binaries in your machine
-    (see
-    [example/get_pixi.sh](https://github.com/fractal-analytics-platform/fractal-server/blob/main/example/get_pixi.sh)
-    for installation example).
-
-    To let Fractal Server use these binaries for Task collection, a JSON file
-    must be prepared with the data to populate `PixiSettings` (arguments with
-    default values may be omitted).
-
-    The path to this JSON file must then be provided to Fractal via the
-    environment variable `FRACTAL_PIXI_CONFIG_FILE_zzz`.
-    """
-
-    versions: DictStrStr
-    """
-    A dictionary with Pixi versions as keys and paths to the corresponding
-    folder as values.
-
-    E.g. let's assume that you have Pixi v0.47.0 at
-    `/pixi-path/0.47.0/bin/pixi` and Pixi v0.48.2 at
-    `/pixi-path/0.48.2/bin/pixi`, then
-    ```json
-    "versions": {
-        "0.47.0": "/pixi-path/0.47.0",
-        "0.48.2": "/pixi-path/0.48.2"
-    }
-    ```
-    """
-    default_version: str
-    """
-    Default Pixi version to be used for Task collection.
-
-    Must be a key of the `versions` dictionary.
-    """
-    PIXI_CONCURRENT_SOLVES: int = 4
-    """
-    Value of
-    [`--concurrent-solves`](https://pixi.sh/latest/reference/cli/pixi/install/#arg---concurrent-solves)
-    for `pixi install`.
-    """
-    PIXI_CONCURRENT_DOWNLOADS: int = 4
-    """
-    Value of
-    [`--concurrent-downloads`](https://pixi.sh/latest/reference/cli/pixi/install/#arg---concurrent-downloads)
-    for `pixi install`.
-    """
-    TOKIO_WORKER_THREADS: int = 2
-    """
-    From
-    [Tokio documentation](
-    https://docs.rs/tokio/latest/tokio/#cpu-bound-tasks-and-blocking-code
-    )
-    :
-
-        The core threads are where all asynchronous code runs,
-        and Tokio will by default spawn one for each CPU core.
-        You can use the environment variable `TOKIO_WORKER_THREADS` to override
-        the default value.
-    """
-    DEFAULT_ENVIRONMENT: str = "default"
-    """
-    Default pixi environment name.
-    """
-    DEFAULT_PLATFORM: str = "linux-64"
-    """
-    Default platform for pixi.
-    """
-    SLURM_CONFIG: PixiSLURMConfig | None = None
-    """
-    Required when using pixi in a SSH/SLURM deployment.
-    """
-
-    @model_validator(mode="after")
-    def check_pixi_settings(self):
-        if self.default_version not in self.versions:
-            raise ValueError(
-                f"Default version '{self.default_version}' not in "
-                f"available version {list(self.versions.keys())}."
-            )
-
-        pixi_base_dir = Path(self.versions[self.default_version]).parent
-
-        for key, value in self.versions.items():
-            pixi_path = Path(value)
-
-            if pixi_path.parent != pixi_base_dir:
-                raise ValueError(
-                    f"{pixi_path=} is not located within the {pixi_base_dir=}."
-                )
-            if pixi_path.name != key:
-                raise ValueError(f"{pixi_path.name=} is not equal to {key=}")
-
-        return self
 
 
 class _ValidResourceBase(BaseModel):
@@ -209,7 +31,14 @@ class _ValidResourceBase(BaseModel):
         if self.tasks_python_config != {}:
             TaskPythonSettings(**self.tasks_python_config)
         if self.tasks_pixi_config != {}:
-            PixiSettings(**self.tasks_pixi_config)
+            pixi_settings = TasksPixiSettings(**self.tasks_pixi_config)
+            if (
+                self.resource_type == "slurm_ssh"
+                and pixi_settings.SLURM_CONFIG is None
+            ):
+                raise ValidationError(
+                    "`tasks_pixi_config` must include `SLURM_CONFIG`."
+                )
         return self
 
 
---
+++ fractal_server/tasks/config/__init__.py
@@ -0,0 +1,2 @@
+from ._pixi import TasksPixiSettings  # noqa F401
+from ._python import TaskPythonSettings  # noqa F401
