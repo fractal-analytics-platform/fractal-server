9a6d472bfa4a3a22820c3c2b4c59383b39883e40
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-10-07 21:33:32 +0200

    Relax user-settings validation schemas

--- fractal_server/app/user_settings.py
+++ fractal_server/app/user_settings.py
@@ -8,18 +8,12 @@ class SlurmSshUserSettings(BaseModel):
     execution when using the Slurm-SSH runner.
 
     Attributes:
-        ssh_host: SSH-reachable host where a SLURM client is available.
-        ssh_username: User on `ssh_host`.
-        ssh_private_key_path: Path of private SSH key for `ssh_username`.
         ssh_tasks_dir: Task-venvs base folder on `ssh_host`.
         ssh_jobs_dir: Jobs base folder on `ssh_host`.
         slurm_accounts:
             List of SLURM accounts, to be used upon Fractal job submission.
     """
 
-    ssh_host: str
-    ssh_username: str
-    ssh_private_key_path: str
     ssh_tasks_dir: str
     ssh_jobs_dir: str
     slurm_accounts: list[str]
@@ -37,6 +31,5 @@ class SlurmSudoUserSettings(BaseModel):
             List of SLURM accounts, to be used upon Fractal job submission.
     """
 
-    slurm_user: str
     project_dir: str
     slurm_accounts: list[str]
