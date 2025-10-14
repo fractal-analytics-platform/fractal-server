3405bb4f7f4bbac8884bce55328ebe1184153e05
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-10-14 16:23:16 +0200

    make name column unique

--- fractal_server/app/models/v2/profile.py
+++ fractal_server/app/models/v2/profile.py
@@ -6,7 +6,7 @@ class Profile(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     resource_id: int = Field(foreign_key="resource.id", ondelete="CASCADE")
 
-    name: str
+    name: str = Field(unique=True)
 
     username: str | None = None
     ssh_key_path: str | None = None
