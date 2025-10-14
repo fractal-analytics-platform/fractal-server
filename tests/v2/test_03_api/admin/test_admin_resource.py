f9898591c2dc2f19af4d0891a487926bfd121bff
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-10-14 10:15:59 +0200

    `test_admin_resource.py`

---
+++ tests/v2/test_03_api/admin/test_admin_resource.py
@@ -0,0 +1,67 @@
+async def test_resource_api(
+    db,
+    client,
+    MockCurrentUser,
+    local_resource_profile_db,
+    slurm_ssh_resource_profile_fake_objects,
+):
+    async with MockCurrentUser(user_kwargs=dict(is_superuser=True)):
+        # GET all resources
+        res = await client.get("/admin/v2/resource/")
+        assert res.status_code == 200
+        assert len(res.json()) == 1
+
+        # POST one resource / fail due to invalid payload
+        res = await client.post(
+            "/admin/v2/resource/",
+            json=dict(something="else"),
+        )
+        assert res.status_code == 422
+        assert "Field required" in str(res.json()["detail"])
+
+        # POST one resource / fail due to wrong resource.type
+        res = await client.post(
+            "/admin/v2/resource/",
+            json=slurm_ssh_resource_profile_fake_objects[0].model_dump(
+                exclude="timestamp_created"
+            ),
+        )
+        assert res.status_code == 422
+        assert "FRACTAL_RUNNER_BACKEND" in str(res.json()["detail"])
+
+        # POST one resource / fail due to non-unique name
+        res = await client.post(
+            "/admin/v2/resource/",
+            json=local_resource_profile_db[0].model_dump(
+                exclude={"timestamp_created"},
+            ),
+        )
+        assert res.status_code == 422
+        assert "already in use" in str(res.json()["detail"])
+
+        # POST one resource / success
+        valid_resource = local_resource_profile_db[0].model_dump(
+            exclude={
+                "timestamp_created",
+                "name",
+                "id",
+            }
+        )
+        NAME = "another resource name"
+        valid_resource["name"] = NAME
+        res = await client.post(
+            "/admin/v2/resource/",
+            json=valid_resource,
+        )
+        assert res.status_code == 201
+        assert res.json()["name"] == NAME
+        resource_id = res.json()["id"]
+
+        # GET one resource / success
+        res = await client.get(f"/admin/v2/resource/{resource_id}/")
+        assert res.status_code == 200
+        assert res.json()["name"] == NAME
+
+        # GET one resource / failure
+        res = await client.get("/admin/v2/resource/9999/")
+        assert res.status_code == 404
