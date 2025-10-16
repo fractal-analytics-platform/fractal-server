3270cd595da9d97ca289279fac0a0a5aace5250b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-10-16 13:25:16 +0200

    coverage

---
+++ tests/v2/test_08_backends/test_local_config.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+import pytest
+
+from fractal_server.runner.config import JobRunnerConfigLocal
+from fractal_server.runner.executors.local.get_local_config import (
+    get_local_backend_config,
+)
+
+
+def test_get_local_backend_config():
+    class WorkflowTaskV2(object):
+        meta_parallel: dict[str, str] = {"parallel_tasks_per_job": 11}
+        meta_non_parallel: dict[str, Any] = {"parallel_tasks_per_job": 22}
+
+    wftask = WorkflowTaskV2()
+    shared_config = JobRunnerConfigLocal()
+
+    out = get_local_backend_config(
+        shared_config=shared_config,
+        wftask=wftask,
+        which_type="parallel",
+    )
+    assert out == JobRunnerConfigLocal(parallel_tasks_per_job=11)
+
+    out = get_local_backend_config(
+        shared_config=shared_config,
+        wftask=wftask,
+        which_type="non_parallel",
+    )
+    assert out == JobRunnerConfigLocal(parallel_tasks_per_job=22)
+
+    with pytest.raises(ValueError):
+        get_local_backend_config(
+            shared_config=shared_config,
+            wftask=wftask,
+            which_type="invalid",
+        )
