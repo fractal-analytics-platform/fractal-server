8d78c7bf188d6046445b149e4eacf3dc68136788
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-10-17 10:00:33 +0200

    re-enable a test

--- tests/no_version/test_init_db_data.py
+++ tests/no_version/test_init_db_data.py
@@ -43,13 +43,13 @@ def test_init_db_data_from_file(
     )
 
 
-def test_init_db_data_failure(db_sync, tmp_path: Path):
+def test_init_db_data_failure(db_create_tables, tmp_path: Path):
     bad_json_file = tmp_path / "bad.json"
     with bad_json_file.open("w") as f:
         json.dump({"invalid": "resource"}, f)
 
-    # with pytest.raises(SystemExit):
-    #     init_db_data(resource=bad_json_file.as_posix(), profile="default")
+    with pytest.raises(SystemExit):
+        init_db_data(resource=bad_json_file.as_posix(), profile="default")
 
     with pytest.raises(SystemExit):
         init_db_data(resource="default", profile=bad_json_file.as_posix())
