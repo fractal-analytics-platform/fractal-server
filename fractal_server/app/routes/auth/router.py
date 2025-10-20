e1c05af239f67f0b1b9441cad9ea8f7dfb97c763
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-10-20 11:59:15 +0200

    review oauth configuration

--- fractal_server/app/routes/auth/router.py
+++ fractal_server/app/routes/auth/router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from .current_user import router_current_user
 from .group import router_group
 from .login import router_login
-from .oauth import router_oauth
+from .oauth import get_oauth_router
 from .register import router_register
 from .users import router_users
 
@@ -14,4 +14,6 @@ router_auth.include_router(router_current_user)
 router_auth.include_router(router_login)
 router_auth.include_router(router_users)
 router_auth.include_router(router_group)
-router_auth.include_router(router_oauth)
+router_oauth = get_oauth_router()
+if router_oauth is not None:
+    router_auth.include_router(router_oauth)
---
+++ fractal_server/config/_oauth.py
@@ -0,0 +1,72 @@
+from typing import Annotated
+from typing import Self
+
+from pydantic import model_validator
+from pydantic import SecretStr
+from pydantic import StringConstraints
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+from ._settings_config import SETTINGS_CONFIG_DICT
+from fractal_server.types import NonEmptyStr
+
+
+class OAuthSettings(BaseSettings):
+    """
+    Minimal set of configurations needed for operating on the database (e.g
+    for schema migrations).
+    """
+
+    model_config = SettingsConfigDict(**SETTINGS_CONFIG_DICT)
+
+    FRACTAL_OAUTH_CLIENT_NAME: (
+        Annotated[
+            NonEmptyStr,
+            StringConstraints(to_lower=True),
+        ]
+        | None
+    ) = None
+    """
+    The name of the client.
+    """
+    FRACTAL_OAUTH_CLIENT_ID: SecretStr | None = None
+    """
+    ID of client.
+    """
+    FRACTAL_OAUTH_CLIENT_SECRET: SecretStr | None = None
+    """
+    Secret to authorise against the identity provider.
+    """
+    FRACTAL_OIDC_CONFIG_ENDPOINT: str | None = None
+    """
+    OpenID configuration endpoint, for autodiscovery of relevant endpoints.
+    """
+    FRACTAL_OAUTH_REDIRECT_URL: str | None = None
+    """
+    String to be used as `redirect_url` argument in
+    `fastapi_users.get_oauth_router`, and then in
+    `httpx_oauth.integrations.fastapi.OAuth2AuthorizeCallback`
+    """
+
+    @property
+    def is_set(self):
+        return all(
+            [
+                self.FRACTAL_OAUTH_CLIENT_NAME is not None,
+                self.FRACTAL_OAUTH_CLIENT_ID is not None,
+                self.FRACTAL_OAUTH_CLIENT_SECRET is not None,
+            ]
+        )
+
+    @model_validator(mode="after")
+    def check_configuration(self: Self) -> Self:
+        if (
+            self.is_set()
+            and self.FRACTAL_OAUTH_CLIENT_NAME not in ["google", "github"]
+            and self.FRACTAL_OIDC_CONFIG_ENDPOINT is None
+        ):
+            raise ValueError(
+                f"{self.FRACTAL_OIDC_CONFIG_ENDPOINT=} but "
+                f"{self.FRACTAL_OAUTH_CLIENT_NAME=}"
+            )
+        return self
