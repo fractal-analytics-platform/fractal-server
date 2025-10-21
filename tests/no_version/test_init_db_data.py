1a6f8fbdbfd54c712e5bbcbdaa1e28d1907e2f7d
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-10-21 15:22:39 +0200

    coverage

--- tests/no_version/test_init_db_data.py
+++ tests/no_version/test_init_db_data.py
@@ -10,13 +10,49 @@ from fractal_server.app.models import Profile
 from fractal_server.app.models import Resource
 
 
-def test_init_db_data(db_sync):
+def test_init_db_data_default(db_sync):
     init_db_data(resource="default", profile="default")
     assert db_sync.execute(select(func.count(Resource.id))).scalar() == 1
     assert db_sync.execute(select(func.count(Profile.id))).scalar() == 1
 
     with pytest.raises(SystemExit):
         init_db_data(resource="default", profile="default")
+
+
+def test_init_db_data_resource_and_profile(
+    tmp_path, local_resource_profile_objects
+):
+    resource, profile = local_resource_profile_objects
+    resource_path = tmp_path / "resource.json"
+    profile_path = tmp_path / "profile.json"
+
+    with resource_path.open("w") as f:
+        json.dump(resource.model_dump(exclude={"id", "timestamp_created"}), f)
+    with profile_path.open("w") as f:
+        json.dump(profile.model_dump(exclude={"id"}), f)
+
+    with pytest.raises(SystemExit):
+        init_db_data(resource=resource_path.as_posix())
+
+    with pytest.raises(SystemExit):
+        init_db_data(profile=profile_path.as_posix())
+
+    init_db_data(
+        resource=resource_path.as_posix(), profile=profile_path.as_posix()
+    )
+
+
+def test_init_db_data_user_and_password(db_sync):
+    email = "admin@example.org"
+    password = "1234"
+
+    with pytest.raises(SystemExit):
+        init_db_data(admin_password=password)
+
+    with pytest.raises(SystemExit):
+        init_db_data(admin_email=email)
+
+    init_db_data(admin_email=email, admin_password=password)
 
 
 def test_init_db_data_from_file(
