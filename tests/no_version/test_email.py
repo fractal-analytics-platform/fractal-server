7a60351a6655f5e240fd4f34f7f89291c8da84f9
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-10-22 16:47:47 +0200

    fix unit email test

--- tests/no_version/test_email.py
+++ tests/no_version/test_email.py
@@ -1,53 +1,47 @@
-# import contextlib
-# from fractal_server.app.models import OAuthAccount
-# from fractal_server.app.models import UserOAuth
-# from fractal_server.app.security import get_user_manager
-# from fractal_server.config._email import PublicEmailSettings
-# FIXME: Review whether to convert or drop
-# async def test_server_not_available(
-#     override_email_settings_factory, db, caplog
-# ):
-#     override_email_settings_factory(
-#         public=PublicEmailSettings(
-#             sender="fractal@fractal.fractal",
-#             recipients=["test@example.org"],
-#             smtp_server="localhost",
-#             port=1234,
-#             encrypted_password=(
-#                 "gAAAAABnoQUGHMsDgLkpDtwUtrKtf9T1so44ahEXExGRceAnf097mVY1EbNuM"
-#                 "P5fjvkndvwCwBJM7lHoSgKQkZ4VbvO9t3PJZg=="
-#             ),
-#             encryption_key="lp3j2FVDkzLd0Rklnzg1pHuV9ClCuDE0aGeJfTNCaW4=",
-#             instance_name="fractal",
-#             use_starttls=False,
-#             use_login=True,
-#         )
-#     )
-#     user = UserOAuth(
-#         email="user@example.org",
-#         hashed_password="xxxxxx",
-#         oauth_accounts=[
-#             OAuthAccount(
-#                 oauth_name="oidc",
-#                 access_token="abcd",
-#                 account_id=1,
-#                 account_email="user@oidc.org",
-#             ),
-#             OAuthAccount(
-#                 oauth_name="google",
-#                 access_token="1234",
-#                 account_id=1,
-#                 account_email="user@gmail.com",
-#             ),
-#         ],
-#     )
-#     db.add(user)
-#     await db.commit()
-#     await db.refresh(user)
-#     import logging
-#     logger = logging.getLogger("fractal_server.app.security")
-#     logger.propagate = True
-#     async with contextlib.asynccontextmanager(get_user_manager)() as um:
-#         await um.on_after_register(user)
-#     assert "ERROR sending notification email" in caplog.text
-#     assert "[Errno 111] Connection refused" in caplog.text
+import logging
+
+from fractal_server.app.security.signup_email import (
+    send_fractal_email_or_log_failure,
+)
+from fractal_server.config._email import PublicEmailSettings
+
+
+async def test_send_fractal_email_or_log_failure(caplog, monkeypatch):
+    email_settings_public = PublicEmailSettings(
+        sender="fractal@fractal.fractal",
+        recipients=["test@example.org"],
+        smtp_server="localhost",
+        port=1234,
+        encrypted_password=(
+            "gAAAAABnoQUGHMsDgLkpDtwUtrKtf9T1so44ahEXExGRceAnf097mVY1EbNuM"
+            "P5fjvkndvwCwBJM7lHoSgKQkZ4VbvO9t3PJZg=="
+        ),
+        encryption_key="lp3j2FVDkzLd0Rklnzg1pHuV9ClCuDE0aGeJfTNCaW4=",
+        instance_name="fractal",
+        use_starttls=False,
+        use_login=True,
+    )
+
+    # Mock the logger, so that `propagate=True`
+    import fractal_server.app.security.signup_email
+
+    _logger = logging.getLogger("some-logger")
+    monkeypatch.setattr(
+        fractal_server.app.security.signup_email, "logger", _logger
+    )
+
+    caplog.clear()
+    send_fractal_email_or_log_failure(
+        subject="subject",
+        msg="msg",
+        email_settings=None,
+    )
+    assert "Cannot send email" in caplog.text
+    assert "email_settings=None" in caplog.text
+
+    caplog.clear()
+    send_fractal_email_or_log_failure(
+        subject="subject", msg="msg", email_settings=email_settings_public
+    )
+    assert "Could not send self-registration email" in caplog.text
+    assert "[Errno 111] Connection refused" in caplog.text
