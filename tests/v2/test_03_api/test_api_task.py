163c080c7578af90c61f8d9f0eb8cfbeb7ba2840
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-10-30 14:56:57 +0100

    fix tests

--- tests/v2/test_03_api/test_api_task.py
+++ tests/v2/test_03_api/test_api_task.py
@@ -113,8 +113,13 @@ async def test_task_get_list(
         assert len(res.json()) == 2
 
 
-async def test_post_task(client, MockCurrentUser):
-    async with MockCurrentUser():
+async def test_post_task(
+    client,
+    MockCurrentUser,
+    local_resource_profile_db,
+):
+    resource, profile = local_resource_profile_db
+    async with MockCurrentUser(user_kwargs=dict(profile_id=profile.id)):
         # Successful task creations
         task = TaskCreateV2(
             name="task_name",
@@ -235,7 +240,10 @@ async def test_post_task_user_group_id(
     MockCurrentUser,
     monkeypatch,
     db,
+    local_resource_profile_db,
 ):
+    resource, profile = local_resource_profile_db
+
     # Create a usergroup
     team1_group = UserGroup(name="team1")
     db.add(team1_group)
@@ -244,8 +252,9 @@ async def test_post_task_user_group_id(
 
     args = dict(command_non_parallel="cmd", type="non_parallel")
 
-    async with MockCurrentUser():
-        # No query parameter
+    async with MockCurrentUser(
+        user_kwargs=dict(profile_id=profile.id)
+    ):  # No query parameter
         res = await client.post(f"{PREFIX}/", json=dict(name="a", **args))
         assert res.status_code == 201
         taskgroup = await db.get(TaskGroupV2, res.json()["taskgroupv2_id"])
@@ -299,9 +308,13 @@ async def test_post_task_user_group_id(
 async def test_patch_task_auth(
     MockCurrentUser,
     client,
+    local_resource_profile_db,
 ):
+    resource, profile = local_resource_profile_db
     # POST-task as user_A
-    async with MockCurrentUser() as user_A:
+    async with MockCurrentUser(
+        user_kwargs=dict(profile_id=profile.id)
+    ) as user_A:
         user_A_id = user_A.id
         payload_obj = TaskCreateV2(
             name="a", category="my-cat", command_parallel="c"
@@ -323,7 +336,7 @@ async def test_patch_task_auth(
         assert res.json()["category"] == "new-cat-1"
 
     # PATCH-task failure as a different user -> failure (task belongs to user)
-    async with MockCurrentUser():
+    async with MockCurrentUser(user_kwargs=dict(profile_id=profile.id)):
         # PATCH-task failure (task does not belong to user)
         payload_obj = TaskUpdateV2(category="new-cat-2")
         res = await client.patch(
