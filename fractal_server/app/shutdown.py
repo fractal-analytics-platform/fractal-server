39a3d8466ca8c6b2a01f35a19d5fab131dee7f89
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-11-11 21:53:27 +0100

    Review `fractal_server/app/shutdown.py`

--- fractal_server/app/shutdown.py
+++ fractal_server/app/shutdown.py
@@ -12,44 +12,48 @@ from fractal_server.syringe import Inject
 
 
 async def cleanup_after_shutdown(*, jobsV2: list[int], logger_name: str):
+    settings = Inject(get_settings)
     logger = get_logger(logger_name)
     logger.info("Cleanup function after shutdown")
-    stm_v2 = (
+    stm_objects = (
         select(JobV2)
+        .where(JobV2.id.in_(jobsV2))
+        .where(JobV2.status == JobStatusTypeV2.SUBMITTED)
+    )
+    stm_ids = (
+        select(JobV2.id)
         .where(JobV2.id.in_(jobsV2))
         .where(JobV2.status == JobStatusTypeV2.SUBMITTED)
     )
 
     async for session in get_async_db():
-        jobsV2_db = (await session.execute(stm_v2)).scalars().all()
-
-        for job in jobsV2_db:
+        # Write shutdown file for all jobs
+        jobs = (await session.execute(stm_objects)).scalars().all()
+        for job in jobs:
             _write_shutdown_file(job=job)
 
-        settings = Inject(get_settings)
-
+        # Wait for completion of all job - with a timeout
+        interval = settings.FRACTAL_GRACEFUL_SHUTDOWN_TIME / 20
         t_start = time.perf_counter()
         while (
             time.perf_counter() - t_start
-        ) < settings.FRACTAL_GRACEFUL_SHUTDOWN_TIME:  # 30 seconds
-            logger.info("Waiting 3 seconds before checking")
-            time.sleep(3)
-            jobsV2_db = (await session.execute(stm_v2)).scalars().all()
-
-            if len(jobsV2_db) == 0:
-                logger.info(
-                    "All jobs associated to this app are "
-                    "either done or failed. Exit."
-                )
+        ) <= settings.FRACTAL_GRACEFUL_SHUTDOWN_TIME:
+            job_ids = (await session.execute(stm_ids)).scalars().all()
+            if len(job_ids) == 0:
+                logger.info("All jobs app are either done or failed. Exit.")
                 return
             else:
-                logger.info(f"Some jobs are still 'submitted' {jobsV2_db=}")
+                logger.info(f"Some jobs are still 'submitted': {job_ids=}")
+                logger.info(f"Wait {interval:.4f} seconds before next check.")
+                time.sleep(interval)
         logger.info(
             "Graceful shutdown reached its maximum time, "
-            "but some jobs are still submitted"
+            "but some jobs are still submitted."
         )
 
-        for job in jobsV2_db:
+        # Mark jobs as failed and update their logs.
+        jobs = (await session.execute(stm_objects)).scalars().all()
+        for job in jobs:
             job.status = "failed"
             job.log = (job.log or "") + "\nJob stopped due to app shutdown\n"
             session.add(job)
