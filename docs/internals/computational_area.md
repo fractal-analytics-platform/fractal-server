af932198e3301dcd40c41724a3da365416cdde5e
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-11-11 14:57:33 +0100

    Revert "BROKEN include json in markdown"
    This reverts commit ba63ef7c0915b7b63683b09a91933a643c4a0f16.

--- docs/internals/computational_area.md
+++
@@ -1,9 +0,0 @@
-# Computational Area
-
-Testo 1
-
---8<-- "docs.resource_switcher.md"
-
-Testo 2
-
---8<-- "docs.profile_switcher.md"
--- docs/scripts/validate_assets.py
+++ docs/scripts/validate_assets.py
@@ -11,50 +11,52 @@ from fractal_server.app.schemas.v2 import ValidResourceSlurmSSH
 from fractal_server.app.schemas.v2 import ValidResourceSlurmSudo
 
 assets_dir = Path("docs/assets/resource_and_profile")
+output_path = Path("internals/computational_area.md")
 
+with mkdocs_gen_files.open(output_path, "w") as fd:
 
-def add_to_switch(title: str, json_item: dict, fd) -> None:
-    indent = "    "
-    fd.write(f'=== "{title}"\n\n')
-    fd.write(f"{indent}```json\n")
-    fd.write(
-        indent
-        + json.dumps(json_item, indent=4).replace("\n", "\n" + indent)
-        + "\n"
-    )
-    fd.write(f"{indent}```\n\n")
+    def add_to_switch(title: str, json_item: dict) -> None:
+        indent = "    "
+        fd.write(f'=== "{title}"\n\n')
+        fd.write(f"{indent}```json\n")
+        fd.write(
+            indent
+            + json.dumps(json_item, indent=4).replace("\n", "\n" + indent)
+            + "\n"
+        )
+        fd.write(f"{indent}```\n\n")
 
+    fd.write("## Resource example\n\n")
 
-with mkdocs_gen_files.open("resource_switcher.md", "w") as fd:
     # Local
     with (assets_dir / "resource_local.json").open("r") as f:
         resource = json.load(f)
         ValidResourceLocal(**resource)
-    add_to_switch("Local", resource, fd)
+    add_to_switch("Local", resource)
 
     # SLURM sudo
     with (assets_dir / "resource_sudo.json").open("r") as f:
         resource = json.load(f)
         ValidResourceSlurmSudo(**resource)
-    add_to_switch("SLURM sudo", resource, fd)
+    add_to_switch("SLURM sudo", resource)
 
     # SLURM ssh
     with (assets_dir / "resource_ssh.json").open("r") as f:
         resource = json.load(f)
         ValidResourceSlurmSSH(**resource)
-    add_to_switch("SLURM ssh", resource, fd)
+    add_to_switch("SLURM ssh", resource)
 
+    fd.write("## Profile example\n\n")
 
-with mkdocs_gen_files.open("profile_switcher.md", "w") as fd:
     with (assets_dir / "profile_local.json").open("r") as f:
         profile = json.load(f)
         ValidProfileLocal(**profile)
-    add_to_switch("Local", profile, fd)
+    add_to_switch("Local", profile)
     with (assets_dir / "profile_sudo.json").open("r") as f:
         profile = json.load(f)
         ValidProfileSlurmSudo(**profile)
-    add_to_switch("SLURM sudo", profile, fd)
+    add_to_switch("SLURM sudo", profile)
     with (assets_dir / "profile_ssh.json").open("r") as f:
         profile = json.load(f)
         ValidProfileSlurmSSH(**profile)
-    add_to_switch("SLURM ssh", profile, fd)
+    add_to_switch("SLURM ssh", profile)
