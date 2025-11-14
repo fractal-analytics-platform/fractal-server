3ed8b1543beb8a35f25ad3ecc9a5f0f9ac8767c0
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-11-14 14:48:52 +0100

    remove admin/v2/project/

--- fractal_server/app/routes/admin/v2/__init__.py
+++ fractal_server/app/routes/admin/v2/__init__.py
@@ -7,7 +7,6 @@ from .accounting import router as accounting_router
 from .impersonate import router as impersonate_router
 from .job import router as job_router
 from .profile import router as profile_router
-from .project import router as project_router
 from .resource import router as resource_router
 from .task import router as task_router
 from .task_group import router as task_group_router
@@ -17,7 +16,6 @@ router_admin_v2 = APIRouter()
 
 router_admin_v2.include_router(accounting_router, prefix="/accounting")
 router_admin_v2.include_router(job_router, prefix="/job")
-router_admin_v2.include_router(project_router, prefix="/project")
 router_admin_v2.include_router(task_router, prefix="/task")
 router_admin_v2.include_router(task_group_router, prefix="/task-group")
 router_admin_v2.include_router(
