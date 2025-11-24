74cf0d07dd647c6f8cac1b589d5f313dae5ab798
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-11-24 10:18:04 +0100

    3001 coverage for slurm batching module (#3011)
    * docstrings
    
    * Expand SLURM-batching-heuristics test
    
    * CHANGELOG

--- tests/v2/test_08_backends/test_slurm_batching.py
+++ tests/v2/test_08_backends/test_slurm_batching.py
@@ -1,10 +1,63 @@
+from copy import copy
+
+import pytest
+
+from fractal_server.runner.executors.slurm_common._batching import (
+    SlurmHeuristicsError,
+)
 from fractal_server.runner.executors.slurm_common._batching import heuristics
 
 
-def test_heuristics_many_tasks():
-    """
-    Test a specific case of slurm-job batching.
-    """
+def test_heuristics():
+    args = dict(
+        tot_tasks=10,
+        cpus_per_task=2,
+        mem_per_task=1_000,
+        target_cpus_per_job=4,
+        max_cpus_per_job=16,
+        target_mem_per_job=10_000,
+        max_mem_per_job=32_000,
+        target_num_jobs=100,
+        max_num_jobs=1_000,
+    )
+
+    with pytest.raises(SlurmHeuristicsError):
+        heuristics(
+            tasks_per_job=1,
+            parallel_tasks_per_job=None,
+            **args,
+        )
+
+    current_args = copy(args) | dict(cpus_per_task=100)
+    with pytest.raises(SlurmHeuristicsError):
+        heuristics(**current_args)
+
+    current_args = copy(args) | dict(mem_per_task=100_000)
+    with pytest.raises(SlurmHeuristicsError):
+        heuristics(**current_args)
+
+    current_args = copy(args) | dict(tasks_per_job=2, parallel_tasks_per_job=5)
+    new_tasks_per_job, new_parallel_tasks_per_job = heuristics(**current_args)
+    assert new_tasks_per_job == 2
+    assert new_parallel_tasks_per_job == 2
+
+    current_args = copy(args) | dict(
+        cpus_per_task=10, tasks_per_job=2, parallel_tasks_per_job=2
+    )
+    with pytest.raises(SlurmHeuristicsError):
+        heuristics(**current_args)
+
+    current_args = copy(args) | dict(
+        mem_per_task=20_000, tasks_per_job=2, parallel_tasks_per_job=2
+    )
+    with pytest.raises(SlurmHeuristicsError):
+        heuristics(**current_args)
+
+    current_args = copy(args) | dict(
+        tot_tasks=100_000, tasks_per_job=2, parallel_tasks_per_job=2
+    )
+    with pytest.raises(SlurmHeuristicsError):
+        heuristics(**current_args)
 
     NUM_TASKS = 10000
     tasks_per_job, parallel_tasks_per_job = heuristics(
