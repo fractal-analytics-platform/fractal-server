237e6a35cceabf3c28e50efddabf550f897bdbfe
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-11-27 14:32:48 +0100

    add logs to data migration script

--- fractal_server/data_migrations/2_18_1.py
+++ fractal_server/data_migrations/2_18_1.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 from sqlalchemy.orm.attributes import flag_modified
@@ -6,16 +7,23 @@ from sqlmodel import select
 from fractal_server.app.db import get_sync_db
 from fractal_server.app.models import UserOAuth
 
+logging.basicConfig(level=logging.INFO)
+
 
 def fix_db():
+    logging.info("START - fix db")
+
     with next(get_sync_db()) as db:
-        res = db.execute(select(UserOAuth))
+        res = db.execute(select(UserOAuth).order_by(UserOAuth.email))
         user_list = res.scalars().unique().all()
 
         for user in user_list:
+            logging.info(f"Now handling user {user.email}.")
             if user.project_dirs != []:
                 sys.exit(f"Non empty `project_dirs` for User[{user.id}]")
             user.project_dirs.append(user.project_dir)
             flag_modified(user, "project_dirs")
 
         db.commit()
+
+    logging.info("END - fix db")
