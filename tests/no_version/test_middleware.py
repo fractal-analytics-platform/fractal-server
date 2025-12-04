613bea048e7e23938a633e2c7a6d68c695340619
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2025-12-04 17:58:18 +0100

    test

--- tests/no_version/test_middleware.py
+++ tests/no_version/test_middleware.py
@@ -1,12 +1,17 @@
+import inspect
 import logging
+import re
 import time
 
 from asgi_lifespan import LifespanManager
+from fastapi import BackgroundTasks
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 from httpx import ASGITransport
 from httpx import AsyncClient
 
 from fractal_server.main import SlowResponseMiddleware
+from fractal_server.main import _endpoint_has_background_task
 
 
 async def test_app_with_middleware(caplog):
@@ -43,3 +48,29 @@ async def test_app_with_middleware(caplog):
         await client.get("/?sleep=0.33&foo=bar")
         assert "0.33," in caplog.text
         assert "GET /?sleep=0.33&foo=bar" in caplog.text
+
+
+async def test_endpoint_has_background_task(app, register_routers):
+    """
+    Test that all and only the endpoints with a BackgroundTask are
+    identified by the function `_endpoint_has_background_task`
+    """
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            path = re.sub(r"\{[^}]+\}", "1", route.path)
+            method = list(route.methods)[0]
+            has_background_task = False
+
+            sig = inspect.signature(route.endpoint)
+            for _, param in sig.parameters.items():
+                if param.annotation == BackgroundTasks:
+                    has_background_task = True
+                    break
+
+            assert (
+                _endpoint_has_background_task(
+                    method=method,
+                    path=path,
+                )
+                == has_background_task
+            )
