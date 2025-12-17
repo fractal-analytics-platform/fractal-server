8f1f305b7f7912bb54cc876f2a23de3df48d8392
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-12-17 13:52:37 +0100

    Update test

--- tests/no_version/test_build.py
+++ tests/no_version/test_build.py
@@ -8,23 +8,25 @@ import pytest
 
 @pytest.fixture(scope="module")
 def tmp_dir_module():
-    with tempfile.TemporaryDirectory(delete=False) as tmpdirname:
+    """
+    Module-scoped temporary directory.
+
+    Set `delete=False` if you need to manually debug the `uv build` artifacts.
+    """
+    with tempfile.TemporaryDirectory(delete=True) as tmpdirname:
         yield tmpdirname
 
 
 @pytest.fixture(scope="module")
-def gitignored_file():
+def dist_files(tmp_dir_module: Path):
+    """
+    Run `uv build` and list the files in the wheel.
+    """
+
+    # File matching `tmp*`, which should *not* be included in the wheel
     path = "fractal_server/tmp_dummy.py"
     Path(path).touch()
-    yield path
-    Path(path).unlink()
 
-
-@pytest.fixture(scope="module")
-def build_namelist(
-    tmp_dir_module: Path,
-    gitignored_file: str,
-) -> list[str]:
     subprocess.run(
         [
             "uv",
@@ -41,36 +43,34 @@ def build_namelist(
 
     with ZipFile(wheel_file_path, "r") as zip:
         namelist = list(zip.namelist())
-    return namelist
+
+    yield namelist
+
+    Path(path).unlink()
 
 
-def test_include_task_template(build_namelist: list[str]):
-    assert (
-        "fractal_server/tasks/v2/templates/1_create_venv.sh" in build_namelist
-    )
+def test_include_task_template(dist_files: list[str]):
+    assert "fractal_server/tasks/v2/templates/1_create_venv.sh" in dist_files
 
 
-def test_include_alembic_ini(build_namelist: list[str]):
-    assert "fractal_server/alembic.ini" in build_namelist
+def test_include_alembic_ini(dist_files: list[str]):
+    assert "fractal_server/alembic.ini" in dist_files
 
 
-@pytest.mark.xfail(reason="FIXME")
-def test_exclude_gitignored_file(
-    build_namelist: list[str],
-    gitignored_file: str,
-):
-    assert gitignored_file not in build_namelist
+def test_exclude_tmp_(dist_files: list[str]):
+    assert not [name for name in dist_files if "tmp" in name]
 
 
-def test_exclude_json_schemas(build_namelist: list[str]):
+def test_exclude_json_schemas(dist_files: list[str]):
     assert not [
-        name for name in build_namelist if "fractal_server/json_schemas" in name
+        name for name in dist_files if "fractal_server/json_schemas" in name
     ]
 
 
-def test_exclude_data_migrations_old(build_namelist: list[str]):
-    assert "fractal_server/data_migrations/old/2_18_0.py" not in build_namelist
+def test_exclude_data_migrations_old(dist_files: list[str]):
+    assert "fractal_server/data_migrations/old" not in dist_files
+    assert "fractal_server/data_migrations/old/2_18_0.py" not in dist_files
 
 
-def test_exclude_mako(build_namelist: list[str]):
-    assert "fractal_server/migrations/script.py.mako" not in build_namelist
+def test_exclude_mako(dist_files: list[str]):
+    assert "fractal_server/migrations/script.py.mako" not in dist_files
