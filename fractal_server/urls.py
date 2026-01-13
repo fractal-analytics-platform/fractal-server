4454f1451104cba2a57422626d68e6394018f3c3
Dominique Roux <dominique.roux.dr1@roche.com>
2026-01-13 09:41:33 +0100

    added support for s3

--- fractal_server/urls.py
+++ fractal_server/urls.py
@@ -5,7 +5,7 @@ def normalize_url(url: str) -> str:
     url = url.strip()
     if url.startswith("/"):
         return normpath(url)
-    elif url.startswith("s3"):
+    elif url.startswith("s3://"):
         return url
     else:
-        raise ValueError("URLs must begin with '/' or 's3'.")
+        raise ValueError("URLs must begin with '/' or 's3://'.")
