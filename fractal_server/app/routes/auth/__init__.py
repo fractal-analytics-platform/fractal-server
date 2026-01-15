036d27eacf0a480f25c1b4ba971646e8074fa1c6
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-01-15 10:57:14 +0100

    simplify get_api_user

--- fractal_server/app/routes/auth/__init__.py
+++ fractal_server/app/routes/auth/__init__.py
@@ -57,26 +57,6 @@ current_user_act_ver = fastapi_users.current_user(
 )
 
 
-async def get_api_user(
-    user: UserOAuth = Depends(current_user_act_ver),
-) -> UserOAuth:
-    """
-    Require a active&verified non-guest user, with a non-null `profile_id`.
-
-    Raises 401 if user does not exist or is not active.
-    Raises 403 if user is not verified, is a guest or has null `profile_id`.
-    """
-    if user.profile_id is None or user.is_guest:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail=(
-                "Forbidden access "
-                f"({user.is_verified=}, {user.is_guest=}, {user.profile_id=})."
-            ),
-        )
-    return user
-
-
 async def get_api_guest(
     user: UserOAuth = Depends(current_user_act_ver),
 ) -> UserOAuth:
@@ -84,14 +64,31 @@ async def get_api_guest(
     Require a active&verified user, with a non-null `profile_id`.
 
     Raises 401 if user does not exist or is not active.
-    Raises 403 if user is not verified, is a guest or has null `profile_id`.
+    Raises 403 if user is not verified or has null `profile_id`.
     """
-    if user.profile_id is None or user.is_guest:
+    if user.profile_id is None:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail=(
                 f"Forbidden access ({user.is_verified=}, {user.profile_id=})."
             ),
+        )
+    return user
+
+
+async def get_api_user(
+    user: UserOAuth = Depends(get_api_guest),
+) -> UserOAuth:
+    """
+    Require a active&verified non-guest user, with a non-null `profile_id`.
+
+    Raises 401 if user does not exist or is not active.
+    Raises 403 if user is not verified, is a guest or has null `profile_id`.
+    """
+    if user.is_guest:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="This feature is not available for guest users.",
         )
     return user
 
