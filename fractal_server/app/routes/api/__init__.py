b34ecba53ab61660361e73a0fba670deac9c6fea
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-01-15 09:32:19 +0100

    Refactor modules for endpoints in `/api/` but not in `/api/v2/`

--- fractal_server/app/routes/api/__init__.py
+++ fractal_server/app/routes/api/__init__.py
@@ -1,57 +1,9 @@
-"""
-`api` module
-"""
-
 from fastapi import APIRouter
-from fastapi import Depends
 
-import fractal_server
-from fractal_server.app.models import UserOAuth
-from fractal_server.app.routes.auth import current_superuser_act
-from fractal_server.config import get_db_settings
-from fractal_server.config import get_email_settings
-from fractal_server.config import get_oauth_settings
-from fractal_server.config import get_settings
-from fractal_server.syringe import Inject
+from .alive import router as router_alive
+from .settings import router as router_settings
 
 router_api = APIRouter()
 
-
-@router_api.get("/alive/")
-async def alive():
-    return dict(
-        alive=True,
-        version=fractal_server.__VERSION__,
-    )
-
-
-@router_api.get("/settings/app/")
-async def view_settings(
-    user: UserOAuth = Depends(current_superuser_act),
-):
-    settings = Inject(get_settings)
-    return settings.model_dump()
-
-
-@router_api.get("/settings/database/")
-async def view_db_settings(
-    user: UserOAuth = Depends(current_superuser_act),
-):
-    settings = Inject(get_db_settings)
-    return settings.model_dump()
-
-
-@router_api.get("/settings/email/")
-async def view_email_settings(
-    user: UserOAuth = Depends(current_superuser_act),
-):
-    settings = Inject(get_email_settings)
-    return settings.model_dump()
-
-
-@router_api.get("/settings/oauth/")
-async def view_oauth_settings(
-    user: UserOAuth = Depends(current_superuser_act),
-):
-    settings = Inject(get_oauth_settings)
-    return settings.model_dump()
+router_api.include_router(router_alive)
+router_api.include_router(router_settings)
