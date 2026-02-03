c1ebf21dcafc133bf2cc3264d7f2c8fd45a2d8bc
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-02-03 10:56:26 +0100

    rename and add comment

---
+++ data-tasks/1/fractal-tasks-mock/0.0.1/scripts/1770107716_collect_3_pip_freeze.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# Variables to be filled within fractal-server
+PACKAGE_ENV_DIR=/home/yuri/lavoro/fractal/fractal-server/data-tasks/1/fractal-tasks-mock/0.0.1/venv
+
+VENVPYTHON=${PACKAGE_ENV_DIR}/bin/python
+
+"$VENVPYTHON" -m pip freeze --all
