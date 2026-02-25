432646e6ac6224faab4422fcc9abe8e791efc165
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-02-25 21:01:25 +0100

    Improve lifespan logging

--- fractal_server/main.py
+++ fractal_server/main.py
@@ -22,6 +22,7 @@ from .config import get_db_settings
 from .config import get_email_settings
 from .config import get_settings
 from .logger import config_uvicorn_loggers
+from .logger import get_logger
 from .logger import reset_logger_handlers
 from .logger import set_logger
 from .syringe import Inject
@@ -46,7 +47,7 @@ def collect_routers(app: FastAPI) -> None:
     app.include_router(router_auth, prefix="/auth", tags=["Authentication"])
 
 
-def check_settings() -> None:
+def check_settings(logger_name: str) -> None:
     """
     Check and register the settings
 
@@ -59,7 +60,7 @@ def check_settings() -> None:
     settings = Inject(get_settings)
     db_settings = Inject(get_db_settings)
     email_settings = Inject(get_email_settings)
-    logger = set_logger("fractal_server_settings")
+    logger = get_logger(logger_name)
     logger.debug("Fractal Settings:")
     for key, value in chain(
         db_settings.model_dump().items(),
@@ -69,15 +70,14 @@ def check_settings() -> None:
         if any(s in key.upper() for s in ["PASSWORD", "SECRET", "KEY"]):
             value = "*****"
         logger.debug(f"  {key}: {value}")
-    reset_logger_handlers(logger)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.state.jobs = []
-    logger_startup = set_logger("fractal_server.lifespan.startup")
+    logger_startup = set_logger("lifespan.startup")
     logger_startup.info(f"START (fractal-server {__VERSION__})")
-    check_settings()
+    check_settings(logger_name="lifespan.startup")
     settings = Inject(get_settings)
 
     if settings.FRACTAL_RUNNER_BACKEND == ResourceType.SLURM_SSH:
@@ -98,7 +98,7 @@ async def lifespan(app: FastAPI):
 
     yield
 
-    logger_teardown = set_logger("fractal_server.lifespan.teardown")
+    logger_teardown = set_logger("lifespan.teardown")
     logger_teardown.info("START")
 
     if settings.FRACTAL_RUNNER_BACKEND == ResourceType.SLURM_SSH:
