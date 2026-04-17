8812ca3524b8304af4f4762092b0b460e1d41ff9
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-04-17 14:19:57 +0200

    Clean up way to generate swagger UI

--- docs/openapi.md
+++ docs/openapi.md
@@ -4,17 +4,16 @@
 <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
 <!-- `SwaggerUIBundle` is now available on the page -->
 <script>
-    const ui = SwaggerUIBundle({
-        url: 'http://127.0.0.1:8000/openapi.json',
-        "dom_id": "#swagger-ui",
-        "layout": "BaseLayout",
-        "deepLinking": true,
-        "showExtensions": true,
-        "showCommonExtensions": true,
-
-        presets: [
-            SwaggerUIBundle.presets.apis,
-            SwaggerUIBundle.SwaggerUIStandalonePreset
-        ],
-    })
+const ui = SwaggerUIBundle({
+url: 'https://fractal-analytics-platform.github.io/fractal-server/openapi/openapi.json',
+"dom_id": "#swagger-ui",
+"layout": "BaseLayout",
+"deepLinking": true,
+"showExtensions": true,
+"showCommonExtensions": true,
+presets: [
+SwaggerUIBundle.presets.apis,
+SwaggerUIBundle.SwaggerUIStandalonePreset
+],
+})
 </script>
--- docs/scripts/generate_swagger_ui.py
+++ docs/scripts/generate_swagger_ui.py
@@ -1,8 +1,28 @@
+from pathlib import Path
+from textwrap import dedent
+
 from fastapi.openapi.docs import get_swagger_ui_html
 
-html = get_swagger_ui_html(
-    # openapi_url="http://127.0.0.1:8000/openapi.json",
-    openapi_url="https://fractal-analytics-platform.github.io/fractal-server/openapi/openapi.json",
+# FIXME: Add something for local deployments, pointing somewhere else or nowhere
+OPENAPI_URL = "https://fractal-analytics-platform.github.io/fractal-server/openapi/openapi.json"
+
+html_response = get_swagger_ui_html(
+    openapi_url=OPENAPI_URL,
     title="Fractal-server API",
 )
-print(html.body.decode())
+body = html_response.body.decode()
+
+with (Path(__file__).parent.parent / "openapi.md").open("w") as fp:
+    for line in body.splitlines():
+        if (
+            not line
+            or "DOCTYPE" in line
+            or "html" in line
+            or "head" in line
+            or "body" in line
+            or "meta" in line
+            or "fastapi" in line
+            or "title" in line
+        ):
+            continue
+        fp.write(f"{dedent(line)}\n")
