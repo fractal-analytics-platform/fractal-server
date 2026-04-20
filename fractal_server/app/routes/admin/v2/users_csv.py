39f23b144af479fb4addb80d67e38e7d24466d5d
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-04-20 12:29:58 +0200

    remove server filename

--- fractal_server/app/routes/admin/v2/users_csv.py
+++ fractal_server/app/routes/admin/v2/users_csv.py
@@ -109,5 +109,4 @@ async def list_users(
     return StreamingResponse(
         csv_string,
         media_type="text/csv",
-        headers={"Content-Disposition": "attachment; filename=users.csv"},
     )
