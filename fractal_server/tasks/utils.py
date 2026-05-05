bef683bb3fee3501aadc4665610f5c4050eb427e
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-05-05 14:32:14 +0200

    remove unused constants

--- fractal_server/tasks/utils.py
+++ fractal_server/tasks/utils.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 
-COLLECTION_FILENAME = "collection.json"
 COLLECTION_LOG_FILENAME = "collection.log"
-COLLECTION_FREEZE_FILENAME = "collection_freeze.txt"
 FORBIDDEN_DEPENDENCY_STRINGS = ["github.com"]
 
 TASK_GROUP_ID_FILENAME = "fractal_task_group_id.txt"
