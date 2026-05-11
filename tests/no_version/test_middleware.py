f2a856fe596fed1d4de64c498a7ca48fea950908
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-05-11 13:11:12 +0200

    fix test

--- tests/no_version/test_middleware.py
+++ tests/no_version/test_middleware.py
@@ -111,4 +111,5 @@ async def test_endpoint_has_background_task(app: FastAPI, register_routers):
         ("POST", "/admin/v2/task-group/{task_group_id}/deactivate/"),
         ("POST", "/admin/v2/task-group/{task_group_id}/reactivate/"),
         ("POST", "/admin/v2/task-group/{task_group_id}/delete/"),
+        ("POST", "/admin/v2/task-group/{task_group_id}/recollect/pip/"),
     }
