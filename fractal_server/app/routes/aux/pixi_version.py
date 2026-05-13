843410ea4b4b5cf74814695831060f3dea517102
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-05-13 15:30:35 +0200

    fix if/else

--- fractal_server/app/routes/aux/pixi_version.py
+++ fractal_server/app/routes/aux/pixi_version.py
@@ -21,15 +21,12 @@ def check_pixi_version(
             status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
             detail="Pixi task collection is not available.",
         )
-    if resource.tasks_pixi_config is None:
-        pass
-    else:
-        if pixi_version not in resource.tasks_pixi_config["versions"]:
-            raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-                detail=(
-                    f"Pixi version '{pixi_version}' is not available. "
-                    "Available versions: "
-                    f"{list(resource.tasks_pixi_config['versions'].keys())}"
-                ),
-            )
+    elif pixi_version not in resource.tasks_pixi_config["versions"]:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            detail=(
+                f"Pixi version '{pixi_version}' is not available. "
+                "Available versions: "
+                f"{list(resource.tasks_pixi_config['versions'].keys())}"
+            ),
+        )
