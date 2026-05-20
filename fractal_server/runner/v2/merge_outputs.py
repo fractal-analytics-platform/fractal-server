cc29b5115b0c69a88de862774d27bbd2ea0ba2e9
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-05-20 16:34:46 +0200

    Simplify `merge_outputs`

--- fractal_server/runner/v2/merge_outputs.py
+++ fractal_server/runner/v2/merge_outputs.py
@@ -1,17 +1,20 @@
+import itertools
+
 from fractal_server.runner.v2.deduplicate_list import deduplicate_list
 from fractal_server.runner.v2.task_interface import TaskOutput
 
 
 def merge_outputs(task_outputs: list[TaskOutput]) -> TaskOutput:
-    if len(task_outputs) == 0:
-        return TaskOutput()
-
-    final_image_list_updates = []
-    final_image_list_removals = []
-
-    for task_output in task_outputs:
-        final_image_list_updates.extend(task_output.image_list_updates)
-        final_image_list_removals.extend(task_output.image_list_removals)
+    final_image_list_updates = list(
+        itertools.chain.from_iterable(
+            task_output.image_list_updates for task_output in task_outputs
+        )
+    )
+    final_image_list_removals = list(
+        itertools.chain.from_iterable(
+            task_output.image_list_removals for task_output in task_outputs
+        )
+    )
 
     # Note: the ordering of `image_list_removals` is not guaranteed
     final_image_list_updates = deduplicate_list(final_image_list_updates)
