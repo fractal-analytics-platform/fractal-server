39b4184b741a94e2699d3a853dfcc67c18a94958
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-05-22 14:38:31 +0200

    Use task-group object for `exists`

--- fractal_server/app/routes/api/v2/task_collection_pixi.py
+++ fractal_server/app/routes/api/v2/task_collection_pixi.py
@@ -148,7 +148,7 @@ async def collect_task_pixi(
     )
 
     if resource.type != ResourceType.SLURM_SSH:
-        if Path(task_group_path).exists():
+        if Path(task_group.path).exists():
             raise HTTPException(
                 status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
                 detail=f"{task_group_path} already exists.",
