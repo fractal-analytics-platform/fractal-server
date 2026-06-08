97b5208d7a8555199c6aba328f873f06720c1881
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-06-08 11:28:57 +0200

    coverage

--- tests/v2/test_04_runner/test_unit_db_tools.py
+++ tests/v2/test_04_runner/test_unit_db_tools.py
@@ -8,6 +8,9 @@ from sqlmodel import select
 from fractal_server.app.models.v2 import HistoryRun
 from fractal_server.app.models.v2 import HistoryUnit
 from fractal_server.app.schemas.v2 import HistoryUnitStatus
+from fractal_server.runner.v2.db_tools import (
+    bulk_update_has_warnings_history_unit,
+)
 from fractal_server.runner.v2.db_tools import bulk_update_status_of_history_unit
 from fractal_server.runner.v2.db_tools import update_history_unit_no_commit
 
@@ -122,3 +125,91 @@ async def test_update_status_of_history_unit(
                 status=HistoryUnitStatus.FAILED,
                 db_sync=db_sync,
             )
+
+
+@pytest.mark.parametrize("num_history_units", [10, 50])
+async def test_bulk_update_has_warnings_history_unit(
+    num_history_units: int,
+    # Fixtures
+    db_sync,
+    db,
+    dataset_factory,
+    project_factory,
+    task_factory,
+    workflow_factory,
+    workflowtask_factory,
+    job_factory,
+    MockCurrentUser,
+    tmp_path,
+):
+    warning_logfile = tmp_path / "warnings.log"
+    with warning_logfile.open("w") as f:
+        f.write("This has WaRnInGs!")
+
+    async with MockCurrentUser() as user:
+        task = await task_factory(user.id)
+        project = await project_factory(user)
+        dataset = await dataset_factory(project_id=project.id)
+        workflow = await workflow_factory(project_id=project.id)
+        wftask = await workflowtask_factory(
+            workflow_id=workflow.id,
+            task_id=task.id,
+        )
+        job = await job_factory(
+            project_id=project.id,
+            dataset_id=dataset.id,
+            workflow_id=workflow.id,
+            working_dir="/foo",
+            status="done",
+        )
+
+        hr = HistoryRun(
+            dataset_id=dataset.id,
+            workflowtask_id=wftask.id,
+            task_group_dump={},
+            workflowtask_dump={},
+            status=HistoryUnitStatus.SUBMITTED,
+            num_available_images=0,
+            job_id=job.id,
+        )
+        db_sync.add(hr)
+        db_sync.commit()
+        db_sync.refresh(hr)
+
+        db_sync.add_all(
+            [
+                HistoryUnit(
+                    history_run_id=hr.id,
+                    status=HistoryUnitStatus.DONE,
+                    has_warnings=False,
+                    logfile=warning_logfile.as_posix(),
+                )
+                for _ in range(num_history_units)
+            ]
+        )
+        db_sync.commit()
+
+        res = db_sync.execute(select(HistoryUnit.id))
+        history_unit_ids = res.scalars().all()
+
+        # Bulk function
+        start = time.perf_counter()
+        bulk_update_has_warnings_history_unit(
+            history_unit_ids=history_unit_ids,
+            db_sync=db_sync,
+        )
+        stop = time.perf_counter()
+        bulk_time = stop - start
+
+        res = db_sync.execute(select(HistoryUnit))
+        history_units = res.scalars().all()
+        assert all(
+            history_unit.has_warnings is True for history_unit in history_units
+        )
+
+        # Benchmark
+
+        debug(
+            f"History Units:    {num_history_units}\n"
+            f"Bulk time:        {bulk_time}\n"
+        )
