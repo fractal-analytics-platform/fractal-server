f304184347124d32326ecbb07c98d99afccb787a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-06-16 13:54:17 +0200

    Remove `GET /api/v2/task/`

--- fractal_server/app/routes/api/v2/task.py
+++ fractal_server/app/routes/api/v2/task.py
@@ -1,19 +1,14 @@
 from copy import deepcopy  # noqa
-from typing import Any
 
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import Response
 from fastapi import status
-from sqlmodel import or_
-from sqlmodel import select
 
 from fractal_server.app.routes.aux.validate_user_profile import (
     validate_user_profile,
 )
-from fractal_server.app.schemas.v2.task import TaskReadSlim
-from ._aux_functions import _get_user_resource_id
 from ._aux_functions_tasks import _get_task_full_access, integrity_error_to_422
 from ._aux_functions_tasks import _get_task_read_access
 from ._aux_functions_tasks import _get_valid_user_group_id
@@ -21,7 +16,6 @@ from ._aux_functions_tasks import _verify_non_duplication_group_constraint
 from ._aux_functions_tasks import _verify_non_duplication_user_constraint
 from fractal_server.app.db import AsyncSession
 from fractal_server.app.db import get_async_db
-from fractal_server.app.models import LinkUserGroup
 from fractal_server.app.models import UserOAuth
 from fractal_server.app.models.v2 import TaskGroupV2
 from fractal_server.app.models.v2 import TaskV2
@@ -50,45 +44,6 @@ _SLIM_TASK_FIELDS = {
     "docs_info",
     "docs_link",
 }
-
-
-@router.get("/", response_model=list[TaskRead] | list[TaskReadSlim])
-async def get_list_task(
-    slim: bool = False,
-    user: UserOAuth = Depends(get_api_guest),
-    db: AsyncSession = Depends(get_async_db),
-) -> list[TaskV2] | list[dict[str, Any]]:
-    """
-    Get list of available tasks
-    """
-
-    user_resource_id = await _get_user_resource_id(user_id=user.id, db=db)
-
-    stm = (
-        select(TaskV2)
-        .join(TaskGroupV2, TaskGroupV2.id == TaskV2.taskgroupv2_id)
-        .where(TaskGroupV2.resource_id == user_resource_id)
-        .where(
-            or_(
-                TaskGroupV2.user_id == user.id,
-                TaskGroupV2.user_group_id.in_(
-                    select(LinkUserGroup.group_id).where(
-                        LinkUserGroup.user_id == user.id
-                    )
-                ),
-            )
-        )
-    )
-
-    stm = stm.order_by(TaskV2.id)
-    res = await db.execute(stm)
-    task_list = list(res.scalars().all())
-    if slim:
-        return [
-            task.model_dump(include=_SLIM_TASK_FIELDS) for task in task_list
-        ]
-
-    return task_list
 
 
 @router.get("/{task_id}/", response_model=TaskRead)
