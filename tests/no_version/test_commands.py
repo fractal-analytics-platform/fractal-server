ca8ba14f0929b1b52a44adedfddfadb0eb21d692
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-06-18 12:07:59 +0200

    test

--- tests/no_version/test_commands.py
+++ tests/no_version/test_commands.py
@@ -3,9 +3,16 @@ import shlex
 import signal
 import subprocess
 import time
+from datetime import timedelta
 
 import pytest
 from devtools import debug
+
+from fractal_server.app.routes.api.v2._aux_functions import (
+    _workflow_insert_task,
+)
+from fractal_server.app.schemas.v2.job import JobStatusType
+from fractal_server.utils import get_timestamp
 
 commands = [
     "fractalctl start --reload --host 0.0.0.0 --port 8000",
@@ -55,3 +62,63 @@ def test_email_settings():
     )
     assert not res.stdout
     assert "usage" in res.stderr
+
+
+async def test_recent_activities(
+    project_factory,
+    dataset_factory,
+    workflow_factory,
+    task_factory,
+    job_factory,
+    tmp_path,
+    MockCurrentUser,
+    db,
+):
+    now = get_timestamp()
+    past = now - timedelta(minutes=30)
+
+    async with MockCurrentUser() as user:
+        project = await project_factory(user)
+        dataset = await dataset_factory(project_id=project.id)
+        workflow = await workflow_factory(project_id=project.id)
+        task = await task_factory(user_id=user.id, name="task")
+        await _workflow_insert_task(
+            workflow_id=workflow.id, task_id=task.id, db=db, order=0
+        )
+        args = dict(
+            working_dir=f"{tmp_path.as_posix()}/x",
+            working_dir_user=f"{tmp_path.as_posix()}/y",
+            project_id=project.id,
+            dataset_id=dataset.id,
+            workflow_id=workflow.id,
+            user_email=user.email,
+        )
+        job1 = await job_factory(
+            **args,
+            status=JobStatusType.SUBMITTED,
+            start_timestamp=now,
+        )
+        job2 = await job_factory(
+            **args,
+            status=JobStatusType.FAILED,
+            start_timestamp=past,
+            end_timestamp=past,
+        )
+
+    cmd = "uv run fractalctl recent"
+    res = subprocess.run(
+        shlex.split(cmd),
+        encoding="utf-8",
+        capture_output=True,
+    )
+    assert f"ID={job1.id}" in res.stdout
+    assert f"ID={job2.id}" not in res.stdout
+
+    cmd = "uv run fractalctl recent --minutes 35"
+    res = subprocess.run(
+        shlex.split(cmd),
+        encoding="utf-8",
+        capture_output=True,
+    )
+    assert f"ID={job1.id}" in res.stdout
+    assert f"ID={job2.id}" in res.stdout
