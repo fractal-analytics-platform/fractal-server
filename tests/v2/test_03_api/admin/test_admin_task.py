cc2baf8c1967f0bff9cddc631a878c70c6ad93b8
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-06-22 11:44:23 +0200

    fix test

--- tests/v2/test_03_api/admin/test_admin_task.py
+++ tests/v2/test_03_api/admin/test_admin_task.py
@@ -189,11 +189,15 @@ async def test_task_query(
         assert len(res.json()["items"]) == 2
 
         # Query only core
-        res = await client.get(f"{PREFIX}/task/?only_core=xxx")
+        res = await client.get(f"{PREFIX}/task/?core=xxx")
         assert res.status_code == 422
-        res = await client.get(f"{PREFIX}/task/?only_core=true")
+        res = await client.get(f"{PREFIX}/task/?core=true")
         assert len(res.json()["items"]) == 1
         assert res.json()["items"][0]["task"]["id"] == task2.id
+        res = await client.get(f"{PREFIX}/task/?core=false")
+        assert len(res.json()["items"]) == 2
+        assert res.json()["items"][0]["task"]["id"] == task1.id
+        assert res.json()["items"][1]["task"]["id"] == task3.id
 
         # --------------------------
         # Relationships after deleting the Project
