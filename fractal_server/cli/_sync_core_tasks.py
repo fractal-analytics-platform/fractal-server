3c9255d2fd55ad5309e990eb1039e333c767fbb4
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-07-10 15:27:12 +0200

    Simplify

--- fractal_server/cli/_sync_core_tasks.py
+++ fractal_server/cli/_sync_core_tasks.py
@@ -5,55 +5,56 @@ from pydantic import RootModel
 from fractal_server.types import Field
 from fractal_server.types import NonEmptyStr
 
-
-class CoreTaskList(RootModel):
-    """
-    List of valid items, with unique names.
-    """
-
-    root: set[
-        tuple[
-            NonEmptyStr,
-            NonEmptyStr,
-            NonEmptyStr,
-        ]
-    ] = Field(default_factory=list)
-    """
-    List of tuples like `(pkg_name, version, task_name)`.
-    """
+ThreeStringsTuple = tuple[
+    NonEmptyStr,
+    NonEmptyStr,
+    NonEmptyStr,
+]
 
 
-def _read_file_or_empty_list(path: Path | None) -> set[tuple[str, str, str]]:
-    if path is None or not path.exists():
+class CoreTasksInfo(RootModel):
+    """
+    Set of valid items, with unique names.
+    """
+
+    root: set[ThreeStringsTuple] = Field(default_factory=list)
+    """
+    Set of tuples like `(pkg_name, version, task_name)`.
+    """
+
+
+def _read_set_from_file(path: Path | None) -> set[ThreeStringsTuple]:
+    if path is None:
         return set()
     else:
-        return CoreTaskList.model_validate_json(path.read_text()).root
+        return CoreTasksInfo.model_validate_json(path.read_text()).root
 
 
-def _get_final_list(
+def _get_final_set(
     *,
-    main_list_path: Path | None = None,
-    custom_list_path: Path | None = None,
-    ignore_list_path: Path | None = None,
-) -> set[tuple[str, str, str]]:
-    main_list = _read_file_or_empty_list(main_list_path)
-    custom_list = _read_file_or_empty_list(custom_list_path)
-    ignore_list = _read_file_or_empty_list(ignore_list_path)
+    public_tasks_path: Path | None = None,
+    instance_tasks_path: Path | None = None,
+    instance_ignore_path: Path | None = None,
+) -> set[ThreeStringsTuple]:
+    public_tasks = _read_set_from_file(public_tasks_path)
+    instance_tasks = _read_set_from_file(instance_tasks_path)
+    instance_ignore = _read_set_from_file(instance_ignore_path)
 
-    final_list = (main_list.union(custom_list)).difference(ignore_list)
-    return final_list
+    final_tasks = (public_tasks.union(instance_tasks)).difference(
+        instance_ignore
+    )
+    return final_tasks
 
 
 def sync_core_tasks(
     *,
-    main_list_path: Path | None = None,
-    custom_list_path: Path | None = None,
-    ignore_list_path: Path | None = None,
+    public_tasks_path: Path | None = None,
+    instance_tasks_path: Path | None = None,
+    instance_ignore_path: Path | None = None,
 ):
-    print(
-        _get_final_list(
-            main_list_path=main_list_path,
-            custom_list_path=custom_list_path,
-            ignore_list_path=ignore_list_path,
-        )
+    final_list = _get_final_set(
+        public_tasks_path=public_tasks_path,
+        instance_tasks_path=instance_tasks_path,
+        instance_ignore_path=instance_ignore_path,
     )
+    print(final_list)
--- tests/no_version/cli/test_sync_core_tasks.py
+++ tests/no_version/cli/test_sync_core_tasks.py
@@ -4,26 +4,26 @@ from pathlib import Path
 import pytest
 from pydantic import ValidationError
 
-from fractal_server.cli._sync_core_tasks import _get_final_list
+from fractal_server.cli._sync_core_tasks import _get_final_set
 
 
-def test__get_final_list(tmp_path: Path):
+def test__get_final_set(tmp_path: Path):
     path1 = tmp_path / "1.json"
     path2 = tmp_path / "2.json"
     path3 = tmp_path / "3.json"
 
     with pytest.raises(ValidationError):
         path1.write_text(json.dumps([["a", "b"]]))
-        _get_final_list(main_list_path=path1)
+        _get_final_set(public_tasks_path=path1)
 
     path1.write_text(json.dumps([["a", "b", "c"], ["a", "b", "c"]]))
-    assert _get_final_list(main_list_path=path1) == {
+    assert _get_final_set(public_tasks_path=path1) == {
         ("a", "b", "c"),
     }
 
     with pytest.raises(ValidationError):
         path1.write_text(json.dumps([["a", "b", "c"], [1, 2, 3, 4]]))
-        _get_final_list(main_list_path=path1)
+        _get_final_set(public_tasks_path=path1)
 
     path1.write_text(
         json.dumps(
@@ -53,10 +53,10 @@ def test__get_final_list(tmp_path: Path):
             ]
         )
     )
-    final_list = _get_final_list(
-        main_list_path=path1,
-        custom_list_path=path2,
-        ignore_list_path=path3,
+    final_list = _get_final_set(
+        public_tasks_path=path1,
+        instance_tasks_path=path2,
+        instance_ignore_path=path3,
     )
     print(final_list)
     assert final_list == {
