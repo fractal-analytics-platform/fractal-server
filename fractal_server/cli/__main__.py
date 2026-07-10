1fd5e6903dba3adcfedb55b2fcc1e587f5312bff
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-07-10 13:29:19 +0200

    match/case

--- fractal_server/cli/__main__.py
+++ fractal_server/cli/__main__.py
@@ -11,27 +11,28 @@ from ._update_db_data import update_db_data
 
 def run() -> None:
     args = parse_args()
-    if args.cmd == "openapi":
-        save_openapi(dest=args.openapi_file)
-    elif args.cmd == "set-db":
-        set_db()
-    elif args.cmd == "init-db-data":
-        init_db_data(
-            resource=args.resource,
-            profile=args.profile,
-            admin_email=args.admin_email,
-            admin_password=args.admin_pwd,
-            admin_project_dir=args.admin_project_dir,
-        )
-    elif args.cmd == "update-db-data":
-        update_db_data()
-    elif args.cmd == "start":
-        start(
-            host=args.host,
-            port=args.port,
-            reload=args.reload,
-        )
-    elif args.cmd == "recent":
-        recent(minutes=args.minutes)
-    else:
-        sys.exit(f"Error: invalid command '{args.cmd}'.")
+    match args.cmd:
+        case "openapi":
+            save_openapi(dest=args.openapi_file)
+        case "set-db":
+            set_db()
+        case "init-db-data":
+            init_db_data(
+                resource=args.resource,
+                profile=args.profile,
+                admin_email=args.admin_email,
+                admin_password=args.admin_pwd,
+                admin_project_dir=args.admin_project_dir,
+            )
+        case "update-db-data":
+            update_db_data()
+        case "start":
+            start(
+                host=args.host,
+                port=args.port,
+                reload=args.reload,
+            )
+        case "recent":
+            recent(minutes=args.minutes)
+        case _:
+            sys.exit(f"Error: invalid command '{args.cmd}'.")
