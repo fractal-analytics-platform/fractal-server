a00efa926481fc6f29bb41dac5709acf1cabee73
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-07-13 09:56:45 +0200

    First implementation and test

--- fractal_server/cli/_sync_core_tasks.py
+++ fractal_server/cli/_sync_core_tasks.py
@@ -1,60 +1,119 @@
+import json
+import sys
 from pathlib import Path
 
 from pydantic import RootModel
+from sqlalchemy import select
+from sqlalchemy import update
 
-from fractal_server.types import Field
+from fractal_server.app.db import get_sync_db
+from fractal_server.app.models import Resource
+from fractal_server.app.models import TaskGroupV2
+from fractal_server.app.models import TaskV2
+from fractal_server.app.models import UserGroup
+from fractal_server.config import get_email_settings
+from fractal_server.send_mail import send_fractal_email_or_log_failure
+from fractal_server.syringe import Inject
 from fractal_server.types import NonEmptyStr
 
-ThreeStringsTuple = tuple[
+_ThreeStringsTuple = tuple[
     NonEmptyStr,
     NonEmptyStr,
     NonEmptyStr,
 ]
 
 
-class CoreTasksInfo(RootModel):
+class _CoreInfoSet(RootModel):
     """
-    Set of valid items, with unique names.
-    """
-
-    root: set[ThreeStringsTuple] = Field(default_factory=list)
-    """
-    Set of tuples like `(pkg_name, version, task_name)`.
+    Set of `(pkg_name, version, task_name)` tuples.
     """
 
+    root: set[_ThreeStringsTuple]
 
-def _read_set_from_file(path: Path | None) -> set[ThreeStringsTuple]:
-    if path is None:
-        return set()
-    else:
-        return CoreTasksInfo.model_validate_json(path.read_text()).root
+
+def _read_set_from_file(path: Path | None) -> set[_ThreeStringsTuple]:
+    """
+    Read a file (if any) and parse into a set of core-task info items.
+    """
+    json_data = path.read_text() if path else "[]"
+    return _CoreInfoSet.model_validate_json(json_data).root
 
 
 def _get_final_set(
     *,
-    public_tasks_path: Path | None = None,
-    instance_tasks_path: Path | None = None,
-    instance_ignore_path: Path | None = None,
-) -> set[ThreeStringsTuple]:
-    public_tasks = _read_set_from_file(public_tasks_path)
-    instance_tasks = _read_set_from_file(instance_tasks_path)
-    instance_ignore = _read_set_from_file(instance_ignore_path)
-
-    final_tasks = (public_tasks.union(instance_tasks)).difference(
-        instance_ignore
-    )
-    return final_tasks
+    base: Path | None = None,
+    additions: Path | None = None,
+    removals: Path | None = None,
+) -> set[_ThreeStringsTuple]:
+    base_set = _read_set_from_file(base)
+    additions_set = _read_set_from_file(additions)
+    removals_set = _read_set_from_file(removals)
+    final_set = (base_set.union(additions_set)).difference(removals_set)
+    return final_set
 
 
 def sync_core_tasks(
     *,
-    public_tasks_path: Path | None = None,
-    instance_tasks_path: Path | None = None,
-    instance_ignore_path: Path | None = None,
+    resources_and_groups: Path,
+    base: Path | None = None,
+    additions: Path | None = None,
+    removals: Path | None = None,
 ):
-    final_list = _get_final_set(
-        public_tasks_path=public_tasks_path,
-        instance_tasks_path=instance_tasks_path,
-        instance_ignore_path=instance_ignore_path,
+    """
+    FIXME
+    """
+    resources_and_groups: list[dict[str, int]] = json.loads(
+        resources_and_groups.read_text()
     )
-    print(final_list)
+    final_set = _get_final_set(
+        base=base,
+        additions=additions,
+        removals=removals,
+    )
+    with next(get_sync_db()) as db:
+        try:
+            # Mark all tasks as not core
+            stm = (
+                update(TaskV2)
+                .where(TaskV2.is_core.is_(True))
+                .values(is_core=False)
+            )
+            db.execute(stm)
+
+            for obj in resources_and_groups:
+                resource_id = obj["resource_id"]
+                user_group_id = obj["user_group_id"]
+                db.get_one(Resource, resource_id)
+                db.get_one(UserGroup, user_group_id)
+                for pkg_name, version, task_name in final_set:
+                    stm = (
+                        select(TaskV2)
+                        .join(
+                            TaskGroupV2, TaskV2.taskgroupv2_id == TaskGroupV2.id
+                        )
+                        .where(TaskGroupV2.user_group_id == user_group_id)
+                        .where(TaskGroupV2.resource_id == resource_id)
+                        .where(TaskGroupV2.pkg_name == pkg_name)
+                        .where(TaskGroupV2.version == version)
+                        .where(TaskV2.name == task_name)
+                    )
+                    res = db.execute(stm)
+                    task = res.scalar_one_or_none()
+                    if task is not None:
+                        task.is_core = True
+                        db.add(task)
+                        db.flush()
+            db.commit()
+        except Exception as exc:
+            db.rollback()
+            msg = (
+                "An exception took place, exit without updating the database."
+                f" Original error: {str(exc)}"
+            )
+            email_settings = Inject(get_email_settings)
+            send_fractal_email_or_log_failure(
+                subject="Failed core-tasks synchronization",
+                msg=msg,
+                email_settings=email_settings.public,
+            )
+            sys.exit(msg)
--- tests/no_version/cli/test_sync_core_tasks.py
+++ tests/no_version/cli/test_sync_core_tasks.py
@@ -3,32 +3,26 @@ from pathlib import Path
 
 import pytest
 from pydantic import ValidationError
+from sqlalchemy import func
+from sqlalchemy import select
 
+from fractal_server.app.models import TaskV2
 from fractal_server.cli._sync_core_tasks import _get_final_set
+from fractal_server.cli._sync_core_tasks import sync_core_tasks
 
 
-def test__get_final_set(tmp_path: Path):
-    path1 = tmp_path / "1.json"
-    path2 = tmp_path / "2.json"
-    path3 = tmp_path / "3.json"
+def test_get_final_set(tmp_path: Path):
+    path_invalid = tmp_path / "invalid.json"
+    path_base = tmp_path / "base.json"
+    path_add = tmp_path / "add.json"
+    path_remove = tmp_path / "remove.json"
 
-    with pytest.raises(ValidationError):
-        path1.write_text(json.dumps([["a", "b"]]))
-        _get_final_set(public_tasks_path=path1)
-
-    path1.write_text(json.dumps([["a", "b", "c"], ["a", "b", "c"]]))
-    assert _get_final_set(public_tasks_path=path1) == {
-        ("a", "b", "c"),
-    }
-
-    with pytest.raises(ValidationError):
-        path1.write_text(json.dumps([["a", "b", "c"], [1, 2, 3, 4]]))
-        _get_final_set(public_tasks_path=path1)
-
-    path1.write_text(
+    path_base.write_text(
         json.dumps(
             [
                 ["pkg1", "1.0.0", "A"],
+                ["pkg1", "1.0.0", "A"],  # Repetition
+                ["pkg1", "1.0.0", "A"],  # Repetition
                 ["pkg1", "1.0.0", "B"],
                 ["pkg1", "1.0.0", "C"],
                 ["pkg2", "1.2.3", "A"],
@@ -36,7 +30,7 @@ def test__get_final_set(tmp_path: Path):
             ]
         )
     )
-    path2.write_text(
+    path_add.write_text(
         json.dumps(
             [
                 ["pkg2", "1.2.3", "D"],
@@ -45,7 +39,7 @@ def test__get_final_set(tmp_path: Path):
             ]
         )
     )
-    path3.write_text(
+    path_remove.write_text(
         json.dumps(
             [
                 ["pkg1", "1.0.0", "A"],
@@ -53,10 +47,17 @@ def test__get_final_set(tmp_path: Path):
             ]
         )
     )
+
+    assert _get_final_set() == set()
+
+    with pytest.raises(ValidationError):
+        path_invalid.write_text(json.dumps([["a", "b"]]))
+        _get_final_set(base=path_invalid)
+
     final_list = _get_final_set(
-        public_tasks_path=path1,
-        instance_tasks_path=path2,
-        instance_ignore_path=path3,
+        base=path_base,
+        additions=path_add,
+        removals=path_remove,
     )
     print(final_list)
     assert final_list == {
@@ -66,3 +67,77 @@ def test__get_final_set(tmp_path: Path):
         ("pkg1", "1.0.0", "C"),
         ("pkg3", "1.2.3", "E"),
     }
+
+
+async def _get_number_core_tasks(db) -> int:
+    res = await db.execute(
+        select(func.count(TaskV2.id)).where(TaskV2.is_core.is_(True))
+    )
+    return res.scalar()
+
+
+async def test_sync_core_tasks(
+    db,
+    tmp_path,
+    local_resource_profile_db,
+    slurm_ssh_resource_profile_fake_db,
+    user_group_factory,
+    MockCurrentUser,
+    task_factory,
+):
+    resource1, profile1 = local_resource_profile_db
+    resource2, profile2 = slurm_ssh_resource_profile_fake_db
+    async with MockCurrentUser(profile_id=profile1.id) as user1:
+        user1_id = user1.id
+    async with MockCurrentUser(profile_id=profile2.id) as user2:
+        user2_id = user2.id
+    async with MockCurrentUser(profile_id=profile2.id) as user3:
+        user3_id = user3.id
+    user_group1 = await user_group_factory(
+        group_name="my group 1", user_id=user1_id
+    )
+    user_group2 = await user_group_factory(
+        group_name="my group 2", user_id=user2_id
+    )
+    user_group3 = await user_group_factory(
+        group_name="my group 3", user_id=user3_id
+    )
+    user_group1_id: int = user_group1.id
+    user_group2_id: int = user_group2.id
+    user_group3_id: int = user_group3.id
+
+    for user_id, user_group_id, resource_id in zip(
+        [user1_id, user2_id, user3_id],
+        [user_group1_id, user_group2_id, user_group3_id],
+        [resource1.id, resource2.id, resource2.id],
+    ):
+        for pkg_name in ["pkg-a", "pkg-b"]:
+            for version in ["1.0.0", "2.0.0"]:
+                await task_factory(
+                    user_id,
+                    task_group_kwargs=dict(
+                        pkg_name=pkg_name,
+                        version=version,
+                        user_group_id=user_group_id,
+                        resource_id=resource_id,
+                    ),
+                    name="my task",
+                    is_core=True,
+                )
+
+    path1 = tmp_path / "resources-usergroups.json"
+    path1.write_text(
+        json.dumps(
+            [
+                {
+                    "resource_id": resource1.id,
+                    "user_group_id": user_group1_id,
+                }
+            ]
+        )
+    )
+
+    assert (await _get_number_core_tasks(db)) > 0
+    sync_core_tasks(resources_and_groups=path1)
+
+    assert (await _get_number_core_tasks(db)) == 0
