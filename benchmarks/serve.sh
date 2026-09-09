e3ec0ab74686da285df058d01c408ede932f0646
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-09-09 21:11:17 +0200

    uvloop

--- benchmarks/serve.sh
+++ benchmarks/serve.sh
@@ -8,6 +8,7 @@ gunicorn "fractal_server.main:app" \
     --bind=$BIND \
     --workers=$N_WORKERS \
     --worker-class=$WORKER_CLASS \
+    --asgi-loop uvloop \
     --daemon \
     --access-logfile fractal-server.out \
     --error-logfile fractal-server.err
