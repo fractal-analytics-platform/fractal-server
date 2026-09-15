7a9f63129a03e39d25c313c961204e9a7f5f7a9b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-09-15 09:12:16 +0200

    Add engine disposal during app teardwon

--- fractal_server/main.py
+++ fractal_server/main.py
@@ -14,6 +14,7 @@ from starlette.types import Scope
 from starlette.types import Send
 
 from fractal_server import __VERSION__
+from fractal_server.app.db import DB
 from fractal_server.app.schemas.v2 import ResourceType
 from fractal_server.exceptions import HTTPExceptionWithData
 
@@ -131,6 +132,18 @@ async def lifespan(app: FastAPI) -> AsyncIterator:
             )
     else:
         logger_teardown.info("Shutdown not available for this backend runner.")
+
+    logger_teardown.info(
+        "Database-connection pool status (before disposal): "
+        f"{DB.engine_sync().pool.status()}"
+    )
+    logger_teardown.info("Now dispose of the pool of sync/async engines.")
+    DB.engine_sync().dispose()
+    await DB.engine_async().dispose()
+    logger_teardown.info(
+        "Database-connection pool status (after disposal): "
+        f"{DB.engine_sync().pool.status()}"
+    )
 
     logger_teardown.info("END")
     reset_logger_handlers(logger_teardown)
