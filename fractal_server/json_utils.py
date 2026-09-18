6b8d3842bdbc3b27ac4ea266259cb7c28aebb232
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-09-18 11:12:55 +0200

    docstrings

--- fractal_server/json_utils.py
+++ fractal_server/json_utils.py
@@ -2,11 +2,16 @@ import json
 from datetime import datetime
 from pathlib import Path
 from typing import Any
+from typing import Self
 from uuid import UUID
 
 
 class CustomEncoder(json.JSONEncoder):
-    def default(self, obj):
+    """
+    Encoder which can serialize a few non-standard selected types.
+    """
+
+    def default(self: Self, obj: Any):
         if isinstance(obj, datetime):
             return obj.isoformat()
         elif isinstance(obj, Path):
@@ -18,8 +23,12 @@ class CustomEncoder(json.JSONEncoder):
 
 def json_dumps(obj: Any) -> str:
     """
-    Possible optimization through `pydantic.TypeAdapter`:
-        return pydantic.TypeAdapter(Any).dump_json(obj).decode()
-    See `benchmarks/bench_json_serializer.py`.
+    The standard `json.dumps`, with a custom encoder.
+
+    This could be optimized through `pydantic.TypeAdapter` if needed, as in
+    ```
+    pydantic.TypeAdapter(Any).dump_json(obj).decode()
+    ```
+    See `benchmarks/json_serialization/` folder for details.
     """
     return json.dumps(obj, cls=CustomEncoder)
